c861bdb0fc4a3867d23fc798c79b17880c9b9aeb
Jack Kittle <jack@gkfamily.org>
2005-07-01 18:28:21 +0000

    updated handling of message file and began to add unit tests NEEDS NEW HASSENT.DLL

--- atcWDM/atcMsgWDM.vb
+++ atcWDM/atcMsgWDM.vb
@@ -2,6 +2,7 @@ Option Strict Off
 Option Explicit On 
 
 Imports atcData
+Imports atcUtility
 
 Friend Class atcMsgWDM
   'Copyright 2005 by AQUA TERRA Consultants - Royalty-free use permitted under open source license
@@ -9,28 +10,53 @@ Friend Class atcMsgWDM
   Dim pMsgUnit As Integer 'fortran unit number of message wdm file
   Dim pAttributes As Collection 'of clsAttributeDefinition
 
-  Public Property MsgUnit() As Integer
+  Public ReadOnly Property MsgUnit() As Integer
     Get
       Return pMsgUnit
     End Get
-    Set(ByVal Value As Integer)
-      Dim lIndex As Integer 'index
-      Dim ilen As Integer 'max dimension
-      Dim itype As Integer 'type (1-Integer, 2-Real, 3-String, 0-Dummy)
-      Dim rmax As Single 'max allowed value
-      Dim rmin As Single 'min allowed value
-      Dim rdef As Single 'default value
-      Dim hpos As Integer 'help position within message file record
-      Dim hlen As Integer 'help length
-      Dim hrec As Integer 'help record withing message file
-      Dim vlen As Integer 'length of valid values
-      Dim desc As String  'description 
-      Dim aName As String 'name
-      Dim valid As String 'valid values, comma delimeted
+  End Property
 
-      Dim myAttr As atcAttributeDefinition
+  'Public ReadOnly Property Attrib(ByVal AttrName As String) As atcAttributeDefinition
+  '  'TODO who uses this? why not use clsDataAttributes.GetDefinition?
+  '  Get
+  '    Try
+  '      Attrib = pAttributes.Item(AttrName)
+  '    Catch
+  '      Attrib = Nothing
+  '    End Try
+  '  End Get
+  'End Property
 
-      pMsgUnit = Value
+  Public ReadOnly Property Attributes() As Collection
+    Get
+      Attributes = pAttributes
+    End Get
+  End Property
+
+  Public Sub New()
+    pAttributes = New Collection
+    Dim lIndex As Integer 'index
+    Dim ilen As Integer 'max dimension
+    Dim itype As Integer 'type (1-Integer, 2-Real, 3-String, 0-Dummy)
+    Dim rmax As Single 'max allowed value
+    Dim rmin As Single 'min allowed value
+    Dim rdef As Single 'default value
+    Dim hpos As Integer 'help position within message file record
+    Dim hlen As Integer 'help length
+    Dim hrec As Integer 'help record withing message file
+    Dim vlen As Integer 'length of valid values
+    Dim desc As String  'description 
+    Dim aName As String 'name
+    Dim valid As String 'valid values, comma delimeted
+
+    Dim myAttr As atcAttributeDefinition
+
+    F90_MSG("WRITE", 5)
+    Dim hspfMsgFileName As String = FindFile("Please locate HSPF message file", "hspfmsg.wdm")
+    If Not FileExists(hspfMsgFileName) Then
+      LogMsg("Could not find hspfmsg.wdm - " & hspfMsgFileName, "atcMsgWdm")
+    Else
+      pMsgUnit = F90_WDBOPN(0, hspfMsgFileName, Len(hspfMsgFileName))
 
       For lIndex = 1 To 500 'loop thru all possible attributes 
 
@@ -78,27 +104,13 @@ Friend Class atcMsgWDM
 
         End If
       Next lIndex
-    End Set
-  End Property
+    End If
+  End Sub
 
-  'Public ReadOnly Property Attrib(ByVal AttrName As String) As atcAttributeDefinition
-  '  'TODO who uses this? why not use clsDataAttributes.GetDefinition?
-  '  Get
-  '    Try
-  '      Attrib = pAttributes.Item(AttrName)
-  '    Catch
-  '      Attrib = Nothing
-  '    End Try
-  '  End Get
-  'End Property
-
-  Public ReadOnly Property Attributes() As Collection
-    Get
-      Attributes = pAttributes
-    End Get
-  End Property
-
-  Public Sub New()
-    pAttributes = New Collection
+  Protected Overrides Sub Finalize()
+    If pMsgUnit > 0 Then
+      Dim i As Integer = F90_WDMCLO(pMsgUnit)
+      pMsgUnit = 0
+    End If
   End Sub
 End Class
