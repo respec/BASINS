cc50a56284ea1ae657aac8d5e7991c96dc541811
Jack Kittle <jack@gkfamily.org>
2005-08-25 16:34:54 +0000

    improvements to freq analysis

--- atcTimeseriesNdayHighLow/modFreq.vb
+++ atcTimeseriesNdayHighLow/modFreq.vb
@@ -1,3 +1,5 @@
+Imports atcData
+
 Module modFreq
   Declare Sub LGPSTX Lib "usgs_swstats.dll" (ByRef N As Integer, _
                                              ByRef NZI As Integer, _
@@ -21,15 +23,21 @@ Module modFreq
                                              ByRef RETCOD As Integer)
 
   'frequency analysis for specified recurrence interval or probability
-  Sub PearsonType3(ByVal lN As Integer, ByVal lNzi As Integer, _
-                   ByVal lMean As Double, ByVal lStd As Double, ByVal lSkew As Double, _
-                   ByVal lRecurOrProb As Double, ByRef lQOut As Double)
+  Function PearsonType3(ByVal aTs As atcTimeseries, ByVal aRecurOrProb As Double, ByVal aHigh As Boolean) As Double
+
+    Dim lN As Integer = aTs.Attributes.GetValue("Count")
+    Dim lNzi As Integer = aTs.numValues - lN
+    Dim lMean As Double = aTs.Attributes.GetValue("Mean")
+    Dim lStd As Double = aTs.Attributes.GetValue("Standard Deviation")
+    Dim lSkew As Double = aTs.Attributes.GetValue("Skew")
 
     Dim lNumons As Integer = 1
-    Dim lLogarh As Integer = 2 'log trans flag 1-yes,2-no
-    Dim lIlh As Integer = 2 'stats option 1-hi, 2-low,3-month
+    Dim lLogarh As Integer = 2 'log trans flag 1-yes,2-no (handle earlier)
     Dim lNqs As Integer = 12 'number of statistics
+
+    'be sure aRecurOrProb is available in lSe
     Dim lSe() As Single = {0.01, 0.02, 0.05, 0.1, 0.2, 0.3333, 0.5, 0.8, 0.9, 0.96, 0.98, 0.99}
+
     Dim lI = lNqs - 1
     Dim lC(lI) As Single
     Dim lCcpa(lI) As Single
@@ -41,16 +49,25 @@ Module modFreq
     Dim lRsout(2 * lI) As Single
     Dim lRetcod As Integer
 
+    Dim lIlh As Integer = 2 'stats option 1-hi, 2-low,3-month
+    If aHigh Then lIlh = 1
+
     LGPSTX(lN, lNzi, lNumons, lNqs, lMean, lStd, lSkew, lLogarh, True, lIlh, lSe(0), _
            lC(0), lCcpa(0), lP(0), lQ(0), lAdp(0), lQnew(0), lRi(0), lRsout(0), lRetcod)
 
-    If lRecurOrProb > 1 Then lRecurOrProb = 1.0 / lRecurOrProb
+    If aRecurOrProb > 1 Then aRecurOrProb = 1.0 / aRecurOrProb
+    If aHigh Then
+      aRecurOrProb = 1 - aRecurOrProb
+    End If
+
+    'TODO: what about lQnew?
 
     For i As Integer = 0 To lI
-      If Math.Abs(lSe(i) - lRecurOrProb) < 0.00001 Then
-        lQOut = lQ(i)
-        Exit For
+      If Math.Abs(lSe(i) - aRecurOrProb) < 0.00001 Then
+        Return lQ(i)
       End If
     Next
-  End Sub
+
+    Return Double.NaN
+  End Function
 End Module
