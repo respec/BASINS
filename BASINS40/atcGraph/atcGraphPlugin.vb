bd49f820a13f9da476bab3bebe1ec11879e7d039
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2005-09-02 15:11:15 +0000

    Added Save as option to atcGraphPlugin

--- BASINS40/atcGraph/atcGraphPlugin.vb
+++ BASINS40/atcGraph/atcGraphPlugin.vb
@@ -2,7 +2,7 @@ Imports atcData
 
 Public Class atcGraphPlugin
   Inherits atcDataDisplay
-  Public g_MapWin As MapWindow.Interfaces.IMapWin
+  Private pMapWin As MapWindow.Interfaces.IMapWin
 
   Public Overrides ReadOnly Property Name() As String
     Get
@@ -10,17 +10,40 @@ Public Class atcGraphPlugin
     End Get
   End Property
 
-  Public Overrides function Show(ByVal aTimeseriesManager As atcData.atcDataManager, _
-                   Optional ByVal aTimeseriesGroup As atcData.atcDataGroup = Nothing)
-    Return New atcGraphForm(aTimeseriesManager, aTimeseriesGroup)
+  Public Sub Save(ByVal aDataManager As atcData.atcDataManager, _
+                  ByVal aDataGroup As atcData.atcDataGroup, _
+                  ByVal aFileName As String, _
+                  ByVal ParamArray aOption() As String)
+
+    If Not aDataGroup Is Nothing AndAlso aDataGroup.Count > 0 Then
+      Dim lForm As New atcGraphForm(aDataManager, aDataGroup)
+      lForm.SaveBitmapToFile(aFileName)
+      lForm.Dispose()
+    End If
+  End Sub
+
+  Public Overrides Function Show(ByVal aDataManager As atcDataManager, _
+                        Optional ByVal aDataGroup As atcDataGroup = Nothing) As Object 'System.Windows.Forms.Form
+    Dim lDataGroup As atcDataGroup = aDataGroup
+    If lDataGroup Is Nothing Then lDataGroup = New atcDataGroup
+
+    Dim lForm As New atcGraphForm(aDataManager, lDataGroup)
+    If lDataGroup.Count > 0 Then
+      lForm.Show()
+      Return lForm
+    Else 'No data to display, don't show or return the form
+      lForm.Dispose()
+      Return Nothing
+    End If
   End Function
 
-  Public Overrides Sub Initialize(ByVal MapWin As MapWindow.Interfaces.IMapWin, ByVal ParentHandle As Integer)
-    g_MapWin = MapWin
-    g_MapWin.Plugins.BroadcastMessage("atcDataPlugin loading atcGraphPlugin")
+  Public Overrides Sub Initialize(ByVal aMapWin As MapWindow.Interfaces.IMapWin, _
+                                  ByVal aParentHandle As Integer)
+    pMapWin = aMapWin
+    pMapWin.Plugins.BroadcastMessage("atcDataPlugin loading atcGraphPlugin")
   End Sub
 
   Public Overrides Sub Terminate()
-    g_MapWin.Plugins.BroadcastMessage("atcDataPlugin unloading atcGraphPlugin")
+    pMapWin.Plugins.BroadcastMessage("atcDataPlugin unloading atcGraphPlugin")
   End Sub
 End Class
