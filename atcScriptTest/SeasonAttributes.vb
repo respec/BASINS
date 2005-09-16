e481a148da0ba28dd92dd21e996b31f77d126a83
Jack Kittle <jack@gkfamily.org>
2005-09-16 13:47:32 +0000

    atcScriptTest - updates for SeasonAttributes

--- atcScriptTest/SeasonAttributes.vb
+++ atcScriptTest/SeasonAttributes.vb
@@ -12,6 +12,7 @@ Public Module ScriptSeasonAttributes
     Dim lGrapher As new atcGraph.atcGraphPlugin
     Dim lWDMfile As atcDataSource = New atcWdm.atcDataSourceWdm
     Dim lSummary As New atcDebugTimser.atcDebugTimserPlugin
+    Dim lSeasonalAttributes as new atcSeasonalAttributes.atcSeasonalAttributesPlugin
 
     Dim lOutFile as string = "SeasonAttributes_output.txt"
     Dim lErr As String
@@ -36,10 +37,20 @@ Public Module ScriptSeasonAttributes
     lAttributes.SetValue("Mean", 0)
     lAttributes.SetValue("Max", 0)
     lAttributes.SetValue("Min", 0)
+    lAttributes.SetValue("Geometric Mean", 0)
+    lAttributes.SetValue("Standard Deviation", 0)
+    lAttributes.SetValue("Skew", 0)
+    lAttributes.SetValue("7Q10", 0)
+    lAttributes.SetValue("1Hi100", 0)
 
     Dim lSeasonsMonth As New atcSeasonsMonth
     lSeasonsMonth.SetSeasonalAttributes(lMatch.ItemByIndex(0), lAttributes)
     lSummary.Save(aDataManager, lMatch, "ListMonthAttributes.txt", "Expand")
+    lSeasonalAttributes.Save(aDataManager, lMatch, "GridMonthAttributes.txt")
+    lSeasonalAttributes.Show(aDataManager, lMatch)
+
+    lAttributes.RemoveByKey("7Q10")
+    lAttributes.RemoveByKey("1Hi100")
 
     Dim lSeasonsDayOfWeek As New atcSeasonsDayOfWeek
     Dim lAttributesTemp As New atcDataAttributes
