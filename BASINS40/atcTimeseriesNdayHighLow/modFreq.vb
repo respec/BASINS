508924edffe38a1e6e03317e5a63590c5e83495a
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2005-09-21 15:48:21 +0000

    atcTimeseriesNdayHighLow/modFreq - add additional recurrence intervals, turn off debugging of fortran code, add better dimensioning of variable size arrays, correct order of arguments in call to fortran

--- BASINS40/atcTimeseriesNdayHighLow/modFreq.vb
+++ BASINS40/atcTimeseriesNdayHighLow/modFreq.vb
@@ -37,12 +37,12 @@ Module modFreq
 
       Dim lNumons As Integer = 1
       Dim lLogarh As Integer = 2 'log trans flag 1-yes,2-no (handle earlier)
-      Dim lNqs As Integer = 12 'number of statistics
 
       'be sure aRecurOrProb is available in lSe
-      Dim lSe() As Single = {0.01, 0.02, 0.05, 0.1, 0.2, 0.3333, 0.5, 0.8, 0.9, 0.96, 0.98, 0.99}
+      Dim lSe() As Single = {0.005, 0.01, 0.02, 0.04, 0.05, 0.1, 0.2, 0.3333, 0.5, _
+                             0.6667, 0.8, 0.9, 0.95, 0.96, 0.98, 0.99, 0.995}
+      Dim lI As Integer = UBound(lSe)  'number of recurrence intervals to calculate-1
 
-      Dim lI = lNqs - 1
       Dim lC(lI) As Single
       Dim lCcpa(lI) As Single
       Dim lP(lI) As Single
@@ -50,24 +50,20 @@ Module modFreq
       Dim lAdp(lI) As Single
       Dim lQnew(lI) As Single
       Dim lRi(lI) As Single
-      Dim lRsout(2 * lI) As Single
+      Dim lRsout(1 + (2 * lI)) As Single
       Dim lRetcod As Integer
 
       Dim lIlh As Integer = 2 'stats option 1-hi, 2-low,3-month
       If aHigh Then lIlh = 1
 
-      LGPSTX(lN, lNzi, lNumons, lNqs, lMean, lStd, lSkew, lLogarh, True, lIlh, lSe(0), _
+      LGPSTX(lN, lNzi, lNumons, lI + 1, lMean, lStd, lSkew, lLogarh, lIlh, False, lSe(0), _
              lC(0), lCcpa(0), lP(0), lQ(0), lAdp(0), lQnew(0), lRi(0), lRsout(0), lRetcod)
 
       If aRecurOrProb > 1 Then aRecurOrProb = 1.0 / aRecurOrProb
-      If aHigh Then
-        aRecurOrProb = 1 - aRecurOrProb
-      End If
 
       'TODO: what about lQnew?
-
       For i As Integer = 0 To lI
-        If Math.Abs(lSe(i) - aRecurOrProb) < 0.00001 Then
+        If Math.Abs(lSe(i) - aRecurOrProb) < 0.0001 Then
           Return lQ(i)
         End If
       Next
