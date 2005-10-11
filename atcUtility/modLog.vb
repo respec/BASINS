5d5011257a5a058133fc8f9eaeec8687f4357142
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2005-10-11 21:17:26 +0000

    modLog: First draft of progress logging (to be added to upcoming separate Status Monitor application)         removed LogCmd frmSelectData: faster sorting of attribute values into lists, sample usage of progress logging modDownload: removed use of LogCmd

--- atcUtility/modLog.vb
+++ atcUtility/modLog.vb
@@ -8,6 +8,9 @@ Imports System.Windows.Forms
 Public Module modLog
   Private pLogFileName As String = PathNameOnly(Application.ExecutablePath) & "\Basins.Log"
   Private pLogTimeStamp As Boolean = True
+  Private pProgressStartTime As Double = Double.NaN
+  Private pProgressLastUpdate As Double = Double.NaN
+  Private pProgressRefresh As Double = 2 / 72000.0# ' 1 / 72000.0 = 1 second
 
   Public Sub SetLogFileName(ByVal aLogFileName As String, Optional ByVal aAppend As Boolean = False)
     If Not aAppend AndAlso FileExists(aLogFileName) Then Kill(aLogFileName)
@@ -61,11 +64,28 @@ Public Module modLog
     'End If
   End Function
 
-  'Send a command to the status monitor
-  Public Sub LogCmd(ByRef aCommand As String)
-    System.Diagnostics.Debug.WriteLine(aCommand)
-    'If Not (gIPC Is Nothing) Then gIPC.SendMonitorMessage((aCommand))
+  Public Sub LogProgress(ByRef aMessage As String, ByRef aCurrent As Integer, ByRef aLast As Integer)
+    LogDbg("Progress: " & aMessage & " " & aCurrent & " of " & aLast)
+    Dim lCurTime As Double = Now.ToOADate
+    If aCurrent = aLast Then
+      pProgressStartTime = Double.NaN
+      pProgressLastUpdate = Double.NaN
+    ElseIf Double.IsNaN(pProgressStartTime) Then
+      pProgressStartTime = lCurTime
+      pProgressLastUpdate = lCurTime
+    Else
+      If lCurTime - pProgressLastUpdate > pProgressRefresh Then
+        'MsgBox(aMessage & vbCrLf & aCurrent & " of " & aLast & " (" & 100 * aCurrent / aLast & "%)")
+        pProgressLastUpdate = lCurTime
+      End If
+    End If
   End Sub
+
+  ''Send a command to the status monitor
+  'Public Sub LogCmd(ByRef aCommand As String)
+  '  System.Diagnostics.Debug.WriteLine(aCommand)
+  '  'If Not (gIPC Is Nothing) Then gIPC.SendMonitorMessage((aCommand))
+  'End Sub
 
   Public Sub SendFeedback()
     'Dim iDrive As Integer
