8823150df373f5776dee354600e220cc8b7a0a89
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2006-01-19 21:42:41 +0000

    atcBasinsPlugIn - code cleanup

--- BASINS40/BASINS/Globals.vb
+++
@@ -1,8 +0,0 @@
-Module Globals
-  'Declare this as global so that it can be accessed throughout the plug-in project.
-  'The variable is initialized in the plugin_Initialize event.
-  Public g_MapWin As MapWindow.Interfaces.IMapWin
-  Public g_MapWinWindowHandle As Integer
-  Public g_AppName As String = "BASINS4"
-  Public g_BasinsDrives As String = ""
-End Module
