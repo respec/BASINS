edcfa1fdc3e1c53a253bec0e4f9504594707a9e8
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2006-08-04 20:49:04 +0000

    atcGraphForm: avoid error when asked to graph a data set with zero values               default min and max x axis to +- Infinity
    atcMsgWdmVb.vb: Last major code in place for reading message WDM. 
                    Now using new atcWdmFileHandle.ReadString
                    Functionally complete but needs testing
    
    atcWDMfile: use new atcWdmFileHandle.ReadString
    
    atcWdmFileHandle: new ReadString

--- atcWdmVb/atcMsgWdmVb.vb
+++ atcWdmVb/atcMsgWdmVb.vb
@@ -10,6 +10,7 @@ Friend Class atcMsgWDMvb
     'Copyright 2006 by AQUA TERRA Consultants - Royalty-free use permitted under open source license
     Private pAttributes As atcCollection 'of clsAttributeDefinition
     Private pAttributeArray(512) As atcAttributeDefinition
+    Private Shared pAttributeTypeName() = {"None", "Integer", "Single", "String"}
 
     Public ReadOnly Property Attributes() As atcCollection
         Get
@@ -19,10 +20,8 @@ Friend Class atcMsgWDMvb
 
     Public Sub New()
         Dim lAttr As atcAttributeDefinition
-
         Dim lFileName As String = "C:\dev\BASINS40\Bin\Plugins\BASINS\hspfmsg.wdm"
         Dim lWdmMsg As New atcWdmFileHandle(1, lFileName)
-
         Dim lAttributeDsn As Int32 = lWdmMsg.ReadInt32(Wdm_Fields.DSFST_Attribute)
         While lAttributeDsn > 0 'loop thru all possible attributes 
             Dim lBaseRec As Int32 = lWdmMsg.FirstRecordNumberFromDsn(lAttributeDsn)
@@ -37,24 +36,22 @@ Friend Class atcMsgWDMvb
                 If lV > 0 Then
                     lAttr = New atcAttributeDefinition
                     lAttr.Category = "WDM"
+
                     lAttr.Name &= Long2String(lV)
                     lV = lWdmMsg.ReadUInt32
                     lAttr.ID = lV And &H1FF
                     lAttr.Name &= Chr((lV >> 9) And &HFF)
                     lAttr.Name &= Chr((lV >> 17) And &HFF)
+                    lAttr.Name = lAttr.Name.Trim
+
                     lAttr.DefaultValue = lWdmMsg.ReadUInt32 'save location of details for later
+
                     lV = lWdmMsg.ReadUInt32
                     Dim lAttrType As Int32 = 1 + (lV >> 28)
-
-                    lAttr.Name = lAttr.Name.Trim
-                    Select Case lAttrType
-                        Case 1
-                            lAttr.TypeString = "Integer"
-                        Case 2 : lAttr.TypeString = "Single"
-                        Case 3
-                            lAttr.TypeString = "String"
-                            lAttr.Max = (lV >> 21) And &H7F
-                    End Select
+                    lAttr.TypeString = pAttributeTypeName(lAttrType)
+                    If lAttrType = 3 Then
+                        lAttr.Max = (lV >> 21) And &H7F
+                    End If
                     If InStr("-TGROUP-TSFORM-VBTIME-COMPFG-TSFILL-TSBYR-TSBMO-TSBDY-TSBHR-TSPREC-TSSTEP-TCODE-Time Units-Time Step-", _
                              "-" & lAttr.Name & "-") > 0 Then
                         lAttr.Editable = False
@@ -73,41 +70,51 @@ Friend Class atcMsgWDMvb
                 lAttr = pAttributeArray(lInd)
                 If Not (lAttr Is Nothing) Then
                     Dim lDataPointerGroup As Int32 = lAttr.DefaultValue
+                    lAttr.DefaultValue = Nothing 'finished using DefaultValue to store pointer
                     If lDataPointerGroup > 0 Then 'data in this group
                         Dim lDataRecord As UInt32 = lDataPointerGroup >> 9
                         Dim lDataOffset As UInt32 = lDataPointerGroup And &H1FF
                         Logger.Dbg("  Attribute:" & lAttr.ID & ":" & lDataRecord & ":" & lDataOffset)
                         lWdmMsg.Seek(lDataRecord, lDataOffset)
+                        Dim lBlockIdentifier As UInt32 = lWdmMsg.ReadInt32
+                        While lBlockIdentifier > 0
+                            lDataOffset += 1 'count reading of lBlockIdentifier
 
-                        Dim lRMax As Single 'max allowed value
-                        Dim lRMin As Single 'min allowed value
-                        Dim lRDef As Single 'default value
-                        Dim lHlpPos As Integer 'help position within message file record
-                        Dim lHlpLen As Integer 'help length
-                        Dim lHlpRec As Integer 'help record withing message file
-                        Dim lValidLen As Integer 'length of valid values
-                        Dim lDesc As String = "" 'description 
-                        Dim lValid As String = "" 'valid values, comma delimeted
+                            Dim lTlen As UInt32 = lBlockIdentifier And &H1FF
+                            Dim lInfoID As UInt32 = (lBlockIdentifier >> 9) And &HFFFF
 
-                        'get info about attribute from message file
-                        'Call F90_WDSAGY(lMsgUnit, lIndex, lLen, lType, lRMin, lRMax, lRDef, lHlpLen, lHlpRec, lHlpPos, lValidLen, lName, lDesc, lValid)
+                            lDataOffset += lTlen 'Is lTlen set for ID = 3?
 
-                        lAttr.Description = lDesc
+                            Select Case lInfoID
+                                Case 0 ' All done
+                                    Exit While
+                                Case 3 ' Range
+                                    If ReferenceEquals(lAttr.TypeString, pAttributeTypeName(1)) Then ' Integer
+                                        lAttr.Min = CDbl(lWdmMsg.ReadInt32)
+                                        lAttr.Max = CDbl(lWdmMsg.ReadInt32)
+                                    Else 'Single
+                                        lAttr.Min = CDbl(lWdmMsg.ReadSingle)
+                                        lAttr.Max = CDbl(lWdmMsg.ReadSingle)
+                                    End If
+                                Case 4 ' Valid responses
+                                    If lAttr.ValidList Is Nothing Then lAttr.ValidList = New ArrayList
+                                    lAttr.ValidList.Add(lWdmMsg.ReadString(lTlen))
+                                Case 5 ' Default
+                                    lAttr.DefaultValue = lWdmMsg.ReadString(lTlen)
+                                Case 6 ' Description
+                                    lAttr.Description &= lWdmMsg.ReadString(lTlen)
+                                Case 7 ' Help
+                                    lAttr.Help &= lWdmMsg.ReadString(lTlen)
+                            End Select
 
-                        lAttr.ValidList = New ArrayList(lValid.Split(Chr(44))) ' 44=comma
-
-                        If lAttr.TypeString <> "String" Then
-                            'numeric, save valid range
-                            lAttr.Min = lRMin
-                            lAttr.Max = lRMax
-                        End If
-
-                        lAttr.DefaultValue = lRDef
-
-                        'use hlen, hrec, hpos to get myAttr.help 
-                        '(missing entry point to WDGCVL in hass_ent?)
-                        lAttr.Help = ""
-
+                            If lDataOffset >= 511 Then 'move to next record
+                                lDataRecord = lWdmMsg.ReadInt32(CInt(lDataRecord), 4)
+                                lDataOffset = 5
+                                lWdmMsg.Seek(lDataRecord, lDataOffset)
+                                'Logger.Dbg("  NewRecord:" & lDataRecord)
+                            End If
+                            lBlockIdentifier = lWdmMsg.ReadInt32
+                        End While
                     End If
                 End If
             Next
