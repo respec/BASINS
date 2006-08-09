14c16d2d4be11d5cd344026f2dc30640a57874b9
Jack Kittle <jack@gkfamily.org>
2006-08-09 18:17:17 +0000

    atcData - allow history - 'fromVb' atcwdmvb - default -999 to NaN, commented debugs for other non zero defaults

--- atcData/atcDataAttributes.vb
+++ atcData/atcDataAttributes.vb
@@ -97,7 +97,7 @@ Public Class atcDataAttributes
                 ElseIf InStr(aAttributeName.ToLower, "history") > 0 Then
                     If InStr(lValue.ToString.ToLower, "read from") Then 'make value shorter by removing path and "read "
                         Dim lString() As String = lValue.ToString.Split(" ")
-                        lString(0) = "from " & FilenameNoPath(lString(2))
+                        lString(0) = lString(1) & " " & FilenameNoPath(lString(2))
                         Return lString(0)
                     Else
                         Return lValue.ToString
