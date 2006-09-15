33b6013c01d8157bc479c8b0dd0453ce1846d5fa
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2006-09-15 13:51:03 +0000

    Removed automatic version numbering from all BASINS projects and set version to 1.1.0.0 or 3.1.0.0 for all

--- BASINS/AssemblyInfo.vb
+++ BASINS/AssemblyInfo.vb
@@ -24,8 +24,5 @@ Imports System.Runtime.InteropServices
 '      Minor Version 
 '      Build Number
 '      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.1.0.0")> 
--- atcLookup/AssemblyInfo.vb
+++ atcLookup/AssemblyInfo.vb
@@ -28,4 +28,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("3.0.*")> 
+<Assembly: AssemblyVersion("3.1.0.0")> 
