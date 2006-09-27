8cf08b880b04b595d41446afc82bfaef4f882240
Jack Kittle <jack@gkfamily.org>
2006-09-27 21:39:10 +0000

    atcUCIForms - refine framework for editing of atcUCI WinHSPF - refine stub to test forms for editing uci

--- atcUCIForms/ctlEdit.vb
+++ atcUCIForms/ctlEdit.vb
@@ -3,6 +3,9 @@
 ''' </summary>
 ''' <remarks></remarks>
 Public Interface ctlEdit
+    Event Change(ByVal aChange As Boolean)
+    ReadOnly Property Caption() As String
+    Property Changed() As Boolean
     Property Data() As Object
     Sub Add()
     Sub Help()
