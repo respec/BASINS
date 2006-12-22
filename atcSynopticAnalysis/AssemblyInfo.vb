7268ed794512dfd8f1f4c64ada5f093322613ed2
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2006-12-22 17:04:56 +0000

    added new atcSynopticAnalysis
    atcTimeseries: avoid an exception by testing for Nothing
    atcEventBase: changed some And to AndAlso, Or to OrElse for efficiency
                  changed a > to >= and < to <= in case we are at threshold

---
+++ atcSynopticAnalysis/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("atcSynopticAnalysis")> 
+<Assembly: AssemblyDescription("Synoptic Analysis")> 
+<Assembly: AssemblyCompany("AQUA TERRA Consultants")> 
+<Assembly: AssemblyProduct("")> 
+<Assembly: AssemblyCopyright("2007")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: AssemblyVersion("1.1.0.0")> 
