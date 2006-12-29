b40a17bb223eeb804e924ee7f877c3b33b43ea76
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2006-12-29 17:58:23 +0000

    atcSynopticAnalysisPlugin - minor tweaks

--- BASINS40/atcSynopticAnalysis/atcSynopticAnalysisPlugin.vb
+++ BASINS40/atcSynopticAnalysis/atcSynopticAnalysisPlugin.vb
@@ -1,5 +1,6 @@
 Imports atcData
 Imports atcUtility
+Imports MapWinUtility
 
 Public Class atcSynopticAnalysisPlugin
     Inherits atcData.atcDataDisplay
@@ -25,34 +26,44 @@ Public Class atcSynopticAnalysisPlugin
                     ByVal ParamArray aOption() As String)
 
         If aDataGroup Is Nothing OrElse aDataGroup.Count = 0 Then
-            SaveFileString(aFileName, "No data sets selected for synoptic analysis")
+            Dim lMsg As String = "No data sets selected for synoptic analysis"
+            SaveFileString(aFileName, lMsg)
+            Logger.Dbg(lMsg)
         Else
-            Dim lThreshold As Double = 0.01
+            'TODO: add an optional 'inter-event time'
+            Dim lThreshold As Double = 0.005 '0.01 'TODO: allow this to be an option
             Dim lHighEvents As Boolean = True
             Dim lReport As New IO.StreamWriter(aFileName)
             lReport.WriteLine("Synoptic Analysis of " & aDataGroup.Count & " Data Sets")
             For Each lDataSet As atcTimeseries In aDataGroup
                 lReport.WriteLine()
                 lReport.WriteLine("Location:" & lDataSet.Attributes.GetValue("Location"))
-                lReport.WriteLine("Time Span:" & DateString(lDataSet.Dates.Value(1)) & " to " & DateString(lDataSet.Dates.Value(lDataSet.Dates.numValues)))
+                lReport.WriteLine("Time Span:" & DateString(lDataSet.Dates.Value(1)) & _
+                                        " to " & DateString(lDataSet.Dates.Value(lDataSet.Dates.numValues)))
 
                 lReport.WriteLine(vbCrLf & "Storm Events" & vbCrLf)
 
-                lReport.WriteLine("Storm #" & vbTab & "Date      " & vbTab & "Hour" & vbTab & "Hours" & vbTab & "Volume")
+                lReport.WriteLine("Storm #" & vbTab & _
+                               "Date      " & vbTab & _
+                                     "Hour" & vbTab & _
+                                    "Hours" & vbTab & _
+                                    "Volume")
+
                 Dim lStorms As atcDataGroup = atcEvents.EventSplit(lDataSet, Nothing, lThreshold, lHighEvents)
                 Dim lStormIndex As Integer = 0
                 For Each lStorm As atcTimeseries In lStorms
                     lStormIndex += 1
                     'Storm Number, Date, Hour
                     lReport.Write(lStormIndex & vbTab & DateTabHour(lStorm.Dates.Value(1)))
-                    'Duration
-                    lReport.Write(vbTab & CInt((lStorm.Dates.Value(lStorm.Dates.numValues) - lStorm.Dates.Value(1)) * 24))
+                    'Duration (hours) 'TODO: dont hard code hours?
+                    lReport.Write(vbTab & CInt((lStorm.Dates.Value(lStorm.Dates.numValues) - lStorm.Dates.Value(1)) * 24) + 1)
                     'Volume
                     lReport.Write(vbTab & Format(lStorm.Attributes.GetValue("Sum"), "0.00"))
                     lReport.WriteLine()
                 Next
             Next
             lReport.Close()
+            Logger.Dbg("SynopticAnalysis Complete, Results in file '" & aFileName & "'")
         End If
     End Sub
 
