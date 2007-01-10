9ddf43280dfd58db9ad141cf456607d5f1b61a98
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2007-01-10 21:24:54 +0000

    atcEvents: added aDaysGapAllowed parameter to EventSplit, plugin. form new plugin atcSynopInput for reading and writing Synop input files as Timeseries atcSynopticAnalysis.vbproj: changed reference to MapWinUtility atcSynopticAnalysisPlugin.vb: using new aDaysGapAllowed for splitting events, added Ave & Max intensity and hours since last event to report
    atcTableFixed: fixed bug in OpenStream that prevented reading right number of header rows, added comment to FieldStart, improved handling of invalid requests for headers

---
+++ atcSynopInput/atcDataSourceSynopInput.vb
@@ -0,0 +1,160 @@
+Option Strict Off
+Option Explicit On
+
+Imports atcData
+Imports atcUtility
+Imports MapWinUtility
+Imports Microsoft.VisualBasic
+Imports System.Collections
+Imports System.IO
+
+Public Class atcDataSourceSynopInput
+    Inherits atcDataSource
+    '##MODULE_REMARKS Copyright 2007 AQUA TERRA Consultants - Royalty-free use permitted under open source license
+
+    Private Shared pFileFilter As String = "Synop Input Files (*.inp)|*.inp"
+    Private pErrorDescription As String
+
+    Public Overrides ReadOnly Property Description() As String
+        Get
+            Return "Synop Input"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Name() As String
+        Get
+            Return "Timeseries::SynopInput"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Category() As String
+        Get
+            Return "File"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property CanOpen() As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property CanSave() As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    Public Overrides Function Open(ByVal aFileName As String, Optional ByVal aAttributes As atcData.atcDataAttributes = Nothing) As Boolean
+
+        If aFileName Is Nothing OrElse aFileName.Length = 0 OrElse Not FileExists(aFileName) Then
+            aFileName = FindFile("Select " & Name & " file to open", , , pFileFilter, True, , 1)
+        End If
+
+        If Not FileExists(aFileName) Then
+            pErrorDescription = "File '" & aFileName & "' not found"
+        Else
+            Me.Specification = aFileName
+
+            Try
+                Dim lTable As New atcTableFixed
+                Dim lTSIndex As Integer = 0
+                Dim lNCons As Integer = 0
+                Dim lConsName As String = "PRECIP"
+                Dim lDate As Double = 0
+                Dim lData As atcTimeseries
+                Dim lDateArr(6) As Integer
+                lDateArr(4) = 0 'No minutes in this file format
+                lDateArr(5) = 0 'No seconds in this file format
+
+                lTable = New atcTableFixed
+                With lTable
+                    .NumHeaderRows = 0
+
+                    .NumFields = 6
+                    .FieldStart(1) = 1
+                    .FieldLength(1) = 6
+                    .FieldName(1) = "Location"
+
+                    .FieldStart(2) = 7
+                    .FieldLength(2) = 10
+                    .FieldName(2) = "Year"
+
+                    .FieldStart(3) = 17
+                    .FieldLength(3) = 10
+                    .FieldName(3) = "Month"
+
+                    .FieldStart(4) = 27
+                    .FieldLength(4) = 10
+                    .FieldName(4) = "Day"
+
+                    .FieldStart(5) = 37
+                    .FieldLength(5) = 10
+                    .FieldName(5) = "Hour"
+
+                    .FieldStart(6) = 47
+                    .FieldLength(6) = 10
+                    .FieldName(6) = "Value"
+
+                    .OpenFile(aFileName)
+
+                    lData = New atcTimeseries(Me)
+                    lData.Dates = New atcTimeseries(Me)
+                    lData.numValues = lTable.NumRecords
+                    lData.Attributes.SetValue("Count", 0)
+                    lData.Attributes.SetValue("Scenario", "OBSERVED")
+                    lData.Attributes.SetValue("Location", lTable.Value(1))
+                    lData.Attributes.SetValue("Constituent", lConsName)
+                    lData.Attributes.SetValue("point", False)
+                    DataSets.Add(lData)
+                    'lData.Value(0) = Double.NaN
+                    'lData.Dates.Value(0) = Double.NaN
+
+                    While Not lTable.atEOF
+                        lDateArr(0) = .Value(2)
+                        lDateArr(1) = .Value(3)
+                        lDateArr(2) = .Value(4)
+                        lDateArr(3) = .Value(5)
+
+                        lDate = Date2J(lDateArr)
+                        If lDate <> 0 Then
+                            lTSIndex += 1
+                            lData.Value(lTSIndex) = .Value(6)
+                            lData.Dates.Value(lTSIndex) = lDate
+                        End If
+                        lTable.MoveNext()
+                    End While
+                End With
+                If lTSIndex <> lTable.NumRecords Then
+                    Logger.Msg("Expected " & lTable.NumRecords & " records in '" & aFileName & "' but found " & lTSIndex, MsgBoxStyle.Exclamation, "Open Synop Input")
+                    lData.numValues = lTSIndex
+                End If
+                Open = True
+            Catch endEx As EndOfStreamException
+                Open = False
+            End Try
+        End If
+    End Function
+
+    Public Overrides Function Save(ByVal aSaveFileName As String, _
+                            Optional ByRef aExistAction As EnumExistAction = EnumExistAction.ExistReplace) As Boolean
+        For Each lDataSet As atcTimeseries In Me.DataSets
+            For lIndex As Integer = 1 To lDataSet.numValues
+                Dim lDateArr(6) As Integer
+                J2Date(lDataSet.Dates.Value(lIndex), lDateArr)
+                timcnv(lDateArr)
+                ' station?   year     month       day      hour hundredths of an inch?
+                ' 11111      2003         1         9        14        14
+
+                AppendFileString(aSaveFileName, " 00000" _
+                                & StrPad(lDateArr(0), 10) _
+                                & StrPad(lDateArr(1), 10) _
+                                & StrPad(lDateArr(2), 10) _
+                                & StrPad(lDateArr(3), 10) _
+                                & StrPad(CInt(100 * lDataSet.Value(lIndex)), 10) & vbLf)
+            Next
+        Next
+    End Function
+
+
+End Class
--- atcSynopticAnalysis/atcSynopticAnalysisPlugin.vb
+++ atcSynopticAnalysis/atcSynopticAnalysisPlugin.vb
@@ -24,47 +24,74 @@ Public Class atcSynopticAnalysisPlugin
                     ByVal aDataGroup As atcDataGroup, _
                     ByVal aFileName As String, _
                     ByVal ParamArray aOption() As String)
-
-        If aDataGroup Is Nothing OrElse aDataGroup.Count = 0 Then
-            Dim lMsg As String = "No data sets selected for synoptic analysis"
-            SaveFileString(aFileName, lMsg)
-            Logger.Dbg(lMsg)
-        Else
-            'TODO: add an optional 'inter-event time'
-            Dim lThreshold As Double = 0.005 '0.01 'TODO: allow this to be an option
-            Dim lHighEvents As Boolean = True
-            Dim lReport As New IO.StreamWriter(aFileName)
-            lReport.WriteLine("Synoptic Analysis of " & aDataGroup.Count & " Data Sets")
-            For Each lDataSet As atcTimeseries In aDataGroup
-                lReport.WriteLine()
-                lReport.WriteLine("Location:" & lDataSet.Attributes.GetValue("Location"))
-                lReport.WriteLine("Time Span:" & DateString(lDataSet.Dates.Value(1)) & _
-                                        " to " & DateString(lDataSet.Dates.Value(lDataSet.Dates.numValues)))
-
-                lReport.WriteLine(vbCrLf & "Storm Events" & vbCrLf)
-
-                lReport.WriteLine("Storm #" & vbTab & _
-                               "Date      " & vbTab & _
-                                     "Hour" & vbTab & _
-                                    "Hours" & vbTab & _
-                                    "Volume")
-
-                Dim lStorms As atcDataGroup = atcEvents.EventSplit(lDataSet, Nothing, lThreshold, lHighEvents)
-                Dim lStormIndex As Integer = 0
-                For Each lStorm As atcTimeseries In lStorms
-                    lStormIndex += 1
-                    'Storm Number, Date, Hour
-                    lReport.Write(lStormIndex & vbTab & DateTabHour(lStorm.Dates.Value(1)))
-                    'Duration (hours) 'TODO: dont hard code hours?
-                    lReport.Write(vbTab & CInt((lStorm.Dates.Value(lStorm.Dates.numValues) - lStorm.Dates.Value(1)) * 24) + 1)
-                    'Volume
-                    lReport.Write(vbTab & Format(lStorm.Attributes.GetValue("Sum"), "0.00"))
-                    lReport.WriteLine()
-                Next
-            Next
-            lReport.Close()
-            Logger.Dbg("SynopticAnalysis Complete, Results in file '" & aFileName & "'")
+        If aDataGroup Is Nothing Then
+            aDataGroup = New atcDataGroup
         End If
+        If aDataGroup.Count = 0 Then
+            aDataManager.UserSelectData("Select data for Synoptic Analysis", aDataGroup)
+            If aDataGroup.Count = 0 Then
+                Dim lMsg As String = "No data sets selected for synoptic analysis"
+                SaveFileString(aFileName, lMsg)
+                Logger.Dbg(lMsg)
+                Exit Sub
+            End If
+        End If
+
+        Dim lThreshold As Double = 0 '0.01 'TODO: allow this to be an option
+        Dim lDaysGapAllowed As Double = JulianSecond + 4 / 24  'TODO: allow this to be an option
+        Dim lHighEvents As Boolean = True
+        Dim lDuration As Double
+        Dim lVolume As Double
+        Dim lTimeSince As Double = 0
+        Dim lReport As New IO.StreamWriter(aFileName)
+
+        lReport.WriteLine("Synoptic Analysis of " & aDataGroup.Count & " Data Sets")
+        For Each lDataSet As atcTimeseries In aDataGroup
+            lDataSet.EnsureValuesRead()
+            lReport.WriteLine()
+            lReport.WriteLine("Location:" & lDataSet.Attributes.GetValue("Location"))
+            lReport.WriteLine("Time Span:" & DateString(lDataSet.Dates.Value(1)) & _
+                                    " to " & DateString(lDataSet.Dates.Value(lDataSet.Dates.numValues)))
+
+            lReport.WriteLine(vbCrLf & "Storm Events" & vbCrLf)
+
+            lReport.WriteLine("Storm " & vbTab & "          " & vbTab & "    " & vbTab & "Duration" & vbTab & "Total " & vbTab & "Average  " & vbTab & "Maximum  " & vbTab & "Time Since Last")
+            lReport.WriteLine("Number" & vbTab & "Date      " & vbTab & "Hour" & vbTab & "(Hours) " & vbTab & "Volume" & vbTab & "Intensity" & vbTab & "Intensity" & vbTab & "(Hours)        ")
+
+            Dim lStorms As atcDataGroup = atcEvents.EventSplit(lDataSet, Nothing, lThreshold, lDaysGapAllowed, lHighEvents)
+            Dim lStormIndex As Integer = 0
+            For Each lStorm As atcTimeseries In lStorms
+                lStormIndex += 1
+
+                'Storm Number, Date, Hour
+                lReport.Write(lStormIndex & vbTab & DateTabHour(lStorm.Dates.Value(1)))
+
+                'Duration (hours) 'TODO: dont hard code hours?
+                'TODO: 1 + should be 1 time unit, not 1 hour
+                lDuration = 1 + 24 * (lStorm.Dates.Value(lStorm.Dates.numValues) - lStorm.Dates.Value(1))
+                lReport.Write(vbTab & StrPad(CInt(lDuration), 8))
+
+                'Volume
+                lVolume = lStorm.Attributes.GetValue("Sum")
+                lReport.Write(vbTab & StrPad(Format(lStorm.Attributes.GetValue("Sum"), "0.00"), 6))
+
+                'Average Intensity
+                lReport.Write(vbTab & StrPad(Format(lVolume / lDuration, "0.00"), 9))
+
+                'Maximum Intensity
+                lReport.Write(vbTab & StrPad(Format(lStorm.Attributes.GetValue("Max"), "0.00"), 9))
+
+                'Time since previous event
+                lTimeSince = lStorm.Attributes.GetValue("EventTimeSincePrevious", 0)
+                If lTimeSince > 0 Then
+                    lReport.Write(vbTab & StrPad(Format(lTimeSince * 24, "#,###"), 9))
+                End If
+
+                lReport.WriteLine()
+            Next
+        Next
+        lReport.Close()
+        Logger.Dbg("SynopticAnalysis Complete, Results in file '" & aFileName & "'")
     End Sub
 
     Private Function DateString(ByVal aJulianDay As Double) As String
