4aec5e7bcbf66215f048a89e2d84e057f92edcfa
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2007-01-30 16:58:11 +0000

    atcSynopticAnalysis: added new frmChooseColumns to select which columns to show                      added new View/Choose Columns item to open new form

---
+++ BASINS40/atcSynopticAnalysis/frmChooseColumns.vb
@@ -0,0 +1,38 @@
+Public Class frmChooseColumns
+
+    Public Function AskUser(ByVal aAvailable() As String, ByVal aSelected() As String) As String()
+        Dim lSelected() As String = aSelected
+        Dim lCurrentIndex As Integer
+
+        lstColumns.Items.Clear()
+        lstColumns.Items.AddRange(aAvailable)
+
+        'Select items in aSelected
+        For lCurrentIndex = 0 To aAvailable.GetUpperBound(0)
+            If Array.IndexOf(aSelected, aAvailable(lCurrentIndex)) >= 0 Then
+                lstColumns.SetItemChecked(lCurrentIndex, True)
+            End If
+        Next
+
+        If Me.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            ReDim lSelected(lstColumns.CheckedIndices.Count - 1)
+            lCurrentIndex = 0
+            For Each lCheckedIndex As Integer In lstColumns.CheckedIndices
+                lSelected(lCurrentIndex) = aAvailable(lCheckedIndex)
+                lCurrentIndex += 1
+            Next
+        End If
+
+        Return lSelected
+    End Function
+
+    Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
+        Me.DialogResult = Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+        Me.DialogResult = Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+End Class
