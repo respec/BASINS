a5e603f45c2ce7a4e9cb26a36bb52d8febcde6c3
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2007-03-22 13:06:21 +0000

    New project for running a script in a plugin

---
+++ BASINS40/ScriptRunner/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ScriptRunner")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ScriptRunner")> 
+<Assembly: AssemblyCopyright("Copyright ©  2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3ce0baa0-659f-4367-99a0-654b0df8d523")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ BASINS40/ScriptRunner/clsScriptPlugin.vb
@@ -0,0 +1,30 @@
+Public Class ScriptPlugin
+    Inherits atcData.atcDataPlugin
+
+    Dim pMapWin As MapWindow.Interfaces.IMapWin
+
+    Public Overrides ReadOnly Property Name() As String
+        Get
+            Return "ScriptPlugin"
+        End Get
+    End Property
+
+    Public Overrides Sub Initialize(ByVal aMapWin As MapWindow.Interfaces.IMapWin, ByVal aParentHandle As Integer)
+        MyBase.Initialize(aMapWin, aParentHandle)
+        pMapWin = aMapWin
+        pMapWin.Toolbar.AddButton("Test")
+    End Sub
+
+    Public Overrides Sub Terminate()
+        MyBase.Terminate()
+        pMapWin.Toolbar.RemoveButton("Test")
+    End Sub
+
+    Public Overrides Sub ItemClicked(ByVal aItemName As String, ByRef aHandled As Boolean)
+        MyBase.ItemClicked(aItemName, aHandled)
+        If aItemName = "Test" Then
+            ScriptStatTest.ScriptMain(pMapWin)
+        End If
+    End Sub
+
+End Class
