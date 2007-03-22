996c31e91165cb6afbc6afbd66041828385052da
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2007-03-22 13:20:39 +0000

    atcSeasons: changed handling of selected seasons to be the same for all season types, added AllSeasonsInDates function to return set of seasons that exist in a set of dates

--- BASINS40/atcSeasons/atcSeasonsCalendarYear.vb
+++ BASINS40/atcSeasons/atcSeasonsCalendarYear.vb
@@ -1,33 +1,17 @@
 Public Class atcSeasonsCalendarYear
     Inherits atcSeasonBase
 
-    Private pSeasonsSelected As New ArrayList
+    Private Shared pAllSeasons As Integer() = {}
 
-    Public Overrides Function Clone() As atcSeasonBase
-        Dim lNewSeason As New atcSeasonsCalendarYear
-        lNewSeason.pSeasonsSelected = pSeasonsSelected.Clone
-        Return lNewSeason
+    Public Overrides Function AllSeasons() As Integer()
+        Return pAllSeasons
     End Function
+
+    Public Sub SetAllSeasons(ByVal aAllSeasons As Integer())
+        pAllSeasons = aAllSeasons.Clone
+    End Sub
 
     Public Overrides Function SeasonIndex(ByVal aDate As Double) As Integer
         Return Date.FromOADate(aDate).Year
     End Function
-
-    Public Overrides Property SeasonSelected(ByVal aSeasonIndex As Integer) As Boolean
-        Get
-            Return pSeasonsSelected.Contains(aSeasonIndex)
-        End Get
-        Set(ByVal newValue As Boolean)
-            If newValue Then
-                If Not pSeasonsSelected.Contains(aSeasonIndex) Then
-                    pSeasonsSelected.Add(newValue)
-                End If
-            Else
-                While pSeasonsSelected.Contains(aSeasonIndex)
-                    pSeasonsSelected.Remove(aSeasonIndex)
-                End While
-            End If
-        End Set
-    End Property
-
 End Class
