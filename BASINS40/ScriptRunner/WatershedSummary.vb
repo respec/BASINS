e65750c69ec5a74842c4d11a53c2ddb48a818834
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2007-03-27 20:30:15 +0000

    ConstituentBalance.vb and WatershedSummary.vb - combine and improve formatting

--- BASINS40/ScriptRunner/WatershedSummary.vb
+++ BASINS40/ScriptRunner/WatershedSummary.vb
@@ -8,27 +8,18 @@ Imports MapWinUtility
 Imports Microsoft.VisualBasic
 Imports System
 
-Public Module ScriptStatTest
+Public Module ScriptStatTestX
     Private Const pTestPath As String = "C:\test\WatershedSummary\"
-    Private Const pSummaryType As String = "TotalN"
 
-    Public Sub ScriptMain(ByRef aMapWin As IMapWin)
+    Public Sub ScriptMainX(ByRef aMapWin As IMapWin)
         Logger.Dbg("Start")
         ChDriveDir(pTestPath)
-        Logger.Dbg(" CurDir:" & CurDir)
+        Logger.Dbg(" CurDir:" & CurDir())
 
         Dim lScenario As String = "base"
 
-        Dim lPrimaryConstituent As String = ""
-        Select Case pSummaryType
-            Case "Water"
-            Case "TotalN"
-                'Total N is a combination of NH4, No3, OrganicN
-                lPrimaryConstituent = "NITROGEN - TOTAL OUTFLOW"
-        End Select
-
-        Dim lString As New Text.StringBuilder
-        lString.AppendLine(pSummaryType & " Watershed Summary Report For " & lScenario)
+        Dim lSummaryTypes As New atcCollection
+        lSummaryTypes.Add("TotalN")
 
         Dim lArgs As Object() = Nothing
         Dim lErr As String = ""
@@ -39,81 +30,114 @@ Public Module ScriptStatTest
         Dim lHspfBinFileName As String = "Input\" & lScenario & ".hbn"
         Logger.Dbg(" AboutToOpen " & lHspfBinFileName)
         lDataManager.OpenDataSource(lHspfBinFile, lHspfBinFileName, Nothing)
-        Logger.Dbg(" DataSetCount " & lHspfBinFile.Datasets.Count)
+        Logger.Dbg(" DataSetCount " & lHspfBinFile.DataSets.Count)
         Dim lFileDetails As System.IO.FileInfo = New System.IO.FileInfo(lHspfBinFileName)
-        lString.AppendLine("   Run Made " & lFileDetails.CreationTime & vbCrLf)
-        lString.AppendLine("Land Use" & vbTab & vbTab & "Area" & vbTab & vbTab & "Load" & vbTab & vbTab & "Total Load" & vbTab & "Total Load")
-        lString.AppendLine(vbTab & vbTab & vbTab & "(acres)" & vbTab & vbTab & "(lbs/acre)" & vbTab & "(lbs)" & vbTab & vbTab & "(%)")
-        lString.AppendLine("")
+
+        DoWatershedSummary(lSummaryTypes, lScenario, lHspfBinFile, lFileDetails.CreationTime)
+
+    End Sub
+
+    Friend Sub DoWatershedSummary(ByVal aSummaryTypes As atcCollection, _
+                                  ByVal aScenario As String, _
+                                  ByVal aScenarioResults As atcDataSource, _
+                                  ByVal aRunMade As String)
 
         'make uci available 
         Dim lMsg As New atcUCI.HspfMsg
         lMsg.Open("hspfmsg.mdb")
         Dim lHspfUci As New atcUCI.HspfUci
-        lHspfUci.FastReadUciForStarter(lMsg, "Input\" & lScenario & ".uci")
+        lHspfUci.FastReadUciForStarter(lMsg, aScenario & ".uci")
 
-        Dim lOper As atcUCI.HspfOperation
-        Dim lLuName As String
-        Dim lLuArea As Single
-        Dim lValue As Single
-        Dim lTotal As Single
-        Dim lTempDataSet As atcDataSet
-        Dim lOperTypes As New Collection
-        lOperTypes.Add("PERLND")
-        lOperTypes.Add("IMPLND")
+        For Each lSummaryType As String In aSummaryTypes
+            Dim lPrimaryConstituent As String = ""
+            Select Case lSummaryType
+                Case "Water"
+                Case "TotalN"
+                    'Total N is a combination of NH4, No3, OrganicN
+                    lPrimaryConstituent = "NITROGEN - TOTAL OUTFLOW"
+            End Select
 
-        Dim lLandUses As New Collection
-        Dim lAreas As New Collection
-        Dim lLoads As New Collection
-        Dim lTotalLoads As New Collection
-        Dim lSum As Double = 0.0
+            Dim lString As New Text.StringBuilder
+            lString.AppendLine(lSummaryType & " Watershed Summary Report For " & aScenario)
+            lString.AppendLine("   Run Made " & aRunMade & vbCrLf)
+            lString.AppendLine("Land Use  " & vbTab & _
+                               "    Area    " & vbTab & _
+                               "    Load    " & vbTab & _
+                               "Total Load  " & vbTab & _
+                               "Total Load")
+            lString.AppendLine("            " & vbTab & _
+                               "   (acres)  " & vbTab & _
+                               " (lbs/acre) " & vbTab & _
+                               "  (lbs)     " & vbTab & _
+                               "    (%)     ")
+            lString.AppendLine("")
 
-        For Each lOperType As String In lOperTypes
-            For Each lOper In lHspfUci.OpnBlks(lOperType).ids
-                'for each operation, get land use name, number of acres, and load/acre
-                lLuName = lOper.Tables("GEN-INFO").parms(1).value
-                lLuArea = LandArea(lOper.Name, lOper.Id, lHspfUci)
+            Dim lOper As atcUCI.HspfOperation
+            Dim lLuName As String
+            Dim lLuArea As Single
+            Dim lValue As Single
+            Dim lTotal As Single
+            Dim lTempDataSet As atcDataSet
+            Dim lOperTypes As New Collection
+            lOperTypes.Add("PERLND")
+            lOperTypes.Add("IMPLND")
 
-                Dim lTempDataGroup As atcDataGroup = lHspfBinFile.DataSets.FindData("Location", Left(lOperType, 1) & ":" & lOper.Id)
-                If lTempDataGroup.FindData("Constituent", lPrimaryConstituent).Count > 0 Then
-                    lTempDataSet = lTempDataGroup.FindData("Constituent", lPrimaryConstituent).Item(0)
-                    lValue = lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
-                Else
-                    If lOperType = "PERLND" Then
-                        lTempDataSet = lTempDataGroup.FindData("Constituent", "POQUAL-NH4").Item(0)
+            Dim lLandUses As New Collection
+            Dim lAreas As New Collection
+            Dim lLoads As New Collection
+            Dim lTotalLoads As New Collection
+            Dim lSum As Double = 0.0
+
+            For Each lOperType As String In lOperTypes
+                For Each lOper In lHspfUci.OpnBlks(lOperType).ids
+                    'for each operation, get land use name, number of acres, and load/acre
+                    lLuName = lOper.Tables("GEN-INFO").parms(1).value
+                    lLuArea = LandArea(lOper.Name, lOper.Id, lHspfUci)
+
+                    Dim lTempDataGroup As atcDataGroup = aScenarioResults.DataSets.FindData("Location", Left(lOperType, 1) & ":" & lOper.Id)
+                    If lTempDataGroup.FindData("Constituent", lPrimaryConstituent).Count > 0 Then
+                        lTempDataSet = lTempDataGroup.FindData("Constituent", lPrimaryConstituent).Item(0)
                         lValue = lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
-                        lTempDataSet = lTempDataGroup.FindData("Constituent", "POQUAL-NO3").Item(0)
-                        lValue = lValue + lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
-                        lTempDataSet = lTempDataGroup.FindData("Constituent", "POQUAL-BOD").Item(0)
-                        lValue = lValue + lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value * 0.048
                     Else
-                        lTempDataSet = lTempDataGroup.FindData("Constituent", "SOQUAL-NH4").Item(0)
-                        lValue = lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
-                        lTempDataSet = lTempDataGroup.FindData("Constituent", "SOQUAL-NO3").Item(0)
-                        lValue = lValue + lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
-                        lTempDataSet = lTempDataGroup.FindData("Constituent", "SOQUAL-BOD").Item(0)
-                        lValue = lValue + lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value * 0.048
+                        If lOperType = "PERLND" Then
+                            lTempDataSet = lTempDataGroup.FindData("Constituent", "POQUAL-NH4").Item(0)
+                            lValue = lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
+                            lTempDataSet = lTempDataGroup.FindData("Constituent", "POQUAL-NO3").Item(0)
+                            lValue = lValue + lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
+                            lTempDataSet = lTempDataGroup.FindData("Constituent", "POQUAL-BOD").Item(0)
+                            lValue = lValue + lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value * 0.048
+                        Else
+                            lTempDataSet = lTempDataGroup.FindData("Constituent", "SOQUAL-NH4").Item(0)
+                            lValue = lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
+                            lTempDataSet = lTempDataGroup.FindData("Constituent", "SOQUAL-NO3").Item(0)
+                            lValue = lValue + lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value
+                            lTempDataSet = lTempDataGroup.FindData("Constituent", "SOQUAL-BOD").Item(0)
+                            lValue = lValue + lTempDataSet.Attributes.GetDefinedValue("SumAnnual").Value * 0.048
+                        End If
                     End If
-                End If
-                lTotal = lLuArea * lValue
-                lLandUses.Add(Left(lOperType, 1) & lOper.Id & ":" & lLuName)
-                lAreas.Add(lLuArea)
-                lLoads.Add(lValue)
-                lTotalLoads.Add(lTotal)
-                lSum = lSum + lTotal
+                    lTotal = lLuArea * lValue
+                    lLandUses.Add(Left(lOperType, 1) & lOper.Id & ":" & lLuName.Trim)
+                    lAreas.Add(lLuArea)
+                    lLoads.Add(lValue)
+                    lTotalLoads.Add(lTotal)
+                    lSum = lSum + lTotal
+                Next
             Next
-        Next
 
-        For lIndex As Integer = 1 To lLandUses.Count
-            lString.AppendLine(lLandUses(lIndex) & vbTab & DoubleToString(lAreas(lIndex)) & vbTab & vbTab & DoubleToString(lLoads(lIndex)) & vbTab & vbTab & DoubleToString(lTotalLoads(lIndex)) & vbTab & vbTab & DoubleToString((lTotalLoads(lIndex) / lSum * 100), , , , , 2))
-        Next
-        lString.AppendLine("")
-        lString.AppendLine(vbTab & vbTab & vbTab & vbTab & vbTab & "Total Load = " & DoubleToString(lSum) & " lbs.")
+            For lIndex As Integer = 1 To lLandUses.Count
+                lString.AppendLine(lLandUses(lIndex) & vbTab & _
+                                   DF(lAreas(lIndex)) & vbTab & _
+                                   DF(lLoads(lIndex)) & vbTab & _
+                                   DF(lTotalLoads(lIndex)) & vbTab & _
+                                   DF((lTotalLoads(lIndex) / lSum * 100), 2))
+            Next
+            lString.AppendLine("")
+            lString.AppendLine(vbTab & vbTab & vbTab & vbTab & "Total Load = " & vbTab & DF(lSum) & vbTab & " lbs.")
 
-        Dim lOutFileName As String = lScenario & "_" & pSummaryType & "_" & "WatershedSummary.txt"
-        Logger.Dbg("  WriteReportTo " & lOutFileName)
-        SaveFileString(lOutFileName, lString.ToString)
-
+            Dim lOutFileName As String = aScenario & "_" & lSummaryType & "_" & "WatershedSummary.txt"
+            Logger.Dbg("  WriteReportTo " & lOutFileName)
+            SaveFileString(lOutFileName, lString.ToString)
+        Next lSummaryType
     End Sub
 
     Private Function LandArea(ByVal aOperName As String, ByVal aOperID As Integer, ByVal aUci As atcUCI.HspfUci) As Single
