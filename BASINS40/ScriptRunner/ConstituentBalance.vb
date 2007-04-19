5067ae81c637f8c19e2a000e0133cbfe13a428a9
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2007-04-19 21:59:19 +0000

    clsScriptPlugin.vb -- modified to call a general script driver ScriptDriver.vb -- new module to reference currently used scripts CatSummary.vb -- new module for cat summary that used to be part of another script ConstituentBalance.vb -- added many more comments WatershedSummary.vb -- added many more comments ScriptRunner.vbproj -- added new modules

--- BASINS40/ScriptRunner/ConstituentBalance.vb
+++ BASINS40/ScriptRunner/ConstituentBalance.vb
@@ -7,47 +7,65 @@ Imports MapWinUtility
 Imports Microsoft.VisualBasic
 Imports System
 
-Public Module ScriptStatTest
+Public Module ScriptConstituentBalance
     Private Const pFieldWidth As Integer = 12
     Private Const pTestPath As String = "C:\test\SegmentBalance\"
-    Private Const pCatSummaryFileName As String = "CatSummary.txt"
     Private Const pDebug As Boolean = False
 
     Public Sub ScriptMain(ByRef aMapWin As IMapWin)
+
+        'For each UCI file in the specified (pTestPath) folder, 
+        'do a Constituent Balance Report for the specified constituents;
+        'ie water balance, total n balance, etc.
+
+        'Requirements:
+        'UCI and HBN must reside in the same folder,
+        'base name of UCI = base name of HBN file
+
+        'Output:
+        'writes to the pTestPath folder, for each UCI and constituent,
+        'a file named as follows:
+        'UCI base name & "_" & constituent name & "_" & "Balance.txt"
+
         Logger.Dbg("Start")
+
+        'change to the directory of the current project
         ChDriveDir(pTestPath)
         Logger.Dbg(" CurDir:" & CurDir())
-        If FileExists(pCatSummaryFileName) Then Kill(pCatSummaryFileName)
 
+        'build collection of constituents to report
         Dim lConstituents As New atcCollection
         lConstituents.Add("Water")
-        'lBalanceTypes.Add("SedimentCopper")
         lConstituents.Add("TotalN")
-        'lConstituents.Add("TotalP")
+        'lConstituents.Add("SedimentCopper")  'implemented but not in use
+        'lConstituents.Add("TotalP")          'not yet implemented
 
+        'build collection of scenarios (uci base names) to report
         Dim lUcis As New System.Collections.Specialized.NameValueCollection
         AddFilesInDir(lUcis, pTestPath, False, "*.uci")
         Dim lScenarios As New atcCollection
-        'lScenarios.Add("USANFRAN")
-        'lScenarios.Add("baseExcerpt")
         For Each lUci As String In lUcis
             lScenarios.Add(FilenameNoPath(FilenameNoExt(lUci)))
         Next
 
+        'build collection of operation types to report
         Dim lOperations As New atcCollection
         lOperations.Add("P:", "PERLND")
         lOperations.Add("I:", "IMPLND")
         lOperations.Add("R:", "RCHRES")
 
-        Dim lArgs As Object() = Nothing
-        Dim lErr As String = ""
-        Dim lDataManager As New atcDataManager(aMapWin) ' = Scripting.Run("vb", "", "subFindDataManager.vb", lErr, False, aMapWin, aMapWin)
+        'declare a new data manager to manage the hbn files
+        Dim lDataManager As New atcDataManager(aMapWin)
 
+        'loop thru each scenario (uci name)
         For Each lScenario As String In lScenarios
+
+            'open the corresponding hbn file
             Dim lHspfBinFile As atcDataSource = New atcHspfBinOut.atcTimeseriesFileHspfBinOut
             Dim lHspfBinFileName As String = lScenario & ".hbn"
             Logger.Dbg(" AboutToOpen " & lHspfBinFileName)
             If Not FileExists(lHspfBinFileName) Then
+                'if hbn doesnt exist, make a guess at what the name might be
                 lHspfBinFileName = lHspfBinFileName.Replace(".hbn", ".base.hbn")
                 Logger.Dbg("  NameUpdated " & lHspfBinFileName)
             End If
@@ -55,46 +73,27 @@ Public Module ScriptStatTest
             lDataManager.OpenDataSource(lHspfBinFile, lHspfBinFileName, Nothing)
             Logger.Dbg(" DataSetCount " & lHspfBinFile.DataSets.Count)
 
-            Dim lHspfWdmFile As atcDataSource = New atcWDM.atcDataSourceWDM
-            Dim lHspfWdmFileName As String = lScenario & ".wdm"
-            Logger.Dbg(" AboutToOpen " & lHspfWdmFileName)
-            If Not FileExists(lHspfWdmFileName) Then
-                lHspfWdmFileName = lHspfWdmFileName.Replace(".wdm", ".base.wdm")
-                Logger.Dbg("  NameUpdated " & lHspfWdmFileName)
-            End If
-            lDataManager.OpenDataSource(lHspfWdmFile, lHspfWdmFileName, Nothing)
-            Logger.Dbg(" DataSetCount " & lHspfWdmFile.DataSets.Count)
-
+            'build collection of locations to report (hspf operations)
             Dim lLocations As atcCollection = lDataManager.DataSets.SortedAttributeValues("Location")
-            'lLocations.Add("I:101")
-            'Logger.Dbg(" LocationCount " & lLocations.ToString(lLocations.Count))
             Logger.Dbg(" LocationCount " & lLocations.Count)
 
-            Dim lConstituentsAvailable As atcCollection = lDataManager.DataSets.SortedAttributeValues("Constituent")
-            Logger.Dbg(" ConstituentCount " & lConstituentsAvailable.Count)
-            'Logger.Dbg(" ConstituentCount " & lConstituents.ToString(lConstituentsAvailable.Count))
+            'call main constituent balances routine
+            DoConstituentBalances(lOperations, lConstituents, lScenario, lHspfBinFile, lLocations, lHspfBinFileInfo.CreationTime)
 
-            DoSegmentBalances(lOperations, lConstituents, lScenario, lHspfBinFile, lLocations, lHspfBinFileInfo.CreationTime)
-
-            DoWatershedSummary(lConstituents, lScenario, lHspfBinFile, lHspfBinFileInfo.CreationTime)
-
-            DoCatSummary(lScenario, lDataManager)
-
+            'clean up 
             lDataManager.DataSources.Remove(lHspfBinFile)
             lHspfBinFile.DataSets.Clear()
             lHspfBinFile = Nothing
-            lDataManager.DataSources.Remove(lHspfWdmFile)
-            lHspfWdmFile.DataSets.Clear()
-            lHspfWdmFile = Nothing
+
         Next lScenario
     End Sub
 
-    Private Sub DoSegmentBalances(ByVal aOperations As atcCollection, _
-                                  ByVal aBalanceTypes As atcCollection, _
-                                  ByVal aScenario As String, _
-                                  ByVal aScenarioResults As atcDataSource, _
-                                  ByVal aLocations As atcCollection, _
-                                  ByVal aRunMade As String)
+    Friend Sub DoConstituentBalances(ByVal aOperations As atcCollection, _
+                                     ByVal aBalanceTypes As atcCollection, _
+                                     ByVal aScenario As String, _
+                                     ByVal aScenarioResults As atcDataSource, _
+                                     ByVal aLocations As atcCollection, _
+                                     ByVal aRunMade As String)
         For Each lBalanceType As String In aBalanceTypes
             Dim lConstituents2Output As New atcCollection
             Select Case lBalanceType
@@ -307,104 +306,7 @@ Public Module ScriptStatTest
         Next lBalanceType
     End Sub
 
-    Private Sub DoCatSummary(ByVal aScenario As String, ByVal aDataManager As atcDataManager)
-        Logger.Dbg("DoCatSummary for " & aScenario)
-
-        Dim lString As New Text.StringBuilder
-        lString.Append(aScenario)
-
-        'Get this hard coded stuff from CAT endpoints/variations!
-
-        Dim lMetDataGroup As atcDataGroup = aDataManager.DataSets.FindData("Location", "SEG1")
-        Logger.Dbg("     MetMatchingDatasetCount " & lMetDataGroup.Count)
-        lMetDataGroup.Add(aDataManager.DataSets.FindData("Location", "_A24013"))
-        lMetDataGroup.Add(aDataManager.DataSets.FindData("Location", "A24013"))
-        Logger.Dbg("     AdditionalMetMatchingDatasetCount " & lMetDataGroup.Count)
-
-        If lMetDataGroup.Count > 0 Then
-            lString.Append(AnnualAndSeasonalValues(lMetDataGroup, "HPRC", "Sum"))
-            lString.Append(AnnualAndSeasonalValues(lMetDataGroup, "ATMP", "Mean"))
-            lString.Append(AnnualAndSeasonalValues(lMetDataGroup, "EVAP", "Sum"))
-        End If
-
-        Dim lRchDataGroupW As atcDataGroup = aDataManager.DataSets.FindData("Location", "RIV9")
-        If lRchDataGroupW.Count > 0 Then
-            lString.Append(AnnualAndSeasonalValues(lRchDataGroupW, "WATR", "Mean"))
-            lString.Append(AnnualAndSeasonalValues(lRchDataGroupW, "WATR", "1Hi100"))
-            lString.Append(AnnualAndSeasonalValues(lRchDataGroupW, "FLOW", "7Q10"))
-        End If
-
-        Dim lRchDataGroup As atcDataGroup = aDataManager.DataSets.FindData("Location", "R:9")
-        If lRchDataGroup.Count > 0 Then
-            lString.Append(AnnualValue(lRchDataGroup, "RO", "Mean"))
-            lString.Append(AnnualValue(lRchDataGroup, "ROSED-TOT", "SumAnnual"))
-            lString.Append(AnnualValue(lRchDataGroup, "ROSED-TOT", "SumAnnual", True))
-            lString.Append(AnnualValue(lRchDataGroup, "ROSED-TOT", "Mean"))
-            lString.Append(AnnualValue(lRchDataGroup, "ROSED-TOT", "Mean", True))
-            lString.Append(AnnualValue(lRchDataGroup, "P-TOT-OUT", "SumAnnual"))
-            lString.Append(AnnualValue(lRchDataGroup, "N-TOT-OUT", "SumAnnual"))
-        End If
-        lString.AppendLine()
-
-        Logger.Dbg("CAT Report Add - " & lString.ToString)
-        AppendFileString(pCatSummaryFileName, lString.ToString)
-    End Sub
-
-    Private Function AnnualValue(ByVal aDataGroup As atcDataGroup, ByVal aCons As String, ByVal aTrans As String, Optional ByVal aSummer As Boolean = False) As String
-        Dim lConsDataGroup As atcDataGroup = aDataGroup.FindData("Constituent", aCons)
-        Logger.Dbg("     " & aCons & "MatchingDatasetCount " & lConsDataGroup.Count)
-        Dim lString As String = ""
-        If lConsDataGroup.Count > 0 Then
-            Dim lTempDataSet As atcDataSet = lConsDataGroup.Item(0)
-            Dim lValue As Double = lTempDataSet.Attributes.GetDefinedValue(aTrans).Value
-            lString = vbTab & DF(lValue)
-            If aSummer Then
-                Dim lSeasons As New atcSeasons.atcSeasonsMonth
-                Dim lSeasonalAttributes As New atcDataAttributes
-                Dim lCalculatedAttributes As New atcDataAttributes
-                lSeasonalAttributes.SetValue(aTrans, 0) 'fluxes are summed from daily, monthly or annual to annual
-                lSeasons.SetSeasonalAttributes(lTempDataSet, lSeasonalAttributes, lCalculatedAttributes)
-                lValue = 0
-                For lIndex As Integer = 3 To 9
-                    lValue += lCalculatedAttributes(lIndex).Value 'AMJJASO
-                Next lIndex
-                lString &= vbTab & DF(lValue)
-            End If
-        End If
-        Return lString
-    End Function
-
-    Private Function AnnualAndSeasonalValues(ByVal aDataGroup As atcDataGroup, ByVal aCons As String, ByVal aTrans As String) As String
-        Dim lConsDataGroup As atcDataGroup = aDataGroup.FindData("Constituent", aCons)
-        Logger.Dbg("     " & aCons & "MatchingDatasetCount " & lConsDataGroup.Count)
-        Dim lString As String = ""
-        If lConsDataGroup.Count > 0 Then
-            Dim lTempDataSet As atcDataSet = lConsDataGroup.Item(0)
-            Dim lValue As Double = lTempDataSet.Attributes.GetDefinedValue(aTrans).Value
-            lString = vbTab & DF(lValue)
-            Dim lSeasons As New atcSeasons.atcSeasonsMonth
-            Dim lSeasonalAttributes As New atcDataAttributes
-            Dim lCalculatedAttributes As New atcDataAttributes
-            lSeasonalAttributes.SetValue(aTrans, 0) 'fluxes are summed from daily, monthly or annual to annual
-            lSeasons.SetSeasonalAttributes(lTempDataSet, lSeasonalAttributes, lCalculatedAttributes)
-            lValue = lCalculatedAttributes(0).Value + lCalculatedAttributes(1).Value + lCalculatedAttributes(11).Value   'DJF
-            If aTrans = "Mean" Then lValue = lValue / 3
-            lString &= vbTab & DF(lValue)
-            lValue = lCalculatedAttributes(2).Value + lCalculatedAttributes(3).Value + lCalculatedAttributes(4).Value  'MAM
-            If aTrans = "Mean" Then lValue = lValue / 3
-            lString &= vbTab & DF(lValue)
-            lValue = lCalculatedAttributes(5).Value + lCalculatedAttributes(6).Value + lCalculatedAttributes(7).Value  'JJA
-            If aTrans = "Mean" Then lValue = lValue / 3
-            lString &= vbTab & DF(lValue)
-            lValue = lCalculatedAttributes(8).Value + lCalculatedAttributes(9).Value + lCalculatedAttributes(10).Value  'SON
-            If aTrans = "Mean" Then lValue = lValue / 3
-            lString &= vbTab & DF(lValue)
-        End If
-        Return lString
-
-    End Function
-
-    Friend Function DF(ByVal aValue As Double, Optional ByVal aDecimalPlaces As Integer = 3) As String
+    Private Function DF(ByVal aValue As Double, Optional ByVal aDecimalPlaces As Integer = 3) As String
         Dim lFormat As String
         If aDecimalPlaces > 1 Then
             lFormat = "###,##0.0" & StrDup(aDecimalPlaces - 1, "#")
@@ -420,4 +322,5 @@ Public Module ScriptStatTest
         Return lString.PadRight(pFieldWidth)
         'Return Trim(Format(aValue, "##########0." & StrDup(aDecimalPlaces, "0")))
     End Function
+
 End Module
