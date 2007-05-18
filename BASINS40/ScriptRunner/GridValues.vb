365be800c6d3c41841e60fa6fcfc37b1bd96cf63
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2007-05-18 16:09:08 +0000

    enhance scripts to work with LIDAR grids and NED grids

--- BASINS40/ScriptRunner/GridValues.vb
+++ BASINS40/ScriptRunner/GridValues.vb
@@ -4,6 +4,7 @@ Imports atcUtility
 Imports MapWindow.Interfaces
 Imports MapWinUtility
 Imports MapWinGeoProc
+Imports atcMwGisUtility
 
 Imports Microsoft.VisualBasic
 Imports System
@@ -19,46 +20,78 @@ Public Module ScriptGridValues
         Logger.Dbg(" CurDir:" & CurDir())
         Dim lString As New Text.StringBuilder
         Dim lPointCollection As atcCollection
-        Dim lRatio As Double
+        Dim lX, lY As Double
 
+        lString.AppendLine("PointsFromGridWithStep " & pStepSize)
         'sample and save points from grid
-        For lX As Double = 688000 To 724000 Step pStepSize
-            For lY As Double = 3568000 To 3604000 Step pStepSize
+        For lX = 688000 To 724000 Step pStepSize
+            For lY = 3568000 To 3604000 Step pStepSize
                 lPointCollection = XY2Z(aMapWin, lX, lY)
-                If lPointCollection.Count > 1 Then
-                    lString.Append(lX.ToString & vbTab & lY.ToString & vbTab & lPointCollection(0) & vbTab & lPointCollection(1))
-                    lRatio = lPointCollection(0) / lPointCollection(1)
-                    lString.Append(vbTab & DoubleToString(lRatio))
-                    For lIndex As Integer = 3 To lPointCollection.Count
-                        lString.Append(vbTab & lPointCollection(lIndex - 1) & vbTab & "???")
-                    Next lIndex
-                    lString.AppendLine()
-                End If
+                ReportPoints(lX, lY, lPointCollection, lString)
             Next lY
         Next lX
-        SaveFileString("GridSample.txt", lString.ToString)
 
         'sample points going up Upatoi Creek
-        Dim lLayer As MapWinGIS.Shapefile = Nothing
-        For lLayerIndex As Integer = 0 To aMapWin.Layers.NumLayers - 1
-            If aMapWin.Layers(lLayerIndex).LayerType = eLayerType.LineShapefile Then
-                lLayer = aMapWin.Layers(lLayerIndex).GetObject
-                If FilenameNoPath(lLayer.Filename) = "UpatoiNHDFlowline.shp" Then
-                    'we need to do better than this!!!!!!!
-                    Logger.Dbg("Found Upatoi:ShapeCount:" & lLayer.NumShapes)
-                    Exit For
-                End If
+        lString.AppendLine("PointsFromStream")
+        GisUtil.MappingObject = aMapWin
+        Dim lLayerIndex As Integer = GisUtil.LayerIndex("UpatoiNHDFlowline")
+        Logger.Dbg("Found Upatoi:ShapeCount:" & GisUtil.NumFeatures(lLayerIndex))
+
+        Dim lComIdFieldIndex As Integer = GisUtil.FieldIndex(lLayerIndex, "COMID")
+        Dim lToComIdFieldIndex As Integer = GisUtil.FieldIndex(lLayerIndex, "TOCOMID")
+        Dim lLengthFieldIndex As Integer = GisUtil.FieldIndex(lLayerIndex, "LENGTHKM")
+        Dim lLengthTotal As Double = 0
+        Dim lLength As Double
+        Dim lCnt As Integer = 0
+        Dim lComId As Integer = 3434322 'downstream COMID for Upitoi Creek from visual inspection
+        Do While lComId > 0
+            Dim lFeatureIndex As Integer = FindFeatureIndex(lLayerIndex, lComIdFieldIndex, lComId.ToString)
+            lLength = GisUtil.FieldValue(lLayerIndex, lFeatureIndex, lLengthFieldIndex)
+            lLengthTotal += lLength
+            Logger.Dbg("  ComId:" & lComId & " FeatureIndex:" & lFeatureIndex & " Length:" & lLength & " LengthTotal:" & lLengthTotal)
+            GisUtil.PointXY(lLayerIndex, lFeatureIndex, lX, lY)
+            lPointCollection = XY2Z(aMapWin, lX, lY)
+            ReportPoints(lX, lY, lPointCollection, lString, lLengthTotal)
+
+            'do more points along this line
+
+            Dim lFeatureIndexUpstream As Integer = FindFeatureIndex(lLayerIndex, lToComIdFieldIndex, lComId.ToString)
+            If lFeatureIndexUpstream >= 0 Then
+                lComId = GisUtil.FieldValue(lLayerIndex, lFeatureIndexUpstream, lComIdFieldIndex)
+                lCnt += 1
+            Else
+                lComId = 0
+            End If
+        Loop
+        Logger.Dbg("ShapeProcessedCount:" & lCnt & " Length:" & lLength)
+        SaveFileString("GridSample.txt", lString.ToString)
+    End Sub
+
+    Function FindFeatureIndex(ByVal aLayerIndex As Integer, ByVal aFieldIndex As Integer, ByVal aValue As String) As Integer
+        For lFeatureIndex As Integer = 0 To GisUtil.NumFeatures(aLayerIndex) - 1
+            If GisUtil.FieldValue(aLayerIndex, lFeatureIndex, aFieldIndex) = aValue Then
+                Return lFeatureIndex
             End If
         Next
+        Return -1
+    End Function
 
-        Dim lComIdField As MapWinGIS.Field = lLayer.FieldByName("COMID")
-        Dim lToComIdField As MapWinGIS.Field = lLayer.FieldByName("TOCOMID")
-        Dim lLength As Double = 0
-        Dim lComId As Integer = 3432490 'downstream COMID for Upitoi Creek from visual inspection
-        Do While lComId > 0
-
-            lComId = 0
-        Loop
+    Sub ReportPoints(ByVal aX As Double, ByVal aY As Double, ByVal aPointCollection As atcCollection, ByRef aString As Text.StringBuilder, Optional ByVal aPos As Double = 0)
+        If aPointCollection.Count > 1 Then
+            Dim lRatio As Double = aPointCollection(0) / aPointCollection(1)
+            If aPos > 0 Then
+                aString.Append(DoubleToString(aPos, 5) & vbTab)
+            End If
+            aString.Append(DoubleToString(aX, 14, , , , 10) & vbTab & _
+                           DoubleToString(aY, 14, , , , 10) & vbTab & _
+                           aPointCollection(0) & vbTab & _
+                           aPointCollection(1) & vbTab & _
+                           DoubleToString(lRatio))
+            For lIndex As Integer = 3 To aPointCollection.Count
+                aString.Append(vbTab & aPointCollection(lIndex - 1) & vbTab & "???")
+            Next lIndex
+            aString.AppendLine()
+        End If
     End Sub
 
     Function XY2Z(ByRef aMapWin As IMapWin, ByVal aX As Double, ByVal aY As Double) As atcCollection
