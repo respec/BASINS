d08b8e151727d151aeac660f50b19e64773c8a04
Jack Kittle <jack@gkfamily.org>
2007-07-18 20:15:22 +0000

    SummarizeTimeseries - polish atcDataAttributes - add additional alias

--- ScriptRunner/SummarizeTimeseries.vb
+++ ScriptRunner/SummarizeTimeseries.vb
@@ -6,14 +6,24 @@ Imports atcUtility
 Imports atcData
 
 Module ScriptSummarizeTimeseries
-    Private Const pInputPath As String = "D:\GisData\SERDP\Semp\Ecology\Stream water chemistry data for small Fort Benning Streams 1999-2003"
-    Private Const pInputFile As String = "MulhollandData.dbf"
-    Private Const pAllOpenData As Boolean = True
-    Private Const pFormat As String = "#,##0.00"
+    Private Const pFormat As String = "#,###,###,##0.00"
 
     Public Sub ScriptMain(ByRef aMapWin As IMapWin)
-        ChDriveDir(pInputPath)
-        Logger.Dbg(" CurDir:" & CurDir())
+        Logger.Dbg("SummarizeTimeseriesStart:CurDir:" & CurDir())
+
+        Dim lAttributes As New atcCollection
+        With lAttributes
+            .Add("ID")
+            .Add("Location")
+            .Add("Constituent")
+            .Add("Start Date")
+            .Add("End Date")
+            .Add("Count")
+            .Add("Mean")
+            .Add("Geometric Mean")
+            .Add("Minimum")
+            .Add("Maximum")
+        End With
 
         Dim lD2SStart As New atcDateFormat
         lD2SStart.IncludeHours = True
@@ -23,48 +33,45 @@ Module ScriptSummarizeTimeseries
         lD2SEnd.IncludeHours = True
         lD2SEnd.IncludeMinutes = True
 
-        Dim lString As New Text.StringBuilder
-        lString.AppendLine("ID" & vbTab & _
-                           "Location" & vbTab & _
-                           "Constituent" & vbTab & _
-                           "StartDate" & vbTab & _
-                           "EndDate" & vbTab & _
-                           "Count" & vbTab & _
-                           "Mean" & vbTab & _
-                           "Geometric Mean" & vbTab & _
-                           "Minimum" & vbTab & _
-                           "Maximum" & vbTab)
+        Dim lStringBuilder As New Text.StringBuilder
+        Dim lString As String = ""
+        For Each lAttribute As String In lAttributes
+            lString &= lAttribute & vbTab
+        Next
+        lString.Trim(vbTab)
+        lStringBuilder.AppendLine(lString)
 
-        Dim lDataSets As atcDataGroup = Nothing
-        If pAllOpenData Then
-            Dim lErr As String = ""
-            lDataSets = atcDataManager.DataSets
-        Else
-            Dim lTimeseriesFile As New atcTimseriesDbf.atcDataSourceTimeseriesDbf
-            Logger.Dbg("Open:" & pInputFile)
-            lTimeseriesFile.Open(pInputFile)
-            lDataSets = lTimeseriesFile.DataSets
-        End If
+        Dim lDataSets As atcDataGroup = atcDataManager.DataSets
         Logger.Dbg("DatasetCount " & lDataSets.Count)
         For Each lDS As atcDataSet In lDataSets
-            Dim lSJDay As Double = lDS.Attributes.GetValue("SJDay")
-            lString.AppendLine(lDS.Attributes.GetValue("ID", "?") & vbTab & _
-                               lDS.Attributes.GetValue("Location", "?") & vbTab & _
-                               lDS.Attributes.GetValue("Constituent", "?") & vbTab & _
-                               "'" & lD2SStart.JDateToString(lDS.Attributes.GetValue("SJDay")) & "'" & vbTab & _
-                               "'" & lD2SEnd.JDateToString(lDS.Attributes.GetValue("EJDay")) & "'" & vbTab & _
-                               lDS.Attributes.GetValue("Count") & vbTab & _
-                               DoubleToString(lDS.Attributes.GetValue("Mean"), , pFormat) & vbTab & _
-                               DoubleToString(lDS.Attributes.GetValue("Geometric Mean"), , pFormat) & vbTab & _
-                               DoubleToString(lDS.Attributes.GetValue("Min"), , pFormat) & vbTab & _
-                               DoubleToString(lDS.Attributes.GetValue("Max"), , pFormat) & vbTab)
+            Dim lValueString As String
+            lString = ""
+            For Each lAttribute As String In lAttributes
+                lValueString = lDS.Attributes.GetValue(lAttribute, "?")
+                If lValueString = "?" Then 'no value available for this attribute
+                    lString &= lValueString
+                Else
+                    Select Case lAttribute
+                        Case "Start Date"
+                            lString &= "'" & lD2SStart.JDateToString(lValueString) & "'"
+                        Case "End Date"
+                            lString &= "'" & lD2SEnd.JDateToString(lValueString) & "'"
+                        Case "Count", "ID"
+                            lString &= lValueString
+                        Case Else
+                            If IsNumeric(lValueString) Then
+                                lString &= DoubleToString(lValueString, , pFormat).TrimEnd("0").TrimEnd(".")
+                            Else
+                                lString &= lValueString
+                            End If
+                    End Select
+                End If
+                lString &= vbTab
+            Next
+            lString.Trim(vbTab)
+            lStringBuilder.AppendLine(lString)
         Next
-        SaveFileString(FilenameNoExt(pInputFile) & ".txt", lString.ToString)
-
-        If Not pAllOpenData Then
-            lDataSets.Clear()
-            lDataSets = Nothing
-        End If
-        Logger.Dbg("All Done")
+        SaveFileString("SummarizeTimeseries.txt", lStringBuilder.ToString)
+        Logger.Dbg("SummarizeTimeseriesDone")
     End Sub
 End Module
