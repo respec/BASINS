40e6f36ba00f062a2e740305246b395497c2c8c6
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2007-08-24 16:19:58 +0000

    atcTableSQLite - further playing around with SQLite as atcTable class - shows much promise!

--- BASINS40/ScriptRunner/SQLiteTester.vb
+++ BASINS40/ScriptRunner/SQLiteTester.vb
@@ -6,9 +6,18 @@ Public Module ScriptSQLLiteTester
     Public Sub ScriptMain(ByRef aMapWin As IMapWin)
         'Dim lConnectionString As String = "Data Source=c:\temp\test.db3"
         Dim lDatabaseFileName As String = "c:\temp\test.db3"
-        Dim lTableName As String = "keyinfotest"
+        Dim lTableName As String = "FullText"
         Dim lTableSQLite As New atcTableSQLite.atcTableSQLite
         Dim lResult As Boolean = lTableSQLite.OpenFile(lDatabaseFileName & vbTab & lTableName)
         Logger.Dbg("OpenResult:" & lResult)
+        Logger.Dbg("NumRows:" & lTableSQLite.NumRecords & ":NumFields:" & lTableSQLite.NumFields)
+        For lRow As Integer = lTableSQLite.NumRecords - 1 To 0 Step -1 'to from last to first
+            Dim lStr As String = "Row:" & lRow
+            lTableSQLite.CurrentRecord = lRow
+            For lColumn As Integer = 0 To lTableSQLite.NumFields - 1
+                lStr &= ":" & lTableSQLite.Value(lColumn)
+            Next
+            Logger.Dbg(lStr)
+        Next
     End Sub
 End Module
--- BASINS40/atcTableSQLite/atcTableSQLite.vb
+++ BASINS40/atcTableSQLite/atcTableSQLite.vb
@@ -3,12 +3,16 @@ Option Explicit On
 Imports atcUtility
 Imports MapWinUtility
 Imports System.Data.SQLite
-Imports System.Data
+Imports System.Data.Common
 
 Public Class atcTableSQLite
     Inherits atcTable
 
     Private pDB As SQLiteConnection
+    Private pCmd As SQLiteCommand
+    Private pAdapter As SQLiteDataAdapter
+    Private pDataSet As DataSet
+    Private pRow As Integer = 0
 
     Public Overrides Function Cousin() As IatcTable
 
@@ -24,17 +28,35 @@ Public Class atcTableSQLite
             Dim lConnectionString As String = "Data Source=" & StrSplit(lFileAndTableName, vbTab, "'")
             pDB = New SQLiteConnection(lConnectionString)
             pDB.Open()
-            Dim lCmd As New SQLiteCommand(pDB)
-            lCmd.CommandText = "Select * from " & lFileAndTableName 'no just table name
-            Dim lReader As Common.DbDataReader = lCmd.ExecuteReader
-            Dim lSchemaTable As DataTable = lReader.GetSchemaTable
-            Logger.Dbg(lSchemaTable.Rows.Count & ":" & lSchemaTable.Columns.Count)
+            pCmd = New SQLiteCommand(pDB)
+            pCmd.CommandText = "Select * from " & lFileAndTableName 'now just table name
+            pAdapter = New SQLiteDataAdapter
+            pAdapter.SelectCommand = pCmd
+            pDataSet = New DataSet
+            Dim iResult As Integer = pAdapter.Fill(pDataSet)
+
+            Logger.Dbg("FieldCount:" & pDataSet.Tables(0).Columns.Count & ":Rows:" & pDataSet.Tables(0).Rows.Count)
+            Logger.Dbg("FieldDetails")
+            Dim lStr As String = "Name"
+            For lField As Integer = 0 To pDataSet.Tables(0).Columns.Count - 1
+                lStr &= ":" & pDataSet.Tables(0).Columns(lField).ColumnName
+            Next
+            Logger.Dbg(lStr)
+            lStr = "DataType"
+            For lField As Integer = 0 To pDataSet.Tables(0).Columns.Count - 1
+                lStr &= ":" & pDataSet.Tables(0).Columns(lField).DataType.ToString
+            Next
+            Logger.Dbg(lStr)
+            lStr = "Length"
+            For lField As Integer = 0 To pDataSet.Tables(0).Columns.Count - 1
+                lStr &= ":" & pDataSet.Tables(0).Columns(lField).MaxLength
+            Next
+            Logger.Dbg(lStr)
         Catch ex As ApplicationException
             Logger.Msg(ex.Message)
             Return False
         End Try
         Return True
-
     End Function
 
     Public Overrides Function WriteFile(ByVal aFileName As String) As Boolean
@@ -43,64 +65,92 @@ Public Class atcTableSQLite
 
     Public Overrides Property NumRecords() As Integer
         Get
-
+            Return pDataSet.Tables(0).Rows.Count
         End Get
         Set(ByVal newValue As Integer)
-
+            Throw New ApplicationException("atcTableSQLite:NumRecords is ReadOnly (for now)")
         End Set
     End Property
 
     Public Overrides Property NumFields() As Integer
         Get
-
+            Return pDataSet.Tables(0).Columns.Count
         End Get
         Set(ByVal newValue As Integer)
-
+            Throw New ApplicationException("atcTableSQLite:NumFields is ReadOnly (for now)")
         End Set
     End Property
 
     Public Overrides Property CurrentRecord() As Integer
         Get
-
+            Return pRow
         End Get
         Set(ByVal newValue As Integer)
-
+            If newValue >= 0 And newValue < pDataSet.Tables(0).Rows.Count Then
+                pRow = newValue
+            Else
+                Throw New ApplicationException("atcTableSQLite:CurrentRecord:" & newValue & ":NotBetween:0:" & pDataSet.Tables(0).Rows.Count - 1)
+            End If
         End Set
     End Property
 
     Public Overrides Property Value(ByVal aFieldNumber As Integer) As String
         Get
-
+            If CheckFieldNumber(aFieldNumber) Then
+                Dim lStr As String
+                Try
+                    lStr = pDataSet.Tables(0).Rows(pRow).Item(aFieldNumber)
+                Catch
+                    Logger.Dbg("ConversionError:Row:" & pRow & ":Column:" & aFieldNumber)
+                    lStr = pDataSet.Tables(0).Columns(aFieldNumber).DataType.ToString
+                End Try
+                Return lStr
+            End If
+            Return Nothing
         End Get
         Set(ByVal newValue As String)
-
+            Throw New ApplicationException("atcTableSQLite:Value is ReadOnly (for now)")
         End Set
     End Property
 
     Public Overrides Property FieldName(ByVal aFieldNumber As Integer) As String
         Get
-
+            If CheckFieldNumber(aFieldNumber) Then
+                Return pDataSet.Tables(0).Columns(aFieldNumber).ColumnName
+            Else
+                Return Nothing
+            End If
         End Get
         Set(ByVal newValue As String)
-
+            If CheckFieldNumber(aFieldNumber) Then
+                pDataSet.Tables(0).Columns(aFieldNumber).ColumnName = newValue
+            End If
         End Set
     End Property
 
     Public Overrides Property FieldLength(ByVal aFieldNumber As Integer) As Integer
         Get
-
+            If CheckFieldNumber(aFieldNumber) Then
+                Return pDataSet.Tables(0).Columns(aFieldNumber).MaxLength
+            Else
+                Return Nothing
+            End If
         End Get
         Set(ByVal newValue As Integer)
-
+            Throw New ApplicationException("atcTableSQLite:FieldLength is ReadOnly (for now)")
         End Set
     End Property
 
     Public Overrides Property FieldType(ByVal aFieldNumber As Integer) As String
         Get
-
+            If CheckFieldNumber(aFieldNumber) Then
+                Return pDataSet.Tables(0).Columns(aFieldNumber).DataType.ToString()
+            Else
+                Return Nothing
+            End If
         End Get
         Set(ByVal newValue As String)
-
+            Throw New ApplicationException("atcTableSQLite:FieldType is ReadOnly (for now)")
         End Set
     End Property
 
@@ -111,5 +161,13 @@ Public Class atcTableSQLite
     Public Overrides Sub Clear()
 
     End Sub
+
+    Private Function CheckFieldNumber(ByVal aFieldNumber As Integer) As Boolean
+        If aFieldNumber >= 0 And aFieldNumber < pDataSet.Tables(0).Columns.Count Then
+            Return True
+        Else
+            Throw New ApplicationException("atcTableSQLite:FieldNumber" & aFieldNumber & ":NotBetween:0:" & pDataSet.Tables(0).Columns.Count - 1)
+        End If
+    End Function
 End Class
 
