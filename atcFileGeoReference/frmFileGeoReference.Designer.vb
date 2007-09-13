1317ef73ad1c71a7d8c487d86c343e70fe44cd01
Jack Kittle <jack@gkfamily.org>
2007-09-13 14:58:33 +0000

    inital commit of new plugin to display images (maybe more), under development

---
+++ atcFileGeoReference/frmFileGeoReference.Designer.vb
@@ -0,0 +1,171 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFileGeoReference
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmFileGeoReference))
+        Me.lblLayer = New System.Windows.Forms.Label
+        Me.cboLayer = New System.Windows.Forms.ComboBox
+        Me.lblFields = New System.Windows.Forms.Label
+        Me.cboFields = New System.Windows.Forms.ComboBox
+        Me.lblRecordInfo = New System.Windows.Forms.Label
+        Me.lblValue = New System.Windows.Forms.Label
+        Me.txtValue = New System.Windows.Forms.TextBox
+        Me.btnNext = New System.Windows.Forms.Button
+        Me.btnPrev = New System.Windows.Forms.Button
+        Me.pbxImage = New System.Windows.Forms.PictureBox
+        CType(Me.pbxImage, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblLayer
+        '
+        Me.lblLayer.AutoSize = True
+        Me.lblLayer.Location = New System.Drawing.Point(29, 16)
+        Me.lblLayer.Name = "lblLayer"
+        Me.lblLayer.Size = New System.Drawing.Size(48, 17)
+        Me.lblLayer.TabIndex = 1
+        Me.lblLayer.Text = "Layer:"
+        Me.lblLayer.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'cboLayer
+        '
+        Me.cboLayer.FormattingEnabled = True
+        Me.cboLayer.Location = New System.Drawing.Point(83, 16)
+        Me.cboLayer.Name = "cboLayer"
+        Me.cboLayer.Size = New System.Drawing.Size(265, 24)
+        Me.cboLayer.TabIndex = 2
+        '
+        'lblFields
+        '
+        Me.lblFields.AutoSize = True
+        Me.lblFields.Location = New System.Drawing.Point(366, 16)
+        Me.lblFields.Name = "lblFields"
+        Me.lblFields.Size = New System.Drawing.Size(94, 17)
+        Me.lblFields.TabIndex = 3
+        Me.lblFields.Text = "File Ref Field:"
+        Me.lblFields.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'cboFields
+        '
+        Me.cboFields.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cboFields.FormattingEnabled = True
+        Me.cboFields.Location = New System.Drawing.Point(477, 16)
+        Me.cboFields.Name = "cboFields"
+        Me.cboFields.Size = New System.Drawing.Size(265, 24)
+        Me.cboFields.TabIndex = 4
+        '
+        'lblRecordInfo
+        '
+        Me.lblRecordInfo.AutoSize = True
+        Me.lblRecordInfo.Location = New System.Drawing.Point(27, 55)
+        Me.lblRecordInfo.Name = "lblRecordInfo"
+        Me.lblRecordInfo.Size = New System.Drawing.Size(91, 17)
+        Me.lblRecordInfo.TabIndex = 5
+        Me.lblRecordInfo.Text = "Record x of y"
+        '
+        'lblValue
+        '
+        Me.lblValue.AutoSize = True
+        Me.lblValue.Location = New System.Drawing.Point(412, 55)
+        Me.lblValue.Name = "lblValue"
+        Me.lblValue.Size = New System.Drawing.Size(48, 17)
+        Me.lblValue.TabIndex = 6
+        Me.lblValue.Text = "Value:"
+        Me.lblValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtValue
+        '
+        Me.txtValue.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtValue.Location = New System.Drawing.Point(477, 55)
+        Me.txtValue.Name = "txtValue"
+        Me.txtValue.Size = New System.Drawing.Size(264, 22)
+        Me.txtValue.TabIndex = 7
+        Me.txtValue.Text = "Value"
+        '
+        'btnNext
+        '
+        Me.btnNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNext.Location = New System.Drawing.Point(321, 55)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(27, 31)
+        Me.btnNext.TabIndex = 8
+        Me.btnNext.Text = ">"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnPrev
+        '
+        Me.btnPrev.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrev.Location = New System.Drawing.Point(277, 55)
+        Me.btnPrev.Name = "btnPrev"
+        Me.btnPrev.Size = New System.Drawing.Size(27, 31)
+        Me.btnPrev.TabIndex = 9
+        Me.btnPrev.Text = "<"
+        Me.btnPrev.UseVisualStyleBackColor = True
+        '
+        'pbxImage
+        '
+        Me.pbxImage.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pbxImage.Location = New System.Drawing.Point(12, 101)
+        Me.pbxImage.Name = "pbxImage"
+        Me.pbxImage.Size = New System.Drawing.Size(729, 296)
+        Me.pbxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pbxImage.TabIndex = 10
+        Me.pbxImage.TabStop = False
+        '
+        'frmFileGeoReference
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(753, 408)
+        Me.Controls.Add(Me.pbxImage)
+        Me.Controls.Add(Me.btnPrev)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.txtValue)
+        Me.Controls.Add(Me.lblValue)
+        Me.Controls.Add(Me.lblRecordInfo)
+        Me.Controls.Add(Me.cboFields)
+        Me.Controls.Add(Me.lblFields)
+        Me.Controls.Add(Me.cboLayer)
+        Me.Controls.Add(Me.lblLayer)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmFileGeoReference"
+        Me.Text = "File Geo Reference"
+        CType(Me.pbxImage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblLayer As System.Windows.Forms.Label
+    Friend WithEvents cboLayer As System.Windows.Forms.ComboBox
+    Friend WithEvents lblFields As System.Windows.Forms.Label
+    Friend WithEvents cboFields As System.Windows.Forms.ComboBox
+    Friend WithEvents lblRecordInfo As System.Windows.Forms.Label
+    Friend WithEvents lblValue As System.Windows.Forms.Label
+    Friend WithEvents txtValue As System.Windows.Forms.TextBox
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+    Friend WithEvents btnPrev As System.Windows.Forms.Button
+    Friend WithEvents pbxImage As System.Windows.Forms.PictureBox
+End Class
