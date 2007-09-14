fa9f15300d9d0c2c8890e3ab01b128dac92ae57c
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2007-09-14 22:46:58 +0000

    atcFileGeoReference:    added ExifWorks    added setting the MappingObject in atcmwgisutility    added allowing dropping image files on frmFileGeoReference to add to current layer    added searching for EXIF lat/lon values on newly dropped images (still need projection from lat/lon)

--- atcFileGeoReference/frmFileGeoReference.Designer.vb
+++ atcFileGeoReference/frmFileGeoReference.Designer.vb
@@ -44,9 +44,10 @@ Partial Class frmFileGeoReference
         'lblLayer
         '
         Me.lblLayer.AutoSize = True
-        Me.lblLayer.Location = New System.Drawing.Point(29, 16)
+        Me.lblLayer.Location = New System.Drawing.Point(22, 13)
+        Me.lblLayer.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblLayer.Name = "lblLayer"
-        Me.lblLayer.Size = New System.Drawing.Size(48, 17)
+        Me.lblLayer.Size = New System.Drawing.Size(36, 13)
         Me.lblLayer.TabIndex = 1
         Me.lblLayer.Text = "Layer:"
         Me.lblLayer.TextAlign = System.Drawing.ContentAlignment.MiddleRight
@@ -54,17 +55,19 @@ Partial Class frmFileGeoReference
         'cboLayer
         '
         Me.cboLayer.FormattingEnabled = True
-        Me.cboLayer.Location = New System.Drawing.Point(83, 16)
+        Me.cboLayer.Location = New System.Drawing.Point(62, 13)
+        Me.cboLayer.Margin = New System.Windows.Forms.Padding(2)
         Me.cboLayer.Name = "cboLayer"
-        Me.cboLayer.Size = New System.Drawing.Size(170, 24)
+        Me.cboLayer.Size = New System.Drawing.Size(128, 21)
         Me.cboLayer.TabIndex = 2
         '
         'lblFields
         '
         Me.lblFields.AutoSize = True
-        Me.lblFields.Location = New System.Drawing.Point(270, 16)
+        Me.lblFields.Location = New System.Drawing.Point(202, 13)
+        Me.lblFields.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblFields.Name = "lblFields"
-        Me.lblFields.Size = New System.Drawing.Size(94, 17)
+        Me.lblFields.Size = New System.Drawing.Size(71, 13)
         Me.lblFields.TabIndex = 3
         Me.lblFields.Text = "File Ref Field:"
         Me.lblFields.TextAlign = System.Drawing.ContentAlignment.MiddleRight
@@ -74,26 +77,29 @@ Partial Class frmFileGeoReference
         Me.cboFields.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
                     Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.cboFields.FormattingEnabled = True
-        Me.cboFields.Location = New System.Drawing.Point(381, 16)
+        Me.cboFields.Location = New System.Drawing.Point(286, 13)
+        Me.cboFields.Margin = New System.Windows.Forms.Padding(2)
         Me.cboFields.Name = "cboFields"
-        Me.cboFields.Size = New System.Drawing.Size(415, 24)
+        Me.cboFields.Size = New System.Drawing.Size(312, 21)
         Me.cboFields.TabIndex = 4
         '
         'lblRecordInfo
         '
         Me.lblRecordInfo.AutoSize = True
-        Me.lblRecordInfo.Location = New System.Drawing.Point(27, 55)
+        Me.lblRecordInfo.Location = New System.Drawing.Point(20, 45)
+        Me.lblRecordInfo.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblRecordInfo.Name = "lblRecordInfo"
-        Me.lblRecordInfo.Size = New System.Drawing.Size(91, 17)
+        Me.lblRecordInfo.Size = New System.Drawing.Size(70, 13)
         Me.lblRecordInfo.TabIndex = 5
         Me.lblRecordInfo.Text = "Record x of y"
         '
         'lblValue
         '
         Me.lblValue.AutoSize = True
-        Me.lblValue.Location = New System.Drawing.Point(316, 55)
+        Me.lblValue.Location = New System.Drawing.Point(237, 45)
+        Me.lblValue.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblValue.Name = "lblValue"
-        Me.lblValue.Size = New System.Drawing.Size(48, 17)
+        Me.lblValue.Size = New System.Drawing.Size(37, 13)
         Me.lblValue.TabIndex = 6
         Me.lblValue.Text = "Value:"
         Me.lblValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight
@@ -102,18 +108,20 @@ Partial Class frmFileGeoReference
         '
         Me.txtValue.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
                     Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.txtValue.Location = New System.Drawing.Point(381, 55)
+        Me.txtValue.Location = New System.Drawing.Point(286, 45)
+        Me.txtValue.Margin = New System.Windows.Forms.Padding(2)
         Me.txtValue.Name = "txtValue"
-        Me.txtValue.Size = New System.Drawing.Size(414, 22)
+        Me.txtValue.Size = New System.Drawing.Size(312, 20)
         Me.txtValue.TabIndex = 7
         Me.txtValue.Text = "Value"
         '
         'btnNext
         '
         Me.btnNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnNext.Location = New System.Drawing.Point(207, 49)
+        Me.btnNext.Location = New System.Drawing.Point(155, 40)
+        Me.btnNext.Margin = New System.Windows.Forms.Padding(2)
         Me.btnNext.Name = "btnNext"
-        Me.btnNext.Size = New System.Drawing.Size(27, 31)
+        Me.btnNext.Size = New System.Drawing.Size(20, 25)
         Me.btnNext.TabIndex = 8
         Me.btnNext.Text = ">"
         Me.ToolTip1.SetToolTip(Me.btnNext, "Move Next")
@@ -122,9 +130,10 @@ Partial Class frmFileGeoReference
         'btnPrev
         '
         Me.btnPrev.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnPrev.Location = New System.Drawing.Point(174, 49)
+        Me.btnPrev.Location = New System.Drawing.Point(130, 40)
+        Me.btnPrev.Margin = New System.Windows.Forms.Padding(2)
         Me.btnPrev.Name = "btnPrev"
-        Me.btnPrev.Size = New System.Drawing.Size(27, 31)
+        Me.btnPrev.Size = New System.Drawing.Size(20, 25)
         Me.btnPrev.TabIndex = 9
         Me.btnPrev.Text = "<"
         Me.ToolTip1.SetToolTip(Me.btnPrev, "Move Previous")
@@ -135,9 +144,10 @@ Partial Class frmFileGeoReference
         Me.pbxImage.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
                     Or System.Windows.Forms.AnchorStyles.Left) _
                     Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.pbxImage.Location = New System.Drawing.Point(12, 86)
+        Me.pbxImage.Location = New System.Drawing.Point(9, 70)
+        Me.pbxImage.Margin = New System.Windows.Forms.Padding(2)
         Me.pbxImage.Name = "pbxImage"
-        Me.pbxImage.Size = New System.Drawing.Size(796, 540)
+        Me.pbxImage.Size = New System.Drawing.Size(597, 439)
         Me.pbxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.pbxImage.TabIndex = 10
         Me.pbxImage.TabStop = False
@@ -145,9 +155,10 @@ Partial Class frmFileGeoReference
         'btnAdd
         '
         Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnAdd.Location = New System.Drawing.Point(240, 49)
+        Me.btnAdd.Location = New System.Drawing.Point(180, 40)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(2)
         Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New System.Drawing.Size(27, 31)
+        Me.btnAdd.Size = New System.Drawing.Size(20, 25)
         Me.btnAdd.TabIndex = 11
         Me.btnAdd.Text = "+"
         Me.ToolTip1.SetToolTip(Me.btnAdd, "Add File Reference Point")
@@ -157,9 +168,10 @@ Partial Class frmFileGeoReference
         '
         Me.lblStatus.AutoSize = True
         Me.lblStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblStatus.Location = New System.Drawing.Point(25, 97)
+        Me.lblStatus.Location = New System.Drawing.Point(19, 79)
+        Me.lblStatus.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblStatus.Name = "lblStatus"
-        Me.lblStatus.Size = New System.Drawing.Size(63, 20)
+        Me.lblStatus.Size = New System.Drawing.Size(54, 17)
         Me.lblStatus.TabIndex = 12
         Me.lblStatus.Text = "Status"
         Me.lblStatus.Visible = False
@@ -167,9 +179,10 @@ Partial Class frmFileGeoReference
         'btnRemove
         '
         Me.btnRemove.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnRemove.Location = New System.Drawing.Point(273, 49)
+        Me.btnRemove.Location = New System.Drawing.Point(205, 40)
+        Me.btnRemove.Margin = New System.Windows.Forms.Padding(2)
         Me.btnRemove.Name = "btnRemove"
-        Me.btnRemove.Size = New System.Drawing.Size(27, 31)
+        Me.btnRemove.Size = New System.Drawing.Size(20, 25)
         Me.btnRemove.TabIndex = 13
         Me.btnRemove.Text = "-"
         Me.ToolTip1.SetToolTip(Me.btnRemove, "Remove File Reference Point")
@@ -177,9 +190,10 @@ Partial Class frmFileGeoReference
         '
         'frmFileGeoReference
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(820, 652)
+        Me.ClientSize = New System.Drawing.Size(615, 530)
         Me.Controls.Add(Me.btnRemove)
         Me.Controls.Add(Me.lblStatus)
         Me.Controls.Add(Me.btnAdd)
@@ -194,6 +208,7 @@ Partial Class frmFileGeoReference
         Me.Controls.Add(Me.cboLayer)
         Me.Controls.Add(Me.lblLayer)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frmFileGeoReference"
         Me.Text = "File Geo Reference"
         CType(Me.pbxImage, System.ComponentModel.ISupportInitialize).EndInit()
