c803b564e84e458f081bb63acde6a52b9aee9859
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2007-10-18 20:04:54 +0000

    HSPFOutputReports - make more generic

--- BASINS40/ScriptRunner/HSPFOutputReports.vb
+++ BASINS40/ScriptRunner/HSPFOutputReports.vb
@@ -1,4 +1,6 @@
 Imports atcUtility.modFile
+Imports atcData
+Imports atcWDM
 Imports MapWindow.Interfaces
 
 Module HSPFOutputReports
@@ -7,8 +9,23 @@ Module HSPFOutputReports
 
     Public Sub ScriptMain(ByRef aMapWin As IMapWin)
         ChDriveDir(pTestPath)
-        Dim lExpSysStats As String
-        lExpSysStats = GetExpSysStats(pBaseName & ".uci", pbasename & ".exs", CurDir)
-        SaveFileString("outfiles\ExpertSysStats.txt", lExpSysStats)
+
+        Dim lMsg As New atcUCI.HspfMsg
+        lMsg.Open("hspfmsg.mdb")
+        Dim lHspfUci As New atcUCI.HspfUci
+        lHspfUci.FastReadUciForStarter(lMsg, pBaseName & ".uci")
+
+        'open WDM file
+        Dim lWdmFileName As String = pTestPath & "\" & pBaseName & ".wdm"
+        Dim lDataSource As New atcDataSourceWDM
+        lDataSource.Open(lWdmFileName)
+
+        Dim lStr As String
+        lStr = ExpertSystemStatistics.Report(lHspfUci, lDataSource)
+        SaveFileString("outfiles\ExpertSysStats.txt", lStr)
+
+        Dim lSites() As String = {"RCH5"}
+        lStr = DailyMonthlyFlowStats.Report(lHspfUci, lDataSource, lSites)
+        SaveFileString("outfiles\DailyMontlyFlowStats.txt", lStr)
     End Sub
 End Module
