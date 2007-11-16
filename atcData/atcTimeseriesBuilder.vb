c3d3b2d837d01d23c93d8be3f1ea71f60dec33d1
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2007-11-16 20:46:19 +0000

    BASINS: rough draft of new atcTimeseriesBuilder

---
+++ atcData/atcTimeseriesBuilder.vb
@@ -0,0 +1,182 @@
+Imports atcUtility
+Imports atcData
+Imports MapWinUtility
+
+Public Class atcTimeseriesBuilder
+
+    Private pDataSource As atcDataSource
+    Private pTS As atcTimeseries
+    Private pValueAttributes As atcCollection 'of atcDataAttributes
+    Private pValues As Generic.List(Of Double)
+    Private pDates As Generic.List(Of Double)
+    Private pLastAddedIndex As Integer
+    Private Shared pLogDateFormat As atcDateFormat
+    Private Shared pNaN As Double = atcUtility.GetNaN
+
+    Public Sub New(ByVal aDataSource As atcDataSource)
+        pDataSource = aDataSource
+        Restart()
+    End Sub
+
+    Public Sub Restart()
+        If Not pValueAttributes Is Nothing Then pValueAttributes.Clear()
+        If Not pValues Is Nothing Then pValues.Clear()
+        If Not pDates Is Nothing Then pDates.Clear()
+
+        pTS = New atcTimeseries(pDataSource)
+        pValueAttributes = New atcUtility.atcCollection
+        pValues = New Generic.List(Of Double)
+        pDates = New Generic.List(Of Double)
+    End Sub
+
+    Public Sub AddValue(ByVal aValue As Double, ByVal aDate As Date)
+        AddValue(aValue, aDate.ToOADate)
+    End Sub
+
+    ''' <summary>
+    ''' Append a value and date to the timeseries being built
+    ''' </summary>
+    ''' <param name="aValue">a number or non-numeric value to append</param>
+    ''' <param name="aDate">Date corresponding to the value</param>
+    ''' <param name="aLogNonNumeric">True to log when non-numeric aValue is passed</param>
+    ''' <remarks></remarks>
+    Public Sub AddValue(ByVal aValue As String, ByVal aDate As Double, Optional ByVal aLogNonNumeric As Boolean = False)
+        Dim lValue As Double = pNaN
+        Dim lFirstNumericIndex As Integer = -1
+        Dim lLenNumeric As Integer = 0
+
+        If aValue Is Nothing OrElse aValue.Trim.Length = 0 Then
+            If aLogNonNumeric Then
+                Logger.Dbg("Missing Value at " & LogDateFormat.JDateToString(aDate))
+            End If
+        ElseIf IsNumeric(aValue) Then
+            lValue = CDbl(aValue)
+        Else
+            While lFirstNumericIndex < aValue.Length
+                If Char.IsNumber(aValue.Chars(lFirstNumericIndex + 1)) Then Exit While
+                lFirstNumericIndex += 1
+            End While
+
+            If lFirstNumericIndex < aValue.Length Then
+                While lFirstNumericIndex + lLenNumeric + 1 < aValue.Length
+                    If Not Char.IsNumber(aValue.Chars(lFirstNumericIndex + lLenNumeric + 1)) Then Exit While
+                End While
+                lValue = CDbl(aValue.Substring(lFirstNumericIndex))
+            End If
+
+            If aLogNonNumeric Then
+                Logger.Dbg("NonNumericValue at " & LogDateFormat.JDateToString(aDate) & " '" & aValue & "' -> '" & lValue & "'")
+            End If
+
+        End If
+
+        AddValue(lValue, aDate)
+
+        If lFirstNumericIndex > 0 Then
+            Me.AddValueAttribute("ValuePrefix", aValue.Substring(0, lFirstNumericIndex))
+        End If
+
+        If lLenNumeric > 0 AndAlso lFirstNumericIndex + lLenNumeric < aValue.Length Then
+            Me.AddValueAttribute("ValueSuffix", aValue.Substring(aValue.Length - lLenNumeric, lLenNumeric))
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Append a value and date to the timeseries being built
+    ''' </summary>
+    ''' <param name="aValue">a number to append to the timeseries</param>
+    ''' <param name="aDate">Date corresponding to the value</param>
+    ''' <remarks></remarks>
+    Public Sub AddValue(ByVal aValue As Double, ByVal aDate As Double)
+        If pValues.Count = 0 AndAlso Not Double.IsNaN(aValue) Then
+            'Value at zero is always NaN (date(0) is start of first interval for constant interval)
+            pValues.Add(pNaN)
+            pDates.Add(pNaN)
+        End If
+
+        Dim lOldDateIndex As Integer = pDates.Count - 1
+        Dim lFoundGreaterDate As Integer = -1
+
+        While lOldDateIndex > 0
+            If Double.IsNaN(pDates(lOldDateIndex)) Then
+                lOldDateIndex -= 1
+            ElseIf pDates(lOldDateIndex) > aDate Then
+                lFoundGreaterDate = lOldDateIndex
+                lOldDateIndex -= 1
+            Else
+                Exit While
+            End If
+        End While
+
+        If lFoundGreaterDate > -1 Then
+            pLastAddedIndex = lFoundGreaterDate
+            pValues.Insert(lFoundGreaterDate, aValue)
+            pDates.Insert(lFoundGreaterDate, aDate)
+
+            'Shift keys of any valueattributes at >= lFoundGreaterDate
+            Dim lOldIndex As Integer
+            For lIndex As Integer = pValues.Count - 1 To lFoundGreaterDate Step -1
+                lOldIndex = pValueAttributes.Keys.IndexOf(pLastAddedIndex)
+                If lOldIndex >= 0 Then
+                    pValueAttributes.Keys(lOldIndex) += 1
+                End If
+            Next
+        Else
+            pValues.Add(aValue)
+            pDates.Add(aDate)
+            pLastAddedIndex = pDates.Count - 1
+        End If
+    End Sub
+
+    Public ReadOnly Property Attributes() As atcDataAttributes
+        Get
+            Return pTS.Attributes
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Add an attribute to the most recently added value
+    ''' </summary>
+    ''' <param name="aAttributeName">Name of attribute to add</param>
+    ''' <param name="aAttributeValue">Value of attribute to add</param>
+    ''' <remarks></remarks>
+    Public Sub AddValueAttribute(ByVal aAttributeName As String, ByVal aAttributeValue As Object)
+        'pTS.ValueAttributes(pValues.Count).Add(aAttributeName, aAttributeValue)
+        Dim lValueAttributes As atcDataAttributes
+        Dim lIndex As Integer = pValueAttributes.Keys.IndexOf(pLastAddedIndex)
+        If lIndex >= 0 Then
+            lValueAttributes = pValueAttributes.ItemByIndex(lIndex)
+        Else
+            lValueAttributes = New atcDataAttributes
+            pValueAttributes.Add(pLastAddedIndex, lValueAttributes)
+        End If
+        lValueAttributes.Add(aAttributeName, aAttributeValue)
+    End Sub
+
+    Public Function CreateTimeseries() As atcTimeseries
+        Dim lDates As New atcTimeseries(pDataSource)
+
+        pTS.Values = pValues.ToArray
+        lDates.Values = pDates.ToArray
+
+        pTS.Dates = lDates
+
+        Dim lLastValueAttribute As Integer = pValueAttributes.Count - 1
+        For lValAttIndex As Integer = 0 To lLastValueAttribute
+            pTS.ValueAttributes(pValueAttributes.Keys(lValAttIndex)) = pValueAttributes.ItemByIndex(lValAttIndex)
+        Next
+
+        Return pTS
+    End Function
+
+    Private ReadOnly Property LogDateFormat() As atcDateFormat
+        Get
+            If pLogDateFormat Is Nothing Then
+                pLogDateFormat = New atcDateFormat
+                pLogDateFormat.IncludeHours = True
+                pLogDateFormat.IncludeMinutes = True
+            End If
+            Return pLogDateFormat
+        End Get
+    End Property
+End Class
