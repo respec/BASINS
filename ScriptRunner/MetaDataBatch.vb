e09807e0ce28b26c3dbf8e8d780de09b9c979a9b
Jack Kittle <jack@gkfamily.org>
2008-01-16 17:03:01 +0000

    ScriptRunner - MetaDataBatch - add check of metadata generated by FGDCMetadata using the FDGC data type definition

--- ScriptRunner/MetaDataBatch.vb
+++ ScriptRunner/MetaDataBatch.vb
@@ -2,6 +2,7 @@ Imports MapWinUtility
 Imports atcUtility
 Imports MapWindow.Interfaces
 Imports mwOpenMetadataManager
+Imports System.Xml
 
 Module MetaDataBatch
     Private pTestPath As String = "C:\dev\BASINS40\ScriptRunner\data\metadata\Orig"
@@ -21,8 +22,30 @@ Module MetaDataBatch
             Catch lEx As Exception
                 Logger.Dbg(lEx.ToString)
             End Try
-            lMetaData.Save("..\rev\" & lFileName)
+            lFileName = "..\rev\" & lFileName
+            lMetaData.Save(lFileName)
+            Logger.Dbg("---Saved " & lFileName)
+
+            Dim lStream As New System.IO.FileStream(lFileName, IO.FileMode.Open)
+            Dim lSettings As New XmlReaderSettings
+            lSettings.ProhibitDtd = False
+            lSettings.ValidationType = ValidationType.DTD
+            AddHandler lSettings.ValidationEventHandler, AddressOf ValidationEventHandler
+
+            Dim lXMLReader As XmlReader = XmlReader.Create(lStream, lSettings)
+            Dim lXML As New XmlDocument
+            lXML.Load(lXMLReader)
         Next
         Logger.Dbg("Done")
+    End Sub
+
+    Private Sub ValidationEventHandler(ByVal sender As Object, ByVal args As Schema.ValidationEventArgs)
+        Dim lMsg As String = ""
+        If args.Severity = Schema.XmlSeverityType.Warning Then
+            lMsg &= "WARNING: "
+        ElseIf args.Severity = Schema.XmlSeverityType.Error Then
+            lMsg &= "ERROR: "
+        End If
+        Logger.Dbg(lMsg & args.Message)
     End Sub
 End Module
