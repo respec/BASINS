85a65430e5c1e7001729c943a167c93e42cdf9d5
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-03-06 20:57:40 +0000

    frmModelSegmentation.vb -- minor tweaks to form

--- BASINS40/atcModelSegmentation/frmModelSegmentation.Designer.vb
+++ BASINS40/atcModelSegmentation/frmModelSegmentation.Designer.vb
@@ -20,28 +20,17 @@ Partial Class frmModelSegmentation
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmModelSegmentation))
-        Me.cmdAssign = New System.Windows.Forms.Button
         Me.fraSubbasins = New System.Windows.Forms.GroupBox
         Me.cmdViewMap = New System.Windows.Forms.Button
         Me.cmdEditTable = New System.Windows.Forms.Button
         Me.cboSubbasins = New System.Windows.Forms.ComboBox
         Me.fraMetStations = New System.Windows.Forms.GroupBox
+        Me.cmdAssign = New System.Windows.Forms.Button
         Me.cbxUseSelected = New System.Windows.Forms.CheckBox
         Me.cboMetStations = New System.Windows.Forms.ComboBox
         Me.fraSubbasins.SuspendLayout()
         Me.fraMetStations.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'cmdAssign
-        '
-        Me.cmdAssign.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.cmdAssign.Location = New System.Drawing.Point(12, 224)
-        Me.cmdAssign.Name = "cmdAssign"
-        Me.cmdAssign.Size = New System.Drawing.Size(344, 34)
-        Me.cmdAssign.TabIndex = 0
-        Me.cmdAssign.Text = "Assign Met Stations To SubBasins By Proximity"
-        Me.cmdAssign.UseVisualStyleBackColor = True
         '
         'fraSubbasins
         '
@@ -52,29 +41,26 @@ Partial Class frmModelSegmentation
         Me.fraSubbasins.Controls.Add(Me.cboSubbasins)
         Me.fraSubbasins.Location = New System.Drawing.Point(12, 12)
         Me.fraSubbasins.Name = "fraSubbasins"
-        Me.fraSubbasins.Size = New System.Drawing.Size(343, 106)
+        Me.fraSubbasins.Size = New System.Drawing.Size(401, 106)
         Me.fraSubbasins.TabIndex = 11
         Me.fraSubbasins.TabStop = False
-        Me.fraSubbasins.Text = "SubBasins Layer"
+        Me.fraSubbasins.Text = "Subbasins Layer"
         '
         'cmdViewMap
         '
-        Me.cmdViewMap.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.cmdViewMap.Location = New System.Drawing.Point(215, 60)
+        Me.cmdViewMap.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdViewMap.Location = New System.Drawing.Point(271, 63)
         Me.cmdViewMap.Name = "cmdViewMap"
-        Me.cmdViewMap.Size = New System.Drawing.Size(106, 31)
+        Me.cmdViewMap.Size = New System.Drawing.Size(110, 28)
         Me.cmdViewMap.TabIndex = 13
         Me.cmdViewMap.Text = "View Map"
         Me.cmdViewMap.UseVisualStyleBackColor = True
         '
         'cmdEditTable
         '
-        Me.cmdEditTable.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.cmdEditTable.Location = New System.Drawing.Point(18, 63)
         Me.cmdEditTable.Name = "cmdEditTable"
-        Me.cmdEditTable.Size = New System.Drawing.Size(87, 28)
+        Me.cmdEditTable.Size = New System.Drawing.Size(110, 28)
         Me.cmdEditTable.TabIndex = 12
         Me.cmdEditTable.Text = "Edit Table"
         Me.cmdEditTable.UseVisualStyleBackColor = True
@@ -87,21 +73,33 @@ Partial Class frmModelSegmentation
         Me.cboSubbasins.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cboSubbasins.Location = New System.Drawing.Point(18, 30)
         Me.cboSubbasins.Name = "cboSubbasins"
-        Me.cboSubbasins.Size = New System.Drawing.Size(305, 24)
+        Me.cboSubbasins.Size = New System.Drawing.Size(363, 24)
         Me.cboSubbasins.TabIndex = 11
         '
         'fraMetStations
         '
         Me.fraMetStations.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
                     Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.fraMetStations.Controls.Add(Me.cmdAssign)
         Me.fraMetStations.Controls.Add(Me.cbxUseSelected)
         Me.fraMetStations.Controls.Add(Me.cboMetStations)
         Me.fraMetStations.Location = New System.Drawing.Point(12, 124)
         Me.fraMetStations.Name = "fraMetStations"
-        Me.fraMetStations.Size = New System.Drawing.Size(341, 94)
+        Me.fraMetStations.Size = New System.Drawing.Size(401, 130)
         Me.fraMetStations.TabIndex = 13
         Me.fraMetStations.TabStop = False
         Me.fraMetStations.Text = "Met Stations Layer"
+        '
+        'cmdAssign
+        '
+        Me.cmdAssign.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdAssign.Location = New System.Drawing.Point(18, 87)
+        Me.cmdAssign.Name = "cmdAssign"
+        Me.cmdAssign.Size = New System.Drawing.Size(363, 34)
+        Me.cmdAssign.TabIndex = 15
+        Me.cmdAssign.Text = "Assign Met Stations To Subbasins By Proximity"
+        Me.cmdAssign.UseVisualStyleBackColor = True
         '
         'cbxUseSelected
         '
@@ -121,27 +119,25 @@ Partial Class frmModelSegmentation
         Me.cboMetStations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cboMetStations.Location = New System.Drawing.Point(18, 30)
         Me.cboMetStations.Name = "cboMetStations"
-        Me.cboMetStations.Size = New System.Drawing.Size(303, 24)
+        Me.cboMetStations.Size = New System.Drawing.Size(363, 24)
         Me.cboMetStations.TabIndex = 11
         '
         'frmModelSegmentation
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(373, 265)
+        Me.ClientSize = New System.Drawing.Size(431, 271)
         Me.Controls.Add(Me.fraMetStations)
         Me.Controls.Add(Me.fraSubbasins)
-        Me.Controls.Add(Me.cmdAssign)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmModelSegmentation"
-        Me.Text = "BASINS Model Segmentation"
+        Me.Text = "BASINS Model Segmentation Specifier"
         Me.fraSubbasins.ResumeLayout(False)
         Me.fraMetStations.ResumeLayout(False)
         Me.fraMetStations.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents cmdAssign As System.Windows.Forms.Button
     Friend WithEvents fraSubbasins As System.Windows.Forms.GroupBox
     Friend WithEvents cboSubbasins As System.Windows.Forms.ComboBox
     Friend WithEvents fraMetStations As System.Windows.Forms.GroupBox
@@ -149,4 +145,5 @@ Partial Class frmModelSegmentation
     Friend WithEvents cboMetStations As System.Windows.Forms.ComboBox
     Friend WithEvents cmdEditTable As System.Windows.Forms.Button
     Friend WithEvents cmdViewMap As System.Windows.Forms.Button
+    Friend WithEvents cmdAssign As System.Windows.Forms.Button
 End Class
