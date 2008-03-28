bbfe64440c8366e39da44e6b2f16dde159e63e3c
Jack Kittle <jack@gkfamily.org>
2008-03-28 17:32:37 +0000

    PeakFQInputCreate.vb - polish details

--- ScriptRunner/PeakFQInputCreate.vb
+++ ScriptRunner/PeakFQInputCreate.vb
@@ -7,80 +7,91 @@ Imports atcData
 
 Module ScriptPeakFQInputCreate
     Private Const pFormat As String = "#,###,###,##0.00"
-    Private Const pWDMFileName As String = "SCRPeaks.wdm"
+    Private Const pTestPath As String = "D:\MountainViewData\SantaClaraResultsWDM"
+    Private Const pWDMFileName As String = "SCRWS.wdm"
+    Private Const pSummarizeData As Boolean = False
 
     Public Sub ScriptMain(ByRef aMapWin As IMapWin)
+        ChDriveDir(pTestPath)
         Logger.Dbg("ScriptPeakFQInputCreate:CurDir:" & CurDir())
 
-        Dim lAttributes As New atcCollection
-        With lAttributes
-            .Add("ID")
-            .Add("Location")
-            .Add("Constituent")
-            .Add("Start Date")
-            .Add("End Date")
-            .Add("Count")
-            .Add("Mean")
-            .Add("Geometric Mean")
-            .Add("Minimum")
-            .Add("Maximum")
-        End With
-
-        Dim lD2SStart As New atcDateFormat
-        lD2SStart.IncludeHours = True
-        lD2SStart.IncludeMinutes = True
-        lD2SStart.Midnight24 = False
-        Dim lD2SEnd As New atcDateFormat
-        lD2SEnd.IncludeHours = True
-        lD2SEnd.IncludeMinutes = True
-
-        Dim lStringBuilder As New Text.StringBuilder
-        Dim lString As String = ""
-        For Each lAttribute As String In lAttributes
-            lString &= lAttribute & vbTab
-        Next
-        lString.Trim(vbTab)
-        lStringBuilder.AppendLine(lString)
-
-        Dim lDataSets As atcDataGroup = atcDataManager.DataSets
+        Dim lWDM As New atcWDM.atcDataSourceWDM
+        lWDM.Open(pWDMFileName)
+        Dim lDataSets As atcDataGroup = lWDM.DataSets
         Logger.Dbg("DatasetCount " & lDataSets.Count)
-        For Each lDS As atcDataSet In lDataSets
-            Dim lValueString As String
-            lString = ""
+
+        Dim lStringBuilder As Text.StringBuilder
+
+        If pSummarizeData Then
+            Dim lAttributes As New atcCollection
+            With lAttributes
+                .Add("ID")
+                .Add("Location")
+                .Add("Constituent")
+                .Add("Start Date")
+                .Add("End Date")
+                .Add("Count")
+                .Add("Mean")
+                .Add("Geometric Mean")
+                .Add("Minimum")
+                .Add("Maximum")
+            End With
+
+            Dim lD2SStart As New atcDateFormat
+            lD2SStart.IncludeHours = True
+            lD2SStart.IncludeMinutes = True
+            lD2SStart.Midnight24 = False
+            Dim lD2SEnd As New atcDateFormat
+            lD2SEnd.IncludeHours = True
+            lD2SEnd.IncludeMinutes = True
+
+            lStringBuilder = New Text.StringBuilder
+            Dim lString As String = ""
             For Each lAttribute As String In lAttributes
-                lValueString = lDS.Attributes.GetValue(lAttribute, "?")
-                If lValueString = "?" Then 'no value available for this attribute
-                    lString &= lValueString
-                Else
-                    Select Case lAttribute
-                        Case "Start Date"
-                            lString &= "'" & lD2SStart.JDateToString(lValueString) & "'"
-                        Case "End Date"
-                            lString &= "'" & lD2SEnd.JDateToString(lValueString) & "'"
-                        Case "Count", "ID"
-                            lString &= lValueString
-                        Case Else
-                            If IsNumeric(lValueString) Then
-                                lString &= DoubleToString(lValueString, , pFormat).TrimEnd("0").TrimEnd(".")
-                            Else
-                                lString &= lValueString
-                            End If
-                    End Select
-                End If
-                lString &= vbTab
+                lString &= lAttribute & vbTab
             Next
             lString.Trim(vbTab)
             lStringBuilder.AppendLine(lString)
-        Next
-        SaveFileString("PeakFQInput.txt", lStringBuilder.ToString)
+
+            For Each lDS As atcDataSet In lDataSets
+                Dim lValueString As String
+                lString = ""
+                For Each lAttribute As String In lAttributes
+                    lValueString = lDS.Attributes.GetValue(lAttribute, "?")
+                    If lValueString = "?" Then 'no value available for this attribute
+                        lString &= lValueString
+                    Else
+                        Select Case lAttribute
+                            Case "Start Date"
+                                lString &= "'" & lD2SStart.JDateToString(lValueString) & "'"
+                            Case "End Date"
+                                lString &= "'" & lD2SEnd.JDateToString(lValueString) & "'"
+                            Case "Count", "ID"
+                                lString &= lValueString
+                            Case Else
+                                If IsNumeric(lValueString) Then
+                                    lString &= DoubleToString(lValueString, , pFormat).TrimEnd("0").TrimEnd(".")
+                                Else
+                                    lString &= lValueString
+                                End If
+                        End Select
+                    End If
+                    lString &= vbTab
+                Next
+                lString.Trim(vbTab)
+                lStringBuilder.AppendLine(lString)
+            Next
+            SaveFileString("PeakFQInput.txt", lStringBuilder.ToString)
+        End If
 
         Dim lNewWDM As New atcWDM.atcDataSourceWDM
-        lNewWDM.Open(pWDMFileName)
+        lNewWDM.Open("Annual.wdm")
         Dim lFreqStringBuilder As New Text.StringBuilder
+        lStringBuilder = New Text.StringBuilder
         For Each lDS As atcDataSet In lDataSets
             Dim lConstituent As String
             lConstituent = lDS.Attributes.GetValue("Constituent", "?")
-            If lConstituent.ToLower = "flow" Then
+            If lConstituent.ToLower.Contains("flow") Then
                 Logger.Dbg("Flow Found DSN " & lDS.Attributes.GetValue("ID", "?"))
                 Dim lCalculator As New atcTimeseriesNdayHighLow.atcTimeseriesNdayHighLow
                 Dim lArgs As New atcDataAttributes
@@ -91,7 +102,9 @@ Module ScriptPeakFQInputCreate
                     If lTimeseries.numValues > 0 Then
                         Logger.Dbg(lTimeseries.ToString & " " & lTimeseries.Value(1))
                         Dim lStaNum As String = lDS.Attributes.GetValue("location", "?").ToString.PadLeft(8)
-                        lStringBuilder = New Text.StringBuilder
+                        If lStringBuilder.Length > 0 Then
+                            lStringBuilder.Remove(0, lStringBuilder.Length)
+                        End If
                         lStringBuilder.AppendLine(lStaNum)
                         With lFreqStringBuilder
                             .AppendLine("*")
@@ -108,7 +121,7 @@ Module ScriptPeakFQInputCreate
                                 lStringBuilder.AppendLine(lPeakDateString & vbTab & DoubleToString(lTimeseries.Value(lIndex), 7, "######0").PadLeft(7))
                             Next
                         End With
-                        SaveFileString("pks_" & lStaNum & ".txt", lFreqStringBuilder.ToString)
+                        SaveFileString("pks_" & lStaNum.Trim & ".txt", lStringBuilder.ToString)
                         If lNewWDM.AddDataset(lTimeseries, atcDataSource.EnumExistAction.ExistRenumber) Then
                             Logger.Dbg("Save timeseries " & lTimeseries.ToString)
                         Else
