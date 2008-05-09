cd8ad5072314dd3c418db3399c85840d75a5a8da
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-05-09 19:00:31 +0000

    atcSWMM -- initial commit

---
+++ BASINS40/atcSWMM/clsPlugin.vb
@@ -0,0 +1,124 @@
+Imports atcMwGisUtility
+Imports atcData
+
+Public Class PlugIn
+    Implements MapWindow.Interfaces.IPlugin
+
+    Private pMapWin As MapWindow.Interfaces.IMapWin
+
+    'TODO: get these from BASINS4 or plugInManager?
+    Private Const ModelsMenuName As String = "BasinsModels"
+    Private Const ModelsMenuString As String = "Models"
+
+    Public ReadOnly Property Name() As String Implements MapWindow.Interfaces.IPlugin.Name
+        Get
+            Return "EPA SWMM 5.0 Setup"
+        End Get
+    End Property
+
+    Public ReadOnly Property Author() As String Implements MapWindow.Interfaces.IPlugin.Author
+        Get
+            Return "AQUA TERRA Consultants"
+        End Get
+    End Property
+
+    Public ReadOnly Property SerialNumber() As String Implements MapWindow.Interfaces.IPlugin.SerialNumber
+        Get
+            Return "G14R/KCU1FOWVVI"
+        End Get
+    End Property
+
+    Public ReadOnly Property Description() As String Implements MapWindow.Interfaces.IPlugin.Description
+        Get
+            Return "An interface for initializing the EPA SWMM 5.0 model."
+        End Get
+    End Property
+
+    Public ReadOnly Property BuildDate() As String Implements MapWindow.Interfaces.IPlugin.BuildDate
+        Get
+            Return System.IO.File.GetLastWriteTime(Me.GetType().Assembly.Location)
+        End Get
+    End Property
+
+    Public ReadOnly Property Version() As String Implements MapWindow.Interfaces.IPlugin.Version
+        Get
+            Return System.Diagnostics.FileVersionInfo.GetVersionInfo(Me.GetType().Assembly.Location).FileVersion
+        End Get
+    End Property
+
+    <CLSCompliant(False)> _
+    Public Sub Initialize(ByVal MapWin As MapWindow.Interfaces.IMapWin, ByVal ParentHandle As Integer) Implements MapWindow.Interfaces.IPlugin.Initialize
+        pMapWin = MapWin
+        atcData.atcDataManager.AddMenuIfMissing(ModelsMenuName, "", ModelsMenuString, "mnuFile")
+        atcData.atcDataManager.AddMenuIfMissing(ModelsMenuName & "_SWMM", ModelsMenuName, "SWMM")
+    End Sub
+
+    Public Sub Terminate() Implements MapWindow.Interfaces.IPlugin.Terminate
+        atcData.atcDataManager.RemoveMenuIfEmpty(ModelsMenuName & "_SWMM")
+        atcData.atcDataManager.RemoveMenuIfEmpty(ModelsMenuName)
+    End Sub
+
+    Public Sub ItemClicked(ByVal aItemName As String, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.ItemClicked
+        If aItemName = ModelsMenuName & "_SWMM" Then
+            Dim main As New frmSWMMSetup
+            GisUtil.MappingObject = pMapWin
+            main.InitializeUI()
+            main.Show()
+            Handled = True
+        End If
+    End Sub
+
+    Public Sub LayerRemoved(ByVal Handle As Integer) Implements MapWindow.Interfaces.IPlugin.LayerRemoved
+    End Sub
+
+    <CLSCompliant(False)> _
+    Public Sub LayersAdded(ByVal Layers() As MapWindow.Interfaces.Layer) Implements MapWindow.Interfaces.IPlugin.LayersAdded
+    End Sub
+
+    Public Sub LayersCleared() Implements MapWindow.Interfaces.IPlugin.LayersCleared
+    End Sub
+
+    Public Sub LayerSelected(ByVal Handle As Integer) Implements MapWindow.Interfaces.IPlugin.LayerSelected
+    End Sub
+
+    <CLSCompliant(False)> _
+    Public Sub LegendDoubleClick(ByVal Handle As Integer, ByVal Location As MapWindow.Interfaces.ClickLocation, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.LegendDoubleClick
+    End Sub
+
+    <CLSCompliant(False)> _
+    Public Sub LegendMouseDown(ByVal Handle As Integer, ByVal Button As Integer, ByVal Location As MapWindow.Interfaces.ClickLocation, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.LegendMouseDown
+    End Sub
+
+    <CLSCompliant(False)> _
+    Public Sub LegendMouseUp(ByVal Handle As Integer, ByVal Button As Integer, ByVal Location As MapWindow.Interfaces.ClickLocation, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.LegendMouseUp
+    End Sub
+
+    Public Sub MapDragFinished(ByVal Bounds As System.Drawing.Rectangle, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.MapDragFinished
+    End Sub
+
+    Public Sub MapExtentsChanged() Implements MapWindow.Interfaces.IPlugin.MapExtentsChanged
+    End Sub
+
+    Public Sub MapMouseDown(ByVal Button As Integer, ByVal Shift As Integer, ByVal x As Integer, ByVal y As Integer, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.MapMouseDown
+    End Sub
+
+    Public Sub MapMouseMove(ByVal ScreenX As Integer, ByVal ScreenY As Integer, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.MapMouseMove
+    End Sub
+
+    Public Sub MapMouseUp(ByVal Button As Integer, ByVal Shift As Integer, ByVal x As Integer, ByVal y As Integer, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.MapMouseUp
+    End Sub
+
+    Public Sub Message(ByVal msg As String, ByRef Handled As Boolean) Implements MapWindow.Interfaces.IPlugin.Message
+    End Sub
+
+    Public Sub ProjectLoading(ByVal ProjectFile As String, ByVal SettingsString As String) Implements MapWindow.Interfaces.IPlugin.ProjectLoading
+    End Sub
+
+    Public Sub ProjectSaving(ByVal ProjectFile As String, ByRef SettingsString As String) Implements MapWindow.Interfaces.IPlugin.ProjectSaving
+    End Sub
+
+    <CLSCompliant(False)> _
+    Public Sub ShapesSelected(ByVal Handle As Integer, ByVal SelectInfo As MapWindow.Interfaces.SelectInfo) Implements MapWindow.Interfaces.IPlugin.ShapesSelected
+    End Sub
+
+End Class
---
+++ BASINS40/atcSWMM/frmSWMMSetup.vb
@@ -0,0 +1,193 @@
+Imports atcUtility
+Imports atcMwGisUtility
+Imports MapWinUtility
+Imports System.Drawing
+
+Public Class frmSWMMSetup
+    Inherits System.Windows.Forms.Form
+
+#Region " Windows Form Designer generated code "
+
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        'Add any initialization after the InitializeComponent() call
+    End Sub
+
+    'Form overrides dispose to clean up the component list.
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing Then
+            If Not (components Is Nothing) Then
+                components.Dispose()
+            End If
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    Friend WithEvents cmdCancel As System.Windows.Forms.Button
+    Friend WithEvents cmdOK As System.Windows.Forms.Button
+    Friend WithEvents cmdExisting As System.Windows.Forms.Button
+    Friend WithEvents cmdHelp As System.Windows.Forms.Button
+    Friend WithEvents cmdAbout As System.Windows.Forms.Button
+    Friend WithEvents ofdExisting As System.Windows.Forms.OpenFileDialog
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSWMMSetup))
+        Me.cmdOK = New System.Windows.Forms.Button
+        Me.cmdExisting = New System.Windows.Forms.Button
+        Me.cmdCancel = New System.Windows.Forms.Button
+        Me.cmdHelp = New System.Windows.Forms.Button
+        Me.cmdAbout = New System.Windows.Forms.Button
+        Me.ofdExisting = New System.Windows.Forms.OpenFileDialog
+        Me.SuspendLayout()
+        '
+        'cmdOK
+        '
+        Me.cmdOK.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdOK.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdOK.Location = New System.Drawing.Point(16, 256)
+        Me.cmdOK.Name = "cmdOK"
+        Me.cmdOK.Size = New System.Drawing.Size(72, 32)
+        Me.cmdOK.TabIndex = 2
+        Me.cmdOK.Text = "OK"
+        '
+        'cmdExisting
+        '
+        Me.cmdExisting.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdExisting.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdExisting.Location = New System.Drawing.Point(96, 256)
+        Me.cmdExisting.Name = "cmdExisting"
+        Me.cmdExisting.Size = New System.Drawing.Size(120, 32)
+        Me.cmdExisting.TabIndex = 4
+        Me.cmdExisting.Text = "Open Existing"
+        '
+        'cmdCancel
+        '
+        Me.cmdCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.cmdCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdCancel.Location = New System.Drawing.Point(224, 256)
+        Me.cmdCancel.Name = "cmdCancel"
+        Me.cmdCancel.Size = New System.Drawing.Size(88, 32)
+        Me.cmdCancel.TabIndex = 5
+        Me.cmdCancel.Text = "Cancel"
+        '
+        'cmdHelp
+        '
+        Me.cmdHelp.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdHelp.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdHelp.Location = New System.Drawing.Point(349, 256)
+        Me.cmdHelp.Name = "cmdHelp"
+        Me.cmdHelp.Size = New System.Drawing.Size(79, 32)
+        Me.cmdHelp.TabIndex = 6
+        Me.cmdHelp.Text = "Help"
+        '
+        'cmdAbout
+        '
+        Me.cmdAbout.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdAbout.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdAbout.Location = New System.Drawing.Point(437, 256)
+        Me.cmdAbout.Name = "cmdAbout"
+        Me.cmdAbout.Size = New System.Drawing.Size(87, 32)
+        Me.cmdAbout.TabIndex = 7
+        Me.cmdAbout.Text = "About"
+        '
+        'ofdExisting
+        '
+        Me.ofdExisting.DefaultExt = "inp"
+        Me.ofdExisting.Filter = "SWMM INP files (*.inp)|*.inp"
+        Me.ofdExisting.InitialDirectory = "/BASINS/modelout/"
+        Me.ofdExisting.Title = "Select SWMM inp file"
+        '
+        'frmSWMMSetup
+        '
+        Me.AcceptButton = Me.cmdOK
+        Me.AutoScaleBaseSize = New System.Drawing.Size(6, 15)
+        Me.CancelButton = Me.cmdCancel
+        Me.ClientSize = New System.Drawing.Size(541, 301)
+        Me.Controls.Add(Me.cmdAbout)
+        Me.Controls.Add(Me.cmdHelp)
+        Me.Controls.Add(Me.cmdCancel)
+        Me.Controls.Add(Me.cmdExisting)
+        Me.Controls.Add(Me.cmdOK)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.Name = "frmSWMMSetup"
+        Me.Text = "BASINS SWMM"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+#End Region
+
+    Private Sub cmdCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub cmdAbout_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAbout.Click
+        Logger.Msg("BASINS SWMM for MapWindow" & vbCrLf & vbCrLf & "Version 1.0", MsgBoxStyle.OkOnly, "BASINS SWMM")
+    End Sub
+
+    Private Sub cmdExisting_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdExisting.Click
+        If ofdExisting.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            StartSWMM(ofdExisting.FileName)
+        End If
+    End Sub
+
+    Private Sub cmdHelp_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdHelp.Click
+        ShowHelp("BASINS Details\Watershed and Instream Model Setup\SWMM.html")
+    End Sub
+
+    Private Sub cmdOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdOK.Click
+        StartSWMM("")
+    End Sub
+
+    Private Sub EnableControls(ByVal aEnabled As Boolean)
+        cmdOK.Enabled = aEnabled
+        cmdExisting.Enabled = aEnabled
+        cmdHelp.Enabled = aEnabled
+        cmdCancel.Enabled = aEnabled
+        cmdAbout.Enabled = aEnabled
+    End Sub
+
+    Public Sub InitializeUI()
+        
+    End Sub
+
+    Private Sub frmSWMMSetup_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyValue = Windows.Forms.Keys.F1 Then
+            ShowHelp("BASINS Details\Watershed and Instream Model Setup\SWMM.html")
+        End If
+    End Sub
+
+    Private Sub StartSWMM(ByVal aCommand As String)
+        Dim lSWMMexe As String
+
+        lSWMMexe = "\Program Files\EPA SWMM 5.0\epaswmm5.exe"
+        If Not FileExists(lSWMMexe) Then
+            Dim lBasinsBinLoc As String = PathNameOnly(System.Reflection.Assembly.GetEntryAssembly.Location)
+            lSWMMexe = lBasinsBinLoc.Substring(1, 2) & lSWMMexe
+        End If
+        If Not FileExists(lSWMMexe) Then
+            'if we cant find it in any of the common places, use findfile
+            lSWMMexe = FindFile("Please locate the EPA SWMM 5.0 Executable", "epaswmm5.exe")
+        End If
+
+        If FileExists(lSWMMexe) Then
+            Logger.Dbg("StartSWMM:" & lSWMMexe & ":" & aCommand)
+            Process.Start(lSWMMexe, aCommand)
+            Logger.Dbg("SWMMStarted")
+        Else
+            Logger.Msg("Cannot find the EPA SWMM 5.0 Executable", MsgBoxStyle.Critical, "BASINS SWMM Problem")
+        End If
+    End Sub
+
+End Class
