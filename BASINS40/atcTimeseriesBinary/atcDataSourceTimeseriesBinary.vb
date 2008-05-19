1143d4105a66f417b9bc444a5c167713f73be27b
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-05-19 20:35:16 +0000

    atcDataSourceTimeseriesBinary - new simple binary timeseries class atcTimeseriesSWAT - polish attributes SWATRunner.vb - write copies of SWAT output to new binary class

---
+++ BASINS40/atcTimeseriesBinary/atcDataSourceTimeseriesBinary.vb
@@ -0,0 +1,187 @@
+Imports atcUtility
+Imports atcData
+Imports MapWinUtility
+
+Public Class atcDataSourceTimeseriesBinary
+    Inherits atcDataSource
+    '##MODULE_REMARKS Copyright 2008 AQUA TERRA Consultants - Royalty-free use permitted under open source license
+
+    Private Shared pFilter As String = "Binary Files (*.tsbin)|*.tsbin"
+    Private Shared pVersion As Integer = &H54534231 'TSB1 
+
+    Public Overrides ReadOnly Property Description() As String
+        Get
+            Return "Timseries Binary"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Name() As String
+        Get
+            Return "Timeseries::Binary"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Category() As String
+        Get
+            Return "File"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property CanOpen() As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property CanSave() As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    Public Overrides Function Open(ByVal aFileName As String, Optional ByVal aAttributes As atcData.atcDataAttributes = Nothing) As Boolean
+        If Not MyBase.Open(aFileName, aAttributes) Then
+            Return False
+        Else
+            If IO.File.Exists(Specification) Then
+                Dim lFileStream As New IO.FileStream(Specification, IO.FileMode.Open, IO.FileAccess.Read)
+                Dim lReader As New IO.BinaryReader(lFileStream)
+                Dim lVersion As Integer = lReader.ReadInt32
+                If lVersion <> pVersion Then
+                    Logger.Dbg("BadMagicNumber for " & Specification & " (" & Hex(lVersion) & "<>" & Hex(pVersion) & ")")
+                    lReader.Close()
+                    Return False
+                Else
+                    Try
+                        Do
+                            Dim lData As New atcTimeseries(Me)
+                            Dim lConstantInterval As Boolean = False
+                            Dim lAttributeName As String
+                            Dim lAttributeType As Byte
+                            Do
+                                lAttributeName = lReader.ReadString
+                                If lAttributeName = "<done>" Then
+                                    Exit Do
+                                ElseIf lAttributeName = "ConstantInterval" Then
+                                    lConstantInterval = True
+                                End If
+                                lAttributeType = lReader.ReadByte
+                                Select Case lAttributeType
+                                    Case 1 : lData.Attributes.SetValue(lAttributeName, lReader.ReadString)
+                                    Case 2 : lData.Attributes.SetValue(lAttributeName, lReader.ReadInt32)
+                                    Case 3 : lData.Attributes.SetValue(lAttributeName, lReader.ReadDouble)
+                                    Case Else
+                                        Debug.Print(lAttributeType)
+                                End Select
+                            Loop
+                            Dim lNumValues As Integer = lReader.ReadInt64
+                            Dim lDates(lNumValues) As Double
+                            If lConstantInterval Then
+                                lDates(0) = lReader.ReadDouble
+                                Dim lTimeUnits = lData.Attributes.GetValue("tu", 6)
+                                Dim lTimeStep = lData.Attributes.GetValue("ts", 1)
+                                For lIndex As Integer = 1 To lNumValues
+                                    lDates(lIndex) = TimAddJ(lDates(0), lTimeUnits, lTimeStep, lIndex)
+                                Next
+                            Else
+                                For lIndex As Integer = 0 To lNumValues
+                                    lDates(lIndex) = lReader.ReadDouble
+                                Next
+                            End If
+                            lData.Dates = New atcTimeseries(Me)
+                            lData.Dates.Values = lDates
+
+                            Dim lValues(lNumValues) As Double
+                            For lIndex As Integer = 0 To lNumValues
+                                lValues(lIndex) = lReader.ReadDouble
+                            Next
+                            lData.Values = lValues
+                            DataSets.Add(lData)
+                        Loop
+                    Catch ex As IO.EndOfStreamException
+                        Logger.Dbg("Read " & DataSets.Count & " from " & Specification)
+                    End Try
+                    lReader.Close()
+                End If
+            End If
+        End If
+        Return True
+    End Function
+
+    Public Overrides Function AddDatasets(ByVal aDataGroup As atcDataGroup) As Boolean
+        Logger.Dbg("StartToWrite " & Specification)
+        For Each lDataSet As atcData.atcDataSet In aDataGroup
+            AddDataset(lDataSet)
+        Next
+        Logger.Dbg("Wrote " & aDataGroup.Count & " Datasets")
+    End Function
+
+    Public Overrides Function AddDataset(ByVal aDataSet As atcData.atcDataSet, _
+                                Optional ByVal aExistAction As atcData.atcDataSource.EnumExistAction = atcData.atcDataSource.EnumExistAction.ExistReplace) _
+                                         As Boolean
+        Dim lConstantInterval As Boolean = False
+        Dim lFileStream As New IO.FileStream(Specification, IO.FileMode.Append)
+        Dim lWriter As New IO.BinaryWriter(lFileStream)
+        If lFileStream.Position = 0 Then
+            lWriter.Write(pVersion)
+        End If
+
+        For Each lAttribute As atcDefinedValue In aDataSet.Attributes
+            If Not lAttribute.Definition.Calculated Then
+                Dim lName As String = lAttribute.Definition.Name
+                If lName = "ConstantInterval" Then
+                    lConstantInterval = True
+                End If
+                Select Case lName
+                    Case "Key", "Data Source"
+                    Case Else
+                        Dim lType As Byte = 0
+                        Select Case lAttribute.Definition.TypeString
+                            Case "String" : lType = 1
+                            Case "Integer" : lType = 2
+                            Case "Double" : lType = 3
+                            Case Else
+                                Debug.Print("AttributeTypeNotDefined:" & lAttribute.Definition.TypeString)
+                        End Select
+                        If lType > 0 Then
+                            lWriter.Write(lName)
+                            lWriter.Write(lType)
+                            lWriter.Write(lAttribute.Value.ToString.TrimEnd)
+                        End If
+                End Select
+            End If
+        Next
+        lWriter.Write("<done>")
+
+        Dim lTimeseries As atcTimeseries = aDataSet
+        lWriter.Write(lTimeseries.numValues)
+        If lConstantInterval Then
+            lWriter.Write(lTimeseries.Dates.Values(0))
+        Else
+            Dim lDates() As Double = lTimeseries.Dates.Values
+            For lIndex As Integer = 0 To lTimeseries.numValues
+                lWriter.Write(lDates(lIndex))
+            Next
+        End If
+        Dim lValues() As Double = lTimeseries.Values
+        For lIndex As Integer = 0 To lTimeseries.numValues
+            lWriter.Write(lValues(lIndex))
+        Next
+        'todo: write value attributes (if any)
+
+        lWriter.Close()
+    End Function
+
+    Public Overrides Function Save(ByVal SaveFileName As String, _
+                          Optional ByVal ExistAction As EnumExistAction = EnumExistAction.ExistReplace) As Boolean
+        If SaveFileName.ToLower.Equals(Me.Specification.ToLower) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Sub New()
+        Filter = pFilter
+    End Sub
+End Class
