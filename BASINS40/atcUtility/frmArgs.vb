60db867d6005c9684b1bf8e83858645464330148
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-05-29 14:53:18 +0000

    BASINS: new atcUtility.frmArgs to make it easier to ask for arguments within scripts

---
+++ BASINS40/atcUtility/frmArgs.vb
@@ -0,0 +1,174 @@
+Public Class frmArgs
+
+    Private pOk As Boolean
+    Private pLeft As Integer
+    Private pTop As Integer
+    Private pRowHeight As Integer
+    Private pLeftColumnWidth As Integer
+    Private pRightColumnItems As ArrayList
+
+    Public Function AskUser(ByVal aTitle As String, ByRef aArgs As atcCollection) As Boolean
+        Text = aTitle
+
+        pOk = False
+        pLeft = btnOk.Left
+        pTop = pLeft
+        pRowHeight = btnOk.Height * 1.5
+        pLeftColumnWidth = btnOk.Width
+        pRightColumnItems = New ArrayList
+
+        Dim lArgIndex As Integer = 0
+        While lArgIndex < aArgs.Count
+            Select Case aArgs.Keys(lArgIndex).GetType.Name
+                Case "String"
+                    Dim lKey As String = aArgs.Keys(lArgIndex)
+                    Dim lArgValue As Object = aArgs.ItemByIndex(lArgIndex)
+                    If lKey.Substring(1, 1) = ":" Then
+                        Select Case lKey.Substring(0, 1).ToUpper
+                            Case "L" 'Just a label
+                                AddLabel(lKey.Substring(2), "lbl" & lArgIndex)
+                            Case "T" 'Label and text box
+                                Dim lLabel As Windows.Forms.Label = AddLabel(lKey.Substring(2), "lbl" & lArgIndex)
+                                If lLabel.Width > pLeftColumnWidth Then pLeftColumnWidth = lLabel.Width
+                                lArgIndex += 1
+                                Me.AddTextbox(aArgs(lArgIndex).ToString, "txt" & lArgIndex)
+                            Case "C" 'Label and checkbox
+                                Dim lLabel As Windows.Forms.Label = AddLabel(lKey.Substring(2), "lbl" & lArgIndex)
+                                If lLabel.Width > pLeftColumnWidth Then pLeftColumnWidth = lLabel.Width
+                                lArgIndex += 1
+                                Me.AddCheckbox(aArgs(lArgIndex), "chk" & lArgIndex)
+                            Case Else
+                                Stop
+                        End Select
+                    Else
+                        Dim lLabel As Windows.Forms.Label = AddLabel(lKey, "lbl" & lArgIndex)
+                        Select Case lArgValue.GetType.Name
+                            Case "Boolean" : Me.AddCheckbox(lArgValue, "chk" & lArgIndex)
+                            Case Else : Me.AddTextbox(lArgValue, "txt" & lArgIndex)
+                        End Select
+                    End If
+            End Select
+            lArgIndex += 1
+        End While
+
+        Me.Width = (pLeftColumnWidth + pLeft) * 2 + pLeft
+
+        For Each lItem As Windows.Forms.Control In pRightColumnItems
+            lItem.Left = pLeftColumnWidth + pLeft * 2
+            lItem.Width = Me.Width - lItem.Left - pLeft
+        Next
+
+        Me.Height = pTop + Me.Height - btnOk.Top
+        'pLabelClicked = "Cancel" 'If form closes without user clicking a button, default to "Cancel"
+        Me.ShowDialog() 'Block until form closes
+        If pOk Then
+            lArgIndex = 0
+            While lArgIndex < aArgs.Count
+                Select Case aArgs(lArgIndex).GetType.Name
+                    'Case "String"
+                    '    Dim lStr As String = aArgs(lArgIndex)
+                    '    If lStr.Substring(1, 1) = ":" Then
+                    '        Select Case lStr.Substring(0, 1).ToUpper
+                    '            Case "L" 'Just a label
+                    '            Case "T" 'Label and text box
+                    '                'Dim lLabel As Windows.Forms.Label = Me.Controls.Item(Me.Controls.IndexOfKey("lbl" & lArgIndex))
+                    '                lArgIndex += 1
+                    '                Dim lTextbox As Windows.Forms.TextBox = Me.Controls.Item(Me.Controls.IndexOfKey("txt" & lArgIndex))
+                    '                aArgs.ItemByIndex(lArgIndex) = lTextbox.Text
+                    '            Case "C" 'Label and checkbox
+                    '                'Dim lLabel As Windows.Forms.Label = AddLabel(lStr.Substring(2), "lbl" & lArgIndex)
+                    '                lArgIndex += 1
+                    '                Dim lCheckbox As Windows.Forms.CheckBox = Me.Controls.Item(Me.Controls.IndexOfKey("chk" & lArgIndex))
+                    '                aArgs.ItemByIndex(lArgIndex) = lCheckbox.Checked
+                    '            Case Else
+                    '                Stop
+                    '        End Select
+                    '    Else
+                    '        Select Case aArgs.ItemByIndex(lArgIndex).GetType.Name
+                    '            Case "Boolean"
+                    '                Dim lCheckbox As Windows.Forms.CheckBox = Me.Controls.Item(Me.Controls.IndexOfKey("chk" & lArgIndex))
+                    '                aArgs.ItemByIndex(lArgIndex) = lCheckbox.Checked
+                    '            Case Else
+                    '                Dim lTextbox As Windows.Forms.TextBox = Me.Controls.Item(Me.Controls.IndexOfKey("txt" & lArgIndex))
+                    '                aArgs.ItemByIndex(lArgIndex) = lTextbox.Text
+                    '        End Select
+                    '    End If
+                    Case "Boolean"
+                        Dim lCheckbox As Windows.Forms.CheckBox = Me.Controls.Item(Me.Controls.IndexOfKey("chk" & lArgIndex))
+                        aArgs.ItemByIndex(lArgIndex) = lCheckbox.Checked
+                    Case Else
+                        Dim lTextbox As Windows.Forms.TextBox = Me.Controls.Item(Me.Controls.IndexOfKey("txt" & lArgIndex))
+                        aArgs.ItemByIndex(lArgIndex) = lTextbox.Text
+
+                End Select
+                lArgIndex += 1
+            End While
+        End If
+        Return pOk
+    End Function
+
+    Private Function AddLabel(ByVal aText As String, ByVal aName As String) As Windows.Forms.Label
+        AddLabel = New Windows.Forms.Label
+        With AddLabel
+            .Text = aText
+            .Name = aName
+            .Top = pTop
+            pTop += pRowHeight
+            .Left = pLeft
+            .Width = Me.ClientSize.Width
+            .Anchor = Windows.Forms.AnchorStyles.Left Or Windows.Forms.AnchorStyles.Top
+            Me.Controls.Add(AddLabel)
+        End With
+    End Function
+
+    Private Function AddTextbox(ByVal aText As String, ByVal aName As String) As Windows.Forms.TextBox
+        AddTextbox = New Windows.Forms.TextBox
+        With AddTextbox
+            .Text = aText
+            .Name = aName
+            .Top = pTop - pRowHeight
+            .Anchor = Windows.Forms.AnchorStyles.Right Or Windows.Forms.AnchorStyles.Top
+            Me.Controls.Add(AddTextbox)
+            pRightColumnItems.Add(AddTextbox)
+        End With
+    End Function
+
+    Private Function AddCheckbox(ByVal aChecked As Boolean, ByVal aName As String) As Windows.Forms.CheckBox
+        AddCheckbox = New Windows.Forms.CheckBox
+        With AddCheckbox
+            .Checked = aChecked
+            .Name = aName
+            .Top = pTop - pRowHeight
+            .Anchor = Windows.Forms.AnchorStyles.Right Or Windows.Forms.AnchorStyles.Top
+            Me.Controls.Add(AddCheckbox)
+            pRightColumnItems.Add(AddCheckbox)
+        End With
+    End Function
+
+    'Private Sub AddButton()
+    '    Dim btn As Windows.Forms.Button = New Windows.Forms.Button
+    '    btn.Text = curLabel
+    '    btn.Top = lTop
+    '    btn.Left = 0
+    '    btn.Width = Me.ClientSize.Width
+    '    btn.Anchor = Windows.Forms.AnchorStyles.Left Or Windows.Forms.AnchorStyles.Right Or Windows.Forms.AnchorStyles.Top
+    '    lTop += btn.Height
+
+    '    Me.Controls.Add(btn)
+    '    AddHandler btn.Click, AddressOf btnClick
+    'End Sub
+
+    'Private Sub btnClick(ByVal sender As Object, ByVal e As System.EventArgs)
+    '    pLabelClicked = sender.text
+    '    Me.Hide()
+    'End Sub
+
+    Private Sub btnOk_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnOk.Click
+        pOk = True
+        Me.Hide()
+    End Sub
+
+    Private Sub btnCancel_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+        Me.Hide()
+    End Sub
+End Class
