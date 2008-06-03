24d725512afc1206fef5afad855a66af2626e197
pdudda <PaulDudaRESPEC@users.noreply.github.com>
2008-06-03 20:17:56 +0000

    atcSWMMCatchments.vb -- added subareas and infiltration tables atcSWMMProject.vb -- added project options

--- atcSWMM/atcSWMMCatchments.vb
+++ atcSWMM/atcSWMMCatchments.vb
@@ -55,6 +55,105 @@ Public Class Catchments
         Return lSB.ToString
     End Function
 
+    Public Function SubareasToString() As String
+        Dim lSB As New StringBuilder
+
+        lSB.Append("[SUBAREAS]" & vbCrLf & _
+                   ";;Subcatchment   N-Imperv   N-Perv     S-Imperv   S-Perv     PctZero    RouteTo    PctRouted " & vbCrLf & _
+                   ";;-------------- ---------- ---------- ---------- ---------- ---------- ---------- ----------" & vbCrLf)
+
+        For Each lCatchment As Catchment In Me
+            With lCatchment
+                lSB.Append(StrPad(.Name, 16, " ", False))
+                lSB.Append(" ")
+                lSB.Append(StrPad(Format(.ManningsNImperv, "0.00"), 10, " ", False))
+                lSB.Append(" ")
+                lSB.Append(StrPad(Format(.ManningsNPerv, "0.00"), 10, " ", False))
+                lSB.Append(" ")
+                lSB.Append(StrPad(Format(.DepressionStorageImperv, "0.00"), 10, " ", False))
+                lSB.Append(" ")
+                lSB.Append(StrPad(Format(.DepressionStoragePerv, "0.00"), 10, " ", False))
+                lSB.Append(" ")
+                lSB.Append(StrPad(Format(.PercentZeroStorage, "0.0"), 10, " ", False))
+                lSB.Append(" ")
+                lSB.Append(StrPad(.RouteTo, 10, " ", False))
+                lSB.Append(" ")
+                lSB.Append(StrPad(Format(.PercentRouted, "0.0"), 10, " ", False))
+                lSB.Append(vbCrLf)
+            End With
+        Next
+
+        Return lSB.ToString
+    End Function
+
+    Public Function InfiltrationToString() As String
+        Dim lSB As New StringBuilder
+
+        If Me.SWMMProject.InfiltrationMethod = "HORTON" Then
+
+            lSB.Append("[INFILTRATION]" & vbCrLf & _
+                       ";;Subcatchment   MaxRate    MinRate    Decay      DryTime    MaxInfil  " & vbCrLf & _
+                       ";;-------------- ---------- ---------- ---------- ---------- ----------" & vbCrLf)
+
+            For Each lCatchment As Catchment In Me
+                With lCatchment
+                    lSB.Append(StrPad(.Name, 16, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.MaxInfiltRate, "0.00"), 10, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.MinInfiltRate, "0.00"), 10, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.DecayRateConstant, "0.00"), 10, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.DryTime, "0.00"), 10, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.MaxInfiltVolume, "0.00"), 10, " ", False))
+                    lSB.Append(vbCrLf)
+                End With
+            Next
+
+        ElseIf Me.SWMMProject.InfiltrationMethod = "GREEN_AMPT" Then
+
+            lSB.Append("[INFILTRATION]" & vbCrLf & _
+                       ";;Subcatchment   Suction    HydCon     IMDmax    " & vbCrLf & _
+                       ";;-------------- ---------- ---------- ----------" & vbCrLf)
+
+            For Each lCatchment As Catchment In Me
+                With lCatchment
+                    lSB.Append(StrPad(.Name, 16, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.Suction, "0.00"), 10, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.Conductivity, "0.00"), 10, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.InitialDeficit, "0.00"), 10, " ", False))
+                    lSB.Append(vbCrLf)
+                End With
+            Next
+
+        ElseIf Me.SWMMProject.InfiltrationMethod = "CURVE_NUMBER" Then
+
+            lSB.Append("[INFILTRATION]" & vbCrLf & _
+                       ";;Subcatchment   CurveNum   HydCon     DryTime   " & vbCrLf & _
+                       ";;-------------- ---------- ---------- ----------" & vbCrLf)
+
+            For Each lCatchment As Catchment In Me
+                With lCatchment
+                    lSB.Append(StrPad(.Name, 16, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.CurveNumber, "0.00"), 10, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.Conductivity, "0.00"), 10, " ", False))
+                    lSB.Append(" ")
+                    lSB.Append(StrPad(Format(.DryTime, "0.00"), 10, " ", False))
+                    lSB.Append(vbCrLf)
+                End With
+            Next
+
+        End If
+        Return lSB.ToString
+    End Function
+
     Public Function PolygonsToString() As String
         Dim lSB As New StringBuilder
         lSB.Append("[Polygons]" & vbCrLf & _
@@ -88,6 +187,25 @@ Public Class Catchment
     Public Slope As Double = 0.0 'percent
     Public CurbLength As Double = 0.0
     Public SnowPackName As String = "" 'blank if none
+
+    Public ManningsNImperv As Double = 0.01
+    Public ManningsNPerv As Double = 0.1
+    Public DepressionStorageImperv As Double = 0.05 'inches or mm
+    Public DepressionStoragePerv As Double = 0.05 'inches or mm
+    Public PercentZeroStorage As Double = 25.0
+    Public RouteTo As String = "OUTLET"
+    Public PercentRouted As Double = 100.0
+
+    Public MaxInfiltRate As Double = 3.0 'inches/hr or mm/hr
+    Public MinInfiltRate As Double = 0.5 'inches/hr or mm/hr
+    Public DecayRateConstant As Double = 4
+    Public DryTime As Double = 7 'days (or 4 days if using curve number)
+    Public MaxInfiltVolume As Double = 0 'inches or mm
+    Public Suction As Double = 3.0 'inches or mm
+    Public Conductivity As Double = 0.5 'inches/hr or mm/hr
+    Public InitialDeficit As Double = 4.0
+    Public CurveNumber As Double = 3.0
+
     Public X() As Double
     Public Y() As Double
 End Class
