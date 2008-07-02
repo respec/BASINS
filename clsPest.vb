f2e17cbb16546a50191bef9e20634a1a3680ed1e
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2008-07-02 18:46:29 +0000

    BatchSWAT.modSwatDatabase: commented out code to copy *.dat since we now handle them as tables SwatObject: added clsPest and clsTill to handle .dat files and tables

---
+++ clsPest.vb
@@ -0,0 +1,109 @@
+'OBJECTID,IPNUM,PNAME,SKOC,WOF,HLIFE_F,HLIFE_S,AP_EF,WSOL,HENRY,PESTNAME
+Partial Class SwatInput
+    Private pPest As clsPest = New clsPest(Me)
+    ReadOnly Property Pest() As clsPest
+        Get
+            Return pPest
+        End Get
+    End Property
+
+    Public Class clsPestItem
+        Public IPNUM As Integer
+        Public PNAME As String '17
+        Public SKOC As Double
+        Public WOF As Single
+        Public HLIFE_F As Single
+        Public HLIFE_S As Single
+        Public AP_EF As Single
+        Public WSOL As Double
+        Public HENRY As Double
+        Public PESTNAME As String '30
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(ByVal aRow As DataRow)
+            With aRow
+                IPNUM = .Item("IPNUM")
+                PNAME = .Item("PNAME")
+                SKOC = .Item("SKOC")
+                WOF = .Item("WOF")
+                HLIFE_F = .Item("HLIFE_F")
+                HLIFE_S = .Item("HLIFE_S")
+                AP_EF = .Item("AP_EF")
+                WSOL = .Item("WSOL")
+                HENRY = .Item("HENRY")
+                PESTNAME = .Item("PESTNAME")
+            End With
+        End Sub
+
+        Public Function AddSQL() As String
+            Return "INSERT INTO Pest ( " & clsDataColumn.ColumnNames(clsHru.Columns) & " ) Values (" _
+                   & IPNUM & ", " _
+                   & PNAME & ", " _
+                   & SKOC & ", " _
+                   & WOF & ", " _
+                   & HLIFE_F & ", " _
+                   & HLIFE_S & ", " _
+                   & AP_EF & ", " _
+                   & WSOL & ", " _
+                   & HENRY & ", " _
+                   & PESTNAME & " )"
+        End Function
+    End Class
+
+    Public Class clsPest
+        Private pSwatInput As SwatInput
+        Private pTableName As String = "Pest"
+
+        Friend Shared Columns As Generic.List(Of clsDataColumn)
+
+        Friend Sub New(ByVal aSwatInput As SwatInput)
+            pSwatInput = aSwatInput
+            If Columns Is Nothing Then InitColumns()
+        End Sub
+
+        Private Shared Sub InitColumns()
+            Columns = New Generic.List(Of clsDataColumn)
+
+            Columns.Add(New clsDataColumn("IPNUM", 1, "Integer", "0", 3, ""))
+            Columns.Add(New clsDataColumn("PNAME", 1, "VARCHAR(17)", "%s", 17, ""))
+            Columns.Add(New clsDataColumn("SKOC", 1, "Double", "0.0", 10, ""))
+            Columns.Add(New clsDataColumn("WOF", 1, "Single", "0.00", 5, ""))
+            Columns.Add(New clsDataColumn("HLIFE_F", 1, "Single", "0.0", 8, ""))
+            Columns.Add(New clsDataColumn("HLIFE_S", 1, "Single", "0.0", 8, ""))
+            Columns.Add(New clsDataColumn("AP_EF", 1, "Single", "0.00", 5, ""))
+            Columns.Add(New clsDataColumn("WSOL", 1, "Double", "0.000", 11, ""))
+            Columns.Add(New clsDataColumn("HENRY", 1, "Double", "", 8, ""))
+            Columns.Add(New clsDataColumn("PESTNAME", 1, "VARCHAR(30)", "", 8, ""))
+        End Sub
+
+        Public Function TableCreate() As Boolean
+        End Function
+
+        Public Function Table() As DataTable
+            pSwatInput.Status("Reading " & pTableName & " from database ...")
+            Return pSwatInput.QueryGDB("SELECT * FROM " & pTableName & ";")
+        End Function
+
+        Public Sub Add(ByVal aItem As clsChmItem)
+            ExecuteNonQuery(aItem.AddSQL, pSwatInput.CnSwatInput)
+        End Sub
+
+        Public Function FindPest(ByVal aPestName As String, Optional ByVal aTable As DataTable = Nothing) As clsPestItem
+            If aTable Is Nothing Then aTable = Table()
+            For Each lRow As DataRow In aTable.Rows
+                If lRow.Item("PNAME").ToString = aPestName Then
+                    Return New clsPestItem(lRow)
+                End If
+            Next
+            Return Nothing
+        End Function
+
+        Public Sub Save(Optional ByVal aTable As DataTable = Nothing)
+            pSwatInput.Status("Writing " & pTableName & " text ...")
+            If aTable Is Nothing Then aTable = Table()
+            SaveTableAsText(aTable, Columns, pSwatInput.TxtInOutFolder & "\" & pTableName & ".dat")
+        End Sub
+    End Class
+End Class
---
+++ clsTill.vb
@@ -0,0 +1,94 @@
+'OBJECTID,ITNUM,TILLNM,EFTMIX,DEPTIL,OPNAME,OPNUM
+Partial Class SwatInput
+    Private pTill As clsTill = New clsTill(Me)
+    ReadOnly Property Till() As clsTill
+        Get
+            Return pTill
+        End Get
+    End Property
+
+    Public Class clsTillItem
+        Public ITNUM As Double
+        Public TILLNM As String '8
+        Public EFTMIX As Single
+        Public DEPTIL As Single
+        Public OPNAME As String '40
+        Public OPNUM As String '4
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(ByVal aRow As DataRow)
+            With aRow
+                ITNUM = .Item("ITNUM")
+                TILLNM = .Item("TILLNM")
+                EFTMIX = .Item("EFTMIX")
+                DEPTIL = .Item("DEPTIL")
+                OPNAME = .Item("OPNAME")
+                OPNUM = .Item("OPNUM")
+            End With
+        End Sub
+
+        Public Function AddSQL() As String
+            Return "INSERT INTO Till ( " & clsDataColumn.ColumnNames(clsHru.Columns) & " ) Values (" _
+                   & ITNUM & ", " _
+                   & TILLNM & ", " _
+                   & EFTMIX & ", " _
+                   & DEPTIL & ", " _
+                   & OPNAME & ", " _
+                   & OPNUM & " )"
+        End Function
+    End Class
+
+    Public Class clsTill
+        Private pSwatInput As SwatInput
+        Private pTableName As String = "Till"
+
+        Friend Shared Columns As Generic.List(Of clsDataColumn)
+
+        Friend Sub New(ByVal aSwatInput As SwatInput)
+            pSwatInput = aSwatInput
+            If Columns Is Nothing Then InitColumns()
+        End Sub
+
+        Private Shared Sub InitColumns()
+            Columns = New Generic.List(Of clsDataColumn)
+
+            Columns.Add(New clsDataColumn("ITNUM", 1, "Double", "0.000", 8, ""))
+            Columns.Add(New clsDataColumn("TILLNM", 1, "VARCHAR(8)", "0.000", 8, ""))
+            Columns.Add(New clsDataColumn("EFTMIX", 1, "Single", "0.000", 8, ""))
+            Columns.Add(New clsDataColumn("DEPTIL", 1, "Single", "0.000", 8, ""))
+            Columns.Add(New clsDataColumn("OPNAME", 1, "VARCHAR(40)", "0.000", 8, ""))
+            Columns.Add(New clsDataColumn("OPNUM", 1, "VARCHAR(4)", "0.000", 8, ""))
+
+        End Sub
+
+        Public Function TableCreate() As Boolean
+        End Function
+
+        Public Function Table() As DataTable
+            pSwatInput.Status("Reading " & pTableName & " from database ...")
+            Return pSwatInput.QueryGDB("SELECT * FROM " & pTableName & ";")
+        End Function
+
+        Public Sub Add(ByVal aItem As clsChmItem)
+            ExecuteNonQuery(aItem.AddSQL, pSwatInput.CnSwatInput)
+        End Sub
+
+        Public Function FindTill(ByVal aTillName As String, Optional ByVal aTable As DataTable = Nothing) As clsTillItem
+            If aTable Is Nothing Then aTable = Table()
+            For Each lRow As DataRow In aTable.Rows
+                If lRow.Item("TILLNM").ToString = aTillName Then
+                    Return New clsTillItem(lRow)
+                End If
+            Next
+            Return Nothing
+        End Function
+
+        Public Sub Save(Optional ByVal aTable As DataTable = Nothing)
+            pSwatInput.Status("Writing " & pTableName & " text ...")
+            If aTable Is Nothing Then aTable = Table()
+            SaveTableAsText(aTable, Columns, pSwatInput.TxtInOutFolder & "\" & pTableName & ".dat")
+        End Sub
+    End Class
+End Class
