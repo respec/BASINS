8da30db6182bcf2d6095a87e9dc9ed67d5d64829
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-07-24 21:03:17 +0000

    frmReach.vb - begin implementing reach editor

--- BASINS40/WinHSPF/frmReach.Designer.vb
+++ BASINS40/WinHSPF/frmReach.Designer.vb
@@ -19,16 +19,32 @@ Partial Class frmReach
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.grdReach = New atcControls.atcGrid
         Me.SuspendLayout()
+        '
+        'grdReach
+        '
+        Me.grdReach.AllowHorizontalScrolling = True
+        Me.grdReach.AllowNewValidValues = False
+        Me.grdReach.CellBackColor = System.Drawing.Color.Empty
+        Me.grdReach.LineColor = System.Drawing.Color.Empty
+        Me.grdReach.LineWidth = 0.0!
+        Me.grdReach.Location = New System.Drawing.Point(13, 16)
+        Me.grdReach.Name = "grdReach"
+        Me.grdReach.Size = New System.Drawing.Size(448, 206)
+        Me.grdReach.Source = Nothing
+        Me.grdReach.TabIndex = 0
         '
         'frmReach
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 272)
+        Me.ClientSize = New System.Drawing.Size(578, 272)
+        Me.Controls.Add(Me.grdReach)
         Me.Name = "frmReach"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents grdReach As atcControls.atcGrid
 End Class
--- BASINS40/WinHSPF/frmReach.vb
+++ BASINS40/WinHSPF/frmReach.vb
@@ -1,3 +1,49 @@
+Imports System.Drawing
+Imports MapWinUtility
+Imports atcUCI
+Imports atcControls
+
 Public Class frmReach
 
+    Public Sub New()
+
+        ' This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        With grdReach
+            .Source = New atcControls.atcGridSource
+            .Clear()
+            .AllowHorizontalScrolling = False
+            .Visible = True
+        End With
+
+        With grdReach.Source
+            .Columns = 5
+            .CellValue(0, 0) = "ID"
+            .CellValue(0, 1) = "Description"
+            .ColorCells = True
+            .FixedRows = 1
+            .FixedColumns = 1
+            .CellColor(0, 0) = SystemColors.ControlLight
+
+            Dim lOperBlock As HspfOpnBlk = pUCI.OpnBlks("RCHRES")
+            .Rows = lOperBlock.Count
+
+            'loop through each operation and populate the table
+            For lRow As Integer = 1 To lOperBlock.Count
+                Dim lOperation As HspfOperation = lOperBlock.NthOper(lRow)
+                .CellValue(lRow, 0) = lOperation.Id
+                Dim lTable As HspfTable = lOperation.Tables("GEN-INFO")
+                .CellValue(lRow, 1) = lTable.Parms("RCHID").Value
+            Next
+        End With
+
+        grdReach.Refresh()
+        grdReach.SizeAllColumnsToContents()
+
+        Me.Icon = pIcon
+
+    End Sub
 End Class
