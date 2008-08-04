ef23142808f687cd0587c2dcbc6c753ee67d52a3
bgonzales <bgonzales@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-08-04 23:34:57 +0000

    ctlEditFTables - added some sub functions to populate table

--- atcUCIForms/ctlEditFTables.vb
+++ atcUCIForms/ctlEditFTables.vb
@@ -2,6 +2,7 @@ Imports System.Drawing
 Imports MapWinUtility
 Imports atcUCI
 Imports atcControls
+Imports atcUCIForms
 
 Public Class ctlEditFTables
     Implements ctlEdit
@@ -9,6 +10,7 @@ Public Class ctlEditFTables
     Dim pHspfFtable As HspfFtable
     Dim pDataSource As atcGridSource
     Dim pChanged As Boolean
+    Private PrevListIndex As Long
     Public Event Change(ByVal aChange As Boolean) Implements ctlEdit.Change
 
     Public ReadOnly Property Caption() As String Implements ctlEdit.Caption
@@ -56,11 +58,128 @@ Public Class ctlEditFTables
         Get
             Return pHspfFtable
         End Get
+
         Set(ByVal aHspfFtables As Object)
             pHspfFtable = aHspfFtables
+            Dim vID As Object
+            For Each vID In pHspfFtable.Operation.OpnBlk.Ids
+                cboID.Items.Add(vID.Id & " - " & vID.Description)
+                If pHspfFtable.Operation.Tables("HYDR-PARM2").ParmValue("FTBUCI") = cboID.Items.Count Then
+                    cboID.SelectedIndex = 0
+                    PrevListIndex = cboID.SelectedIndex
+                End If
+            Next
+            txtNRows.Value = pHspfFtable.Nrows
+            txtNCols.Value = pHspfFtable.Ncols
+            RefreshFtables()
         End Set
+
+
     End Property
 
+    Private Sub RefreshFtables()
+        Dim i, j, units As Integer
+        units = pHspfFtable.Operation.OpnBlk.Uci.GlobalBlock.emfg
+
+        With grdEdit
+            .Source = New atcControls.atcGridSource
+            .Clear()
+            .AllowHorizontalScrolling = False
+            .AllowNewValidValues = True
+            .Visible = True
+        End With
+
+        With grdEdit.Source
+            .Rows = txtNRows.Value
+            .Columns = txtNCols.Value
+            For j = 0 To .Columns - 1
+                .CellEditable(0, j) = False
+            Next
+            If units = 1 Then
+                .CellValue(0, 0) = "Depth (ft)"
+                .CellValue(0, 1) = "Area (acres)"
+                .CellValue(0, 2) = "Volume (acre-ft)"
+                For j = 3 To .Columns - 1
+                    .CellValue(0, j) = "Outflow" & j - 2 & "(ft3/s)"
+                Next
+            Else 'metric
+                .CellValue(0, 0) = "Depth (m)"
+                .CellValue(0, 1) = "Area (ha)"
+                .CellValue(0, 2) = "Volume (Mm3)"
+                For j = 3 To .Columns - 1
+                    .CellValue(0, j) = "Outflow" & j - 2 & "(m3/s)"
+                Next
+            End If
+
+            For lCol As Integer = 0 To .Columns - 1
+                .CellColor(0, lCol) = SystemColors.ControlLight
+            Next
+
+            For m As Integer = 0 To .Columns - 1
+                For k As Integer = 1 To .Rows - 1
+                    .CellEditable(k, m) = True
+                Next
+            Next
+
+            For i = 1 To .Rows
+                .CellValue(i, 0) = pHspfFtable.Depth(i)
+                .CellValue(i, 1) = pHspfFtable.Area(i)
+                .CellValue(i, 2) = pHspfFtable.Volume(i)
+                For j = 3 To .Columns - 1
+                    If j = 3 Then
+                        .CellValue(i, j) = pHspfFtable.Outflow1(i)
+                    End If
+                    If j = 3 Then
+                        .CellValue(i, j) = pHspfFtable.Outflow2(i)
+                    End If
+                    If j = 3 Then
+                        .CellValue(i, j) = pHspfFtable.Outflow3(i)
+                    End If
+                    If j = 3 Then
+                        .CellValue(i, j) = pHspfFtable.Outflow4(i)
+                    End If
+                    If j = 3 Then
+                        .CellValue(i, j) = pHspfFtable.Outflow5(i)
+                    End If
+
+                Next
+            Next
+        End With
+
+        grdEdit.Refresh()
+        grdEdit.SizeAllColumnsToContents()
+
+    End Sub
+    Private Sub refreshGrid()
+        Dim i, j, units As Integer
+        units = pHspfFtable.Operation.OpnBlk.Uci.GlobalBlock.emfg
+
+        With grdEdit.Source
+            .Rows = txtNRows.Value
+            .Columns = txtNCols.Value
+            For j = 3 To .Columns - 1
+                If units = 1 Then
+                    .CellValue(0, j) = "Outflow" & j - 2 & " (ft3/s)"
+                Else
+                    .CellValue(0, j) = "Outflow" & j - 2 & " (m3/s)"
+                End If
+                For m As Integer = 0 To .Columns - 1
+                    For k As Integer = 1 To .Rows - 1
+                        .CellEditable(k, m) = True
+                    Next
+                Next
+            Next
+            For j = 0 To .Columns - 1
+                For i = 1 To .Rows
+                    If Len(.CellValue(i, j)) = 0 Then
+                        .CellValue(i, j) = 0
+                    End If
+                Next
+            Next
+            grdEdit.Refresh()
+            grdEdit.SizeAllColumnsToContents()
+        End With
+    End Sub
     Public Sub New(ByVal aHspfFtables As Object, ByVal aParent As Windows.Forms.Form)
 
         ' This call is required by the Windows Form Designer.
@@ -85,5 +204,15 @@ Public Class ctlEditFTables
         Dim frmNewFTable As New frmNewFTable
         frmNewFTable.Show()
     End Sub
+
+    'Private Sub cboID_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboID.SelectedIndexChanged
+    '    Dim discard, tempID, i As Integer
+
+    '    i = InStr(1, cboID, "-")
+    '    If i > 0 Then
+    '        tempID = CInt(Mid(cboID, 1, i - 2))
+
+    '    End If
+    'End Sub
 End Class
 
