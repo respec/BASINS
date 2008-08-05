8acd954a8908e394d4e7867b7f7da1aa5b307664
Jack Kittle <jack@gkfamily.org>
2008-08-05 19:24:23 +0000

    atcHspfSupport - add modAnnualCompareStats.vb, move code from atcHspfDailyMonthlyCompareStats to modUtility for sharing ScriptRunner - HSPFOutputReports - call new annual report, update pointer to Upatoi project

--- atcHspfSupport/modAnnualCompareStats.vb
+++ atcHspfSupport/modAnnualCompareStats.vb
@@ -2,17 +2,19 @@ Imports atcUtility
 Imports atcData
 Imports atcTimeseriesMath
 
-Public Module DailyMonthlyCompareStats
+Public Module AnnualCompareStats
     Public Function Report(ByVal aUci As atcUCI.HspfUci, _
                            ByVal aCons As String, _
                            ByVal aSite As String, _
+                           ByVal aUnits As String, _
+                           ByVal aPrecTSer As atcTimeseries, _
                            ByVal aSimTSer As atcTimeseries, _
                            ByVal aObsTSer As atcTimeseries, _
                            Optional ByVal aSDateJ As Double = 0, _
                            Optional ByVal aEDateJ As Double = 0) As String
 
         Dim lStr As String
-        lStr = "Daily and Monthly " & aCons & " Statistics for '" & IO.Path.GetFileNameWithoutExtension(aUci.Name) & "' scenario." & vbCrLf
+        lStr = "Annual Simulated and Observed " & aCons & " Statistics for '" & IO.Path.GetFileNameWithoutExtension(aUci.Name) & "' scenario." & vbCrLf
         lStr &= "   Run Made " & FileDateTime(aUci.Name) & vbCrLf
         lStr &= "   " & aUci.GlobalBlock.RunInf.Value & vbCrLf
 
@@ -26,20 +28,20 @@ Public Module DailyMonthlyCompareStats
         lStr &= " from " & Format(Date.FromOADate(lSDateJ), "yyyy/MM/dd")
         lStr &= " to " & Format(Date.FromOADate(lEDateJ), "yyyy/MM/dd") & vbCrLf
 
-        lStr &= "   (Units:CFS days)" & vbCrLf & vbCrLf 'TODO: do this in inches too?
+        lStr &= "   (Units:" & aUnits & ")" & vbCrLf & vbCrLf
 
         CheckDateJ(aObsTSer, "Observed", lSDateJ, lEDateJ, lStr)
         CheckDateJ(aSimTSer, "Simulated", lSDateJ, lEDateJ, lStr)
 
         Dim lNewSimTSer As atcTimeseries = SubsetByDate(aSimTSer, lSDateJ, lEDateJ, Nothing)
         Dim lNewObsTSer As atcTimeseries = SubsetByDate(aObsTSer, lSDateJ, lEDateJ, Nothing)
+        Dim lNewPrecTSer As atcTimeseries = SubsetByDate(aPrecTSer, lSDateJ, lEDateJ, Nothing)
 
         If lNewSimTSer.numValues <> lNewObsTSer.numValues Then
             lStr &= "   SimCount " & lNewSimTSer.numValues & " ObsCount " & lNewObsTSer.numValues & vbCrLf & vbCrLf
         End If
 
-        lStr &= IntervalReport(aSite, atcTimeUnit.TUDay, lNewSimTSer, lNewObsTSer)
-        lStr &= IntervalReport(aSite, atcTimeUnit.TUMonth, lNewSimTSer, lNewObsTSer)
+        lStr &= IntervalReport(aSite, atcTimeUnit.TUYear, lNewSimTSer, lNewObsTSer, True, lNewPrecTSer)
 
         Return lStr
     End Function
