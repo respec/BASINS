969b6fd020f74f6cd2e8abea5c60babbed391c5b
bgonzales <bgonzales@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-08-06 23:37:18 +0000

    ctlEditCategory - Form Skeleton. Still does not read categories from UCIForms frmWinHSPF.vb - updated for category menu selection UCIForms - updated for category menu selection

---
+++ atcUCIForms/ctlEditCategoryBlock.Designer.vb
@@ -0,0 +1,51 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ctlEditCategory
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grdEdit = New atcControls.atcGrid
+        Me.SuspendLayout()
+        '
+        'grdEdit
+        '
+        Me.grdEdit.AllowHorizontalScrolling = True
+        Me.grdEdit.AllowNewValidValues = False
+        Me.grdEdit.CellBackColor = System.Drawing.Color.Empty
+        Me.grdEdit.LineColor = System.Drawing.Color.Empty
+        Me.grdEdit.LineWidth = 0.0!
+        Me.grdEdit.Location = New System.Drawing.Point(21, 13)
+        Me.grdEdit.Name = "grdEdit"
+        Me.grdEdit.Size = New System.Drawing.Size(439, 122)
+        Me.grdEdit.Source = Nothing
+        Me.grdEdit.TabIndex = 0
+        '
+        'ctlEditCategory
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.grdEdit)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "ctlEditCategory"
+        Me.Size = New System.Drawing.Size(490, 148)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents grdEdit As atcControls.atcGrid
+
+End Class
