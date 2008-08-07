643f34c2dd100353c39e88c90722d5bc8a574ee6
bgonzales <bgonzales@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-08-07 22:58:37 +0000

    ctlEditCategoryBlock.vb - made sure Remove() code was in fact not there (yet) ctlEditConnection.vb - Added populate NETWORK Table case. (need to check that index vals are correct)

--- atcUCIForms/ctlEditConnections.vb
+++ atcUCIForms/ctlEditConnections.vb
@@ -68,9 +68,63 @@ Public Class ctlEditConnections
         Set(ByVal aConnection As Object)
             pConnection = aConnection
             pConnections = pConnection.Uci.Connections
+            Dim lOper As New HspfOperation
+            Dim lConn As New HspfConnection
+
             With grdEdit.Source
                 If pConnectionType = "NETWORK" Then
+                    .Columns = 15
+                    .CellValue(0, 0) = "VolName"
+                    .CellValue(0, 1) = "VolID"
+                    .CellValue(0, 2) = "Group"
+                    .CellValue(0, 3) = "MemName"
+                    .CellValue(0, 4) = "MemSub1"
+                    .CellValue(0, 5) = "MemSub2"
+                    .CellValue(0, 6) = "MultFact"
+                    .CellValue(0, 7) = "Tran"
+                    .CellValue(0, 8) = "VolName"
+                    .CellValue(0, 9) = "VolId"
+                    .CellValue(0, 10) = "Group"
+                    .CellValue(0, 11) = "MemName"
+                    .CellValue(0, 12) = "MemSub1"
+                    .CellValue(0, 13) = "MemSub2"
+                    .CellValue(0, 14) = "HIDE"
 
+                    For j As Integer = 1 To .Columns - 1
+                        For k As Integer = 1 To .Rows - 1
+                            .CellEditable(k, j) = True
+                        Next
+                    Next
+
+                    For lCol As Integer = 0 To .Columns
+                        .CellColor(0, lCol) = SystemColors.ControlLight
+                    Next
+
+                    For i As Integer = 1 To pConnection.Uci.OpnSeqBlock.Opns.Count - 1 'NOTE: Index values may need adjusting
+                        lOper = pConnection.Uci.OpnSeqBlock.Opn(i)
+                        For j As Integer = 1 To lOper.Sources.Count - 1 'NOTE: Index values may need adjusting
+                            lConn = lOper.Sources(j)
+                            If lConn.Typ = 2 Then
+                                .Rows += 1
+                                .CellValue(.Rows, 0) = lConn.Source.VolName
+                                .CellValue(.Rows, 1) = lConn.Source.VolId
+                                .CellValue(.Rows, 2) = lConn.Source.Group
+                                .CellValue(.Rows, 3) = lConn.Source.Member
+                                .CellValue(.Rows, 4) = lConn.Source.MemSub1
+                                .CellValue(.Rows, 5) = lConn.Source.MemSub2
+                                .CellValue(.Rows, 6) = lConn.MFact
+                                .CellValue(.Rows, 7) = lConn.Tran
+                                .CellValue(.Rows, 8) = lOper.Name
+                                .CellValue(.Rows, 9) = lOper.Id
+                                .CellValue(.Rows, 10) = lConn.Target.Group
+                                .CellValue(.Rows, 11) = lConn.Target.Member
+                                .CellValue(.Rows, 12) = lConn.Target.MemSub1
+                                .CellValue(.Rows, 13) = lConn.Target.MemSub2
+                                .CellValue(.Rows, 14) = lConn.Comment
+
+                            End If
+                        Next
+                    Next
                 End If
             End With
             With grdEdit
