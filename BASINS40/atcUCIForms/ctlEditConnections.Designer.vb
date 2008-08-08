f42dcb719cf9a37eb799b7b98a4a7b6bcebda210
bgonzales <bgonzales@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-08-08 18:38:20 +0000

    ctlEditConnections - Added NETWORK and SCHEMATIC populate functions. Resized Connections form to fit more table data.

--- BASINS40/atcUCIForms/ctlEditConnections.Designer.vb
+++ BASINS40/atcUCIForms/ctlEditConnections.Designer.vb
@@ -19,9 +19,18 @@ Partial Class ctlEditConnections
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.grdEdit = New atcControls.atcGrid
         Me.txtDefine = New System.Windows.Forms.TextBox
+        Me.grdEdit = New atcControls.atcGrid
         Me.SuspendLayout()
+        '
+        'txtDefine
+        '
+        Me.txtDefine.Location = New System.Drawing.Point(0, 244)
+        Me.txtDefine.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtDefine.Multiline = True
+        Me.txtDefine.Name = "txtDefine"
+        Me.txtDefine.Size = New System.Drawing.Size(715, 54)
+        Me.txtDefine.TabIndex = 1
         '
         'grdEdit
         '
@@ -31,27 +40,21 @@ Partial Class ctlEditConnections
         Me.grdEdit.LineColor = System.Drawing.Color.Empty
         Me.grdEdit.LineWidth = 0.0!
         Me.grdEdit.Location = New System.Drawing.Point(0, 2)
+        Me.grdEdit.Margin = New System.Windows.Forms.Padding(2)
         Me.grdEdit.Name = "grdEdit"
-        Me.grdEdit.Size = New System.Drawing.Size(767, 191)
+        Me.grdEdit.Size = New System.Drawing.Size(700, 238)
         Me.grdEdit.Source = Nothing
         Me.grdEdit.TabIndex = 0
         '
-        'txtDefine
-        '
-        Me.txtDefine.Location = New System.Drawing.Point(0, 199)
-        Me.txtDefine.Multiline = True
-        Me.txtDefine.Name = "txtDefine"
-        Me.txtDefine.Size = New System.Drawing.Size(768, 66)
-        Me.txtDefine.TabIndex = 1
-        '
         'ctlEditConnections
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.txtDefine)
         Me.Controls.Add(Me.grdEdit)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "ctlEditConnections"
-        Me.Size = New System.Drawing.Size(768, 265)
+        Me.Size = New System.Drawing.Size(715, 300)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
