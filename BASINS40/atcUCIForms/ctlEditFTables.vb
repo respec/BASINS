8009dff99642670929ade3e5f7ae832a67ca32b2
bgonzales <bgonzales@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-09-02 23:18:06 +0000

    ctlEditFTables.vb - Modified call to SubForms frmXSect.vb - Added Open File dialog and other form controls.

--- BASINS40/atcUCIForms/ctlEditFTables.vb
+++ BASINS40/atcUCIForms/ctlEditFTables.vb
@@ -267,8 +267,8 @@ Public Class ctlEditFTables
     Private Sub cmdImport_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdImport.Click
         Dim frmXSect As New frmXSect
         'frmXSect.CurrentReach(pHspfFtable.Operation.Id, pHspfFtable.Operation.FTable)
-        txtNRows.Value = pHspfFtable.Nrows
-        txtNCols.Value = pHspfFtable.Ncols
+        'txtNRows.Value = pHspfFtable.Nrows
+        'txtNCols.Value = pHspfFtable.Ncols
         frmXSect.Init(Me)
         frmXSect.Show()
     End Sub
