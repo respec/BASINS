9bd973024a519300318ba8b537ee0ae64c4c39ba
bgonzales <bgonzales@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-09-17 19:21:28 +0000

    frmPwatEdit - Enabled all functions and made form non-resizable.

--- BASINS40/atcUCIForms/frmPwatEdit.vb
+++ BASINS40/atcUCIForms/frmPwatEdit.vb
@@ -1,41 +1,27 @@
-Imports System.Drawing
-Imports MapWinUtility
 Imports atcUCI
-Imports atcUtility
-Imports atcSegmentation
 Imports atcControls
-Imports System.Collections.ObjectModel
-Imports System.IO
 Imports System
 
 Public Class frmPwatEdit
 
-    'Private pParent As Windows.Forms.Form
-    Dim myTable As HspfTable
-    Dim cVals As New ArrayList
+    Dim lMyTable As HspfTable
+    Dim lOptionVals As New ArrayList
+    Dim lIdList As New ArrayList
     Dim initing As Boolean
-
-
-    Private Sub chkHigh_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkHigh.Click
-        'StoreChanges(cmbLand.ListIndex)
-    End Sub
-
-
-    Private Sub chkSnow_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkSnow.Click
-        'StoreChanges(cmbLand.ListIndex)
-    End Sub
-
+    Dim lPrevCboLandIndex As Integer
 
     Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboLand.SelectedIndexChanged
-        Dim j As Integer
+        Dim lOper As Integer
+
+        StoreChanges(lPrevCboLandIndex)
         initing = True
         'after user changed combo list, use to refresh values
-        If chkAssign.Checked <> True Then
+        If chkAssign.Checked = False Then
             'assign to all is not checked
-            j = cboLand.SelectedIndex * myTable.Parms.Count
+            lOper = cboLand.SelectedIndex * lMyTable.Parms.Count
 
             'CSNOFG
-            If cVals(j) = 1 Then
+            If lOptionVals(lOper) = 1 Then
                 chkSnow.Checked = True
             Else
                 chkSnow.Checked = False
@@ -43,137 +29,119 @@ Public Class frmPwatEdit
 
 
             'RTOPFG
-            If cVals(j + 1) = 1 Then
+            If lOptionVals(lOper + 1) = 1 Then
                 radio1n1.Checked = True
-            ElseIf cVals(j + 1) = 0 Then
+            ElseIf lOptionVals(lOper + 1) = 0 Then
                 radio1n2.Checked = True
-            ElseIf cVals(j + 1) = 2 Then
+            ElseIf lOptionVals(lOper + 1) = 2 Then
                 radio1n3.Checked = True
-            ElseIf cVals(j + 1) = 3 Then
+            ElseIf lOptionVals(lOper + 1) = 3 Then
                 radio1n4.Checked = True
             End If
 
             'UZFG
-            If cVals(j + 2) = 1 Then
+            If lOptionVals(lOper + 2) = 1 Then
                 radio2n1.Checked = True
             Else
                 radio2n2.Checked = True
             End If
 
             'VCSFG
-            If cVals(j + 3) = 1 Then
+            If lOptionVals(lOper + 3) = 1 Then
                 radio3n2.Checked = True
             Else
                 radio3n1.Checked = True
             End If
 
             'VUZFG
-            If cVals(j + 4) = 1 Then
+            If lOptionVals(lOper + 4) = 1 Then
                 radio4n2.Checked = True
             Else
                 radio4n1.Checked = True
             End If
 
             'VMNFG
-            If cVals(j + 5) = 1 Then
+            If lOptionVals(lOper + 5) = 1 Then
                 radio5n2.Checked = True
             Else
                 radio5n1.Checked = True
             End If
 
             'VIFWFG
-            If cVals(j + 6) = 1 Then
+            If lOptionVals(lOper + 6) = 1 Then
                 radio6n2.Checked = True
             Else
                 radio6n1.Checked = True
             End If
 
             'VIRCFG
-            If cVals(j + 7) = 1 Then
+            If lOptionVals(lOper + 7) = 1 Then
                 radio7n2.Checked = True
             Else
                 radio7n1.Checked = True
             End If
 
             'VLEFG
-            If cVals(j + 8) = 1 Then
+            If lOptionVals(lOper + 8) = 1 Then
                 radio8n2.Checked = True
             Else
                 radio8n1.Checked = True
             End If
 
             'IFFCFG
-            If cVals(j + 9) = 1 Then
+            If lOptionVals(lOper + 9) = 1 Then
                 radio9n1.Checked = True
             Else
                 radio9n2.Checked = True
             End If
 
             'HWTFG
-            If cVals(j + 10) = 1 Then
+            If lOptionVals(lOper + 10) = 1 Then
                 chkHigh.Checked = True
             Else
                 chkHigh.Checked = False
             End If
 
-
             'IRRGFG
-            If cVals(j + 11) = 0 Then
+            If lOptionVals(lOper + 11) = 0 Then
                 radio10n1.Checked = True
-            ElseIf cVals(j + 11) = 1 Then
+            ElseIf lOptionVals(lOper + 11) = 1 Then
                 radio10n2.Checked = True
-            ElseIf cVals(j + 11) = 2 Then
+            ElseIf lOptionVals(lOper + 11) = 2 Then
                 radio10n3.Checked = True
-            ElseIf cVals(j + 11) = 3 Then
+            ElseIf lOptionVals(lOper + 11) = 3 Then
                 radio10n4.Checked = True
             End If
 
         End If
-            initing = False
+        lPrevCboLandIndex = cboLand.SelectedIndex
+        initing = False
     End Sub
-
-    'Private Sub cmdCancel_Click()
-    '    Unload(Me)
-    'End Sub
-
-    'Private Sub cmdOK_Click()
-    '    Dim i&, istart&, iend&, j&
-
-    '    If chkAssign.Value = 1 Then
-    '        For j = 0 To cmbLand.ListCount - 1
-    '            StoreChanges(j)
-    '        Next j
-    '    Else
-    '        StoreChanges(cmbLand.ListIndex)
-    '    End If
-
-    '    For j = 0 To cmbLand.ListCount - 1
-    '        SaveChanges(j)
-    '    Next j
-    '    Unload(Me)
-    'End Sub
 
     Public Sub Init(ByVal aParent As Object)
         'Me.Icon = aParent.ParentForm.Icon
         Me.Text = "PWATER Simulation and Input Options"
-        myTable = aParent
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedSingle
+        lMyTable = aParent
 
-
-        Dim vOpn As Object
+        Dim lVOpn As Object
         Dim lOpn As HspfOperation
-        Dim j As Integer
+        Dim lOper As Integer
         Dim tempTable As HspfTable
 
         cboLand.Items.Clear()
+        cboLand.Cursor = Windows.Forms.Cursors.Hand
+        cboLand.DropDownStyle = Windows.Forms.ComboBoxStyle.DropDownList
+
         initing = True
-        For Each vOpn In myTable.Opn.Uci.OpnBlks("PERLND").Ids
-            lOpn = vOpn
+        For Each lVOpn In lMyTable.Opn.Uci.OpnBlks("PERLND").Ids
+            lOpn = lVOpn
             cboLand.Items.Add(lOpn.Description & " (" & lOpn.Id & ")")
-            cboLand.ValueMember = lOpn.Id
+            lIdList.Add(lOpn.Id)
             'save a local copy of the values
-            tempTable = lOpn.Tables(myTable.Name)
-            For j = 0 To tempTable.Parms.Count - 1
-                cVals.Add(tempTable.Parms(j).Value)
+            tempTable = lOpn.Tables(lMyTable.Name)
+            For lOper = 0 To tempTable.Parms.Count - 1
+                lOptionVals.Add(tempTable.Parms(lOper).Value)
             Next
         Next
 
@@ -181,89 +149,144 @@ Public Class frmPwatEdit
         initing = False
     End Sub
 
-    'Private Sub SaveChanges(ByVal ioper&)
-    '    Dim i As Long
-    '    Dim j As Long
-    '    Dim lOpn As HspfOperation
-    '    Dim ltable As HspfTable
+    Private Sub SaveChanges(ByVal lSaveIndex As Integer)
+        Dim lOper1 As Integer
+        Dim lOper2 As Integer
+        Dim lOpn As HspfOperation
+        Dim lTable As HspfTable
 
-    '    j = ioper * myTable.Parms.Count
-    '    lOpn = myTable.Opn.Uci.OpnBlks("PERLND").OperFromID(cmbLand.ItemData(ioper))
-    '    ltable = lOpn.Tables(myTable.Name)
+        lOper2 = lSaveIndex * lMyTable.Parms.Count
 
-    '    For i = 1 To myTable.Parms.Count
-    '        ltable.Parms(i).Value = cVals(j + i)
-    '    Next i
+        lOpn = lMyTable.Opn.Uci.OpnBlks("PERLND").OperFromID(lIdList(lSaveIndex))
+        lTable = lOpn.Tables(lMyTable.Name)
 
-    'End Sub
+        For lOper1 = 0 To lMyTable.Parms.Count - 1
+            lTable.Parms(lOper1).Value = lOptionVals(lOper2 + lOper1)
+        Next
 
-    Private Sub StoreChanges(ByVal ioper As Long)
-        Dim i As Long
-        Dim j As Long
-        i += 1
+    End Sub
+
+    Private Sub StoreChanges(ByVal lStoreIndex As Long)
+        Dim lOper As Integer
+
         If Not initing Then
-            j = ioper * myTable.Parms.Count
+            lOper = lStoreIndex * lMyTable.Parms.Count
 
-            cVals(j + 1) = chkSnow.Checked
-            If radio1n1.Checked = True Then
-                cVals(j + 1) = 1
-                'ElseIf opt2(0).Value = True Then
-                '    cVals(j + 2) = 0
-                'ElseIf opt3(0).Value = True Then
-                '    cVals(j + 2) = 2
-                'ElseIf opt4(0).Value = True Then
-                '    cVals(j + 2) = 3
+            'CSNOFG
+            If chkSnow.Checked = True Then
+                lOptionVals(lOper) = 1
+            Else
+                lOptionVals(lOper) = 0
             End If
-            '    If opt1(1).Value = True Then
-            '        cVals(j + 3) = 1
-            '    Else
-            '        cVals(j + 3) = 0
-            '    End If
-            '    For i = 4 To 9
-            '        If opt2(i - 2).Value = True Then
-            '            cVals(j + i) = 1
-            '        Else
-            '            cVals(j + i) = 0
-            '        End If
-            '    Next i
-            '    If opt1(8).Value = True Then
-            '        cVals(j + 10) = 1
-            '    Else
-            '        cVals(j + 10) = 2
-            '    End If
-            '    cVals(j + 11) = chkHigh.Value
-            '    If opt1(10).Value = True Then
-            '        cVals(j + 12) = 0
-            '    ElseIf opt2(10).Value = True Then
-            '        cVals(j + 12) = 1
-            '    ElseIf opt3(1).Value = True Then
-            '        cVals(j + 12) = 2
-            '    ElseIf opt4(1).Value = True Then
-            '        cVals(j + 12) = 3
-            '    End If
+
+
+            'RTOPFG
+            If radio1n1.Checked = True Then
+                lOptionVals(lOper + 1) = 1
+            ElseIf radio1n2.Checked = True Then
+                lOptionVals(lOper + 1) = 0
+            ElseIf radio1n3.Checked = True Then
+                lOptionVals(lOper + 1) = 2
+            ElseIf radio1n4.Checked = True Then
+                lOptionVals(lOper + 1) = 3
+            End If
+
+            'UZFG
+            If radio2n1.Checked = True Then
+                lOptionVals(lOper + 2) = 1
+            Else
+                lOptionVals(lOper + 2) = 0
+            End If
+
+            'VCSFG
+            If radio3n1.Checked = True Then
+                lOptionVals(lOper + 3) = 0
+            Else
+                lOptionVals(lOper + 3) = 1
+            End If
+
+            'VUZFG
+            If radio4n1.Checked = True Then
+                lOptionVals(lOper + 4) = 0
+            Else
+                lOptionVals(lOper + 4) = 1
+            End If
+
+            'VMNFG
+            If radio5n1.Checked = True Then
+                lOptionVals(lOper + 5) = 0
+            Else
+                lOptionVals(lOper + 5) = 1
+            End If
+
+            'VIFWFG
+            If radio6n1.Checked = True Then
+                lOptionVals(lOper + 6) = 0
+            Else
+                lOptionVals(lOper + 6) = 1
+            End If
+
+            'VIRCFG
+            If radio7n1.Checked = True Then
+                lOptionVals(lOper + 7) = 0
+            Else
+                lOptionVals(lOper + 7) = 1
+            End If
+
+            'VLEFG
+            If radio8n1.Checked = True Then
+                lOptionVals(lOper + 8) = 0
+            Else
+                lOptionVals(lOper + 8) = 1
+            End If
+
+            'IFFCFG
+            If radio9n2.Checked = True Then
+                lOptionVals(lOper + 9) = 0
+            Else
+                lOptionVals(lOper + 9) = 1
+            End If
+
+            'HWTFG
+            If chkHigh.Checked = True Then
+                lOptionVals(lOper + 10) = 1
+            Else
+                lOptionVals(lOper + 10) = 0
+            End If
+
+            'IRRGFG
+            If radio10n1.Checked = True Then
+                lOptionVals(lOper + 11) = 0
+            ElseIf radio10n2.Checked = True Then
+                lOptionVals(lOper + 11) = 1
+            ElseIf radio10n3.Checked = True Then
+                lOptionVals(lOper + 11) = 2
+            ElseIf radio10n4.Checked = True Then
+                lOptionVals(lOper + 11) = 3
+            End If
+
         End If
     End Sub
 
-    'Private Sub opt1_Click(ByVal Index As Integer)
-    '    StoreChanges(cmbLand.ListIndex)
-    'End Sub
-
-    'Private Sub opt2_Click(ByVal Index As Integer)
-    '    StoreChanges(cmbLand.ListIndex)
-    'End Sub
-
-    'Private Sub opt3_Click(ByVal Index As Integer)
-    '    StoreChanges(cmbLand.ListIndex)
-    'End Sub
-
-    'Private Sub opt4_Click(ByVal Index As Integer)
-    '    StoreChanges(cmbLand.ListIndex)
-    'End Sub
-
-
-
     Private Sub cmdOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdOK.Click
+        Dim lOper As Integer
 
+        If chkAssign.Checked = True Then
+            For lOper = 0 To cboLand.Items.Count - 1
+                StoreChanges(lOper)
+            Next
+        Else
+            StoreChanges(cboLand.SelectedIndex)
+        End If
+
+        For lOper = 0 To cboLand.Items.Count - 1
+            SaveChanges(lOper)
+        Next
+
+        Me.Close()
     End Sub
 
+    Private Sub cmdCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCancel.Click
+        Me.Close()
+    End Sub
 End Class
