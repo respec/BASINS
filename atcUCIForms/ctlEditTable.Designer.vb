859c1b63396dd35e720522681e38b74d0e200b81
bgonzales <bgonzales@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2008-09-22 23:07:55 +0000

    ctleditTable.vb - Began populating the table. More to do.

--- atcUCIForms/ctlEditTable.Designer.vb
+++ atcUCIForms/ctlEditTable.Designer.vb
@@ -19,16 +19,96 @@ Partial Class ctlEditTable
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtDefine = New System.Windows.Forms.TextBox
+        Me.chkDesc = New System.Windows.Forms.CheckBox
+        Me.cboOccur = New System.Windows.Forms.ComboBox
+        Me.lblOccur = New System.Windows.Forms.Label
+        Me.grdTable = New atcControls.atcGrid
+        Me.Button1 = New System.Windows.Forms.Button
         Me.SuspendLayout()
         '
-        'UserControl1
+        'txtDefine
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.txtDefine.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.txtDefine.BackColor = System.Drawing.SystemColors.Control
+        Me.txtDefine.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDefine.Location = New System.Drawing.Point(8, 229)
+        Me.txtDefine.Multiline = True
+        Me.txtDefine.Name = "txtDefine"
+        Me.txtDefine.Size = New System.Drawing.Size(662, 111)
+        Me.txtDefine.TabIndex = 0
+        '
+        'chkDesc
+        '
+        Me.chkDesc.AutoSize = True
+        Me.chkDesc.Location = New System.Drawing.Point(13, 10)
+        Me.chkDesc.Name = "chkDesc"
+        Me.chkDesc.Size = New System.Drawing.Size(107, 17)
+        Me.chkDesc.TabIndex = 2
+        Me.chkDesc.Text = "Show description"
+        Me.chkDesc.UseVisualStyleBackColor = True
+        '
+        'cboOccur
+        '
+        Me.cboOccur.FormattingEnabled = True
+        Me.cboOccur.Location = New System.Drawing.Point(213, 8)
+        Me.cboOccur.Name = "cboOccur"
+        Me.cboOccur.Size = New System.Drawing.Size(215, 21)
+        Me.cboOccur.TabIndex = 3
+        '
+        'lblOccur
+        '
+        Me.lblOccur.AutoSize = True
+        Me.lblOccur.Location = New System.Drawing.Point(158, 12)
+        Me.lblOccur.Name = "lblOccur"
+        Me.lblOccur.Size = New System.Drawing.Size(39, 13)
+        Me.lblOccur.TabIndex = 4
+        Me.lblOccur.Text = "Label1"
+        '
+        'grdTable
+        '
+        Me.grdTable.AllowHorizontalScrolling = True
+        Me.grdTable.AllowNewValidValues = False
+        Me.grdTable.CellBackColor = System.Drawing.Color.Empty
+        Me.grdTable.LineColor = System.Drawing.Color.Empty
+        Me.grdTable.LineWidth = 0.0!
+        Me.grdTable.Location = New System.Drawing.Point(31, 60)
+        Me.grdTable.Name = "grdTable"
+        Me.grdTable.Size = New System.Drawing.Size(616, 152)
+        Me.grdTable.Source = Nothing
+        Me.grdTable.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(485, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 18)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ctlEditTable
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Name = "UserControl1"
-        Me.Size = New System.Drawing.Size(587, 292)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.grdTable)
+        Me.Controls.Add(Me.lblOccur)
+        Me.Controls.Add(Me.cboOccur)
+        Me.Controls.Add(Me.chkDesc)
+        Me.Controls.Add(Me.txtDefine)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "ctlEditTable"
+        Me.Size = New System.Drawing.Size(686, 346)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents txtDefine As System.Windows.Forms.TextBox
+    Friend WithEvents chkDesc As System.Windows.Forms.CheckBox
+    Friend WithEvents cboOccur As System.Windows.Forms.ComboBox
+    Friend WithEvents lblOccur As System.Windows.Forms.Label
+    Friend WithEvents grdTable As atcControls.atcGrid
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- atcUCIForms/ctlEditTable.vb
+++ atcUCIForms/ctlEditTable.vb
@@ -36,10 +36,168 @@ Public Class ctlEditTable
         Get
             Return pHspfTable
         End Get
+
         Set(ByVal aHspfTable As Object)
+
+            With grdTable
+                .Source = New atcControls.atcGridSource
+                .Clear()
+                .AllowHorizontalScrolling = True
+                .AllowNewValidValues = True
+                .Visible = True
+            End With
+
             pHspfTable = aHspfTable
+            Dim i, loi As Long
+            Dim ctemp As String
+
+            If pHspfTable.Def.NumOccur > 1 Then
+                cboOccur.Items.Clear()
+
+                For i = 1 To pHspfTable.OccurCount 'how about later ones?
+                    If pHspfTable.Def.OccurGroup > 0 Then
+                        'this is part of an occurance group, add name of occurance to combo box
+                        ctemp = ""
+                        If pHspfTable.Opn.Name = "PERLND" Or pHspfTable.Opn.Name = "IMPLND" Then
+                            ctemp = "QUAL-PROPS"
+                        ElseIf pHspfTable.Opn.Name = "RCHRES" Then
+                            ctemp = "GQ-QALDATA"
+                        End If
+                        If i > 1 Then
+                            loi = pHspfTable.Opn.OpnBlk.Ids(1).Tables(pHspfTable.Name & ":" & i).OccurIndex
+                        Else
+                            loi = pHspfTable.Opn.OpnBlk.Ids(1).Tables(pHspfTable.Name).OccurIndex
+                        End If
+                        If loi = 0 Then
+                            loi = i
+                        End If
+                        If loi > 1 Then
+                            ctemp = ctemp & ":" & loi
+                        End If
+                        If pHspfTable.Opn.OpnBlk.Ids(1).TableExists(ctemp) Then
+                            cboOccur.Items.Add(i & " - " & pHspfTable.Opn.OpnBlk.Ids(1).Tables(ctemp).Parms(1).Value)
+                        Else
+                            cboOccur.Items.Add(i)
+                        End If
+                    Else
+                        cboOccur.Items.Add(i)
+                    End If
+                Next i
+                cboOccur.SelectedIndex = pHspfTable.OccurNum - 1
+                lblOccur.Visible = True
+                cboOccur.Visible = True
+            Else
+                lblOccur.Visible = False
+                cboOccur.Visible = False
+            End If
+
+
+
+            If Len(pHspfTable.Opn.Description) > 0 And pHspfTable.Name <> "GEN-INFO" Then
+                chkDesc.Checked = True
+                refreshGrid()
+            Else
+                chkDesc.Checked = False
+                chkDesc.Visible = False
+                'grdTable.Height = grdTable.Top - chkDesc.Top + grdTable.Height
+                'grdTable.Top = chkDesc.Top
+                refreshGrid()
+            End If
         End Set
     End Property
+
+    Private Sub refreshGrid()
+        Dim lParm As HspfParm
+        Dim ltable As HspfTable = Nothing
+        Dim tname As String = Nothing
+        Dim unitfg As String = Nothing
+        'Dim more, skip As Boolean
+        Dim i, lchkDescInteger As Integer
+
+        With grdTable.Source
+            .Columns = pHspfTable.Parms.Count + 1
+            .CellValue(0, 0) = "OpNum"
+            If chkDesc.Checked = True Then
+                .CellValue(0, 1) = "Description"
+                lchkDescInteger = 1
+            Else
+                .Columns += 1
+                lchkDescInteger = 0
+            End If
+
+            For i = 0 To pHspfTable.Parms.Count - 1
+                lParm = pHspfTable.Parms(i)
+                .CellValue(0, i + lchkDescInteger + 1) = lParm.Name
+                If lParm.Def.Typ = 2 Then
+                    '.ColType(i + lchkDescInteger) = ATCoSng  'causes formatting problems
+                End If
+                If lParm.Def.Typ = 1 Then
+                    '.ColType(i + lchkDescInteger) = ATCoInt0
+                End If
+                If lParm.Def.Typ = 0 Then
+                    '.ColType(i + lchkDescInteger) = ATCoTxt
+                End If
+                unitfg = pHspfTable.Opn.OpnBlk.Uci.GlobalBlock.EmFg
+                If unitfg = 1 Then 'english
+                    '.ColMax(i + lchkDescInteger) = lParm.Def.Max
+                    '.ColMin(i + lchkDescInteger) = lParm.Def.Min
+                ElseIf unitfg = 2 Then 'metric
+                    '.ColMax(i + lchkDescInteger) = lParm.Def.MetricMax
+                    '.ColMin(i + lchkDescInteger) = lParm.Def.MetricMin
+                End If
+            Next i
+
+            'may need index here
+            'tname = pHspfTable.Name
+            'If pHspfTable.OccurCount > 1 Then
+            '    If cboOccur.SelectedIndex > 0 Then
+            '        tname = tname & ":" & cboOccur.SelectedIndex + 1
+            '    End If
+            'End If
+
+            'more = True
+            '.Rows = 0
+            'j = 1
+            'Do While more = True
+            '    If pHspfTable.EditAllSimilar Then
+            '        If pHspfTable.Opn.OpnBlk.NthOper(j).TableExists(tname) Then
+            '            ltable = pHspfTable.Opn.OpnBlk.NthOper(j).Tables(tname)
+            '            skip = False
+            '        Else
+            '            skip = True
+            '        End If
+            '        j = j + 1
+            '        If j > pHspfTable.Opn.OpnBlk.Ids.Count Then
+            '            more = False
+            '        End If
+            '    ElseIf pHspfTable.OccurCount > 1 Then
+            '        ltable = pHspfTable.Opn.OpnBlk.OperFromID(pHspfTable.Opn.Id).Tables(tname)
+            '        skip = False
+            '        more = False
+            '    Else
+            '        ltable = pHspfTable
+            '        skip = False
+            '        more = False
+            '    End If
+
+            '    If skip = False Then
+            '        .Rows = .Rows + 1
+            '        .CellValue(.Rows, 0) = ltable.Opn.Id
+            '        If chkDesc.Checked = False Then
+            '            .CellValue(.Rows, 1) = ltable.Opn.Description
+            '        End If
+            '        For i = 1 To pHspfTable.Parms.Count
+            '            .CellValue(.Rows, i + lchkDescInteger) = ltable.Parms(i).Value
+            '        Next i
+            '    End If
+            'Loop
+
+        End With
+
+        grdTable.SizeAllColumnsToContents()
+        grdTable.Refresh()
+
+    End Sub
 
     Public Sub Help() Implements ctlEdit.Help
         'TODO: add this code
@@ -60,5 +218,9 @@ Public Class ctlEditTable
 
         ' Add any initialization after the InitializeComponent() call.
         Data = aHspfTable
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        refreshGrid()
     End Sub
 End Class
