b2be4ef17d9d6d8aaa35ba3862638e8ef25baa88
Jack Kittle <jack@gkfamily.org>
2008-10-14 19:15:27 +0000

    atcUCIEditor - new project to support batch editing of HSPF UCI files

---
+++ atcUCIEditor/modUCIEditor.vb
@@ -0,0 +1,27 @@
+
+Imports MapWinUtility
+Imports atcUtility
+Imports atcData
+Imports atcWDM
+Imports System.Collections.Specialized
+
+Module modUCIEditor
+    Private pBaseDrive As String = "d:\"
+    Private pBaseDir As String = pBaseDrive & "mono_luChange\"
+    Private pOutputDir As String = pBaseDir & "output\"
+    Private pScenarioName As String = "base"
+
+    Sub main()
+        Logger.StartToFile(pOutputDir & "UCIEditor.log")
+        ChDriveDir(pBaseDir)
+        Logger.Dbg("BaseDir " & CurDir())
+
+        Dim lMsg As New atcUCI.HspfMsg("hspfmsg.mdb")
+        Dim lUci As New atcUCI.HspfUci
+        lUci.FastReadUciForStarter(lMsg, pScenarioName & ".uci")
+
+        lUci.Name = "LU_Ch.uci"
+        lUci.Save()
+
+    End Sub
+End Module
