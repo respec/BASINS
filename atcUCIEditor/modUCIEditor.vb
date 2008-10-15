da82747ec3abeec706f0426e00ff08c9b3226777
tong.zhai <tong.zhai@respec.com>
2008-10-15 16:10:50 +0000

    modUCIEditor.vb - add initialize routine

--- atcUCIEditor/modUCIEditor.vb
+++ atcUCIEditor/modUCIEditor.vb
@@ -6,12 +6,35 @@ Imports atcWDM
 Imports System.Collections.Specialized
 
 Module modUCIEditor
-    Private pBaseDrive As String = "d:\"
-    Private pBaseDir As String = pBaseDrive & "mono_luChange\"
-    Private pOutputDir As String = pBaseDir & "output\"
-    Private pScenarioName As String = "base"
+    Private pBaseDrive As String = "c:\"
+    'Private pScenario As String = "base"
+    Private pScenario As String = "hspf12"
+    Private pBaseDir As String
+    Private pOutputDir As String
+    Private pScenarioName As String
+    Private pScenarioNameNew As String
 
+    Sub Initialize()
+        Select Case pScenario
+            Case "mono"
+                pBaseDir = pBaseDrive & "mono_luChange\"
+                pOutputDir = pBaseDir & "output\"
+                pScenarioName = "base"
+                pScenarioNameNew = "LU_Ch"
+            Case "hspf10"
+                pBaseDir = pBaseDrive & "test\HSPF\"
+                pOutputDir = pBaseDir & "output\"
+                pScenarioName = "test10"
+                pScenarioNameNew = "test10Rev"
+            Case "hspf12"
+                pBaseDir = pBaseDrive & "test\HSPF\"
+                pOutputDir = pBaseDir & "output\"
+                pScenarioName = "test12"
+                pScenarioNameNew = "test12Rev"
+        End Select
+    End Sub
     Sub main()
+        Initialize()
         Logger.StartToFile(pOutputDir & "UCIEditor.log")
         ChDriveDir(pBaseDir)
         Logger.Dbg("BaseDir " & CurDir())
@@ -21,7 +44,9 @@ Module modUCIEditor
         lUci.FastReadUciForStarter(lMsg, pScenarioName & ".uci")
         Logger.Dbg("UCI " & lUci.Name & " Opened")
 
-        lUci.Name = "LU_Ch.uci"
+        'add changes to scematic block here
+
+        lUci.Name = pScenarioNameNew & ".uci"
         lUci.Save()
         Logger.Dbg("UCI " & lUci.Name & " Saved")
 
