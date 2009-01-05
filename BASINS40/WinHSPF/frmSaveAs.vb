e99229a6e9b78d61314df6480c30c88c367d2086
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2009-01-05 21:49:59 +0000

    frmSaveAs -- begin implementing SaveAs

--- BASINS40/WinHSPF/frmSaveAs.vb
+++ BASINS40/WinHSPF/frmSaveAs.vb
@@ -1,4 +1,9 @@
+Imports atcUtility
+Imports MapWinUtility
+
 Public Class frmSaveAs
+
+    Dim pCurrentDirectory As String = ""
 
     Public Sub New()
 
@@ -9,6 +14,10 @@ Public Class frmSaveAs
         Me.MinimumSize = Me.Size
         Me.MaximumSize = Me.Size
 
+        pCurrentDirectory = PathNameOnly(AbsolutePath(pUCI.Name, CurDir))
+        atxName.Text = FilenameNoExt(pUCI.Name)
+        'HSPFMain.newname = ""
+
     End Sub
 
     Private Sub cmdCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCancel.Click
@@ -16,7 +25,43 @@ Public Class frmSaveAs
     End Sub
 
     Private Sub cmdOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdOK.Click
+        'HSPFMain.newname = txtPath
+        'If optRelAbs(0).Value Then
+        '    HSPFMain.relabs = 1
+        'Else
+        '    HSPFMain.relabs = 2
+        'End If
+        'HSPFMain.basedsn = ATCoTextBase.Value
+
+        '    myUci.Name = newname
+        '    If Len(myUci.Name) > 3 Then
+        '        If UCase(Right(myUci.Name, 4)) <> ".UCI" Then  'force to have uci extension
+        '            myUci.Name = myUci.Name & ".uci"
+        '        End If
+        '    End If
+        '    newname = FilenameOnly(myUci.Name)
+        '    myUci.SaveAs(oldname, newname, basedsn, relabs)
+        '    AddRecentFile(mnuRecent, myUci.Name)
+        '    Caption = BaseCaption & ": " & FilenameOnly(myUci.Name)
         Me.Dispose()
     End Sub
 
+    Private Sub cmdFile_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdFile.Click
+
+        SaveFileDialog1.Filter = "HSPF User Control Input Files (*.uci)|*.uci"
+        SaveFileDialog1.FileName = atxName.Text & ".uci"
+        SaveFileDialog1.InitialDirectory = CurDir()
+        SaveFileDialog1.Title = "Select Scenario Path"
+
+        If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            pCurrentDirectory = PathNameOnly(SaveFileDialog1.FileName)
+            atxName.Text = FilenameNoPath(FilenameNoExt(SaveFileDialog1.FileName))
+            atxName_Change()
+        End If
+    End Sub
+
+    Private Sub atxName_Change() Handles atxName.Change
+        If Len(atxName.Text) > 8 Then atxName.Text = atxName.Text.Substring(0, 8)
+        txtPath.Text = pCurrentDirectory & "\" & atxName.Text & ".uci"
+    End Sub
 End Class
