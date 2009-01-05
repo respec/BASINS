f59410affad7bb6ad2c97d0661c75e53c921724b
pdudda <PaulDudaRESPEC@users.noreply.github.com>
2009-01-05 21:49:59 +0000

    frmSaveAs -- begin implementing SaveAs

--- WinHSPF/frmSaveAs.Designer.vb
+++ WinHSPF/frmSaveAs.Designer.vb
@@ -25,120 +25,129 @@ Partial Class frmSaveAs
         Me.Label2 = New System.Windows.Forms.Label
         Me.Label3 = New System.Windows.Forms.Label
         Me.Label4 = New System.Windows.Forms.Label
-        Me.TextBox1 = New System.Windows.Forms.TextBox
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton
-        Me.TextBox2 = New System.Windows.Forms.TextBox
-        Me.AtcText1 = New atcControls.atcText
+        Me.rbnRelative = New System.Windows.Forms.RadioButton
+        Me.rbnAbsolute = New System.Windows.Forms.RadioButton
+        Me.txtPath = New System.Windows.Forms.TextBox
+        Me.atxName = New atcControls.atcText
         Me.cmdFile = New System.Windows.Forms.Button
         Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
         Me.cmdCancel = New System.Windows.Forms.Button
         Me.cmdOK = New System.Windows.Forms.Button
+        Me.atxBase = New atcControls.atcText
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Location = New System.Drawing.Point(16, 15)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(134, 13)
+        Me.Label1.Size = New System.Drawing.Size(177, 17)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "New Project (8 chars max):"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 47)
+        Me.Label2.Location = New System.Drawing.Point(16, 58)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.Size = New System.Drawing.Size(41, 17)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Path:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 77)
+        Me.Label3.Location = New System.Drawing.Point(16, 95)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(150, 13)
+        Me.Label3.Size = New System.Drawing.Size(198, 17)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Number Data Sets As Follows:"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(24, 107)
+        Me.Label4.Location = New System.Drawing.Point(32, 132)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(60, 13)
+        Me.Label4.Size = New System.Drawing.Size(77, 17)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Base DSN:"
         '
-        'TextBox1
+        'rbnRelative
         '
-        Me.TextBox1.Location = New System.Drawing.Point(94, 103)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(106, 20)
-        Me.TextBox1.TabIndex = 4
+        Me.rbnRelative.AutoSize = True
+        Me.rbnRelative.Location = New System.Drawing.Point(304, 118)
+        Me.rbnRelative.Margin = New System.Windows.Forms.Padding(4)
+        Me.rbnRelative.Name = "rbnRelative"
+        Me.rbnRelative.Size = New System.Drawing.Size(77, 21)
+        Me.rbnRelative.TabIndex = 5
+        Me.rbnRelative.Text = "Relative"
+        Me.rbnRelative.UseVisualStyleBackColor = True
         '
-        'RadioButton1
+        'rbnAbsolute
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(228, 96)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(64, 17)
-        Me.RadioButton1.TabIndex = 5
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "Relative"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.rbnAbsolute.AutoSize = True
+        Me.rbnAbsolute.Checked = True
+        Me.rbnAbsolute.Location = New System.Drawing.Point(304, 142)
+        Me.rbnAbsolute.Margin = New System.Windows.Forms.Padding(4)
+        Me.rbnAbsolute.Name = "rbnAbsolute"
+        Me.rbnAbsolute.Size = New System.Drawing.Size(81, 21)
+        Me.rbnAbsolute.TabIndex = 6
+        Me.rbnAbsolute.TabStop = True
+        Me.rbnAbsolute.Text = "Absolute"
+        Me.rbnAbsolute.UseVisualStyleBackColor = True
         '
-        'RadioButton2
+        'txtPath
         '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(228, 115)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(66, 17)
-        Me.RadioButton2.TabIndex = 6
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "Absolute"
-        Me.RadioButton2.UseVisualStyleBackColor = True
+        Me.txtPath.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtPath.Location = New System.Drawing.Point(69, 50)
+        Me.txtPath.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(407, 22)
+        Me.txtPath.TabIndex = 7
         '
-        'TextBox2
+        'atxName
         '
-        Me.TextBox2.Location = New System.Drawing.Point(52, 41)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(306, 20)
-        Me.TextBox2.TabIndex = 7
-        '
-        'AtcText1
-        '
-        Me.AtcText1.Alignment = System.Windows.Forms.HorizontalAlignment.Left
-        Me.AtcText1.DataType = atcControls.atcText.ATCoDataType.ATCoTxt
-        Me.AtcText1.DefaultValue = ""
-        Me.AtcText1.HardMax = -999
-        Me.AtcText1.HardMin = -999
-        Me.AtcText1.InsideLimitsBackground = System.Drawing.Color.White
-        Me.AtcText1.Location = New System.Drawing.Point(152, 9)
-        Me.AtcText1.MaxWidth = 12
-        Me.AtcText1.Name = "AtcText1"
-        Me.AtcText1.NumericFormat = "0.#####"
-        Me.AtcText1.OutsideHardLimitBackground = System.Drawing.Color.Coral
-        Me.AtcText1.OutsideSoftLimitBackground = System.Drawing.Color.Yellow
-        Me.AtcText1.SelLength = 1
-        Me.AtcText1.SelStart = 0
-        Me.AtcText1.Size = New System.Drawing.Size(89, 27)
-        Me.AtcText1.SoftMax = -999
-        Me.AtcText1.SoftMin = -999
-        Me.AtcText1.TabIndex = 8
-        Me.AtcText1.ValueDouble = 0
-        Me.AtcText1.ValueInteger = 0
+        Me.atxName.Alignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.atxName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.atxName.DataType = atcControls.atcText.ATCoDataType.ATCoTxt
+        Me.atxName.DefaultValue = " "
+        Me.atxName.HardMax = -999
+        Me.atxName.HardMin = -999
+        Me.atxName.InsideLimitsBackground = System.Drawing.Color.White
+        Me.atxName.Location = New System.Drawing.Point(203, 11)
+        Me.atxName.Margin = New System.Windows.Forms.Padding(4)
+        Me.atxName.MaxWidth = 12
+        Me.atxName.Name = "atxName"
+        Me.atxName.NumericFormat = "0.#####"
+        Me.atxName.OutsideHardLimitBackground = System.Drawing.Color.Coral
+        Me.atxName.OutsideSoftLimitBackground = System.Drawing.Color.Yellow
+        Me.atxName.SelLength = 1
+        Me.atxName.SelStart = 0
+        Me.atxName.Size = New System.Drawing.Size(119, 33)
+        Me.atxName.SoftMax = -999
+        Me.atxName.SoftMin = -999
+        Me.atxName.TabIndex = 8
+        Me.atxName.ValueDouble = 1
+        Me.atxName.ValueInteger = 1
         '
         'cmdFile
         '
+        Me.cmdFile.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.cmdFile.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.cmdFile.ImageIndex = 0
         Me.cmdFile.ImageList = Me.ImageList1
-        Me.cmdFile.Location = New System.Drawing.Point(288, 5)
+        Me.cmdFile.Location = New System.Drawing.Point(384, 6)
+        Me.cmdFile.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdFile.Name = "cmdFile"
-        Me.cmdFile.Size = New System.Drawing.Size(70, 26)
+        Me.cmdFile.Size = New System.Drawing.Size(93, 32)
         Me.cmdFile.TabIndex = 9
         Me.cmdFile.Text = "Browse"
         Me.cmdFile.TextAlign = System.Drawing.ContentAlignment.MiddleRight
@@ -153,9 +162,10 @@ Partial Class frmSaveAs
         'cmdCancel
         '
         Me.cmdCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.cmdCancel.Location = New System.Drawing.Point(193, 144)
+        Me.cmdCancel.Location = New System.Drawing.Point(257, 177)
+        Me.cmdCancel.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdCancel.Name = "cmdCancel"
-        Me.cmdCancel.Size = New System.Drawing.Size(101, 26)
+        Me.cmdCancel.Size = New System.Drawing.Size(135, 32)
         Me.cmdCancel.TabIndex = 22
         Me.cmdCancel.Text = "&Cancel"
         Me.cmdCancel.UseVisualStyleBackColor = True
@@ -163,30 +173,56 @@ Partial Class frmSaveAs
         'cmdOK
         '
         Me.cmdOK.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.cmdOK.Location = New System.Drawing.Point(84, 144)
+        Me.cmdOK.Location = New System.Drawing.Point(112, 177)
+        Me.cmdOK.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdOK.Name = "cmdOK"
-        Me.cmdOK.Size = New System.Drawing.Size(101, 26)
+        Me.cmdOK.Size = New System.Drawing.Size(135, 32)
         Me.cmdOK.TabIndex = 21
         Me.cmdOK.Text = "&OK"
         Me.cmdOK.UseVisualStyleBackColor = True
         '
+        'atxBase
+        '
+        Me.atxBase.Alignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.atxBase.DataType = atcControls.atcText.ATCoDataType.ATCoInt
+        Me.atxBase.DefaultValue = " 1000"
+        Me.atxBase.HardMax = 32000
+        Me.atxBase.HardMin = 1
+        Me.atxBase.InsideLimitsBackground = System.Drawing.Color.White
+        Me.atxBase.Location = New System.Drawing.Point(117, 130)
+        Me.atxBase.Margin = New System.Windows.Forms.Padding(4)
+        Me.atxBase.MaxWidth = 12
+        Me.atxBase.Name = "atxBase"
+        Me.atxBase.NumericFormat = "0.#####"
+        Me.atxBase.OutsideHardLimitBackground = System.Drawing.Color.Coral
+        Me.atxBase.OutsideSoftLimitBackground = System.Drawing.Color.Yellow
+        Me.atxBase.SelLength = 4
+        Me.atxBase.SelStart = 0
+        Me.atxBase.Size = New System.Drawing.Size(119, 33)
+        Me.atxBase.SoftMax = -999
+        Me.atxBase.SoftMin = -999
+        Me.atxBase.TabIndex = 23
+        Me.atxBase.ValueDouble = 1000
+        Me.atxBase.ValueInteger = 1000
+        '
         'frmSaveAs
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(378, 182)
+        Me.ClientSize = New System.Drawing.Size(495, 224)
+        Me.Controls.Add(Me.atxBase)
         Me.Controls.Add(Me.cmdCancel)
         Me.Controls.Add(Me.cmdOK)
         Me.Controls.Add(Me.cmdFile)
-        Me.Controls.Add(Me.AtcText1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.RadioButton2)
-        Me.Controls.Add(Me.RadioButton1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.atxName)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.rbnAbsolute)
+        Me.Controls.Add(Me.rbnRelative)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "frmSaveAs"
         Me.Text = "WinHSPF - Save As"
         Me.ResumeLayout(False)
@@ -197,13 +233,14 @@ Partial Class frmSaveAs
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
-    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents AtcText1 As atcControls.atcText
+    Friend WithEvents rbnRelative As System.Windows.Forms.RadioButton
+    Friend WithEvents rbnAbsolute As System.Windows.Forms.RadioButton
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents atxName As atcControls.atcText
     Friend WithEvents cmdFile As System.Windows.Forms.Button
     Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
     Friend WithEvents cmdCancel As System.Windows.Forms.Button
     Friend WithEvents cmdOK As System.Windows.Forms.Button
+    Friend WithEvents atxBase As atcControls.atcText
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
 End Class
