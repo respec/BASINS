e09bf0e6f785b633e11a6cd4042b3aff689717d1
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2009-01-16 21:16:56 +0000

    HspfUci.vb -- add functions to default uci modCreateUci.vb -- moved functions to default the uci over to HspfUci.vb frmStarter.vb -- begin implementing form

--- BASINS40/WinHSPF/frmStarter.Designer.vb
+++ BASINS40/WinHSPF/frmStarter.Designer.vb
@@ -19,51 +19,41 @@ Partial Class frmStarter
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.AtcGrid1 = New atcControls.atcGrid
-        Me.Button1 = New System.Windows.Forms.Button
-        Me.Button2 = New System.Windows.Forms.Button
+        Me.cmdApply = New System.Windows.Forms.Button
+        Me.cmdStarter = New System.Windows.Forms.Button
         Me.cmdCancel = New System.Windows.Forms.Button
         Me.cmdOK = New System.Windows.Forms.Button
+        Me.agdStarter = New atcControls.atcGrid
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog
         Me.SuspendLayout()
         '
-        'AtcGrid1
+        'cmdApply
         '
-        Me.AtcGrid1.AllowHorizontalScrolling = True
-        Me.AtcGrid1.AllowNewValidValues = False
-        Me.AtcGrid1.CellBackColor = System.Drawing.Color.Empty
-        Me.AtcGrid1.Fixed3D = False
-        Me.AtcGrid1.LineColor = System.Drawing.Color.Empty
-        Me.AtcGrid1.LineWidth = 0.0!
-        Me.AtcGrid1.Location = New System.Drawing.Point(13, 12)
-        Me.AtcGrid1.Name = "AtcGrid1"
-        Me.AtcGrid1.Size = New System.Drawing.Size(450, 258)
-        Me.AtcGrid1.Source = Nothing
-        Me.AtcGrid1.TabIndex = 0
+        Me.cmdApply.Location = New System.Drawing.Point(195, 340)
+        Me.cmdApply.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmdApply.Name = "cmdApply"
+        Me.cmdApply.Size = New System.Drawing.Size(117, 36)
+        Me.cmdApply.TabIndex = 1
+        Me.cmdApply.Text = "Apply to UCI"
+        Me.cmdApply.UseVisualStyleBackColor = True
         '
-        'Button1
+        'cmdStarter
         '
-        Me.Button1.Location = New System.Drawing.Point(146, 276)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(88, 29)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Apply to UCI"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(240, 276)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(88, 29)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Set Starter"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.cmdStarter.Location = New System.Drawing.Point(320, 340)
+        Me.cmdStarter.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmdStarter.Name = "cmdStarter"
+        Me.cmdStarter.Size = New System.Drawing.Size(117, 36)
+        Me.cmdStarter.TabIndex = 2
+        Me.cmdStarter.Text = "Set Starter"
+        Me.cmdStarter.UseVisualStyleBackColor = True
         '
         'cmdCancel
         '
         Me.cmdCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.cmdCancel.Location = New System.Drawing.Point(240, 322)
+        Me.cmdCancel.Location = New System.Drawing.Point(320, 396)
+        Me.cmdCancel.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdCancel.Name = "cmdCancel"
-        Me.cmdCancel.Size = New System.Drawing.Size(101, 26)
+        Me.cmdCancel.Size = New System.Drawing.Size(135, 32)
         Me.cmdCancel.TabIndex = 24
         Me.cmdCancel.Text = "&Cancel"
         Me.cmdCancel.UseVisualStyleBackColor = True
@@ -71,31 +61,53 @@ Partial Class frmStarter
         'cmdOK
         '
         Me.cmdOK.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.cmdOK.Location = New System.Drawing.Point(133, 322)
+        Me.cmdOK.Location = New System.Drawing.Point(177, 396)
+        Me.cmdOK.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdOK.Name = "cmdOK"
-        Me.cmdOK.Size = New System.Drawing.Size(101, 26)
+        Me.cmdOK.Size = New System.Drawing.Size(135, 32)
         Me.cmdOK.TabIndex = 23
         Me.cmdOK.Text = "&OK"
         Me.cmdOK.UseVisualStyleBackColor = True
         '
+        'agdStarter
+        '
+        Me.agdStarter.AllowHorizontalScrolling = True
+        Me.agdStarter.AllowNewValidValues = False
+        Me.agdStarter.CellBackColor = System.Drawing.Color.Empty
+        Me.agdStarter.Fixed3D = False
+        Me.agdStarter.LineColor = System.Drawing.Color.Empty
+        Me.agdStarter.LineWidth = 0.0!
+        Me.agdStarter.Location = New System.Drawing.Point(17, 15)
+        Me.agdStarter.Margin = New System.Windows.Forms.Padding(4)
+        Me.agdStarter.Name = "agdStarter"
+        Me.agdStarter.Size = New System.Drawing.Size(600, 318)
+        Me.agdStarter.Source = Nothing
+        Me.agdStarter.TabIndex = 0
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
         'frmStarter
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(477, 360)
+        Me.ClientSize = New System.Drawing.Size(636, 443)
         Me.Controls.Add(Me.cmdCancel)
         Me.Controls.Add(Me.cmdOK)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.AtcGrid1)
+        Me.Controls.Add(Me.cmdStarter)
+        Me.Controls.Add(Me.cmdApply)
+        Me.Controls.Add(Me.agdStarter)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "frmStarter"
         Me.Text = "WinHSPF - Starting Values Manager"
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents AtcGrid1 As atcControls.atcGrid
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents agdStarter As atcControls.atcGrid
+    Friend WithEvents cmdApply As System.Windows.Forms.Button
+    Friend WithEvents cmdStarter As System.Windows.Forms.Button
     Friend WithEvents cmdCancel As System.Windows.Forms.Button
     Friend WithEvents cmdOK As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
 End Class
