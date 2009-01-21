205319f67e8e5f090a8e20f0563adccefc9f264f
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2009-01-21 20:44:33 +0000

    ctlEditConnections.vb -- add limits for ext sources editing modEditLimits.vb -- move some code to ctlEditConnections.vb atcUCIForms.vbproj -- include atcData for some limits clsUCIForms.vb -- take edit button off opn seq block edit form ctlEditOpnSeqBlock.vb -- implement add, remove, save of opn seq block frmEdit.vb -- handle remove button click

--- BASINS40/atcUCI/modEditLimits.vb
+++ BASINS40/atcUCI/modEditLimits.vb
@@ -11,40 +11,6 @@ Module modEditLimits
     Private cons() As String
     Private cwdmid() As String
 	
-    'Public Sub CheckLimitsExtSources(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci)
-    '       g.ClearValues()
-    '       If g.col = 0 Then 'svol
-    '           Call SetLimitsWDM(g, myUci)
-    '       ElseIf g.col = 1 Then  'svolno
-    '           Call CheckValidDsn(g, myUci)
-    '       ElseIf g.col = 2 Then  'smemn
-    '           Call CheckValidMemberName(g, myUci)
-    '       ElseIf g.col = 3 Then  'qflg
-    '           g.ColMin(g.col) = 0
-    '           g.ColMax(g.col) = 31
-    '       ElseIf g.col = 4 Then  'ssyst
-    '           g.addValue("ENGL")
-    '           g.addValue("METR")
-    '       ElseIf g.col = 5 Then  'sgapst
-    '           g.addValue(" ") 'allow default blank
-    '           g.addValue("UNDF")
-    '           g.addValue("ZERO")
-    '       ElseIf g.col = 6 Then  'mfactr
-    '       ElseIf g.col = 7 Then  'tran
-    '           Call SetValidTrans(g)
-    '       ElseIf g.col = 8 Then  'tvol
-    '           Call SetValidOperations(g, myUci)
-    '       ElseIf g.col = 9 Then  'topfst
-    '           Call SetOperationMinMax(g, myUci, 1)
-    '       ElseIf g.col = 10 Then  'group
-    '           Call SetGroupNames(g, (myUci.Msg), 2)
-    '       ElseIf g.col = 11 Then  'tmemn
-    '           Call SetMemberNames(g, (myUci.Msg), 3)
-    '       ElseIf g.col = 12 Then  'tmems1
-    '       ElseIf g.col = 13 Then  'tmems2
-    '       End If
-    'End Sub
-	
     'Public Sub CheckLimitsExtTargets(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci)
 
     '	'UPGRADE_WARNING: Couldn't resolve default property of object g.ClearValues. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
@@ -514,74 +480,6 @@ Module modEditLimits
     '		'conditional
     '	End If
     'End Sub
-	
-    'Private Sub SetLimitsWDM(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci)
-    '	Dim myFiles As HspfFilesBlk
-    '	Dim i As Integer
-    '	Dim s As String
-
-    '	myFiles = myUci.FilesBlock
-    '	For i = 1 To myFiles.Count
-    '		s = myFiles.Value(i).typ
-    '		If Left(s, 3) = "WDM" Then
-    '               g.addValue(s)
-    '		End If
-    '	Next i
-    'End Sub
-	
-    'Private Sub CheckValidDsn(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci)
-    '       Dim dsnObj As ATCData.ATCclsTserData
-    '	Dim s, wdmid As String
-    '	Dim dsn As Integer
-
-    '	s = g.Text
-    '	If IsNumeric(s) Then
-    '		dsn = CInt(s)
-    '		'UPGRADE_WARNING: Couldn't resolve default property of object g.col. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '		'UPGRADE_WARNING: Couldn't resolve default property of object g.row. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '		'UPGRADE_WARNING: Couldn't resolve default property of object g.TextMatrix. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '		wdmid = g.TextMatrix(g.row, g.col - 1)
-    '		dsnObj = myUci.GetDataSetFromDsn(WDMInd(wdmid), dsn)
-    '		If dsnObj Is Nothing Then
-    '			'g.TextBackColor = vbRed
-    '		Else
-    '			'UPGRADE_WARNING: Couldn't resolve default property of object g.TextBackColor. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '			g.TextBackColor = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.White)
-    '		End If
-    '	Else
-    '		'UPGRADE_WARNING: Couldn't resolve default property of object g.TextBackColor. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '		g.TextBackColor = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red)
-    '		s = ""
-    '	End If
-    'End Sub
-	
-    'Private Sub CheckValidMemberName(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci)
-    '       Dim dsnObj As ATCData.ATCclsTserData
-    '	Dim s, wdmid As String
-    '	Dim dsn As Integer
-
-    '	s = g.Text
-    '	dsn = g.TextMatrix(g.row, g.col - 1)
-    '	wdmid = g.TextMatrix(g.row, g.col - 2)
-    '	dsnObj = myUci.GetDataSetFromDsn(WDMInd(wdmid), dsn)
-    '	If Not dsnObj Is Nothing Then
-    '		'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '		g.addValue(dsnObj.Attrib("TSTYPE"))
-    '	End If
-    'End Sub
-	
-    'Private Sub SetValidOperations(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci)
-
-    '	Dim vOpnBlk As Object
-    '	Dim lopnblk As HspfOpnBlk
-
-    '	For	Each vOpnBlk In myUci.OpnBlks
-    '		lopnblk = vOpnBlk
-    '		If lopnblk.Count > 0 Then
-    '               g.addValue(lopnblk.Name)
-    '		End If
-    '	Next vOpnBlk
-    'End Sub
 
     'Private Sub SetAllOperations(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci)
 
@@ -593,93 +491,6 @@ Module modEditLimits
     '		'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
     '		g.addValue(lopnblk.Name)
     '	Next vOpnBlk
-    'End Sub
-
-    'Private Sub SetOperationMinMax(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci, ByRef firstlast As Integer)
-
-    '	Dim lopnblk As HspfOpnBlk
-    '	Dim i, itmp As Integer
-
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.col. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.row. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.TextMatrix. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	If Len(g.TextMatrix(g.row, g.col - firstlast)) > 0 Then
-    '		'UPGRADE_WARNING: Couldn't resolve default property of object g.col. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '		'UPGRADE_WARNING: Couldn't resolve default property of object g.row. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '		'UPGRADE_WARNING: Couldn't resolve default property of object g.TextMatrix. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '		lopnblk = myUci.OpnBlks.Item(Trim(g.TextMatrix(g.row, g.col - firstlast)))
-    '		If lopnblk.Count > 0 Then
-    '			For i = 1 To lopnblk.Count
-    '				'UPGRADE_WARNING: Couldn't resolve default property of object lopnblk.Ids().Id. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '				'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '				g.addValue(lopnblk.Ids.Item(i).Id)
-    '			Next i
-    '		End If
-    '	End If
-
-    'End Sub
-
-    'Private Sub SetGroupNames(ByRef g As System.Windows.Forms.Control, ByRef myMsg As HspfMsg, ByRef opercol As Integer)
-    '	Dim vGroup As Object
-    '	Dim lGroup As HspfTSGroupDef
-    '	Dim i, ioper As Integer
-    '	Dim s, soper As String
-
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.col. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.row. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.TextMatrix. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	soper = Trim(g.TextMatrix(g.row, g.col - opercol))
-    '	If Len(soper) > 0 Then
-    '		ioper = HspfOperNum(soper)
-    '		If ioper > 0 Then
-    '			For	Each vGroup In myMsg.TSGroupDefs
-    '				lGroup = vGroup
-    '				If lGroup.BlockId = 120 + ioper Then
-    '					s = lGroup.Name
-    '					'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '					g.addValue(s)
-    '				End If
-    '			Next vGroup
-    '		End If
-    '	End If
-    'End Sub
-
-    'Private Sub SetMemberNames(ByRef g As System.Windows.Forms.Control, ByRef myMsg As HspfMsg, ByRef opercol As Integer)
-    '	Dim vMember As Object
-    '	Dim lMember As HspfTSMemberDef
-    '	Dim i, ioper As Integer
-    '	Dim skey, s, sgroup, soper As String
-    '	Dim vGroup As Object
-    '	Dim lGroup As HspfTSGroupDef
-
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.col. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.row. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.TextMatrix. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	soper = Trim(g.TextMatrix(g.row, g.col - opercol))
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.col. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.row. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.TextMatrix. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	sgroup = Trim(g.TextMatrix(g.row, g.col - 1))
-    '	If Len(soper) > 0 Then
-    '		ioper = HspfOperNum(soper)
-    '		If ioper > 0 Then
-    '			For	Each vGroup In myMsg.TSGroupDefs
-    '				lGroup = vGroup
-    '				If lGroup.BlockId = 120 + ioper And lGroup.Name = sgroup Then
-    '					'this is the one we want
-    '					skey = CStr(lGroup.Id)
-    '					'UPGRADE_WARNING: Couldn't resolve default property of object myMsg.TSGroupDefs(skey).MemberDefs. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '					For	Each vMember In myMsg.TSGroupDefs.Item(skey).MemberDefs
-    '						lMember = vMember
-    '						s = lMember.Name
-    '						'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '						g.addValue(s)
-    '					Next vMember
-    '					Exit Sub
-    '				End If
-    '			Next vGroup
-    '		End If
-    '	End If
     'End Sub
 
     'Private Sub SetValidMassLinks(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci)
@@ -713,27 +524,6 @@ Module modEditLimits
     '	Next i
     'End Sub
 
-    'Private Sub SetValidTrans(ByRef g As System.Windows.Forms.Control)
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue(" ") 'allow default blank
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue("SAME")
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue("AVER")
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue("DIV ")
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue("INTP")
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue("LAST")
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue("MAX ")
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue("MIN ")
-    '	'UPGRADE_WARNING: Couldn't resolve default property of object g.addValue. Click for more: 'ms-help://MS.VSExpressCC.v80/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-    '	g.addValue("SUM ")
-    'End Sub
-	
     'Public Sub CheckDataSetExistance(ByRef g As System.Windows.Forms.Control, ByRef myUci As HspfUci, ByRef retcod As Integer)
     '       Dim dsnObj As ATCData.ATCclsTserData
     '	Dim dsn, wdmid As String
@@ -789,9 +579,9 @@ Module modEditLimits
         c = cons(i)
     End Sub
 	
-	Public Sub GetNonExistentDataSetCount(ByRef n As Integer)
-		n = NoDsnCount
-	End Sub
+    Public Sub GetNonExistentDataSetCount(ByRef n As Integer)
+        n = NoDsnCount
+    End Sub
 	
     Public Sub UpdateRespFromAddDataSet(ByRef i As Integer)
         '1 - cancel from 'add data set'
