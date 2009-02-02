c66397a95631a0b3f635c2747ce23d0466ad8889
pdudda <PaulDudaRESPEC@users.noreply.github.com>
2009-02-02 21:23:55 +0000

    frmSWMMSetup.vb -- add new buttons to calculate attributes

--- atcSWMMPlugIn/My Project/Resources.Designer.vb
+++ atcSWMMPlugIn/My Project/Resources.Designer.vb
@@ -60,6 +60,13 @@ Namespace My.Resources
             End Set
         End Property
         
+        Friend ReadOnly Property field_calculator() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("field_calculator", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
         Friend ReadOnly Property NewShapefile() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("NewShapefile", resourceCulture)
