d4b490664986d701af4422fa853b2aaf99900dad
Paul Meems <pmeems@users.noreply.github.com>
2009-07-31 06:47:56 +0000

    Converted solution to VS2008 32bit and updated AssemblyInfo

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MapWinUtility")> 
+<Assembly: AssemblyDescription("A set of utilities that's not strictly MapWindow but which are helpful when writing plugins")> 
+<Assembly: AssemblyCompany("MapWindow OSS Team - www.mapwindow.org")> 
+<Assembly: AssemblyProduct("MapWindow GIS - MapWinUtility")> 
+<Assembly: AssemblyCopyright("Copyright © 1998-2009 MapWindow OSS Team")> 
+<Assembly: AssemblyTrademark("MapWindow GIS is a trademark of Daniel P. Ames, 2005-2009")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("37eadf2b-8cde-46d0-9bb2-60db08e12c5c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("4.7.2.*")> 
+<Assembly: AssemblyFileVersion("4.7.2.0")> 
