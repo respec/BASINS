3a0e9e8b49d92faa51ea0a094dfdb2cbefcd9dc1
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2009-10-06 17:52:23 +0000

    BatchCAT - initial commit of new standalone version of CAT

---
+++ BASINS40/BatchCAT/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BatchCAT")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Aqua Terra Consultants")> 
+<Assembly: AssemblyProduct("BatchCAT")> 
+<Assembly: AssemblyCopyright("Copyright © Aqua Terra Consultants 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f60ddbf6-4647-4198-b932-275814d9715b")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ BASINS40/BatchCAT/modBatchCAT.vb
@@ -0,0 +1,28 @@
+﻿Imports atcClimateAssessmentTool
+Imports atcUtility
+Imports MapWinUtility
+
+''' <summary>
+''' updates HSPF PERLND parameter tables with values extracted from an ATC standard spreadsheet of parameter values.
+''' </summary>
+''' <remarks></remarks>
+Module modBatchCAT
+    Sub main()
+        If My.Application.CommandLineArgs.Count = 0 Then
+            Logger.Msg("ERROR - No Command Line Argument Containing CAT Scenario Found", "BatchCAT Problem")
+        Else
+            Dim lCatSpecFile As String = My.Application.CommandLineArgs.Item(0)
+            Dim lPath As String = IO.Path.GetDirectoryName(lCatSpecFile)
+            My.Computer.FileSystem.CurrentDirectory = lPath
+            Logger.StartToFile("logs\" & Format(Now, "yyyy-MM-dd") & "at" & Format(Now, "HH-mm") & "-BatchCAT.txt", , False)
+
+            atcData.atcDataManager.Clear()
+            Dim lCat As New atcClimateAssessmentTool.clsCat
+            With lCat
+                .XML = atcUtility.WholeFileString(lCatSpecFile)
+                .RunModel = False
+                .StartRun("")
+            End With
+        End If
+    End Sub
+End Module
