d57cc97b9d381322926e37b13daad6aa4d580beb
Jack Kittle <jack@gkfamily.org>
2009-10-07 13:04:58 +0000

    BatchCAT - better comments and error checking

--- BatchCAT/modBatchCAT.vb
+++ BatchCAT/modBatchCAT.vb
@@ -3,7 +3,7 @@ Imports atcUtility
 Imports MapWinUtility
 
 ''' <summary>
-''' updates HSPF PERLND parameter tables with values extracted from an ATC standard spreadsheet of parameter values.
+''' batch CAT for use from command line
 ''' </summary>
 ''' <remarks></remarks>
 Module modBatchCAT
@@ -12,17 +12,21 @@ Module modBatchCAT
             Logger.Msg("ERROR - No Command Line Argument Containing CAT Scenario Found", "BatchCAT Problem")
         Else
             Dim lCatSpecFile As String = My.Application.CommandLineArgs.Item(0)
-            Dim lPath As String = IO.Path.GetDirectoryName(lCatSpecFile)
-            My.Computer.FileSystem.CurrentDirectory = lPath
-            Logger.StartToFile("logs\" & Format(Now, "yyyy-MM-dd") & "at" & Format(Now, "HH-mm") & "-BatchCAT.txt", , False)
-            Initialize()
+            If IO.File.Exists(lCatSpecFile) Then
+                Dim lPath As String = IO.Path.GetDirectoryName(lCatSpecFile)
+                My.Computer.FileSystem.CurrentDirectory = lPath
+                Logger.StartToFile("logs\" & Format(Now, "yyyy-MM-dd") & "at" & Format(Now, "HH-mm") & "-BatchCAT.txt", , False)
+                Initialize()
 
-            Dim lCat As New atcClimateAssessmentTool.clsCat
-            With lCat
-                .XML = atcUtility.WholeFileString(lCatSpecFile)
-                .RunModel = False
-                .StartRun("ModifyOriginal")
-            End With
+                Dim lCat As New atcClimateAssessmentTool.clsCat
+                With lCat
+                    .XML = atcUtility.WholeFileString(lCatSpecFile)
+                    .RunModel = False
+                    .StartRun("ModifyOriginal")
+                End With
+            Else
+                Logger.Msg("ERROR - CAT specification file '" & lCatSpecFile & "' not found")
+            End If
         End If
     End Sub
 
