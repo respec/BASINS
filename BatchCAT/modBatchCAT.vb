a1297a0200e0f13e2514322d9d2a2e9cac1de6e6
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2009-10-07 14:35:39 +0000

    BatchCAT/modBatchCAT: changed main to function so we can send back an exit code                       added return codes 0-4                       added Try/Catch around everything

--- BatchCAT/modBatchCAT.vb
+++ BatchCAT/modBatchCAT.vb
@@ -7,32 +7,40 @@ Imports MapWinUtility
 ''' </summary>
 ''' <remarks></remarks>
 Module modBatchCAT
-    Sub main()
-        If My.Application.CommandLineArgs.Count = 0 Then
-            Logger.Msg("ERROR - No Command Line Argument Containing CAT Scenario Found", "BatchCAT Problem")
-        Else
-            Dim lCatSpecFile As String = My.Application.CommandLineArgs.Item(0)
-            If IO.File.Exists(lCatSpecFile) Then
-                Dim lPath As String = IO.Path.GetDirectoryName(lCatSpecFile)
-                My.Computer.FileSystem.CurrentDirectory = lPath
-                Logger.StartToFile("logs\" & Format(Now, "yyyy-MM-dd") & "at" & Format(Now, "HH-mm") & "-BatchCAT.txt", , False)
-                Logger.AutoFlush = True
-
-                Initialize()
-
-                Dim lCat As New atcClimateAssessmentTool.clsCat
-                With lCat
-                    .XML = atcUtility.WholeFileString(lCatSpecFile)
-                    .RunModel = False
-                    Logger.Dbg("StartRunWith " & lCat.Inputs.Count & " Inputs")
-                    .StartRun("ModifyOriginal")
-                End With
+    Public Function Main(ByVal args() As String) As Integer
+        Try
+            If args.Count = 0 Then
+                Logger.Msg("ERROR - No Command Line Argument Containing CAT Scenario Found", "BatchCAT Problem")
+                Return 1
             Else
-                Logger.Msg("ERROR - CAT specification file '" & lCatSpecFile & "' not found")
+                Dim lCatSpecFile As String = args(0)
+                If IO.File.Exists(lCatSpecFile) Then
+                    Dim lPath As String = IO.Path.GetDirectoryName(lCatSpecFile)
+                    My.Computer.FileSystem.CurrentDirectory = lPath
+                    Logger.StartToFile("logs\" & Format(Now, "yyyy-MM-dd") & "at" & Format(Now, "HH-mm") & "-BatchCAT.txt", , False)
+                    Logger.AutoFlush = True
+                    Initialize()
+
+                    Dim lCat As New atcClimateAssessmentTool.clsCat
+                    With lCat
+                        .XML = atcUtility.WholeFileString(lCatSpecFile)
+                        .RunModel = False
+                        Logger.Dbg("StartRunWith " & lCat.Inputs.Count & " Inputs")
+                        .StartRun("ModifyOriginal")
+                    End With
+                    Return 0
+                Else
+                    Logger.Msg("ERROR - CAT specification file '" & lCatSpecFile & "' not found")
+                    Return 2
+                End If
             End If
-        End If
-        Logger.Dbg("BatchCAT:AllDone")
-    End Sub
+            Logger.Dbg("BatchCAT:AllDone")
+        Catch e As Exception
+            Logger.Msg("ERROR - " & e.Message)
+            Return 3
+        End Try
+        Return 4
+    End Function
 
     Sub Initialize()
         atcData.atcDataManager.Clear()
