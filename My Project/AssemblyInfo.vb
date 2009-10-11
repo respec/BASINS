331aab122d9bd18b39febd6adfa155536c143ce0
Paul Meems <pmeems@users.noreply.github.com>
2009-10-11 22:44:24 +0000

    Added 64Bit configuration

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("4.7.3.*")> 
-<Assembly: AssemblyFileVersion("4.7.3.0")> 
+<Assembly: AssemblyVersion("4.7.4.*")> 
+<Assembly: AssemblyFileVersion("4.7.4.0")> 
