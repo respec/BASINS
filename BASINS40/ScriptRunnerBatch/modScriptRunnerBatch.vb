46a8cb42a95cadee543a9244991a410d8eca4143
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2009-11-12 21:29:13 +0000

    ScriptRunnerBatch - new stub main program - useful for command line execution of scripts.

---
+++ BASINS40/ScriptRunnerBatch/modScriptRunnerBatch.vb
@@ -0,0 +1,22 @@
+﻿Imports ScriptRunner
+
+Module modScriptRunnerBatch
+    Private g_Statistics As New atcTimeseriesStatistics.atcTimeseriesStatistics
+    Sub main()
+        Dim lCommandLine As String = Command()
+        If lCommandLine.Length = 0 Then
+            lCommandLine = "SantaClara"
+        End If
+
+        Try
+            If g_Statistics.AvailableOperations.Count > 0 Then
+                For Each lOperation As atcData.atcDefinedValue In g_Statistics.AvailableOperations
+                    atcData.atcDataAttributes.AddDefinition(lOperation.Definition)
+                Next
+            End If
+        Catch
+        End Try
+
+        HSPFOutputReports.ScriptMainStandAlone(lCommandLine)
+    End Sub
+End Module
