1bddcc847c521bd663f5fa9a6622786a9fb1be20
Paul Meems <pmeems@users.noreply.github.com>
2009-11-13 13:20:25 +0000

    Getting ready for the stable release of v4.7 Updated version number to 4.7.5

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("4.7.4.*")> 
-<Assembly: AssemblyFileVersion("4.7.4.0")> 
+<Assembly: AssemblyVersion("4.7.5.*")> 
+<Assembly: AssemblyFileVersion("4.7.5.0")> 
