93e5b3bcb415f7d2f14e3d08e7922e19ecdc047e
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2010-02-02 15:18:55 +0000

    atcHspfSupport: removed clsHspfSupportPluginDuration.vb                 clsHspfSupportPlugin: added Name="Analysis::Duration/Compare"                                       create Analysis/Duration menu (Compare added but commented out)                                       respond to ItemClicked by creating New frmDuration                 frmDuration: changed to display report in large textbox instead of save to file then open                              added menu bar

--- BASINS40/atcHspfSupport/clsHspfSupportPlugin.vb
+++ BASINS40/atcHspfSupport/clsHspfSupportPlugin.vb
@@ -1,17 +1,55 @@
 Imports atcData
-'Public Class clsHspfSupportPlugin
-'    Inherits atcDataTool
 
-'    Public Overrides ReadOnly Property Name() As String
-'        Get
-'            Return "Analysis::Compare"
-'        End Get
-'    End Property
+Public Class clsHspfSupportPlugin
+    Inherits atcDataPlugin
 
-'    Public Overrides Function Show() As Object
-'        Dim lFrm As New frmCompare
-'        lFrm.Show()
-'        Return lFrm
-'    End Function
+    Public Overrides ReadOnly Property Name() As String
+        Get
+            Return "Analysis::Duration/Compare"
+        End Get
+    End Property
 
-'End Class
+    <CLSCompliant(False)> _
+    Public Overrides Sub Initialize(ByVal aMapWin As MapWindow.Interfaces.IMapWin, _
+                                      ByVal aParentHandle As Integer)
+
+        pMapWin = aMapWin
+        pMapWinWindowHandle = aParentHandle
+        atcDataManager.MapWindow = aMapWin
+
+        atcDataManager.AddMenuIfMissing(atcDataManager.AnalysisMenuName, "", atcDataManager.AnalysisMenuString, atcDataManager.FileMenuName)
+        pMenusAdded.Add(atcDataManager.AddMenuWithIcon(atcDataManager.AnalysisMenuName & "_Duration", atcDataManager.AnalysisMenuName, "Duration", Me.Icon, , , True))
+        'pMenusAdded.Add(atcDataManager.AddMenuWithIcon(atcDataManager.AnalysisMenuName & "_Compare", atcDataManager.AnalysisMenuName, "Compare", Me.Icon, , , True))
+    End Sub
+
+    Public Overrides Sub ItemClicked(ByVal aItemName As String, ByRef aHandled As Boolean)
+        Dim lFrm As Object = Nothing 'Windows.Forms.Form
+        Select Case aItemName
+            Case atcDataManager.AnalysisMenuName & "_Duration" : lFrm = New frmDuration
+                'Case atcDataManager.AnalysisMenuName & "_Compare" : lFrm = New frmCompare
+        End Select
+
+        If lFrm IsNot Nothing Then
+            If pMapWin IsNot Nothing AndAlso pMapWin.ApplicationInfo.FormIcon IsNot Nothing Then lFrm.Icon = pMapWin.ApplicationInfo.FormIcon
+            Dim lTimeseriesGroup As atcTimeseriesGroup = atcDataManager.UserSelectData("Select Data For " & aItemName.Substring(atcDataManager.AnalysisMenuName.Length + 1))
+            If lTimeseriesGroup.Count > 0 Then
+                lFrm.Initialize(lTimeseriesGroup)
+                lFrm.Show()
+            End If
+        End If
+
+    End Sub
+
+    '    Public Overrides ReadOnly Property Name() As String
+    '        Get
+    '            Return "Analysis::Compare"
+    '        End Get
+    '    End Property
+
+    '    Public Overrides Function Show() As Object
+    '        Dim lFrm As New frmCompare
+    '        lFrm.Show()
+    '        Return lFrm
+    '    End Function
+
+End Class
--- BASINS40/atcHspfSupport/frmDuration.vb
+++ BASINS40/atcHspfSupport/frmDuration.vb
@@ -4,7 +4,14 @@ Imports atcUtility
 Public Class frmDuration
     Private pTSer As atcTimeseries
 
-    Private Sub btnDuration_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDuration.Click
+    Public Sub Initialize(ByVal aTimeseriesGroup As atcData.atcTimeseriesGroup)
+        txtReport.Text = ""
+        For Each lTs As atcTimeseries In aTimeseriesGroup
+            txtReport.Text &= "Duration Report for " & lTs.ToString & vbCrLf & DurationStats(lTs, GetClassLimits(lTs))
+        Next
+    End Sub
+
+    Private Sub mnuSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mnuSave.Click
         Dim cdlg As New Windows.Forms.SaveFileDialog
         With cdlg
             .Title = "Save Duration Report As..."
@@ -12,51 +19,9 @@ Public Class frmDuration
             .Filter = "Text Files|*.txt|All Files|*.*"
             .FilterIndex = 0
             If .ShowDialog = Windows.Forms.DialogResult.OK Then
-                Dim lDataGroup As atcTimeseriesGroup = DateChooser.CreateSelectedDataGroupSubset
-
-                'Dim lTU As atcTimeUnit
-                'If radioTUDaily.Checked Then
-                '    lTU = atcTimeUnit.TUDay
-                'ElseIf radioTUMonthly.Checked Then
-                '    lTU = atcTimeUnit.TUMonth
-                'Else
-                '    lTU = atcTimeUnit.TUYear
-                'End If
-
-                Dim lTran As atcTran = atcTran.TranAverSame
-
-                pTSer = Aggregate(lDataGroup(0), atcTimeUnit.TUDay, 1, lTran)
-                'pSimulated = Aggregate(lDataGroup(1), lTU, 1, lTran)
-                IO.File.WriteAllText(.FileName, DurationStats(pTSer, GetClassLimits(pTSer)))
+                IO.File.WriteAllText(.FileName, txtReport.Text)
                 OpenFile(.FileName)
             End If
         End With
-
     End Sub
-
-    Private Sub btnTSDir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTSDir.Click
-        Dim lHad As New atcTimeseriesGroup
-        If pTSer IsNot Nothing Then lHad.Add(pTSer)
-        Dim lUserSelected As atcTimeseriesGroup = atcDataManager.UserSelectData("Select Dataset", lHad)
-        If lUserSelected.Count > 0 Then
-            pTSer = lUserSelected(0)
-            txtTSDir.Text = pTSer.ToString
-        Else
-            pTSer = Nothing
-            txtTSDir.Text = ""
-        End If
-        SetDates()
-
-    End Sub
-
-    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtTSDir.TextChanged
-        btnTSDir_Click(sender, e)
-    End Sub
-
-    Private Sub SetDates()
-        Dim lDataGroup As New atcTimeseriesGroup
-        If pTSer IsNot Nothing Then lDataGroup.Add(pTSer)
-        DateChooser.DataGroup = lDataGroup
-    End Sub
-
 End Class
