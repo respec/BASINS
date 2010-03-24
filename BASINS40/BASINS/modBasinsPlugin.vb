3cf26a3b7d9e48dfe08ddb8aa58a886b04e17d64
jkittle <jkittle@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2010-03-24 18:47:56 +0000

    BASINS - project setup - may only select polygons

--- BASINS40/BASINS/modBasinsPlugin.vb
+++ BASINS40/BASINS/modBasinsPlugin.vb
@@ -210,52 +210,53 @@ Public Module modBasinsPlugin
 
             RefreshView()
             ctext = "Selected Features:" & vbCrLf & "  <none>"
-            lCurLayer = g_MapWin.Layers.Item(g_MapWin.Layers.CurrentLayer).GetObject
+            If g_MapWin.Layers.Item(g_MapWin.Layers.CurrentLayer).LayerType = MapWindow.Interfaces.eLayerType.PolygonShapefile Then
+                lCurLayer = g_MapWin.Layers.Item(g_MapWin.Layers.CurrentLayer).GetObject
+                If g_MapWin.View.SelectedShapes.NumSelected > 0 Then
+                    ctext = "Selected Features:"
+                    Select Case IO.Path.GetFileNameWithoutExtension(lCurLayer.Filename).ToLower
+                        Case "cat", "huc", "huc250d3"
+                            lFieldName = "CU"
+                            lFieldDesc = "catname"
+                        Case "cnty"
+                            lFieldName = "FIPS"
+                            lFieldDesc = "cntyname"
+                        Case "st"
+                            lFieldName = "ST"
+                            lFieldDesc = "name"
+                    End Select
 
-            If g_MapWin.View.SelectedShapes.NumSelected > 0 Then
-                ctext = "Selected Features:"
-                Select Case IO.Path.GetFileNameWithoutExtension(lCurLayer.Filename).ToLower
-                    Case "cat", "huc", "huc250d3"
-                        lFieldName = "CU"
-                        lFieldDesc = "catname"
-                    Case "cnty"
-                        lFieldName = "FIPS"
-                        lFieldDesc = "cntyname"
-                    Case "st"
-                        lFieldName = "ST"
-                        lFieldDesc = "name"
-                End Select
+                    lFieldName = lFieldName.ToLower
+                    lFieldDesc = lFieldDesc.ToLower
+                    For lField = 0 To lCurLayer.NumFields - 1
+                        If lCurLayer.Field(lField).Name.ToLower = lFieldName Then
+                            lNameIndex = lField
+                        End If
+                        If lCurLayer.Field(lField).Name.ToLower = lFieldDesc Then
+                            lDescIndex = lField
+                        End If
+                    Next
 
-                lFieldName = lFieldName.ToLower
-                lFieldDesc = lFieldDesc.ToLower
-                For lField = 0 To lCurLayer.NumFields - 1
-                    If lCurLayer.Field(lField).Name.ToLower = lFieldName Then
-                        lNameIndex = lField
-                    End If
-                    If lCurLayer.Field(lField).Name.ToLower = lFieldDesc Then
-                        lDescIndex = lField
-                    End If
-                Next
-
-                Dim lSelected As Integer
-                Dim lShape As Integer
-                Dim lname As String
-                Dim ldesc As String
-                Dim lSf As MapWinGIS.Shapefile = g_MapWin.Layers.Item(g_MapWin.Layers.CurrentLayer).GetObject
-                For lSelected = 0 To g_MapWin.View.SelectedShapes.NumSelected - 1
-                    lShape = g_MapWin.View.SelectedShapes.Item(lSelected).ShapeIndex()
-                    lname = ""
-                    ldesc = ""
-                    If lNameIndex > -1 Then
-                        lname = lSf.CellValue(lNameIndex, lShape)
-                    End If
-                    If lDescIndex > -1 Then
-                        ldesc = lSf.CellValue(lDescIndex, lShape)
-                    End If
-                    ctext = ctext & vbCrLf & "  " & lname & " : " & ldesc
-                Next
+                    Dim lSelected As Integer
+                    Dim lShape As Integer
+                    Dim lname As String
+                    Dim ldesc As String
+                    Dim lSf As MapWinGIS.Shapefile = g_MapWin.Layers.Item(g_MapWin.Layers.CurrentLayer).GetObject
+                    For lSelected = 0 To g_MapWin.View.SelectedShapes.NumSelected - 1
+                        lShape = g_MapWin.View.SelectedShapes.Item(lSelected).ShapeIndex()
+                        lname = ""
+                        ldesc = ""
+                        If lNameIndex > -1 Then
+                            lname = lSf.CellValue(lNameIndex, lShape)
+                        End If
+                        If lDescIndex > -1 Then
+                            ldesc = lSf.CellValue(lDescIndex, lShape)
+                        End If
+                        ctext = ctext & vbCrLf & "  " & lname & " : " & ldesc
+                    Next
+                End If
+                pBuildFrm.txtSelected.Text = ctext
             End If
-            pBuildFrm.txtSelected.Text = ctext
         End If
     End Sub
 
