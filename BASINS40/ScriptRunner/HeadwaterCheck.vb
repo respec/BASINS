baa559172001292990e8f49338a9940cc8182e42
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2010-04-22 22:25:10 +0000

    BASINS40/ScriptRunner: two new scripts for processing HUC-12 shapes

---
+++ BASINS40/ScriptRunner/HeadwaterCheck.vb
@@ -0,0 +1,103 @@
+﻿Imports atcData
+Imports atcData.atcTimeseriesGroup
+Imports atcUtility
+Imports atcUtility.modFile
+Imports MapWindow.Interfaces
+Imports MapWinUtility
+Imports MapWinGIS
+
+Imports Microsoft.VisualBasic
+Imports System
+
+'WBD HUC 12 field names: 
+'HUC_8	HUC_10	HUC_12	ACRES	NCONTRB_A	
+'HU_10_GNIS	HU_12_GNIS	
+'HU_10_DS	HU_10_NAME	HU_10_MOD	HU_10_TYPE	
+'HU_12_DS	HU_12_NAME	HU_12_MOD	HU_12_TYPE	META_ID	STATES
+
+Public Module HeadwaterCheck
+    Public Sub ScriptMain(ByRef aMapWin As IMapWin)
+        Dim lOriginalShapefileName As String = ""
+        Dim lHucFieldName As String = "HUC_12"
+        Dim lDownstreamFieldName As String = "HU_12_DS"
+        Dim lNewShapefileName As String = ""
+        Dim lUserParms As New atcCollection
+        With lUserParms
+            .Add("Original Shape File", lOriginalShapefileName)
+            .Add("New Shape File", lNewShapefileName)
+            .Add("HUC Field Name", lHucFieldName)
+            .Add("Downstream Field Name", lDownstreamFieldName)
+        End With
+        Dim lAsk As New frmArgs
+        If lAsk.AskUser("Specify full path of text file to import and WDM file to write into", lUserParms) Then
+            With lUserParms
+                lOriginalShapefileName = .ItemByKey("Original Shape File")
+                lNewShapefileName = .ItemByKey("New Shape File")
+                lHucFieldName = .ItemByKey("HUC Field Name")
+                lDownstreamFieldName = .ItemByKey("Downstream Field Name")
+            End With
+            Dim lOriginalShapefile As New MapWinGIS.Shapefile
+            If lOriginalShapefile.Open(lOriginalShapefileName) Then
+                Dim lLastField As Integer = lOriginalShapefile.NumFields - 1
+                Dim lHucField As Integer = -1
+                Dim lDownstreamField As Integer = -1
+                Dim lNewShapefile As New MapWinGIS.Shapefile
+                lNewShapefile.CreateNewWithShapeID(lNewShapefileName, lOriginalShapefile.ShapefileType)
+                lNewShapefile.StartEditingTable()
+                Dim lCurField As Integer
+                For lCurField = 0 To lLastField
+                    lNewShapefile.EditInsertField(lOriginalShapefile.Field(lCurField), lCurField)
+                    Select Case lOriginalShapefile.Field(lCurField).Name
+                        Case lHucFieldName : lHucField = lCurField
+                        Case lDownstreamFieldName : lDownstreamField = lCurField
+
+                    End Select
+                Next
+                If lHucField < 0 Then
+                    lOriginalShapefile.Close()
+                    Logger.Msg("Could not find field '" & lHucFieldName & "'")
+                ElseIf lDownstreamField < 0 Then
+                    lOriginalShapefile.Close()
+                    Logger.Msg("Could not find field '" & lDownstreamFieldName & "'")
+                Else
+                    Dim lNewField As New MapWinGIS.Field
+                    lNewField.Name = "headwater"
+                    lNewField.Width = 1
+                    lNewField.Type = MapWinGIS.FieldType.STRING_FIELD
+                    lNewShapefile.EditInsertField(lNewField, lCurField)
+                    Dim lDownstreamHUCs As New SortedList(Of String, String)
+                    Dim lLastShape As Integer = lOriginalShapefile.NumShapes - 1
+                    Logger.Status("Creating new shape file")
+                    lNewShapefile.StartEditingShapes()
+                    For lShapeIndex As Integer = 0 To lLastShape
+                        lNewShapefile.EditInsertShape(lOriginalShapefile.Shape(lShapeIndex), lShapeIndex)
+                        For lCurField = 0 To lLastField
+                            Dim lCellValue As Object = lOriginalShapefile.CellValue(lCurField, lShapeIndex)
+                            lNewShapefile.EditCellValue(lCurField, lShapeIndex, lCellValue)
+                            If lCurField = lDownstreamField AndAlso Not lDownstreamHUCs.ContainsKey(lCellValue) Then
+                                lDownstreamHUCs.Add(lOriginalShapefile.CellValue(lDownstreamField, lShapeIndex), Nothing)
+                            End If
+                        Next
+                        lNewShapefile.EditCellValue(lCurField, lShapeIndex, "Y")
+                        Logger.Progress("DSHucCnt " & lDownstreamHUCs.Count, lShapeIndex, lLastShape)
+                    Next
+                    lNewShapefile.StopEditingShapes()
+
+                    lOriginalShapefile.Close()
+                    Logger.Status("Setting Headwater Field")
+                    For lShapeIndex As Integer = 0 To lLastShape
+                        If lDownstreamHUCs.ContainsKey(lNewShapefile.CellValue(lHucField, lShapeIndex)) Then
+                            lNewShapefile.EditCellValue(lLastField, lShapeIndex, "N")
+                        End If
+                        If CInt(lShapeIndex / 10) * 10 = lShapeIndex Then Logger.Progress(lShapeIndex, lLastShape)
+                    Next
+                End If
+                lNewShapefile.StopEditingTable()
+                lNewShapefile.Save()
+                lNewShapefile.Close()
+                Logger.Status("")
+            End If
+        End If
+    End Sub
+
+End Module
---
+++ BASINS40/ScriptRunner/SplitShapefileByField.vb
@@ -0,0 +1,104 @@
+﻿Imports atcData
+Imports atcData.atcTimeseriesGroup
+Imports atcUtility
+Imports atcUtility.modFile
+Imports MapWindow.Interfaces
+Imports MapWinUtility
+Imports MapWinGIS
+
+Imports Microsoft.VisualBasic
+Imports System
+
+
+Public Module SplitShapefileByField
+    Public Sub ScriptMain(ByRef aMapWin As IMapWin)
+        Dim lOriginalShapefileName As String = ""
+        Dim lSplitFieldName As String = "HUC_8"
+        Dim lSplitFilenamePattern As String = "split\$SPLIT$\huc12.shp"
+        Dim lUserParms As New atcCollection
+        With lUserParms
+            .Add("Shape File To Split", lOriginalShapefileName)
+            .Add("Split Field Name", lSplitFieldName)
+            .Add("Split Filename Pattern", lSplitFilenamePattern)
+        End With
+        Dim lAsk As New frmArgs
+        If lAsk.AskUser("Specify full path of shape file to split and pattern of split file names to write into", lUserParms) Then
+            With lUserParms
+                lOriginalShapefileName = .ItemByKey("Shape File To Split")
+                lSplitFieldName = .ItemByKey("Split Field Name")
+                lSplitFilenamePattern = .ItemByKey("Split Filename Pattern")
+            End With
+            Dim lOriginalShapefile As New MapWinGIS.Shapefile
+            Dim lOriginalDBF As New atcTableDBF
+            If lOriginalShapefile.Open(lOriginalShapefileName) AndAlso lOriginalDBF.OpenFile(IO.Path.ChangeExtension(lOriginalShapefileName, "dbf")) Then
+                Dim lSplitField As Integer = lOriginalDBF.FieldNumber(lSplitFieldName)
+                If lSplitField > 0 Then
+                    Dim lProjectionFilename As String = IO.Path.ChangeExtension(lOriginalShapefileName, "prj")
+                    Dim lProjectionFile As String = ""
+                    If IO.File.Exists(lProjectionFilename) Then
+                        lProjectionFile = atcUtility.WholeFileString(lProjectionFilename)
+                    End If
+                    Dim lCurrentRecord As Integer
+                    Dim lNumRecords As Integer = lOriginalDBF.NumRecords
+                    Dim lSplitValue As String
+                    Dim lSplitValues As New atcCollection
+
+                    Logger.Status("Finding unique values for " & lSplitFieldName)
+
+                    For lCurrentRecord = 1 To lNumRecords
+                        lOriginalDBF.CurrentRecord = lCurrentRecord
+                        lSplitValue = lOriginalDBF.Value(lSplitField)
+                        lSplitValues.Increment(lSplitValue)
+                        Logger.Progress(lCurrentRecord, lNumRecords)
+                    Next
+                    Dim lNumSplitValues As Integer = lSplitValues.Count
+                    Logger.Status("Splitting into " & lNumSplitValues & " files")
+                    lSplitValues.Sort()
+
+                    For lSplitValueIndex As Integer = 0 To lNumSplitValues - 1
+                        lSplitValue = lSplitValues.Keys(lSplitValueIndex)
+                        Logger.Status("MSG2 " & lSplitValue)
+                        Dim lNewShapefileName As String = lSplitFilenamePattern.Replace("$SPLIT$", lSplitValue)
+                        If Not IO.Path.IsPathRooted(lNewShapefileName) Then
+                            lNewShapefileName = IO.Path.Combine(IO.Path.GetDirectoryName(lOriginalShapefileName), lNewShapefileName)
+                        End If
+                        IO.Directory.CreateDirectory(IO.Path.GetDirectoryName(lNewShapefileName))
+                        Dim lNewShapefile As New MapWinGIS.Shapefile
+                        lNewShapefile.CreateNewWithShapeID(lNewShapefileName, lOriginalShapefile.ShapefileType)
+                        lNewShapefile.StartEditingShapes()
+
+                        Dim lNewShapeIndex As Integer = 0
+
+                        Dim lNewDBF As atcTableDBF = lOriginalDBF.Cousin
+                        lNewDBF.NumRecords = lSplitValues.ItemByIndex(lSplitValueIndex)
+                        lNewDBF.InitData()
+
+                        If lOriginalDBF.FindFirst(lSplitField, lSplitValue) Then
+                            Do
+                                lNewShapefile.EditInsertShape(lOriginalShapefile.Shape(lOriginalDBF.CurrentRecord - 1), lNewShapeIndex)
+                                lNewShapeIndex += 1
+                                lNewDBF.CurrentRecord = lNewShapeIndex
+                                lNewDBF.RawRecord = lOriginalDBF.RawRecord
+                            Loop While lOriginalDBF.FindNext(lSplitField, lSplitValue)
+                        End If
+                        lNewShapefile.StopEditingShapes()
+                        lNewShapefile.Save()
+                        lNewShapefile.Close()
+
+                        If lProjectionFile.Length > 0 Then
+                            lProjectionFilename = IO.Path.ChangeExtension(lNewShapefileName, "prj")
+                            atcUtility.SaveFileString(lProjectionFilename, lProjectionFile)
+                        End If
+
+                        lNewDBF.WriteFile(IO.Path.ChangeExtension(lNewShapefileName, "dbf"))
+                        lNewDBF.Clear()
+
+                        Logger.Progress(lSplitValueIndex + 1, lNumSplitValues)
+                    Next
+                End If
+                Logger.Status("")
+            End If
+        End If
+    End Sub
+
+End Module
