5d52c8675b1fdda30e613651ed31a1e67d0b4083
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2010-05-13 22:08:11 +0000

    Initial checkin of FRAMES SDM Project Builder based on customized BASINS components TODO: download data as in BatchSWAT instead of or in addition to BASINS core data       get additional parameters from user       process downloaded data as in BatchSWAT

---
+++ BASINS40/SDMProjectBuilder/frmBuildNew.vb
@@ -0,0 +1,153 @@
+Imports atcUtility
+Imports MapWinUtility
+
+Public Class frmBuildNew
+    Inherits System.Windows.Forms.Form
+
+#Region " Windows Form Designer generated code "
+
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        'Add any initialization after the InitializeComponent() call
+        txtInstructions.Text = "To Build a New " & g_AppNameShort & " Project, " & _
+           "zoom/pan to your geographic area of interest, select (highlight) it, " & _
+           "and then click 'Build'.  " & _
+           "If your area is outside the US, then click 'Build' " & _
+           "with no features selected to create an international project."
+
+    End Sub
+
+    'Form overrides dispose to clean up the component list.
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing Then
+            If Not (components Is Nothing) Then
+                components.Dispose()
+            End If
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    Friend WithEvents btnBuild As System.Windows.Forms.Button
+    Friend WithEvents txtInstructions As System.Windows.Forms.TextBox
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents txtSelected As System.Windows.Forms.TextBox
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        Me.btnBuild = New System.Windows.Forms.Button
+        Me.txtInstructions = New System.Windows.Forms.TextBox
+        Me.txtSelected = New System.Windows.Forms.TextBox
+        Me.btnCancel = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'btnBuild
+        '
+        Me.btnBuild.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBuild.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBuild.Location = New System.Drawing.Point(248, 200)
+        Me.btnBuild.Name = "btnBuild"
+        Me.btnBuild.Size = New System.Drawing.Size(80, 29)
+        Me.btnBuild.TabIndex = 1
+        Me.btnBuild.Text = "Build"
+        '
+        'txtInstructions
+        '
+        Me.txtInstructions.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtInstructions.BackColor = System.Drawing.SystemColors.Control
+        Me.txtInstructions.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtInstructions.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInstructions.Location = New System.Drawing.Point(13, 14)
+        Me.txtInstructions.Multiline = True
+        Me.txtInstructions.Name = "txtInstructions"
+        Me.txtInstructions.Size = New System.Drawing.Size(398, 83)
+        Me.txtInstructions.TabIndex = 2
+        Me.txtInstructions.TabStop = False
+        '
+        'txtSelected
+        '
+        Me.txtSelected.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSelected.BackColor = System.Drawing.SystemColors.Menu
+        Me.txtSelected.Location = New System.Drawing.Point(13, 97)
+        Me.txtSelected.Multiline = True
+        Me.txtSelected.Name = "txtSelected"
+        Me.txtSelected.Size = New System.Drawing.Size(398, 88)
+        Me.txtSelected.TabIndex = 3
+        Me.txtSelected.TabStop = False
+        Me.txtSelected.Text = "Selected Features:"
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(333, 200)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(80, 29)
+        Me.btnCancel.TabIndex = 4
+        Me.btnCancel.Text = "Cancel"
+        '
+        'frmBuildNew
+        '
+        Me.AcceptButton = Me.btnBuild
+        Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+        Me.CancelButton = Me.btnCancel
+        Me.ClientSize = New System.Drawing.Size(423, 239)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.txtSelected)
+        Me.Controls.Add(Me.txtInstructions)
+        Me.Controls.Add(Me.btnBuild)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmBuildNew"
+        Me.Opacity = 0.8
+        Me.Text = "Build New BASINS 4 Project"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+#End Region
+
+    Private Sub cmdBuild_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBuild.Click
+        SaveSetting(g_AppNameRegistry, "Window Positions", "BuildTop", Me.Top)
+        SaveSetting(g_AppNameRegistry, "Window Positions", "BuildLeft", Me.Left)
+        Me.Close()
+        SpecifyAndCreateNewProject()
+    End Sub
+
+    Private Sub frmBuildNew_Activated(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Activated
+        Me.Opacity = 1
+    End Sub
+
+    Private Sub frmBuildNew_Deactivate(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Deactivate
+        Me.Opacity = 0.8
+    End Sub
+
+    Private Sub frmBuildNew_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyValue = Windows.Forms.Keys.F1 Then
+            ShowHelp("BASINS Details\Welcome to BASINS 4 Window\Build BASINS Project.html")
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmBuildNew_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Icon = g_MapWin.ApplicationInfo.FormIcon
+        Me.Text = "Build New " & g_AppNameLong & " Project"
+    End Sub
+End Class
