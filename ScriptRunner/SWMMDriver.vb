e91ee185e739e990b88434e63767c32dcf5cecab
Jack Kittle <jack@gkfamily.org>
2010-07-07 13:45:30 +0000

    atcSWMMProject - change Run to RunSimulation, polish details SWMMDriver - test script for atcSWMMProject

---
+++ ScriptRunner/SWMMDriver.vb
@@ -0,0 +1,14 @@
+﻿Imports MapWindow.Interfaces
+Imports MapWinUtility
+Imports atcSWMM
+
+Module SWMMDriver
+    Public Function ScriptMain(ByRef aMapWin As IMapWin)
+        Logger.Dbg("SWMMDriver Entry")
+        Dim lSWMM As New SWMMProject
+        lSWMM.FileName = "c:\Program Files (x86)\EPA SWMM 5.0\Examples\Example1.inp"
+        Dim lResult As Boolean = lSWMM.RunSimulation()
+        Logger.Dbg("SWMM Simulation Complete " & lResult)
+        Return lResult
+    End Function
+End Module
