ad9068d833d7d8374bcb9613c96ea51e4eaeb3c9
Jack Kittle <jack@gkfamily.org>
2010-07-07 15:57:16 +0000

    atcSWMMProject - save all blocks as text on load, save all blocks SWMMDriver - add all standard examples(1,2,3) to driver

--- ScriptRunner/SWMMDriver.vb
+++ ScriptRunner/SWMMDriver.vb
@@ -3,12 +3,23 @@ Imports MapWinUtility
 Imports atcSWMM
 
 Module SWMMDriver
+    Private pPathName As String = "c:\Program Files (x86)\EPA SWMM 5.0\Examples\"
+    Private pFileNames() As String = {"Example1.inp", "Example2.inp", "Example3.inp"}
+
     Public Function ScriptMain(ByRef aMapWin As IMapWin)
         Logger.Dbg("SWMMDriver Entry")
-        Dim lSWMM As New SWMMProject
-        lSWMM.FileName = "c:\Program Files (x86)\EPA SWMM 5.0\Examples\Example1.inp"
-        Dim lResult As Boolean = lSWMM.RunSimulation()
-        Logger.Dbg("SWMM Simulation Complete " & lResult)
-        Return lResult
+        Dim lResultAll As Boolean = True
+        For Each lFileName As String In pFileNames
+            Logger.Dbg("Test " & lFileName)
+            Dim lSWMM As New SWMMProject
+            lSWMM.Load(pPathName & lFileName)
+            Dim lResult = lSWMM.RunSimulation()
+            Logger.Dbg("SWMM Simulation Complete " & lResult)
+ 
+            IO.Directory.SetCurrentDirectory(IO.Path.GetDirectoryName(pPathName))
+            lSWMM.Save(IO.Path.ChangeExtension(lFileName, ".xxx"))
+            lResultAll = lResult And lResultAll
+        Next
+        Return lResultAll
     End Function
 End Module
