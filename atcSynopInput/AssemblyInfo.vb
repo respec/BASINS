ebe5f06c8203823bd2407cf30f4d358f5f5492c9
Paul Meems <pmeems@users.noreply.github.com>
2010-12-19 23:20:55 +0000

    Updated files for VS2010

--- atcSynopInput/AssemblyInfo.vb
+++ atcSynopInput/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("3.1.1.0")> 
+<Assembly: AssemblyVersion("3.1.1.*")> 
 <Assembly: AssemblyFileVersion("3.1.1.0")> 
