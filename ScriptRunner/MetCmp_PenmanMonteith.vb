0c25b03501d69d541e24fb3425871e16fe462a14
Jack Kittle <jack@gkfamily.org>
2011-02-23 13:33:14 +0000

    atcMetCmp - start implementing SWAT/Cligen weather generator natively with focus on PenmanMonteith PET computation ScriptRunner - MetCmp_PenmanMonteith.vb - tests for SWAT/Cligen weather generator

---
+++ ScriptRunner/MetCmp_PenmanMonteith.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Collections.Specialized
+Imports MapWindow.Interfaces
+Imports MapWinUtility
+Imports atcMetCmp
+
+Module MetCmp_PenmanMonteith
+    Public Sub ScriptMain(ByRef aMapWin As IMapWin)
+        Logger.Dbg("MetCmp_PenmanMonteith Start")
+        Dim lStations As New SwatWeatherStations
+        Logger.Dbg("StationCount " & lStations.Count)
+    End Sub
+End Module
--- atcMetCmp/modSWAT_MetUtil.vb
+++ atcMetCmp/modSWAT_MetUtil.vb
@@ -72,56 +72,139 @@ Module modSWAT_MetUtil
         Return lAtri
     End Function
 
-    Sub Rdwgn()
+    ''' <summary>
+    ''' generates solar radiation 
+    ''' </summary>
+    ''' <param name="Precip"></param>
+    ''' <param name="SolarRadiationAver"></param>
+    ''' <remarks>based on SWAT 2005 routine SlrGen</remarks>
+    Function ComputeSolarRadiation(ByVal Precip As Double, ByVal SolarRadiationAver As Double)
+!!    ~ ~ ~ INCOMING VARIABLES ~ ~ ~
+!!    name        |units         |definition
+!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+!!    hru_rmx(:)  |MJ/m^2        |maximum possible radiation for the day in HRU
+!!    j           |none          |HRU number
+!!    i_mo        |none          |month being simulated
+!!    pr_w(3,:,:) |none          |proportion of wet days in a month
+!!    solarav(:,:)|MJ/m^2/day    |average daily solar radiation for the month
+!!    subp(:)     |mm H2O        |precipitation for the day in HRU
+!!    wgncur(3,:) |none          |parameter which predicts impact of precip on
+!!                               |daily solar radiation
+!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
+!!    ~ ~ ~ OUTGOING VARIABLES ~ ~ ~
+!!    name        |units         |definition
+!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+!!    hru_ra(:)   |MJ/m^2        |solar radiation for the day in HRU
+!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+!!    ~ ~ ~ LOCAL DEFINITIONS ~ ~ ~
+!!    name        |units         |definition
+!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+!!    rav         |MJ/m^2        |modified monthly average solar radiation
+!!    rx          |none          |variable to hold intermediate calculation
+!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+!!    ~ ~ ~ ~ ~ ~ END SPECIFICATIONS ~ ~ ~ ~ ~ ~
+
+        use(parm)
+
+      integer, intent (in) :: j
+
+real:   rx : , rav
+
+
+      rav = 0.
+      rav = solarav(i_mo,hru_sub(j)) /                                  &
+     &                              (1. - 0.5 * pr_w(3,i_mo,hru_sub(j)))
+      if (subp(j) > 0.0) rav = 0.5 * rav
+            rx = 0.
+            rx = hru_rmx(j) - rav
+      hru_ra(j) = rav + wgncur(3,j) * rx / 4.
+      if (hru_ra(j) <= 0.) hru_ra(j) = .05 * hru_rmx(j)
+
+            Return
+            End
+End Module
+
+Public Class SwatWeatherStations
+    Inherits System.Collections.ObjectModel.KeyedCollection(Of String, SwatWeatherStation)
+    Protected Overrides Function GetKeyForItem(ByVal aStation As SwatWeatherStation) As String
+        Return aStation.Key
+    End Function
+
+    Public Sub New(Optional ByVal aFileName As String = "AllStns.txt")
+        Dim lStationTable As New atcUtility.atcTableDelimited
+        With lStationTable
+            .Delimiter = " "
+            If IO.File.Exists(aFileName) Then
+                MapWinUtility.Logger.Dbg("UsingExternalFile " & aFileName & " in " & IO.Directory.GetCurrentDirectory)
+                .OpenFile(aFileName)
+            Else
+                MapWinUtility.Logger.Dbg("UsingInternalFile " & aFileName)
+                Dim lAssembly As System.Reflection.Assembly = System.Reflection.Assembly.GetExecutingAssembly
+                Dim lReader As New IO.StreamReader(lAssembly.GetManifestResourceStream("atcMetCmp." + aFileName))
+                .OpenString(lReader.ReadToEnd())
+            End If
+            MapWinUtility.Logger.Dbg("  Opened with FieldCount " & .NumFields & " RecordCount " & .NumRecords)
+            While Not .EOF
+                If .Value(1).ToString.Length > 0 Then
+                    AddStation(lStationTable)
+                End If
+                .MoveNext()
+            End While
+        End With
+        MapWinUtility.Logger.Dbg("  Built PointLocations with Count " & Count)
     End Sub
 
-    Public Class SwatWeatherStations
-        Inherits System.Collections.ObjectModel.KeyedCollection(Of String, SwatWeatherStation)
-        Protected Overrides Function GetKeyForItem(ByVal aStationLocation As SwatWeatherStation) As String
-            Return aStationLocation.Key
-        End Function
+    ''' <summary>Fill specific details about point from record in table</summary>
+    ''' <param name="aTable">table containing locations</param>
+    ''' <remarks>uses current record from table</remarks>
+    Public Sub AddStation(ByVal aTable As atcUtility.atcTable)
+        Dim lFieldValues() As String = Split(aTable.CurrentRecordAsDelimitedString, ",")
+        Dim lStation As New SwatWeatherStation
+        With lStation
+            .State = lFieldValues(0)
+            .NameKey = lFieldValues(1)
+            .Name = lFieldValues(2).Replace("_", " ")
+            .Id = lFieldValues(3)
+            .Lat = lFieldValues(4)
+            .Lng = lFieldValues(5)
+            For lMonthIndex As Integer = 1 To 12
+                .AirTempMaxAv(lMonthIndex) = lFieldValues(7 + lMonthIndex)
+                .AirTempMinAv(lMonthIndex) = lFieldValues(19 + lMonthIndex)
+                .SolarAv(lMonthIndex) = lFieldValues(139 + lMonthIndex)
+                .DewpointAv(lMonthIndex) = lFieldValues(151 + lMonthIndex)
+                .WindAv(lMonthIndex) = lFieldValues(163 + lMonthIndex)
+            Next
+        End With
+        Me.Add(lStation)
+    End Sub
+End Class
 
-        Public Sub New(Optional ByVal aFileName As String = "AllStns.txt")
-            Dim lStationTable As New atcUtility.atcTableDelimited
-            With lStationTable
-                .Delimiter = " """
-                If IO.File.Exists(aFileName) Then
-                    MapWinUtility.Logger.Dbg("UsingExternalFile " & aFileName & " in " & IO.Directory.GetCurrentDirectory)
-                    .OpenFile(aFileName)
-                Else
-                    MapWinUtility.Logger.Dbg("UsingInternalFile " & aFileName)
-                    Dim lAssembly As System.Reflection.Assembly = System.Reflection.Assembly.GetExecutingAssembly
-                    Dim lReader As New IO.StreamReader(lAssembly.GetManifestResourceStream("D4EMLite." + aFileName))
-                    .OpenString(lReader.ReadToEnd())
-                End If
-                MapWinUtility.Logger.Dbg("  Opened with FieldCount " & .NumFields & " RecordCount " & .NumRecords)
-                While Not .EOF
-                    If .Value(1).ToString.Length > 0 Then
-                        AddStation(lStationTable)
-                    End If
-                    .MoveNext()
-                End While
-            End With
-            MapWinUtility.Logger.Dbg("  Built PointLocations with Count " & Count)
-        End Sub
+Public Class SwatWeatherStation
+    Public State As String
+    Public NameKey As String
+    Public Name As String
+    Public Id As Integer
+    Public Lat As Double
+    Public Lng As Double
+    Public Elev As Double
+    Public PrecDaysPerMonth As Double
+    Public PrecAv(12) As Double
+    Public AirTempMaxAv(12) As Double '9-20
+    Public AirTempMinAv(12) As Double '21-32
+    Public Prob_WetAfterDry(12) As Double  '
+    Public Prob_WetAfterWet(12) As Double  '
+    Public SolarAv(12) As Double '141-152
+    Public DewpointAv(12) As Double '153-164
+    Public WindAv(12) As Double '165-176
 
-        ''' <summary>Fill specific details about point from record in table</summary>
-        ''' <param name="aTable">table containing locations</param>
-        ''' <remarks>uses current record from table</remarks>
-        Public Sub AddStation(ByVal aTable As atcUtility.atcTable)
+    ''' <summary></summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Overridable Function Key() As String
+        Return NameKey
+    End Function
+End Class
 
-        End Sub
-    End Class
-
-    Public Class SwatWeatherStation
-        Public Name As String
-
-        ''' <summary></summary>
-        ''' <returns></returns>
-        ''' <remarks></remarks>
-        Public Overridable Function Key() As String
-            Return Name
-        End Function
-    End Class
-End Module
