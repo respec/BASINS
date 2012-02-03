ebcbaf0f21e11958aee0f35d9665a2d176c92309
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2012-02-03 22:08:58 +0000

    added new plugin atcDownscaling

---
+++ atcDownscaling/clsDownscalingPlugin.vb
@@ -0,0 +1,51 @@
+﻿Imports atcData
+Imports atcUtility
+
+Imports MapWinUtility
+Imports atcData.atcDataManager
+
+Imports System.Reflection
+
+Public Class clsDownscalingPlugin
+    Inherits atcData.atcDataPlugin
+
+    Public Overrides ReadOnly Property Name() As String
+        Get
+            Return "Downscale"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Author() As String
+        Get
+            Return "AQUA TERRA Consultants"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Description() As String
+        Get
+            Return "An interface for downscaling code in the R environment."
+        End Get
+    End Property
+
+    <CLSCompliant(False)> _
+    Public Overrides Sub Initialize(ByVal aMapWin As MapWindow.Interfaces.IMapWin, ByVal aParentHandle As Integer)
+        MyBase.Initialize(aMapWin, aParentHandle)
+        atcMwGisUtility.GisUtil.MappingObject = aMapWin
+        AddMenuIfMissing(ModelsMenuName, "", ModelsMenuString, "mnuFile")
+        AddMenuIfMissing(ModelsMenuName & "_Downscale", ModelsMenuName, "Downscale")
+        'pWASPProject = New atcWASPProject
+    End Sub
+
+    Public Overrides Sub Terminate()
+        atcData.atcDataManager.RemoveMenuIfEmpty(ModelsMenuName & "_Downscale")
+        atcData.atcDataManager.RemoveMenuIfEmpty(ModelsMenuName)
+    End Sub
+
+    Public Overrides Sub ItemClicked(ByVal aItemName As String, ByRef aHandled As Boolean)
+        If aItemName = ModelsMenuName & "_Downscale" Then
+            Dim lForm As New frmDownscale
+            lForm.ShowDialog()
+        End If
+    End Sub
+
+End Class
