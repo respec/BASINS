885df2feb3fb1a30a90fcaa76f8788a99cd5e107
Jack Kittle <jack@gkfamily.org>
2012-02-28 17:13:15 +0000

    begin framing in opening of HSPFParm MDB file

---
+++ atcHSPFParm/HSPFParmDB.vb
@@ -0,0 +1,31 @@
+﻿Option Strict Off
+Option Explicit On
+
+Imports System.Collections.ObjectModel
+Imports MapWinUtility
+
+'Copyright 2012 AQUA TERRA Consultants - Royalty-free use permitted under open source license
+
+Public Class HSPFParmDB
+    Public Name As String = ""
+
+    Public Sub Open(ByVal aFilename As String)
+        Name = aFilename
+        If Name.Length = 0 Then
+            Name = "HSPFParm2003.mdb"
+        End If
+        If Not IO.File.Exists(Name) Then
+            Name = GetSetting("HSPF", "ParameterMDB", "Path")
+            If Not IO.File.Exists(Name) Then
+                Name = atcUtility.FindFile("Please locate 'HSPFParm2003.mdb' in a writable directory", "HSPFParm2003.mdb")
+                SaveSetting("HSPF", "ParameterMDB", "Path", Name)
+            End If
+        End If
+
+        Dim pParmMDB As New atcUCI.atcMDB(Name)
+    End Sub
+
+    Public Sub New(ByVal aFilename As String)
+        Open(aFilename)
+    End Sub
+End Class
