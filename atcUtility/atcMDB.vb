71e3604b8e7caffcea89cd4d9036611624f86bc6
tong.zhai <tong.zhai@respec.com>
2012-06-08 14:01:57 +0000

    atcMDB.vb   Add 'Name' to hold its file name   more safe guard for opening connection to mdb files

--- atcUtility/atcMDB.vb
+++ atcUtility/atcMDB.vb
@@ -10,6 +10,7 @@ Public Class atcMDB
     Private pDataBaseType As String = ""
     Private pConnection As Object = Nothing
     Private pServiceManager As Object = Nothing
+    Public Name As String 'this is the MDB file's full path and name
 
     ''' <summary>
     ''' Open an MDB file given its filename
@@ -28,20 +29,32 @@ Public Class atcMDB
                 pDataBaseType = "ODBC" ',"OLE",
         End Select
 
-        Select Case pDataBaseType
-            Case "ODBC"
-                pConnection = New OdbcConnection("Driver={Microsoft Access Driver (*.mdb)};Dbq=" & aFilename & ";")
-            Case "OLE"
-                pConnection = New OleDbConnection
-                pConnection.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & aFilename & ";"
-            Case "OO"
-                'Root object for accessing OpenOffice from VB    
-                pServiceManager = CreateObject("com.sun.star.ServiceManager")
-                Dim lDatabaseContext As Object = pServiceManager.createInstance("com.sun.star.sdb.DatabaseContext")
-                Dim lDataSource As Object = lDatabaseContext.getByName("file:///" & IO.Path.ChangeExtension(aFilename, "odb"))
-                'TODO: points to mdb in the OO program folder - now kludged by making a copy there.
-                pConnection = lDataSource.getConnection("", "")
-        End Select
+        Dim lOpened As Boolean = False
+        Try
+            Select Case pDataBaseType
+                Case "ODBC"
+                    pConnection = New OdbcConnection("Driver={Microsoft Access Driver (*.mdb)};Dbq=" & aFilename & ";")
+                Case "OLE"
+                    pConnection = New OleDbConnection
+                    pConnection.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & aFilename & ";"
+                Case "OO"
+                    'Root object for accessing OpenOffice from VB    
+                    pServiceManager = CreateObject("com.sun.star.ServiceManager")
+                    Dim lDatabaseContext As Object = pServiceManager.createInstance("com.sun.star.sdb.DatabaseContext")
+                    Dim lDataSource As Object = lDatabaseContext.getByName("file:///" & IO.Path.ChangeExtension(aFilename, "odb"))
+                    'TODO: points to mdb in the OO program folder - now kludged by making a copy there.
+                    pConnection = lDataSource.getConnection("", "")
+            End Select
+            lOpened = True
+        Catch ex As Exception
+            Throw New ApplicationException("Failed to open " & aFilename)
+        End Try
+
+        If lOpened Then
+            Name = aFilename
+        Else
+            Name = ""
+        End If
     End Sub
 
     ''' <summary>
@@ -51,10 +64,14 @@ Public Class atcMDB
     ''' <returns>System.Data.DataTable</returns>
     ''' <remarks>aTableName can be a query, "SELECT * from " will be prepended within GetTable</remarks>
     Public Function GetTable(ByVal aTableName As String) As DataTable
+
+        Dim lSQL As String = aTableName
+        If Not aTableName.ToUpper.StartsWith("SELECT") Then lSQL = "SELECT * from " & aTableName
+
         If pDataBaseType = "OO" Then
             Dim lTable As New DataTable
             Dim lStatement As Object = pConnection.createStatement
-            Dim lSQL As String = "SELECT * FROM " & aTableName
+
             Dim lResultSet As Object = lStatement.executeQuery(lSQL)
             If lResultSet Is Nothing Then
                 Debug.Print("No Result From Query '" & lSQL & "'")
@@ -84,15 +101,11 @@ Public Class atcMDB
         Else
             pConnection.Open()
             Dim lDataAdapter As Object = Nothing
-            Dim lStr As String = aTableName
-            If Not aTableName.StartsWith("SELECT") Then
-                lStr = "SELECT * from " & aTableName
-            End If
             Select Case pDataBaseType
                 Case "ODBC"
-                    lDataAdapter = New OdbcDataAdapter(lStr, DirectCast(pConnection, OdbcConnection))
+                    lDataAdapter = New OdbcDataAdapter(lSQL, DirectCast(pConnection, OdbcConnection))
                 Case "OLE"
-                    lDataAdapter = New OleDbDataAdapter(lStr, DirectCast(pConnection, OleDbConnection))
+                    lDataAdapter = New OleDbDataAdapter(lSQL, DirectCast(pConnection, OleDbConnection))
             End Select
             Dim lDataSet As New DataSet
             lDataAdapter.Fill(lDataSet)
