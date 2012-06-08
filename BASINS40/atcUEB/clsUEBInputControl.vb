b18c25adf2dc6bf4e3cef40e227303c9f53735c0
prhummel <prhummel@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2012-06-08 02:54:07 +0000

    atcUEB.vbproj - added clsUEBOutputControl.vb clsUEBInputControl.vb - added start/end date and time step/UTC offset control variables clsUEBOutputControl.vb - very early draft frmUEB.vb - removed unneeded met timeseries controls, added Input controls for start/end date and time step/UTC offset

--- BASINS40/atcUEB/clsUEBInputControl.vb
+++ BASINS40/atcUEB/clsUEBInputControl.vb
@@ -21,10 +21,18 @@ Public Class clsUEBInputControl
 
     Public Variables As Generic.List(Of clsUEBVariable)
     Public Header As String
+    Public SDate(4) As Integer
+    Public EDate(4) As Integer
+    Public TimeStep As Integer
+    Public UTCOffset As Integer
 
     Public FileName As String
 
     Public Sub New(ByVal aFilename As String)
+
+        Dim lStr As String
+        Dim lChrSep() As String = {" ", "."}
+        Dim lStrArray() As String
 
         FileName = aFilename
         Dim lFileContents As String
@@ -35,6 +43,21 @@ Public Class clsUEBInputControl
         End If
         'read header line in file
         Header = StrSplit(lFileContents, vbCrLf, "")
+        'read start/end dates
+        lStr = StrSplit(lFileContents, vbCrLf, "")
+        lStr = ReplaceRepeats(lStr, " ") 'remove extra blanks
+        lStrArray = lStr.Split(lChrSep, StringSplitOptions.None)
+        Array.Copy(lStrArray, SDate, SDate.Length)
+        lStr = StrSplit(lFileContents, vbCrLf, "")
+        lStr = ReplaceRepeats(lStr, " ") 'remove extra blanks
+        lStrArray = lStr.Split(lChrSep, StringSplitOptions.None)
+        Array.Copy(lStrArray, EDate, EDate.Length)
+        'read time step and UTC offset
+        lStr = StrSplit(lFileContents, vbCrLf, "")
+        TimeStep = CInt(StrSplit(lFileContents, " ", ""))
+        lStr = StrSplit(lFileContents, vbCrLf, "")
+        UTCOffset = CInt(StrSplit(lFileContents, " ", ""))
+
         While lFileContents.Length > 0
             Variables.Add(clsUEBVariable.FromInputVariableString(lFileContents))
         End While
@@ -46,6 +69,9 @@ Public Class clsUEBInputControl
 
         If FileName.Length > 0 Then
             Try
+                lStr &= SDate(0) & " " & SDate(1) & " " & SDate(2) & " " & SDate(3) & "." & SDate(4) & vbCrLf
+                lStr &= EDate(0) & " " & EDate(1) & " " & EDate(2) & " " & EDate(3) & "." & EDate(4) & vbCrLf
+                lStr &= TimeStep & vbCrLf & UTCOffset & vbCrLf
                 For Each lUEBParm As clsUEBVariable In Variables
                     lStr &= lUEBParm.InputVariableString
                 Next
@@ -61,41 +87,41 @@ Public Class clsUEBInputControl
 
     Public Sub ReadWeatherFile()
 
-        Dim lSR As New IO.StreamReader(FileName)
-        Dim lStr As String
-        Dim lCnt As Integer = 9
-        Dim lDate(5) As Integer
-        Dim lCurDate As Double
-        Dim lStrArray(7) As String
-        Dim lChrSep() As String = {" ", vbTab, vbCrLf}
+        'Dim lSR As New IO.StreamReader(FileName)
+        'Dim lStr As String
+        'Dim lCnt As Integer = 9
+        'Dim lDate(5) As Integer
+        'Dim lCurDate As Double
+        'Dim lStrArray(7) As String
+        'Dim lChrSep() As String = {" ", vbTab, vbCrLf}
 
-        lStr = lSR.ReadLine
-        lStrArray = lStr.Split(lChrSep, lCnt, StringSplitOptions.None)
-        lDate(1) = CInt(lStrArray(0))
-        lDate(2) = CInt(lStrArray(1))
-        lDate(0) = CInt(lStrArray(2))
-        If lDate(0) < 100 Then
-            lDate(0) += 1900
-        End If
-        lDate(3) = CInt(lStrArray(3))
-        SJDate = Date2J(lDate)
-        TimeStep = CInt(lStrArray(4))
-        InitialEnergy = CDbl(lStrArray(5))
-        InitialH2OEquiv = CDbl(lStrArray(6))
-        InitialSnowAge = CDbl(lStrArray(7))
+        'lStr = lSR.ReadLine
+        'lStrArray = lStr.Split(lChrSep, lCnt, StringSplitOptions.None)
+        'lDate(1) = CInt(lStrArray(0))
+        'lDate(2) = CInt(lStrArray(1))
+        'lDate(0) = CInt(lStrArray(2))
+        'If lDate(0) < 100 Then
+        '    lDate(0) += 1900
+        'End If
+        'lDate(3) = CInt(lStrArray(3))
+        'SJDate = Date2J(lDate)
+        'TimeStep = CInt(lStrArray(4))
+        'InitialEnergy = CDbl(lStrArray(5))
+        'InitialH2OEquiv = CDbl(lStrArray(6))
+        'InitialSnowAge = CDbl(lStrArray(7))
 
-        lCurDate = SJDate
-        lStr = lSR.ReadLine
-        lStrArray = lStr.Split(lChrSep, lCnt, StringSplitOptions.None)
-        NSkip = lStrArray(0)
-        If NSkip > 0 Then
-            For i As Integer = 1 To NSkip
-                lStr = lSR.ReadLine
-            Next
-            lCurDate += TimeStep * NSkip / 24
-        End If
+        'lCurDate = SJDate
+        'lStr = lSR.ReadLine
+        'lStrArray = lStr.Split(lChrSep, lCnt, StringSplitOptions.None)
+        'NSkip = lStrArray(0)
+        'If NSkip > 0 Then
+        '    For i As Integer = 1 To NSkip
+        '        lStr = lSR.ReadLine
+        '    Next
+        '    lCurDate += TimeStep * NSkip / 24
+        'End If
 
-        'TODO: read weather data into TSers
+        ''TODO: read weather data into TSers
 
     End Sub
 
