c6b527f547987e5f638fed41bdd7f9ab07e187d2
tzhai <tzhai@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2012-06-13 16:21:28 +0000

    atcHSPFParm   -New frmBulletin, for displaying parameter range violation per operation during import or add scenario   -new modHSPFParmUtil.vb, contains standalone routines for adding watershed, scenarios (largely refactored from frmAddScenario.vb)   -frmHSPFParm.vb, add import capability to do batch import of UCI parameters, some refactoring

---
+++ BASINS40/atcHSPFParm/frmBulletin.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBulletin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmBulletin))
+        Me.txtDataSummary = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'txtDataSummary
+        '
+        Me.txtDataSummary.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtDataSummary.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtDataSummary.Location = New System.Drawing.Point(0, 0)
+        Me.txtDataSummary.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtDataSummary.Multiline = True
+        Me.txtDataSummary.Name = "txtDataSummary"
+        Me.txtDataSummary.ReadOnly = True
+        Me.txtDataSummary.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDataSummary.Size = New System.Drawing.Size(522, 473)
+        Me.txtDataSummary.TabIndex = 0
+        '
+        'frmBulletin
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(522, 473)
+        Me.Controls.Add(Me.txtDataSummary)
+        Me.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmBulletin"
+        Me.Text = "Data Summary"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtDataSummary As System.Windows.Forms.TextBox
+End Class
---
+++ BASINS40/atcHSPFParm/frmBulletin.vb
@@ -0,0 +1,29 @@
+﻿Public Class frmBulletin
+
+    Public Sub New(ByVal aSummary As String)
+
+        ' This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        txtDataSummary.Text = aSummary
+    End Sub
+
+    Public Sub ClearSelection()
+        txtDataSummary.SelectionStart = 0
+    End Sub
+
+    Public Sub ShowTimedDialog(ByVal aTimeInterval As Integer)
+        Dim lTimer As New System.Windows.Forms.Timer
+        lTimer.Interval = aTimeInterval
+        AddHandler lTimer.Tick, AddressOf Timer_Tick
+        lTimer.Enabled = True
+
+        Me.Show()
+    End Sub
+
+    Private Sub Timer_Tick(ByVal Sender As Object, ByVal aEventArg As EventArgs)
+        txtDataSummary.Text = ""
+        Me.Close()
+    End Sub
+End Class
