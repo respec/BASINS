1cd11904163aeeb22788ed8496674d297be3bfef
prhummel <prhummel@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2012-08-15 18:51:56 +0000

    atcDataSourceTimeseriesUEBGrid.vb - Refined Open to properly process latest aggregate output format from Dave Tarboton (UEBGrid author).

--- BASINS40/atcTimeseriesUEBGrid/atcDataSourceTimeseriesUEBGrid.vb
+++ BASINS40/atcTimeseriesUEBGrid/atcDataSourceTimeseriesUEBGrid.vb
@@ -41,63 +41,68 @@ Public Class atcDataSourceTimeseriesUEBGrid
     End Property
 
     Public Overrides Function Open(ByVal aFileName As String, Optional ByVal aAttributes As atcData.atcDataAttributes = Nothing) As Boolean
-        '        If MyBase.Open(aFileName, aAttributes) Then
-        Dim lFileContents As String = WholeFileString(aFileName)
-        Dim lRec As String
-        Dim lDate(4) As Integer
-        Dim lChrSepDates() As String = {" ", ".", vbTab}
-        Dim lChrSepVals() As String = {" ", vbTab}
-        Dim lDateStrArray() As String
-        Dim lValStrArray() As String
-        Dim lVarName As String
-        Dim lWatershedID As Integer
-        Dim lJDate As Double
-        Dim lVal As Double
-        Dim lDatasets As New atcTimeseriesGroup
-        Dim lGroupBuilder As New atcTimeseriesGroupBuilder(Me)
-        Dim lTSBuilder As atcTimeseriesBuilder
+        If MyBase.Open(aFileName, aAttributes) Then
+            Dim lFileContents As String = WholeFileString(Specification)
+            Dim lRec As String
+            Dim lDate(4) As Integer
+            Dim lChrSepVals() As String = {" ", vbTab}
+            Dim lStrArray() As String
+            Dim lValStrArray() As String
+            Dim lVarName As String
+            Dim lWatershedID As Integer
+            Dim lJDate As Double
+            Dim lVal As Double
+            Dim lDatasets As New atcTimeseriesGroup
+            Dim lGroupBuilder As New atcTimeseriesGroupBuilder(Me)
+            Dim lTSBuilder As atcTimeseriesBuilder
 
-        Specification = aFileName
-        Logger.Status("Opening '" & Specification & "'", True)
-        Try
-            While lFileContents.Length > 0
-                lRec = StrSplit(lFileContents, vbCrLf, "")
-                lDateStrArray = lRec.Split(lChrSepDates, StringSplitOptions.None)
-                For i As Integer = 0 To 4
-                    Integer.TryParse(lDateStrArray(i), lDate(i))
-                Next
-                If lDate(4) > 0 Then 'process fraction of hour
-                    lDate(4) = lDate(4) / 100 * 60
-                End If
-                lRec = StrSplit(lFileContents, vbCrLf, "") 'SHOULD BE REMOVED
-                lValStrArray = lRec.Split(lChrSepVals, StringSplitOptions.None)
-                lVarName = lValStrArray(0)
-                lRec = StrSplit(lFileContents, vbCrLf, "") 'SHOULD BE REMOVED
-                lValStrArray = lRec.Split(lChrSepVals, StringSplitOptions.None)
-                Integer.TryParse(lValStrArray(1), lWatershedID)
-                Double.TryParse(lValStrArray(2), lVal)
-                lJDate = Jday(lDate(0), lDate(1), lDate(2), lDate(3), lDate(4), 0)
-                lTSBuilder = lGroupBuilder.Builder(lVarName & ":" & lWatershedID.ToString)
-                With lTSBuilder.Attributes
-                    'Set attributes of newly created builder
-                    If Not .ContainsAttribute("Location") Then
-                        .SetValue("Scenario", FilenameNoExt(FilenameNoPath(aFileName)))
-                        .SetValue("Location", lWatershedID)
-                        .SetValue("Constituent", lVarName)
-                        .SetValue("Description", "UEB Aggregated Output")
-                        .AddHistory("Read from " & Specification)
+            Logger.Status("Opening '" & Specification & "'", True)
+            Try
+                Do 'may need to process header record
+                    lRec = StrSplit(lFileContents, vbCrLf, "")
+                    lRec = ReplaceRepeats(lRec, " ")
+                    lStrArray = lRec.Split(lChrSepVals, StringSplitOptions.None)
+                Loop Until IsNumeric(lStrArray(0))
+                While lFileContents.Length > 0
+                    For i As Integer = 0 To 2
+                        Integer.TryParse(lStrArray(i), lDate(i))
+                    Next
+                    If lStrArray(3).Contains(".") Then 'process fraction of hour
+                        Integer.TryParse(lStrArray(3).Substring(0, lStrArray(3).IndexOf(".")), lDate(3))
+                        Integer.TryParse(lStrArray(3).Substring(lStrArray(3).IndexOf(".") + 1), lDate(4))
+                        lDate(4) = lDate(4) / 100 * 60
+                    Else
+                        Integer.TryParse(lStrArray(3), lDate(3))
+                        lDate(4) = 0
                     End If
-                End With
-                lTSBuilder.AddValue(lJDate, lVal)
-            End While
-            lGroupBuilder.CreateTimeseriesAddToGroup(DataSets)
-            Open = True
+                    lVarName = lStrArray(4)
+                    Integer.TryParse(lStrArray(5), lWatershedID)
+                    Double.TryParse(lStrArray(6), lVal)
+                    lJDate = Jday(lDate(0), lDate(1), lDate(2), lDate(3), lDate(4), 0)
+                    lTSBuilder = lGroupBuilder.Builder(lVarName & ":" & lWatershedID.ToString)
+                    With lTSBuilder.Attributes
+                        'Set attributes of newly created builder
+                        If Not .ContainsAttribute("Location") Then
+                            .SetValue("Scenario", FilenameNoExt(FilenameNoPath(Specification)))
+                            .SetValue("Location", lWatershedID)
+                            .SetValue("Constituent", lVarName)
+                            .SetValue("Description", "UEB Aggregated Output")
+                            .AddHistory("Read from " & Specification)
+                        End If
+                    End With
+                    lTSBuilder.AddValue(lJDate, lVal)
+                    lRec = StrSplit(lFileContents, vbCrLf, "")
+                    lRec = ReplaceRepeats(lRec, " ")
+                    lStrArray = lRec.Split(lChrSepVals, StringSplitOptions.None)
+                End While
+                lGroupBuilder.CreateTimeseriesAddToGroup(DataSets)
+                Open = True
 
-        Catch endEx As Exception
-            Open = False
-        End Try
-        Logger.Status("")
-        '        End If
+            Catch endEx As Exception
+                Open = False
+            End Try
+            Logger.Status("")
+        End If
     End Function
 
     Public Sub New()
