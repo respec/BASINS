eb450055c2897bd39e5919d63f013dc82b939585
Jack Kittle <jack@gkfamily.org>
2012-11-20 19:05:08 +0000

    write dump to folder with name of library version, only write netcdf fileId and version if debug flag on, add more test data

--- netCDF/netCDFViewer/netCDFViewer.vb
+++ netCDF/netCDFViewer/netCDFViewer.vb
@@ -1,5 +1,7 @@
 Module netCDFViewer
     Sub main()
+        Dim lDebug As Boolean = False
+
         If IO.File.Exists("netcdf.dll") Then IO.File.Delete("netcdf.dll")
         'IO.File.Copy("netcdf4.2.1.1.dll", "netcdf.dll")
         IO.File.Copy("netcdf3.6.dll", "netcdf.dll")
@@ -7,132 +9,139 @@ Module netCDFViewer
         Dim lReport As New System.Text.StringBuilder
 
         Dim lNetCDF_Version As String = NetCDF.nc_inq_libvers
-        lReport.AppendLine(lNetCDF_Version)
         Dim lPathName As String = "..\..\..\..\Data\"
-        Dim lFileName As String = lPathName & "watershed.nc"
-        lReport.AppendLine("File '" & lFileName & "'")
+        Dim lOutFolder As String = lPathName & lNetCDF_Version.Substring(0, 3) & ".dump\"
+        If Not IO.Directory.Exists(lOutFolder) Then IO.Directory.CreateDirectory(lOutFolder)
 
-        Dim lNCId As Int32
-        Dim lResult As Int32 = NetCDF.nc_open(lFileName, _
-                                              NetCDF.cmode.NC_NOWRITE, _
-                                              lNCId)
-        If lResult <> 0 Then
-            lReport.AppendLine("Open Problem " & NetCDF.nc_strerror(lResult))
-        Else
-            lReport.AppendLine("Open OK, ID: " & lNCId)
-            Dim lNDims As Int32
-            Dim lNVars As Int32
-            Dim lNGatts As Int32
-            Dim lUnlimdimid As Int32
-            NetCDF.nc_inq(lNCId, lNDims, lNVars, lNGatts, lUnlimdimid)
-            lReport.AppendLine("NDims " & lNDims & " NVars " & lNVars & " NGatts " & lNGatts & " Unlimdim id " & lUnlimdimid)
+        Dim lBaseNames() As String = {"aspect", "ccgridfile", "hcanfile", "lafile", "lat", "longitude", "slope", "SubType", "Ta1", "Watershed"}
+        For Each lBaseName As String In lBaseNames
+            If lDebug Then lReport.AppendLine(lNetCDF_Version)
+            Dim lFileName As String = lPathName & lBaseName & ".nc"
+            lReport.AppendLine("File '" & lFileName & "'")
 
-            Dim lLen As Int32
-            For lDimId As Integer = 0 To lNDims - 1
-                Dim lName As New System.Text.StringBuilder(NetCDF.netCDF_limits.NC_MAX_NAME)
-                NetCDF.nc_inq_dim(lNCId, lDimId, lName, lLen)
-                lReport.AppendLine("  DimId " & lDimId & " Name '" & lName.ToString & "' Len " & lLen)
-            Next
+            Dim lNCId As Int32
+            Dim lResult As Int32 = NetCDF.nc_open(lFileName, _
+                                                  NetCDF.cmode.NC_NOWRITE, _
+                                                  lNCId)
+            If lResult <> 0 Then
+                lReport.AppendLine("Open Problem " & NetCDF.nc_strerror(lResult))
+            Else
+                If lDebug Then lReport.AppendLine("Open OK, ID: " & lNCId)
+                Dim lNDims As Int32
+                Dim lNVars As Int32
+                Dim lNGatts As Int32
+                Dim lUnlimdimid As Int32
+                NetCDF.nc_inq(lNCId, lNDims, lNVars, lNGatts, lUnlimdimid)
+                lReport.AppendLine("NDims " & lNDims & " NVars " & lNVars & " NGatts " & lNGatts & " Unlimdim id " & lUnlimdimid)
 
-            For lVarId As Integer = NetCDF.NC_GLOBAL To lNVars - 1 'start with global at -1
-                If lVarId = -1 Then
-                    lReport.AppendLine("Global")
-                    lReport.Append(AttsToString(lNCId, lVarId))
-                Else
-                    lResult = NetCDF.nc_inq_varndims(lNCId, lVarId, lNDims)
-                    If lResult <> 0 Then
-                        lReport.AppendLine(ErrorString(lResult, lNCId, lVarId))
+                Dim lLen As Int32
+                For lDimId As Integer = 0 To lNDims - 1
+                    Dim lName As New System.Text.StringBuilder(NetCDF.netCDF_limits.NC_MAX_NAME)
+                    NetCDF.nc_inq_dim(lNCId, lDimId, lName, lLen)
+                    lReport.AppendLine("  DimId " & lDimId & " Name '" & lName.ToString & "' Len " & lLen)
+                Next
+
+                For lVarId As Integer = NetCDF.NC_GLOBAL To lNVars - 1 'start with global at -1
+                    If lVarId = -1 Then
+                        lReport.AppendLine("Global")
+                        lReport.Append(AttsToString(lNCId, lVarId))
                     Else
-                        Dim lXtype As NetCDF.nc_type
-                        Dim lDimIds(lNDims) As Int32
-                        Dim lNAtts As Int32
-                        Dim lName As New System.Text.StringBuilder(NetCDF.netCDF_limits.NC_MAX_NAME)
-                        NetCDF.nc_inq_var(lNCId, lVarId, lName, lXtype, lNDims, lDimIds, lNAtts)
-                        lReport.Append("VarId " & lVarId & " Name '" & lName.ToString & _
-                                           "' Type " & lXtype & " NAtts " & lNAtts & _
-                                           " NDims " & lNDims)
-                        Dim lDimString As String = ""
-                        Dim lDimLen As Integer
-                        Dim lCount() As Integer
-                        If lNDims > 0 Then
-                            ReDim lCount(lNDims - 1)
-                            lDimString = " Dims '"
-                            For lDimIndex As Integer = 0 To lNDims - 1
-                                lResult = NetCDF.nc_inq_dimlen(lNCId, lDimIds(lDimIndex), lDimLen)
-                                lCount(lDimIndex) = lDimLen
-                                lDimString &= Str(lDimIds(lDimIndex)) & ":" & lDimLen & ", "
-                            Next
-                            lDimString = lDimString.Remove(lDimString.Length - 2)
-                            lDimString &= "'"
-                            lReport.AppendLine(lDimString)
-                            lReport.Append(AttsToString(lNCId, lVarId))
-                            If lDimLen > 0 Then
-                                Dim lBase(lNDims) As Integer
-
-                                Dim lArraySize As Integer = 1
+                        lResult = NetCDF.nc_inq_varndims(lNCId, lVarId, lNDims)
+                        If lResult <> 0 Then
+                            lReport.AppendLine(ErrorString(lResult, lNCId, lVarId))
+                        Else
+                            Dim lXtype As NetCDF.nc_type
+                            Dim lDimIds(lNDims) As Int32
+                            Dim lNAtts As Int32
+                            Dim lName As New System.Text.StringBuilder(NetCDF.netCDF_limits.NC_MAX_NAME)
+                            NetCDF.nc_inq_var(lNCId, lVarId, lName, lXtype, lNDims, lDimIds, lNAtts)
+                            lReport.Append("VarId " & lVarId & " Name '" & lName.ToString & _
+                                               "' Type " & lXtype & " NAtts " & lNAtts & _
+                                               " NDims " & lNDims)
+                            Dim lDimString As String = ""
+                            Dim lDimLen As Integer
+                            Dim lCount() As Integer
+                            If lNDims > 0 Then
+                                ReDim lCount(lNDims - 1)
+                                lDimString = " Dims '"
                                 For lDimIndex As Integer = 0 To lNDims - 1
-                                    lArraySize *= lCount(lDimIndex)
+                                    lResult = NetCDF.nc_inq_dimlen(lNCId, lDimIds(lDimIndex), lDimLen)
+                                    lCount(lDimIndex) = lDimLen
+                                    lDimString &= Str(lDimIds(lDimIndex)) & ":" & lDimLen & ", "
                                 Next
-                                Dim lValuesDouble(lArraySize - 1) As Double
-                                Dim lValuesFloat(lArraySize - 1) As Single
-                                Dim lValuesInt(lArraySize - 1) As Int32
-                                Select Case lXtype
-                                    Case NetCDF.nc_type.NC_DOUBLE
-                                        lResult = NetCDF.nc_get_vara_double(lNCId, lVarId, lBase, lCount, lValuesDouble)
-                                    Case NetCDF.nc_type.NC_FLOAT
-                                        lResult = NetCDF.nc_get_vara_float(lNCId, lVarId, lBase, lCount, lValuesFloat)
-                                    Case NetCDF.nc_type.NC_INT
-                                        lResult = NetCDF.nc_get_vara_int(lNCId, lVarId, lBase, lCount, lValuesInt)
-                                End Select
-                                If lResult <> 0 Then
-                                    lReport.AppendLine(ErrorString(lResult, lNCId, lVarId))
-                                Else
-                                    Dim lOutputPosition() = {0, 0, 0, 0, 0}
+                                lDimString = lDimString.Remove(lDimString.Length - 2)
+                                lDimString &= "'"
+                                lReport.AppendLine(lDimString)
+                                lReport.Append(AttsToString(lNCId, lVarId))
+                                If lDimLen > 0 Then
+                                    Dim lBase(lNDims) As Integer
 
-                                    Dim lFormatValueIndex As String = "0"
-                                    If lArraySize > 1 Then
-                                        lFormatValueIndex = StrDup(CInt(Math.Ceiling(Math.Log10(lArraySize - 1))), "0")
-                                    End If
-                                    Dim lFormatDimIndex(lNDims)
+                                    Dim lArraySize As Integer = 1
                                     For lDimIndex As Integer = 0 To lNDims - 1
-                                        Dim lDimPosMax As Integer = lBase(lDimIndex) + lCount(lDimIndex) - 1
-                                        If lDimPosMax > 0 Then
-                                            lFormatDimIndex(lDimIndex) = StrDup(CInt(Math.Ceiling(Math.Log10(lDimPosMax))), "0")
-                                        Else
-                                            lFormatDimIndex(lDimIndex) = "0"
+                                        lArraySize *= lCount(lDimIndex)
+                                    Next
+                                    Dim lValuesDouble(lArraySize - 1) As Double
+                                    Dim lValuesFloat(lArraySize - 1) As Single
+                                    Dim lValuesInt(lArraySize - 1) As Int32
+                                    Select Case lXtype
+                                        Case NetCDF.nc_type.NC_DOUBLE
+                                            lResult = NetCDF.nc_get_vara_double(lNCId, lVarId, lBase, lCount, lValuesDouble)
+                                        Case NetCDF.nc_type.NC_FLOAT
+                                            lResult = NetCDF.nc_get_vara_float(lNCId, lVarId, lBase, lCount, lValuesFloat)
+                                        Case NetCDF.nc_type.NC_INT
+                                            lResult = NetCDF.nc_get_vara_int(lNCId, lVarId, lBase, lCount, lValuesInt)
+                                    End Select
+                                    If lResult <> 0 Then
+                                        lReport.AppendLine(ErrorString(lResult, lNCId, lVarId))
+                                    Else
+                                        Dim lOutputPosition() = {0, 0, 0, 0, 0}
+
+                                        Dim lFormatValueIndex As String = "0"
+                                        If lArraySize > 1 Then
+                                            lFormatValueIndex = StrDup(CInt(Math.Ceiling(Math.Log10(lArraySize - 1))), "0")
                                         End If
-                                    Next
-                                    For lValueIndex As Integer = 0 To lArraySize - 1
-                                        Dim lString As String = "    (" & Format(lValueIndex, lFormatValueIndex) & "-"
+                                        Dim lFormatDimIndex(lNDims)
                                         For lDimIndex As Integer = 0 To lNDims - 1
-                                            Dim lDimPos As Integer = lBase(lDimIndex) + lOutputPosition(lDimIndex)
-                                            lString &= Format(lDimPos, lFormatDimIndex(lDimIndex)) & ":"
+                                            Dim lDimPosMax As Integer = lBase(lDimIndex) + lCount(lDimIndex) - 1
+                                            If lDimPosMax > 0 Then
+                                                lFormatDimIndex(lDimIndex) = StrDup(CInt(Math.Ceiling(Math.Log10(lDimPosMax))), "0")
+                                            Else
+                                                lFormatDimIndex(lDimIndex) = "0"
+                                            End If
                                         Next
-                                        lString = lString.Remove(lString.Length - 1) & ") "
-                                        Select Case lXtype
-                                            Case NetCDF.nc_type.NC_DOUBLE
-                                                lReport.AppendLine(lString & lValuesDouble(lValueIndex))
-                                            Case NetCDF.nc_type.NC_FLOAT
-                                                Dim lValue As Double = lValuesFloat(lValueIndex) * 10800 'sec/3hr TODO: make generic!
-                                                lReport.AppendLine(lString & Format(lValue, "##0.000"))
-                                        End Select
-                                        Dim lOutIndex As Integer = lNDims - 1
-                                        lOutputPosition(lOutIndex) += 1
-                                        While lOutIndex > 0 AndAlso lOutputPosition(lOutIndex) = lCount(lOutIndex)
-                                            lOutputPosition(lOutIndex - 1) += 1
-                                            lOutputPosition(lOutIndex) = 0
-                                            lOutIndex -= 1
-                                        End While
-                                    Next
+                                        For lValueIndex As Integer = 0 To lArraySize - 1
+                                            Dim lString As String = "    (" & Format(lValueIndex, lFormatValueIndex) & "-"
+                                            For lDimIndex As Integer = 0 To lNDims - 1
+                                                Dim lDimPos As Integer = lBase(lDimIndex) + lOutputPosition(lDimIndex)
+                                                lString &= Format(lDimPos, lFormatDimIndex(lDimIndex)) & ":"
+                                            Next
+                                            lString = lString.Remove(lString.Length - 1) & ") "
+                                            Select Case lXtype
+                                                Case NetCDF.nc_type.NC_DOUBLE
+                                                    lReport.AppendLine(lString & lValuesDouble(lValueIndex))
+                                                Case NetCDF.nc_type.NC_FLOAT
+                                                    Dim lValue As Double = lValuesFloat(lValueIndex) * 10800 'sec/3hr TODO: make generic!
+                                                    lReport.AppendLine(lString & Format(lValue, "##0.000"))
+                                            End Select
+                                            Dim lOutIndex As Integer = lNDims - 1
+                                            lOutputPosition(lOutIndex) += 1
+                                            While lOutIndex > 0 AndAlso lOutputPosition(lOutIndex) = lCount(lOutIndex)
+                                                lOutputPosition(lOutIndex - 1) += 1
+                                                lOutputPosition(lOutIndex) = 0
+                                                lOutIndex -= 1
+                                            End While
+                                        Next
+                                    End If
                                 End If
                             End If
                         End If
                     End If
-                End If
-            Next
-            lResult = NetCDF.nc_close(lNCId)
-        End If
-        IO.File.WriteAllText(IO.Path.ChangeExtension(lFileName, lNetCDF_Version.Substring(0, 3) & ".dump.log"), lReport.ToString)
+                Next
+                lResult = NetCDF.nc_close(lNCId)
+            End If
+            IO.File.WriteAllText(lOutFolder & lBaseName & ".log", lReport.ToString)
+            lReport.Length = 0
+        Next
     End Sub
 
     Function AttsToString(ByVal aNcid As Int32, ByVal aVarId As Int32) As String
