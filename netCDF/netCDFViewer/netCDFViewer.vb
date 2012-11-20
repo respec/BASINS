b763263f95235edb73ab109c06c91f128faa3f6b
Jack Kittle <jack@gkfamily.org>
2012-11-20 17:03:40 +0000

    remove hard coded offsets

--- netCDF/netCDFViewer/netCDFViewer.vb
+++ netCDF/netCDFViewer/netCDFViewer.vb
@@ -1,14 +1,8 @@
 Module netCDFViewer
-    Dim pCount() As Integer = {5, 1, 3, 1}
-    Dim pBase() As Integer = {0, 0, 0, 0}
-    Dim pCountData() As Integer = {5848, 1, 1, 0}
-    'Dim pBaseData() As Integer = {0, 60, 70, 0}
-    Dim pBaseData() As Integer = {0, 0, 0, 0}
-
     Sub main()
         If IO.File.Exists("netcdf.dll") Then IO.File.Delete("netcdf.dll")
-        IO.File.Copy("netcdf4.2.1.1.dll", "netcdf.dll")
-        'IO.File.Copy("netcdf3.6.dll", "netcdf.dll")
+        'IO.File.Copy("netcdf4.2.1.1.dll", "netcdf.dll")
+        IO.File.Copy("netcdf3.6.dll", "netcdf.dll")
 
         Dim lReport As New System.Text.StringBuilder
 
@@ -59,82 +53,78 @@ Module netCDFViewer
                                            " NDims " & lNDims)
                         Dim lDimString As String = ""
                         Dim lDimLen As Integer
+                        Dim lCount() As Integer
                         If lNDims > 0 Then
+                            ReDim lCount(lNDims - 1)
                             lDimString = " Dims '"
                             For lDimIndex As Integer = 0 To lNDims - 1
                                 lResult = NetCDF.nc_inq_dimlen(lNCId, lDimIds(lDimIndex), lDimLen)
+                                lCount(lDimIndex) = lDimLen
                                 lDimString &= Str(lDimIds(lDimIndex)) & ":" & lDimLen & ", "
                             Next
                             lDimString = lDimString.Remove(lDimString.Length - 2)
                             lDimString &= "'"
-                        End If
-                        lReport.AppendLine(lDimString)
-                        lReport.Append(AttsToString(lNCId, lVarId))
-                        If lNDims > 0 AndAlso lDimLen > 0 Then
-                            Dim lCount() As Integer
-                            Dim lBase() As Integer
-                            If lNDims <= 2 Then
-                                lCount = pCount
-                                lBase = pBase
-                            Else
-                                lCount = pCountData
-                                lBase = pBaseData
-                            End If
-                            Dim lArraySize As Integer = 1
-                            For lDimIndex As Integer = 0 To lNDims - 1
-                                lArraySize *= lCount(lDimIndex)
-                            Next
-                            Dim lValuesDouble(lArraySize - 1) As Double
-                            Dim lValuesFloat(lArraySize - 1) As Single
-                            Dim lValuesInt(lArraySize - 1) As Int32
-                            Select Case lXtype
-                                Case NetCDF.nc_type.NC_DOUBLE
-                                    lResult = NetCDF.nc_get_vara_double(lNCId, lVarId, lBase, lCount, lValuesDouble)
-                                Case NetCDF.nc_type.NC_FLOAT
-                                    lResult = NetCDF.nc_get_vara_float(lNCId, lVarId, lBase, lCount, lValuesFloat)
-                                Case NetCDF.nc_type.NC_INT
-                                    lResult = NetCDF.nc_get_vara_int(lNCId, lVarId, lBase, lCount, lValuesInt)
-                            End Select
-                            If lResult <> 0 Then
-                                lReport.AppendLine(ErrorString(lResult, lNCId, lVarId))
-                            Else
-                                Dim lOutputPosition() = {0, 0, 0, 0, 0}
+                            lReport.AppendLine(lDimString)
+                            lReport.Append(AttsToString(lNCId, lVarId))
+                            If lDimLen > 0 Then
+                                Dim lBase(lNDims) As Integer
 
-                                Dim lFormatValueIndex As String = "0"
-                                If lArraySize > 1 Then
-                                    lFormatValueIndex = StrDup(CInt(Math.Ceiling(Math.Log10(lArraySize - 1))), "0")
-                                End If
-                                Dim lFormatDimIndex(lNDims)
+                                Dim lArraySize As Integer = 1
                                 For lDimIndex As Integer = 0 To lNDims - 1
-                                    Dim lDimPosMax As Integer = lBase(lDimIndex) + lCount(lDimIndex) - 1
-                                    If lDimPosMax > 0 Then
-                                        lFormatDimIndex(lDimIndex) = StrDup(CInt(Math.Ceiling(Math.Log10(lDimPosMax))), "0")
-                                    Else
-                                        lFormatDimIndex(lDimIndex) = "0"
+                                    lArraySize *= lCount(lDimIndex)
+                                Next
+                                Dim lValuesDouble(lArraySize - 1) As Double
+                                Dim lValuesFloat(lArraySize - 1) As Single
+                                Dim lValuesInt(lArraySize - 1) As Int32
+                                Select Case lXtype
+                                    Case NetCDF.nc_type.NC_DOUBLE
+                                        lResult = NetCDF.nc_get_vara_double(lNCId, lVarId, lBase, lCount, lValuesDouble)
+                                    Case NetCDF.nc_type.NC_FLOAT
+                                        lResult = NetCDF.nc_get_vara_float(lNCId, lVarId, lBase, lCount, lValuesFloat)
+                                    Case NetCDF.nc_type.NC_INT
+                                        lResult = NetCDF.nc_get_vara_int(lNCId, lVarId, lBase, lCount, lValuesInt)
+                                End Select
+                                If lResult <> 0 Then
+                                    lReport.AppendLine(ErrorString(lResult, lNCId, lVarId))
+                                Else
+                                    Dim lOutputPosition() = {0, 0, 0, 0, 0}
+
+                                    Dim lFormatValueIndex As String = "0"
+                                    If lArraySize > 1 Then
+                                        lFormatValueIndex = StrDup(CInt(Math.Ceiling(Math.Log10(lArraySize - 1))), "0")
                                     End If
-                                Next
-                                For lValueIndex As Integer = 0 To lArraySize - 1
-                                    Dim lString As String = "    (" & Format(lValueIndex, lFormatValueIndex) & "-"
+                                    Dim lFormatDimIndex(lNDims)
                                     For lDimIndex As Integer = 0 To lNDims - 1
-                                        Dim lDimPos As Integer = lBase(lDimIndex) + lOutputPosition(lDimIndex)
-                                        lString &= Format(lDimPos, lFormatDimIndex(lDimIndex)) & ":"
+                                        Dim lDimPosMax As Integer = lBase(lDimIndex) + lCount(lDimIndex) - 1
+                                        If lDimPosMax > 0 Then
+                                            lFormatDimIndex(lDimIndex) = StrDup(CInt(Math.Ceiling(Math.Log10(lDimPosMax))), "0")
+                                        Else
+                                            lFormatDimIndex(lDimIndex) = "0"
+                                        End If
                                     Next
-                                    lString = lString.Remove(lString.Length - 1) & ") "
-                                    Select Case lXtype
-                                        Case NetCDF.nc_type.NC_DOUBLE
-                                            lReport.AppendLine(lString & lValuesDouble(lValueIndex))
-                                        Case NetCDF.nc_type.NC_FLOAT
-                                            Dim lValue As Double = lValuesFloat(lValueIndex) * 10800 'sec/3hr TODO: make generic!
-                                            lReport.AppendLine(lString & Format(lValue, "##0.000"))
-                                    End Select
-                                    Dim lOutIndex As Integer = lNDims - 1
-                                    lOutputPosition(lOutIndex) += 1
-                                    While lOutIndex > 0 AndAlso lOutputPosition(lOutIndex) = lCount(lOutIndex)
-                                        lOutputPosition(lOutIndex - 1) += 1
-                                        lOutputPosition(lOutIndex) = 0
-                                        lOutIndex -= 1
-                                    End While
-                                Next
+                                    For lValueIndex As Integer = 0 To lArraySize - 1
+                                        Dim lString As String = "    (" & Format(lValueIndex, lFormatValueIndex) & "-"
+                                        For lDimIndex As Integer = 0 To lNDims - 1
+                                            Dim lDimPos As Integer = lBase(lDimIndex) + lOutputPosition(lDimIndex)
+                                            lString &= Format(lDimPos, lFormatDimIndex(lDimIndex)) & ":"
+                                        Next
+                                        lString = lString.Remove(lString.Length - 1) & ") "
+                                        Select Case lXtype
+                                            Case NetCDF.nc_type.NC_DOUBLE
+                                                lReport.AppendLine(lString & lValuesDouble(lValueIndex))
+                                            Case NetCDF.nc_type.NC_FLOAT
+                                                Dim lValue As Double = lValuesFloat(lValueIndex) * 10800 'sec/3hr TODO: make generic!
+                                                lReport.AppendLine(lString & Format(lValue, "##0.000"))
+                                        End Select
+                                        Dim lOutIndex As Integer = lNDims - 1
+                                        lOutputPosition(lOutIndex) += 1
+                                        While lOutIndex > 0 AndAlso lOutputPosition(lOutIndex) = lCount(lOutIndex)
+                                            lOutputPosition(lOutIndex - 1) += 1
+                                            lOutputPosition(lOutIndex) = 0
+                                            lOutIndex -= 1
+                                        End While
+                                    Next
+                                End If
                             End If
                         End If
                     End If
