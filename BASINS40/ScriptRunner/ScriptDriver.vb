6cc08866bc51c14df0540855ae1af1d15c842979
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2013-01-02 16:03:36 +0000

    ScriptRunner/ScriptDriver:   Main: refactor ScriptNames, RunScript out         Select Case number of scripts, if only one just run it   RunScript: copied from frmSelectScript
    atcMwGisUtility/GisUtil.vb: try/catch around My.Computer.FileSystem.SpecialDirectories
    
    D4EMDownloadData/frmDownload.vb: comment out unused SetColorsFromAvailability
     (caused exception in SpecialDirectories.MyDocuments for user who may not have had access to that folder)

--- BASINS40/ScriptRunner/ScriptDriver.vb
+++ BASINS40/ScriptRunner/ScriptDriver.vb
@@ -1,3 +1,4 @@
+Imports MapWinUtility
 Imports MapWindow.Interfaces
 Imports System.Reflection
 
@@ -6,27 +7,68 @@ Public Module ScriptDriver
 
     Public Sub Main(ByRef aMapWin As IMapWin)
         pMapWin = aMapWin
-        Dim lSelectScript As New frmSelectScript
+
+        Dim lScriptNames As Generic.List(Of String) = ScriptNames()
+
+        Select Case lScriptNames.Count
+            Case 0
+                MapWinUtility.Logger.Msg("No scripts compiled")
+            Case 1
+                RunScript(lScriptNames(0))
+            Case Else
+                Dim lSelectScript As New frmSelectScript
+                For Each lScriptName As String In lScriptNames
+                    lSelectScript.lstScripts.Items.Add(lScriptName)
+                Next
+                Dim lLastScriptName As String = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\VB and VBA Program Settings\ScriptDriver", "LastScript", "")
+                If Not lLastScriptName Is Nothing AndAlso lLastScriptName.Length > 0 Then
+                    With lSelectScript.lstScripts
+                        Dim lLastScriptIndex As Integer = .Items.IndexOf(lLastScriptName)
+                        If lLastScriptIndex > -1 Then
+                            .SelectedIndex = lLastScriptIndex
+                        End If
+                    End With
+                End If
+                lSelectScript.ShowDialog()
+        End Select
+    End Sub
+
+    Public Function ScriptNames() As Generic.List(Of String)
+        Dim lScriptNames As New Generic.List(Of String)
         Dim lAssembly As Assembly = Assembly.GetExecutingAssembly
         Dim lCoClassList() As Type = lAssembly.GetTypes 'GetExportedTypes gets only public types
         For Each lCoClass As Type In lCoClassList
             Dim lMethod As MethodBase = lCoClass.GetMethod("ScriptMain")
             If Not lMethod Is Nothing Then
                 Dim lName As String = lCoClass.Name
-                lSelectScript.lstScripts.Items.Add(lName)
+                lScriptNames.Add(lName)
             End If
         Next
+        Return lScriptNames
+    End Function
 
-        Dim lLastScriptName As String = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\VB and VBA Program Settings\ScriptDriver", "LastScript", "")
-        If Not lLastScriptName Is Nothing AndAlso lLastScriptName.Length > 0 Then
-            With lSelectScript.lstScripts
-                Dim lLastScriptIndex As Integer = .Items.IndexOf(lLastScriptName)
-                If lLastScriptIndex > -1 Then
-                    .SelectedIndex = lLastScriptIndex
-                End If
-            End With
-        End If
-
-        lSelectScript.Show()
+    Public Sub RunScript(ByVal aScriptName As String)
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\VB and VBA Program Settings\ScriptDriver", "LastScript", aScriptName)
+        Logger.Dbg("RunScript " & aScriptName)
+        Dim lOriginalCursor As Windows.Forms.Cursor = Windows.Forms.Cursor.Current
+        Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.WaitCursor
+        Dim lAssembly As Assembly = Assembly.GetExecutingAssembly
+        Dim lCoClass As Type = lAssembly.GetType(lAssembly.GetName.Name & "." & aScriptName)
+        Dim lMethod As MethodInfo = lCoClass.GetMethod("ScriptMain")
+        Dim lArgs(0) As Object
+        lArgs(0) = CObj(pMapWin)
+        Dim lResult As Boolean
+        Try
+            lResult = lMethod.Invoke(Nothing, lArgs)
+        Catch lEx As Exception
+            Logger.Dbg(vbCrLf & "Exception: " & lEx.ToString & vbCrLf)
+            If lEx.InnerException IsNot Nothing Then
+                Logger.Dbg(vbCrLf & "Inner Exception: " & lEx.InnerException.Message & vbCrLf & lEx.InnerException.StackTrace & vbCrLf)
+            End If
+            Logger.Flush()
+            lResult = False
+        End Try
+        Logger.Dbg("  Done:Result:" & lResult)
+        Windows.Forms.Cursor.Current = lOriginalCursor
     End Sub
 End Module
