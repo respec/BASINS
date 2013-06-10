fd11b7cbaaff4eb2a78874a32c3bfb7817591591
Paul Meems <pmeems@users.noreply.github.com>
2013-06-10 16:47:44 +0000

    Updated the version numbers to 4.8.8.0 which is the first Release Candidate for v4.8.8

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("4.8.7.*")> 
-<Assembly: AssemblyFileVersion("4.8.7.6")> 
+<Assembly: AssemblyVersion("4.8.8.*")> 
+<Assembly: AssemblyFileVersion("4.8.8.0")> 
