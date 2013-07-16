c0f247156863e0b2290ebdd4c34e63a9795d15fa
prhummel <prhummel@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2013-07-16 15:48:08 +0000

    frmIndex.vb - added File Dialog pop-up when user clicks on Aggregate file text box.

--- BASINS40/atcTimeseriesNetCDF/frmIndex.vb
+++ BASINS40/atcTimeseriesNetCDF/frmIndex.vb
@@ -1,4 +1,8 @@
-﻿Public Class frmIndex
+﻿Imports MapWinUtility
+
+Public Class frmIndex
+
+    Private Shared pFilter As String = "NetCDF Files (*.nc)|*.nc|All Files (*.*)|*.*"
 
     Private Sub rdoAggregationFile_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rdoAggregationFile.CheckedChanged
         txtGridFileName.Enabled = True
@@ -14,5 +18,18 @@
 
     Private Sub frmIndex_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         rdoAggregationFile.Checked = True
+    End Sub
+
+    Private Sub txtGridFileName_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtGridFileName.Click
+        With New Windows.Forms.OpenFileDialog
+            .Title = "Select " & Name & " file to open"
+            .Filter = pFilter
+            .FilterIndex = 1
+            .DefaultExt = ".nc"
+            If .ShowDialog() = Windows.Forms.DialogResult.OK Then
+                txtGridFileName.Text = .FileName
+            End If
+        End With
+
     End Sub
 End Class
