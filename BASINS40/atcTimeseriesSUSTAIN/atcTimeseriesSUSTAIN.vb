d09839c35de2b9188f322d00b20fc924236bae68
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2013-11-08 16:15:59 +0000

    New project file for D4EM10 Added reading, added PLTGEN format

--- BASINS40/atcTimeseriesSUSTAIN/atcTimeseriesSUSTAIN.vb
+++ BASINS40/atcTimeseriesSUSTAIN/atcTimeseriesSUSTAIN.vb
@@ -14,17 +14,18 @@ Public Class atcTimeseriesSUSTAIN
 
     Public WatershedNumber As Integer
     Public WatershedAcres As Double
-    Public Delimiter As Char = vbTab
+    Public Delimiter As String = vbTab
+    Public HeaderLineStart As String = "TT"
+    Public BodyLineStart As String = "WatershedNumber"
 
-    Private pFilter As String = "SUSTAIN Timeseries Files (*.txt)|*.txt"
-    Private pName As String = "Timeseries::SUSTAIN"
+    Private pFilter As String = "SUSTAIN Timeseries Files (*.txt)|*.txt|PLTGEN Files (*.p*)|*.p*|All Files|*.*"
+    Private pName As String = "Timeseries::SUSTAIN/PLTGEN"
     Private Shared pNaN As Double = GetNaN()
     Private Const pUnknownUnits As String = "<unknown>"
 
     Public ReadOnly Property AvailableAttributes() As Collection
         Get
-            'needed to edit attributes? that can't be done for this type!
-            Return New Collection 'empty!
+            Return New Collection
         End Get
     End Property
 
@@ -36,7 +37,7 @@ Public Class atcTimeseriesSUSTAIN
 
     Public Overrides ReadOnly Property Description() As String
         Get
-            Return "SUSTAIN Timeseries"
+            Return "SUSTAIN or PLTGEN Timeseries"
         End Get
     End Property
 
@@ -49,46 +50,126 @@ Public Class atcTimeseriesSUSTAIN
 
     Public ReadOnly Property Label() As String
         Get
-            Return "SUSTAIN"
+            Return "SUSTAIN/PLTGEN"
         End Get
     End Property
 
     Public Overrides Function Open(ByVal aSpecification As String, Optional ByVal aAttributes As atcData.atcDataAttributes = Nothing) As Boolean
         If MyBase.Open(aSpecification, aAttributes) Then
             Dim lInHeader As Boolean = True
-            Dim lDelimiters(0) As Char
-            Logger.Dbg("Opening atcTimeseriesSUSTAIN file: " & aSpecification)
-            lDelimiters(0) = " "c
+            Dim lInCurveLabels As Boolean = False
+            Dim lLabelStart As Integer = 0
+            Dim lLabelEnd As Integer = 0
+            Dim lDescStart As Integer = 0
+            Dim lTRANStart As Integer = 0
+            'Dim lTRANCODStart As Integer = 0
+            Dim lArea As Double = 0
+            Dim lSpaceSeparator(0) As Char
+            lSpaceSeparator(0) = " "c
+            Logger.Dbg("Opening atcTimeseriesSUSTAIN file: " & Specification)
             Dim lGroupBuilder As New atcTimeseriesGroupBuilder(Me)
-            For Each lLine As String In LinesInFile(aSpecification)
+            For Each lLine As String In LinesInFile(Specification)
                 If lInHeader Then
                     If lLine.ToLower.Contains("date/time") Then
                         lInHeader = False
-                    Else
-                        'TODO: parse constituents, descriptions, units
+                    ElseIf lLine.Contains("Area:") Then
+                        lInCurveLabels = False
+                        Dim lAreaStart As Integer = lLine.IndexOf("Area:") + 5
+                        Dim lAreaEnd As Integer = lLine.IndexOf("(", lAreaStart)
+                        If lAreaEnd > lAreaStart Then
+                            Double.TryParse(lLine.Substring(lAreaStart, lAreaEnd - lAreaStart).Trim, lArea)
+                        End If
+                    ElseIf lInCurveLabels Then
+                        Dim lConstituent As String = SafeSubstring(lLine, lLabelStart, lLabelEnd - lLabelStart + 1).Trim
+                        If lConstituent.Length = 0 OrElse lConstituent.StartsWith("Time series (") Then
+                            lInCurveLabels = False
+                        Else
+                            Dim lDescription As String = lConstituent
+                            Dim lUnits As String = ""
+                            Dim lTran As String = ""
+                            Dim lTranCode As Integer = atcTran.TranNative
+                            If lDescStart > 0 Then
+                                lDescription = SafeSubstring(lLine, lDescStart).Trim()
+                                Dim lOpenParen As Integer = lDescription.IndexOf("("c)
+                                If lOpenParen > 0 Then
+                                    lUnits = modString.SafeSubstring(lDescription, lOpenParen + 1, lDescription.Length - lOpenParen - 2)
+                                    lDescription = modString.SafeSubstring(lDescription, 0, lDescription.Length - lUnits.Length - 2).TrimEnd
+                                End If
+                            End If
+                            If lTRANStart > 0 Then
+                                Dim lTranAndCode() As String = lLine.Substring(lTRANStart).Split(lSpaceSeparator, StringSplitOptions.RemoveEmptyEntries)
+                                lTran = lTranAndCode(0)
+                                If lTranAndCode.Length > 1 Then
+                                    Integer.TryParse(lTranAndCode(1), lTranCode)
+                                End If
+                            End If
+                            'Dim lKey As String = lConstituent & "," & lDescription & "," & lUnits & "," & lTranCode
+                            With lGroupBuilder.Builder(lGroupBuilder.Count + 1)
+                                .Attributes.SetValue("Constituent", lConstituent)
+                                .Attributes.SetValue("Description", lDescription)
+                                If Not String.IsNullOrEmpty(lUnits) Then .Attributes.SetValue("Units", lUnits)
+                                If Not String.IsNullOrEmpty(lTran) Then .Attributes.SetValue("Transformation", lTran)
+                                If lTranCode <> atcTran.TranNative Then .Attributes.SetValue("TransformationCode", lTranCode)
+                            End With
+                        End If
+
+                    ElseIf lLine.EndsWith("Label") OrElse lLine.EndsWith("TRANCOD") Then
+                            lInCurveLabels = True
+                            lLabelStart = lLine.IndexOf("Label")
+                            lLabelEnd = lLine.IndexOf("LINTYP")
+                            If lLabelEnd < 0 Then
+                                lLabelEnd = lLabelStart + 8
+                                lDescStart = lLabelEnd + 1
+                            Else
+                                lTRANStart = lLine.IndexOf("TRAN ")
+                            'lTRANCODStart = lLine.IndexOf("TRANCOD")
+                            End If
                     End If
                 Else
+                    Dim lDate As Date
                     Dim lDateDouble As Double
                     Dim lFieldValues() As String
-                    If lLine.Contains(vbTab) Then
-                        lFieldValues = lLine.Split(vbTab)
-                    Else
-                        lFieldValues = lLine.Split(lDelimiters, StringSplitOptions.RemoveEmptyEntries)
-                    End If
                     Try
+                        If lLine.Contains(vbTab) Then
+                            lFieldValues = lLine.Split(vbTab)
+                        ElseIf lLine.Length > 22 + (lGroupBuilder.Count - 1) * 13 Then
+                            ReDim lFieldValues(lGroupBuilder.Count + 6)
+                            lFieldValues(1) = lLine.Substring(6, 4) 'Year
+                            lFieldValues(2) = lLine.Substring(11, 2) 'Month
+                            lFieldValues(3) = lLine.Substring(14, 2) 'Day
+                            lFieldValues(4) = lLine.Substring(17, 2) 'Hour
+                            lFieldValues(5) = lLine.Substring(20, 2) 'Minute
+                            For lDatasetIndex As Integer = 0 To lGroupBuilder.Count - 1
+                                lFieldValues(lDatasetIndex + 6) = SafeSubstring(lLine, 22 + 14 * lDatasetIndex, 14)
+                            Next
+                        Else
+                            ReDim lFieldValues(0)
+                        End If
                         If lFieldValues.Count > 6 Then
                             lDateDouble = atcUtility.modDate.Date2J(lFieldValues(1), lFieldValues(2), lFieldValues(3), lFieldValues(4), lFieldValues(5), 0)
+                            lDate = Date.FromOADate(lDateDouble) ' New Date(lFieldValues(1), lFieldValues(2), lFieldValues(3), lFieldValues(4), lFieldValues(5), 0)
+                            Dim lDataValues(lGroupBuilder.Count - 1) As Double
+                            For lDatasetIndex As Integer = 0 To lGroupBuilder.Count - 1
+                                Select Case lFieldValues(lDatasetIndex + 6)
+                                    Case "-1.0000000E+30"
+                                        lDataValues(lDatasetIndex) = pNaN
+                                    Case Else
+                                        If Not Double.TryParse(lFieldValues(lDatasetIndex + 6), lDataValues(lDatasetIndex)) Then
+                                            Logger.Dbg("Unable to parse value in dataset " & lDatasetIndex + 1 & "' value = '" & lFieldValues(lDatasetIndex + 6) & "'" & vbCrLf & " line: '" & lLine & "'")
+                                            lDataValues(lDatasetIndex) = pNaN
+                                        End If
+                                End Select
+                            Next
+                            lGroupBuilder.AddValues(lDate, lDataValues)
                         End If
-                        While DataSets.Count < lFieldValues.Count - 6
-                            DataSets.Add(New atcTimeseries(Me))
-                        End While
-
-
                     Catch exParse As Exception
                         Logger.Dbg("Unable to find dates/values in line: '" & lLine & "' " & vbCrLf & exParse.Message)
                     End Try
                 End If
             Next
+            Logger.Dbg("Read " & lGroupBuilder.Count & " Timeseries")
+            Logger.Status("")
+            lGroupBuilder.CreateTimeseriesAddToGroup(Me.DataSets)
             Return True
         End If
         Return False
@@ -135,45 +216,61 @@ Public Class atcTimeseriesSUSTAIN
 
             Dim lTimeseries As atcTimeseries = Me.DataSets(0)
             Dim lWriter As New System.IO.StreamWriter(aSaveFileName)
-            lWriter.WriteLine("TT-----------------------------------------------------------------------------------------")
-            lWriter.WriteLine("TT LSPC -- Formatted file for use by SUSTAIN")
-            lWriter.WriteLine("TT Version 4.1 ")
-            lWriter.WriteLine("TT")
-            lWriter.WriteLine("TT Designed and maintained by:")
-            lWriter.WriteLine("TT     AQUA TERRA Consultants")
-            lWriter.WriteLine("TT     http://aquaterra.com/")
-            lWriter.WriteLine("TT     Mountain View, CA")
-            lWriter.WriteLine("TT     (650) 962-1864")
-            lWriter.WriteLine("TT-----------------------------------------------------------------------------------------")
-            lWriter.WriteLine("TT LSPC MODEL OUTPUT FILE")
-            lWriter.WriteLine("TT Time interval: " & lTimeseries.Attributes.GetValue("Interval", JulianHour) / JulianMinute & " min      Output option: timestep")
-            lWriter.WriteLine("TT Label")
+            lWriter.WriteLine(HeaderLineStart & "-----------------------------------------------------------------------------------------")
+            lWriter.WriteLine(HeaderLineStart & " LSPC -- Formatted file for use by SUSTAIN")
+            lWriter.WriteLine(HeaderLineStart & " Version 4.1 ")
+            lWriter.WriteLine(HeaderLineStart & "")
+            lWriter.WriteLine(HeaderLineStart & " Designed and maintained by:")
+            lWriter.WriteLine(HeaderLineStart & "     AQUA TERRA Consultants")
+            lWriter.WriteLine(HeaderLineStart & "     http://aquaterra.com/")
+            lWriter.WriteLine(HeaderLineStart & "     Mountain View, CA")
+            lWriter.WriteLine(HeaderLineStart & "     (650) 962-1864")
+            lWriter.WriteLine(HeaderLineStart & "-----------------------------------------------------------------------------------------")
+            lWriter.WriteLine(HeaderLineStart & " LSPC MODEL OUTPUT FILE")
+            lWriter.WriteLine(HeaderLineStart & " Time interval: " & lTimeseries.Attributes.GetValue("Interval", JulianHour) / JulianMinute & " min      Output option: timestep")
+            lWriter.WriteLine(HeaderLineStart & " Label")
 
-            'lWriter.WriteLine("TT SURO     surface outflow volume (in-acre/timestep)")
-            'lWriter.WriteLine("TT AGWI     groundwater recharge volume (in-acre/timestep)")
-            'lWriter.WriteLine("TT SOSED    sediments load from land (tons/timestep)")
-            'lWriter.WriteLine("TT SOQUAL   surface flux of QUAL from the PLS_TP (lb/timestep)")
+            'lWriter.WriteLine(HeaderLineStart & " SURO     surface outflow volume (in-acre/timestep)")
+            'lWriter.WriteLine(HeaderLineStart & " AGWI     groundwater recharge volume (in-acre/timestep)")
+            'lWriter.WriteLine(HeaderLineStart & " SOSED    sediments load from land (tons/timestep)")
+            'lWriter.WriteLine(HeaderLineStart & " SOQUAL   surface flux of QUAL from the PLS_TP (lb/timestep)")
             For Each lTimeseries In Me.DataSets
-                lWriter.WriteLine("TT " & lTimeseries.Attributes.GetValue("Constituent").ToString.PadRight(8) & " " _
-                                        & lTimeseries.Attributes.GetValue("Description") & " (" _
-                                        & lTimeseries.Attributes.GetValue("Units") & ")")
+                lWriter.WriteLine(HeaderLineStart & " " _
+                                  & lTimeseries.Attributes.GetValue("Constituent").ToString.PadRight(8) & " " _
+                                  & lTimeseries.Attributes.GetValue("Description") & " (" _
+                                  & lTimeseries.Attributes.GetValue("Units") & ")")
             Next
 
-            lWriter.WriteLine("TT")
-            lWriter.WriteLine("TT WATERSHED_" & WatershedNumber & " Area:    " & Format(WatershedAcres, "0.000") & " (acres)")
-            lWriter.WriteLine("TT Date/time					Values")
-            lWriter.WriteLine("TT")
+            lWriter.WriteLine(HeaderLineStart)
+            lWriter.WriteLine(HeaderLineStart & " WATERSHED_" & WatershedNumber & " Area:    " & Format(WatershedAcres, "0.000") & " (acres)")
+            lWriter.WriteLine(HeaderLineStart & " Date/time					Values")
+            lWriter.WriteLine(HeaderLineStart)
 
             Dim lLastTimeStep As Integer = Me.DataSets(0).Dates.numValues
-
+            Dim lBodyLineStart As String = BodyLineStart.Replace("WatershedNumber", WatershedNumber)
             For lTimeStep As Integer = 1 To lLastTimeStep
+                lWriter.Write(lBodyLineStart)
                 Dim lDateArray(5) As Integer
                 modDate.J2Date(Me.DataSets(0).Dates.Value(lTimeStep), lDateArray)
-                lWriter.Write(WatershedNumber & Delimiter & lDateArray(0) & Delimiter & lDateArray(1) & Delimiter & lDateArray(2) & Delimiter & lDateArray(3) & Delimiter & lDateArray(4))
-                For Each lTimeseries In Me.DataSets
-                    'TODO: if delimiter is a space, fill column to some width?
-                    lWriter.Write(Delimiter & Format(lTimeseries.Value(lTimeStep), "0.00E+00"))
-                Next
+                If Delimiter = " " Then
+                    lWriter.Write(lDateArray(0).ToString.PadLeft(6) & _
+                                  lDateArray(1).ToString.PadLeft(3) & _
+                                  lDateArray(2).ToString.PadLeft(3) & _
+                                  lDateArray(3).ToString.PadLeft(3) & _
+                                  lDateArray(4).ToString.PadLeft(3))
+                    For Each lTimeseries In Me.DataSets
+                        lWriter.Write(Format(lTimeseries.Value(lTimeStep), "0.00000E+00").PadLeft(14))
+                    Next
+                Else
+                    lWriter.Write(Delimiter & lDateArray(0) & _
+                                  Delimiter & lDateArray(1) & _
+                                  Delimiter & lDateArray(2) & _
+                                  Delimiter & lDateArray(3) & _
+                                  Delimiter & lDateArray(4))
+                    For Each lTimeseries In Me.DataSets
+                        lWriter.Write(Delimiter & Format(lTimeseries.Value(lTimeStep), "0.00E+00"))
+                    Next
+                End If
                 lWriter.WriteLine()
             Next
 
