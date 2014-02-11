901f9c2a5a619513e42eac43b86d9e5dac80152e
anmishra <mishranurag@users.noreply.github.com>
2014-02-11 01:34:06 +0000

    Initial design of a form that can accept inputs and create an EXS file for the user.

---
+++ HSPEXP/MakeEXSFile.vb
@@ -0,0 +1,21 @@
+﻿Public Class MakeEXSFile
+
+    
+    Private Sub cmbNumberOfSites_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbNumberOfSites.SelectedIndexChanged
+
+    End Sub
+
+   
+
+   
+
+  
+
+    Private Sub pLocations_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles pLocations.Paint
+
+        If cmbNumberOfSites.Text = 2 Then
+
+        End If
+
+    End Sub
+End Class
