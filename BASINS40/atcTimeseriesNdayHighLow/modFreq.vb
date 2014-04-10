a2d9aab640c73a7ff45cfd35da58f77bbb7676a8
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2014-04-10 21:47:59 +0000

    atcTimeseriesNdayHighLow.vbproj   added System.Runtime.InteropServices import include for new dll import syntax
    modFreq.vb
      made line continuations backward-compatible with VS2008/.Net 3.5 by adding underscores

--- BASINS40/atcTimeseriesNdayHighLow/modFreq.vb
+++ BASINS40/atcTimeseriesNdayHighLow/modFreq.vb
@@ -43,100 +43,103 @@ Module modFreq
     '''               -32 - error in interpolation routine</param>
     ''' <remarks></remarks>
     <DllImport("peakfq.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Unicode)> _
-    Private Sub LGPSTX(ByRef N As Integer,
-                       ByRef NZI As Integer,
-                       ByRef NUMONS As Integer,
-                       ByRef NQS As Integer,
-                       ByRef XBAR As Single,
-                       ByRef STD As Single,
-                       ByRef SKEW As Single,
-                       ByRef LOGARH As Integer,
-                       ByRef ILH As Integer,
-                       ByRef DBG As Boolean,
-                       ByVal SE() As Single,
-                       ByVal C() As Single,
-                       ByVal CCPA() As Single,
-                       ByVal P() As Single,
-                       ByVal Q() As Single,
-                       ByVal ADP() As Single,
-                       ByVal QNEW() As Single,
-                       ByVal RI() As Single,
-                       ByVal RSOUT() As Single,
-                       ByVal KP3DEV() As Single,
+    Private Sub LGPSTX(ByRef N As Integer, _
+                       ByRef NZI As Integer, _
+                       ByRef NUMONS As Integer, _
+                       ByRef NQS As Integer, _
+                       ByRef XBAR As Single, _
+                       ByRef STD As Single, _
+                       ByRef SKEW As Single, _
+                       ByRef LOGARH As Integer, _
+                       ByRef ILH As Integer, _
+                       ByRef DBG As Boolean, _
+                       ByVal SE() As Single, _
+                       ByVal C() As Single, _
+                       ByVal CCPA() As Single, _
+                       ByVal P() As Single, _
+                       ByVal Q() As Single, _
+                       ByVal ADP() As Single, _
+                       ByVal QNEW() As Single, _
+                       ByVal RI() As Single, _
+                       ByVal RSOUT() As Single, _
+                       ByVal KP3DEV() As Single, _
                        ByRef RETCOD As Integer)
     End Sub
+
     <DllImport("peakfq.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Unicode)> _
-    Private Sub KENT(ByVal X() As Single,
-                     ByRef N As Integer,
-                     ByRef TAU As Single,
-                     ByRef PLEVEL As Single,
+    Private Sub KENT(ByVal X() As Single, _
+                     ByRef N As Integer, _
+                     ByRef TAU As Single, _
+                     ByRef PLEVEL As Single, _
                      ByRef SLOPE As Single)
     End Sub
+
     <DllImport("peakfq.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Unicode)> _
-    Private Sub EMAFITB(ByRef N As Integer,
-                        ByVal QL_IN() As Single,
-                        ByVal QU_IN() As Single,
-                        ByVal TL_IN() As Single,
-                        ByVal TU_IN() As Single,
-                        ByRef REG_SKEW As Single,
-                        ByRef REG_MSE As Single,
-                        ByRef NEPS As Integer,
-                        ByVal EPS() As Single,
-                        ByRef GBTHRSH0 As Single,
-                        ByRef PQ As Single,
-                        ByVal CMOMS(,) As Single,
-                        ByRef YP As Single,
-                        ByVal CI_LOW() As Single,
-                        ByVal CI_HIGH() As Single,
+    Private Sub EMAFITB(ByRef N As Integer, _
+                        ByVal QL_IN() As Single, _
+                        ByVal QU_IN() As Single, _
+                        ByVal TL_IN() As Single, _
+                        ByVal TU_IN() As Single, _
+                        ByRef REG_SKEW As Single, _
+                        ByRef REG_MSE As Single, _
+                        ByRef NEPS As Integer, _
+                        ByVal EPS() As Single, _
+                        ByRef GBTHRSH0 As Single, _
+                        ByRef PQ As Single, _
+                        ByVal CMOMS(,) As Single, _
+                        ByRef YP As Single, _
+                        ByVal CI_LOW() As Single, _
+                        ByVal CI_HIGH() As Single, _
                         ByVal VAR_EST() As Single)
     End Sub
+
     <DllImport("peakfq.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Unicode)> _
-    Private Sub VAR_EMAB(ByRef NTH As Integer,
-                         ByVal NOBS() As Double,
-                         ByVal TL() As Double,
-                         ByVal TU() As Double,
-                         ByVal MC() As Double,
-                         ByVal PQ() As Double,
-                         ByRef NQ As Integer,
-                         ByRef EPS As Double,
-                         ByRef R_S2 As Double,
-                         ByRef R_M_MSE As Double,
-                         ByRef R_S2_MSE As Double,
-                         ByRef R_G_MSE As Double,
-                         ByVal YP() As Double,
-                         ByVal CV_YP_SYP(,) As Double,
-                         ByVal CIL() As Double,
+    Private Sub VAR_EMAB(ByRef NTH As Integer, _
+                         ByVal NOBS() As Double, _
+                         ByVal TL() As Double, _
+                         ByVal TU() As Double, _
+                         ByVal MC() As Double, _
+                         ByVal PQ() As Double, _
+                         ByRef NQ As Integer, _
+                         ByRef EPS As Double, _
+                         ByRef R_S2 As Double, _
+                         ByRef R_M_MSE As Double, _
+                         ByRef R_S2_MSE As Double, _
+                         ByRef R_G_MSE As Double, _
+                         ByVal YP() As Double, _
+                         ByVal CV_YP_SYP(,) As Double, _
+                         ByVal CIL() As Double, _
                          ByVal CIH() As Double)
     End Sub
 
     <DllImport("usgs_swstats.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Unicode)> _
-    Private Sub EMAFITB_V1(ByRef N As Integer,
-                           ByVal QL_IN() As Double,
-                           ByVal QU_IN() As Double,
-                           ByVal TL_IN() As Double,
-                           ByVal TU_IN() As Double,
-                           ByRef REG_SKEW As Double,
-                           ByRef REG_MSE As Double,
-                           ByRef NEPS As Integer,
-                           ByVal EPS() As Single,
-                           ByRef GBTHRSH0 As Double,
-                           ByRef PQ As Double,
-                           ByVal CMOMS(,) As Double,
-                           ByRef YP As Double,
-                           ByVal CI_LOW() As Double,
-                           ByVal CI_HIGH() As Double,
+    Private Sub EMAFITB_V1(ByRef N As Integer, _
+                           ByVal QL_IN() As Double, _
+                           ByVal QU_IN() As Double, _
+                           ByVal TL_IN() As Double, _
+                           ByVal TU_IN() As Double, _
+                           ByRef REG_SKEW As Double, _
+                           ByRef REG_MSE As Double, _
+                           ByRef NEPS As Integer, _
+                           ByVal EPS() As Single, _
+                           ByRef GBTHRSH0 As Double, _
+                           ByRef PQ As Double, _
+                           ByVal CMOMS(,) As Double, _
+                           ByRef YP As Double, _
+                           ByVal CI_LOW() As Double, _
+                           ByVal CI_HIGH() As Double, _
                            ByVal VAR_EST() As Double)
     End Sub
 
     <DllImport("usgs_swstats.dll", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Unicode)> _
-    Private Sub VAR_EMA(ByRef NT As Integer,
-                        ByVal NOBS() As Double,
-                        ByVal TL_IN() As Double,
-                        ByVal TU_IN() As Double,
-                        ByVal CMOMS() As Double,
-                        ByRef PQ As Double,
-                        ByRef REG_MSE As Double,
-                        ByRef YP As Double,
+    Private Sub VAR_EMA(ByRef NT As Integer, _
+                        ByVal NOBS() As Double, _
+                        ByVal TL_IN() As Double, _
+                        ByVal TU_IN() As Double, _
+                        ByVal CMOMS() As Double, _
+                        ByRef PQ As Double, _
+                        ByRef REG_MSE As Double, _
+                        ByRef YP As Double, _
                         ByVal VAR_EST(,) As Double)
     End Sub
 
@@ -190,7 +193,7 @@ Module modFreq
     End Sub
 
     'frequency analysis for specified recurrence interval or probability
-    Friend Sub PearsonType3(ByVal aUseVersion_1 As Boolean,
+    Friend Sub PearsonType3(ByVal aUseVersion_1 As Boolean, _
                             ByVal aTs As atcTimeseries, _
                             ByVal aRecurOrProbs() As Double, _
                             ByVal aHigh As Boolean, _
@@ -312,7 +315,7 @@ Module modFreq
                         Dim lR_SD_mse As Double = -999
                         Dim lR_S2_MSE As Double = 4.0 * lR_S2 * lR_SD_mse
                         Dim lR_G_MSE As Double = 1.0
-                        VAR_EMAB(lneps, lNobs, lTL, lTU, lCMoms, lPQA, (lIntervalMax + 1), leps(0),
+                        VAR_EMAB(lneps, lNobs, lTL, lTU, lCMoms, lPQA, (lIntervalMax + 1), leps(0), _
                                  lR_S2, lR_M_MSE, lR_S2_MSE, lR_G_MSE, lVarEst, lVarEstArray, lCILow, lCIHigh)
                         Logger.Dbg("BackFrom VAR_EMAB")
                         For i As Integer = 0 To lIntervalMax
@@ -324,40 +327,40 @@ Module modFreq
                                 lVarEst(i) = lVarEstArray(0, 0)
                             End If
                         Next
-                Else
-                    For i As Integer = 0 To lIntervalMax
-                        If aHigh Then
-                            lPQ = 1 - lP(i)
-                        Else
-                            lPQ = lP(i)
-                        End If
-                        If Math.Abs(lCMoms(2)) > lSkewMin Then
-                            VAR_EMA(lneps, lNobs, lTL, lTU, lCMoms, lPQ, lmse, lyp, lVarEstArray) 'CDbl(lP(i)), lmse, lyp(i), lVarEstArray)
-                            CI_EMA_M3(lyp, lVarEstArray, lneps, leps, lCILowVal, lCIHighVal)
-                            lCILow(i) = lCILowVal(0)
-                            lCIHigh(i) = lCIHighVal(0)
-                        Else 'for skews close to zero, compute a weighted sum/interpolate values
-                            lCMoms(2) = -lSkewMin
-                            VAR_EMA(lneps, lNobs, lTL, lTU, lCMoms, lPQ, lmse, lyp, lVarEstArray)
-                            CI_EMA_M3(lyp, lVarEstArray, lneps, leps, lCILowVal, lCIHighVal)
+                    Else
+                        For i As Integer = 0 To lIntervalMax
+                            If aHigh Then
+                                lPQ = 1 - lP(i)
+                            Else
+                                lPQ = lP(i)
+                            End If
+                            If Math.Abs(lCMoms(2)) > lSkewMin Then
+                                VAR_EMA(lneps, lNobs, lTL, lTU, lCMoms, lPQ, lmse, lyp, lVarEstArray) 'CDbl(lP(i)), lmse, lyp(i), lVarEstArray)
+                                CI_EMA_M3(lyp, lVarEstArray, lneps, leps, lCILowVal, lCIHighVal)
+                                lCILow(i) = lCILowVal(0)
+                                lCIHigh(i) = lCIHighVal(0)
+                            Else 'for skews close to zero, compute a weighted sum/interpolate values
+                                lCMoms(2) = -lSkewMin
+                                VAR_EMA(lneps, lNobs, lTL, lTU, lCMoms, lPQ, lmse, lyp, lVarEstArray)
+                                CI_EMA_M3(lyp, lVarEstArray, lneps, leps, lCILowVal, lCIHighVal)
 
-                            lCMoms(2) = lSkewMin
-                            VAR_EMA(lneps, lNobs, lTL, lTU, lCMoms, lPQ, lmse, lyp, lVarEstArray)
-                            CI_EMA_M3(lyp, lVarEstArray, lneps, leps, lCILowVal2, lCIHighVal2)
+                                lCMoms(2) = lSkewMin
+                                VAR_EMA(lneps, lNobs, lTL, lTU, lCMoms, lPQ, lmse, lyp, lVarEstArray)
+                                CI_EMA_M3(lyp, lVarEstArray, lneps, leps, lCILowVal2, lCIHighVal2)
 
-                            lWt = (lSkew + lSkewMin) / (2 * lSkewMin) 'weight to attach to positive skew
-                            lCILow(i) = (1 - lWt) * lCILowVal(0) + lWt * lCILowVal2(0)
-                            lCIHigh(i) = (1 - lWt) * lCIHighVal2(0) + lWt * lCIHighVal2(0)
-                        End If
-                        If aLogFg Then
-                            lCILow(i) = 10 ^ lCILow(i)
-                            lCIHigh(i) = 10 ^ lCIHigh(i)
-                            lVarEst(i) = 10 ^ lVarEstArray(0, 0)
-                        Else
-                            lVarEst(i) = lVarEstArray(0, 0)
-                        End If
-                    Next i
-                End If
+                                lWt = (lSkew + lSkewMin) / (2 * lSkewMin) 'weight to attach to positive skew
+                                lCILow(i) = (1 - lWt) * lCILowVal(0) + lWt * lCILowVal2(0)
+                                lCIHigh(i) = (1 - lWt) * lCIHighVal2(0) + lWt * lCIHighVal2(0)
+                            End If
+                            If aLogFg Then
+                                lCILow(i) = 10 ^ lCILow(i)
+                                lCIHigh(i) = 10 ^ lCIHigh(i)
+                                lVarEst(i) = 10 ^ lVarEstArray(0, 0)
+                            Else
+                                lVarEst(i) = lVarEstArray(0, 0)
+                            End If
+                        Next i
+                    End If
                 End If
 
                 Dim lMsg As String = ""
