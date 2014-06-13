c06df177183da1771e95626526c0c8374b05cba7
anmishra <mishranurag@users.noreply.github.com>
2014-06-13 20:21:05 +0000

    Added the option of selecting the output units. Some design changes in the form and now it can be expanded and contracted (With Mark's help).

--- WinHSPF/frmAddExpert.Designer.vb
+++ WinHSPF/frmAddExpert.Designer.vb
@@ -30,49 +30,54 @@ Partial Class frmAddExpert
         Me.cmdClose = New System.Windows.Forms.Button
         Me.lstOperation = New System.Windows.Forms.ListBox
         Me.lstGroup = New System.Windows.Forms.ListBox
-        Me.atxBase = New atcControls.atcText
         Me.txtLoc = New System.Windows.Forms.TextBox
+        Me.atxBase = New atcControls.atcText
+        Me.optMetric = New System.Windows.Forms.RadioButton
+        Me.optEnglish = New System.Windows.Forms.RadioButton
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.lblTimeStep = New System.Windows.Forms.Label
+        Me.Panel2 = New System.Windows.Forms.Panel
+        Me.lblUnits = New System.Windows.Forms.Label
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblOperation
         '
         Me.lblOperation.AutoSize = True
-        Me.lblOperation.Location = New System.Drawing.Point(16, 9)
-        Me.lblOperation.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblOperation.Location = New System.Drawing.Point(12, 7)
         Me.lblOperation.Name = "lblOperation"
-        Me.lblOperation.Size = New System.Drawing.Size(75, 17)
+        Me.lblOperation.Size = New System.Drawing.Size(56, 13)
         Me.lblOperation.TabIndex = 0
         Me.lblOperation.Text = "Operation:"
         '
         'lblGroup
         '
+        Me.lblGroup.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lblGroup.AutoSize = True
-        Me.lblGroup.Location = New System.Drawing.Point(260, 9)
-        Me.lblGroup.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblGroup.Location = New System.Drawing.Point(195, 7)
         Me.lblGroup.Name = "lblGroup"
-        Me.lblGroup.Size = New System.Drawing.Size(107, 17)
+        Me.lblGroup.Size = New System.Drawing.Size(82, 13)
         Me.lblGroup.TabIndex = 1
         Me.lblGroup.Text = "Group/Member:"
         '
         'lblLoc
         '
-        Me.lblLoc.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblLoc.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.lblLoc.AutoSize = True
-        Me.lblLoc.Location = New System.Drawing.Point(91, 271)
-        Me.lblLoc.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblLoc.Location = New System.Drawing.Point(86, 255)
         Me.lblLoc.Name = "lblLoc"
-        Me.lblLoc.Size = New System.Drawing.Size(121, 17)
+        Me.lblLoc.Size = New System.Drawing.Size(96, 13)
         Me.lblLoc.TabIndex = 8
         Me.lblLoc.Text = "WDM Location ID:"
         '
         'lblBase
         '
-        Me.lblBase.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblBase.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.lblBase.AutoSize = True
-        Me.lblBase.Location = New System.Drawing.Point(97, 303)
-        Me.lblBase.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblBase.Location = New System.Drawing.Point(91, 285)
         Me.lblBase.Name = "lblBase"
-        Me.lblBase.Size = New System.Drawing.Size(115, 17)
+        Me.lblBase.Size = New System.Drawing.Size(91, 13)
         Me.lblBase.TabIndex = 9
         Me.lblBase.Text = "Base WDM DSN:"
         '
@@ -82,35 +87,30 @@ Partial Class frmAddExpert
                     Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtDescription.BackColor = System.Drawing.SystemColors.Control
         Me.txtDescription.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtDescription.Location = New System.Drawing.Point(20, 185)
-        Me.txtDescription.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtDescription.Location = New System.Drawing.Point(12, 197)
         Me.txtDescription.Multiline = True
         Me.txtDescription.Name = "txtDescription"
         Me.txtDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.txtDescription.Size = New System.Drawing.Size(465, 59)
+        Me.txtDescription.Size = New System.Drawing.Size(350, 49)
         Me.txtDescription.TabIndex = 10
         '
         'optH
         '
-        Me.optH.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.optH.AutoSize = True
-        Me.optH.Location = New System.Drawing.Point(339, 268)
-        Me.optH.Margin = New System.Windows.Forms.Padding(4)
+        Me.optH.Location = New System.Drawing.Point(222, 3)
         Me.optH.Name = "optH"
-        Me.optH.Size = New System.Drawing.Size(67, 21)
+        Me.optH.Size = New System.Drawing.Size(55, 17)
         Me.optH.TabIndex = 11
         Me.optH.Text = "Hourly"
         Me.optH.UseVisualStyleBackColor = True
         '
         'optD
         '
-        Me.optD.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.optD.AutoSize = True
         Me.optD.Checked = True
-        Me.optD.Location = New System.Drawing.Point(339, 300)
-        Me.optD.Margin = New System.Windows.Forms.Padding(4)
+        Me.optD.Location = New System.Drawing.Point(157, 3)
         Me.optD.Name = "optD"
-        Me.optD.Size = New System.Drawing.Size(57, 21)
+        Me.optD.Size = New System.Drawing.Size(48, 17)
         Me.optD.TabIndex = 12
         Me.optD.TabStop = True
         Me.optD.Text = "Daily"
@@ -118,62 +118,64 @@ Partial Class frmAddExpert
         '
         'cmdOK
         '
-        Me.cmdOK.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.cmdOK.Location = New System.Drawing.Point(136, 353)
-        Me.cmdOK.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmdOK.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdOK.Location = New System.Drawing.Point(102, 365)
         Me.cmdOK.Name = "cmdOK"
-        Me.cmdOK.Size = New System.Drawing.Size(100, 30)
+        Me.cmdOK.Size = New System.Drawing.Size(75, 24)
         Me.cmdOK.TabIndex = 13
         Me.cmdOK.Text = "&OK"
         Me.cmdOK.UseVisualStyleBackColor = True
         '
         'cmdClose
         '
-        Me.cmdClose.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.cmdClose.Location = New System.Drawing.Point(267, 353)
-        Me.cmdClose.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmdClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdClose.Location = New System.Drawing.Point(200, 365)
         Me.cmdClose.Name = "cmdClose"
-        Me.cmdClose.Size = New System.Drawing.Size(100, 30)
+        Me.cmdClose.Size = New System.Drawing.Size(75, 24)
         Me.cmdClose.TabIndex = 14
         Me.cmdClose.Text = "&Close"
         Me.cmdClose.UseVisualStyleBackColor = True
         '
         'lstOperation
         '
-        Me.lstOperation.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-                    Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lstOperation.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lstOperation.FormattingEnabled = True
-        Me.lstOperation.ItemHeight = 16
-        Me.lstOperation.Location = New System.Drawing.Point(20, 33)
-        Me.lstOperation.Margin = New System.Windows.Forms.Padding(4)
+        Me.lstOperation.Location = New System.Drawing.Point(15, 27)
         Me.lstOperation.Name = "lstOperation"
-        Me.lstOperation.Size = New System.Drawing.Size(221, 132)
+        Me.lstOperation.Size = New System.Drawing.Size(167, 160)
         Me.lstOperation.TabIndex = 15
         '
         'lstGroup
         '
         Me.lstGroup.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-                    Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lstGroup.FormattingEnabled = True
-        Me.lstGroup.ItemHeight = 16
-        Me.lstGroup.Location = New System.Drawing.Point(264, 33)
-        Me.lstGroup.Margin = New System.Windows.Forms.Padding(4)
+        Me.lstGroup.Location = New System.Drawing.Point(198, 27)
         Me.lstGroup.Name = "lstGroup"
-        Me.lstGroup.Size = New System.Drawing.Size(221, 132)
+        Me.lstGroup.Size = New System.Drawing.Size(167, 160)
         Me.lstGroup.TabIndex = 16
+        '
+        'txtLoc
+        '
+        Me.txtLoc.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txtLoc.Location = New System.Drawing.Point(198, 252)
+        Me.txtLoc.Name = "txtLoc"
+        Me.txtLoc.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.txtLoc.Size = New System.Drawing.Size(80, 20)
+        Me.txtLoc.TabIndex = 18
         '
         'atxBase
         '
         Me.atxBase.Alignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.atxBase.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.atxBase.DataType = atcControls.atcText.ATCoDataType.ATCoInt
         Me.atxBase.DefaultValue = ""
         Me.atxBase.HardMax = -999
         Me.atxBase.HardMin = 1
         Me.atxBase.InsideLimitsBackground = System.Drawing.Color.White
-        Me.atxBase.Location = New System.Drawing.Point(224, 298)
-        Me.atxBase.Margin = New System.Windows.Forms.Padding(4)
+        Me.atxBase.Location = New System.Drawing.Point(198, 278)
         Me.atxBase.MaxWidth = 20
         Me.atxBase.Name = "atxBase"
         Me.atxBase.NumericFormat = "0.#####"
@@ -182,44 +184,100 @@ Partial Class frmAddExpert
         Me.atxBase.RightToLeft = System.Windows.Forms.RightToLeft.Yes
         Me.atxBase.SelLength = 0
         Me.atxBase.SelStart = 0
-        Me.atxBase.Size = New System.Drawing.Size(107, 25)
+        Me.atxBase.Size = New System.Drawing.Size(80, 20)
         Me.atxBase.SoftMax = -999
         Me.atxBase.SoftMin = -999
         Me.atxBase.TabIndex = 17
         Me.atxBase.ValueDouble = 1000
         Me.atxBase.ValueInteger = 1000
         '
-        'txtLoc
+        'optMetric
         '
-        Me.txtLoc.Location = New System.Drawing.Point(224, 266)
-        Me.txtLoc.Margin = New System.Windows.Forms.Padding(4)
-        Me.txtLoc.Name = "txtLoc"
-        Me.txtLoc.RightToLeft = System.Windows.Forms.RightToLeft.Yes
-        Me.txtLoc.Size = New System.Drawing.Size(105, 22)
-        Me.txtLoc.TabIndex = 18
+        Me.optMetric.AutoSize = True
+        Me.optMetric.Location = New System.Drawing.Point(222, 0)
+        Me.optMetric.Name = "optMetric"
+        Me.optMetric.Size = New System.Drawing.Size(54, 17)
+        Me.optMetric.TabIndex = 20
+        Me.optMetric.Text = "Metric"
+        Me.optMetric.UseVisualStyleBackColor = True
+        '
+        'optEnglish
+        '
+        Me.optEnglish.AutoSize = True
+        Me.optEnglish.Checked = True
+        Me.optEnglish.Location = New System.Drawing.Point(157, 0)
+        Me.optEnglish.Name = "optEnglish"
+        Me.optEnglish.Size = New System.Drawing.Size(59, 17)
+        Me.optEnglish.TabIndex = 19
+        Me.optEnglish.TabStop = True
+        Me.optEnglish.Text = "English"
+        Me.optEnglish.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.lblTimeStep)
+        Me.Panel1.Controls.Add(Me.optH)
+        Me.Panel1.Controls.Add(Me.optD)
+        Me.Panel1.Location = New System.Drawing.Point(41, 304)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(290, 24)
+        Me.Panel1.TabIndex = 21
+        '
+        'lblTimeStep
+        '
+        Me.lblTimeStep.AutoSize = True
+        Me.lblTimeStep.Location = New System.Drawing.Point(48, 5)
+        Me.lblTimeStep.Name = "lblTimeStep"
+        Me.lblTimeStep.Size = New System.Drawing.Size(93, 13)
+        Me.lblTimeStep.TabIndex = 13
+        Me.lblTimeStep.Text = "Output Time Step:"
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.Controls.Add(Me.lblUnits)
+        Me.Panel2.Controls.Add(Me.optEnglish)
+        Me.Panel2.Controls.Add(Me.optMetric)
+        Me.Panel2.Location = New System.Drawing.Point(41, 334)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(289, 18)
+        Me.Panel2.TabIndex = 22
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Location = New System.Drawing.Point(72, 2)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(69, 13)
+        Me.lblUnits.TabIndex = 21
+        Me.lblUnits.Text = "Output Units:"
         '
         'frmAddExpert
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(503, 396)
+        Me.ClientSize = New System.Drawing.Size(377, 400)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.txtLoc)
         Me.Controls.Add(Me.atxBase)
         Me.Controls.Add(Me.lstGroup)
         Me.Controls.Add(Me.lstOperation)
         Me.Controls.Add(Me.cmdClose)
         Me.Controls.Add(Me.cmdOK)
-        Me.Controls.Add(Me.optD)
-        Me.Controls.Add(Me.optH)
         Me.Controls.Add(Me.txtDescription)
         Me.Controls.Add(Me.lblBase)
         Me.Controls.Add(Me.lblLoc)
         Me.Controls.Add(Me.lblGroup)
         Me.Controls.Add(Me.lblOperation)
+        Me.Controls.Add(Me.Panel2)
         Me.KeyPreview = True
-        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "frmAddExpert"
         Me.Text = "WinHSPF - Add Output"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -237,4 +295,10 @@ Partial Class frmAddExpert
     Friend WithEvents lstGroup As System.Windows.Forms.ListBox
     Friend WithEvents atxBase As atcControls.atcText
     Friend WithEvents txtLoc As System.Windows.Forms.TextBox
+    Friend WithEvents optMetric As System.Windows.Forms.RadioButton
+    Friend WithEvents optEnglish As System.Windows.Forms.RadioButton
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents lblTimeStep As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents lblUnits As System.Windows.Forms.Label
 End Class
