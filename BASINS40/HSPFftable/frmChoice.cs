a7091b3c232ace210eb8d591de0b56d4e4b1345c
tzhai <tzhai@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2014-07-14 21:15:23 +0000

    for choosing tool type

---
+++ BASINS40/HSPFftable/frmChoice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HSPFftable
+{
+    public partial class frmChoice : Form
+    {
+        public frmChoice()
+        {
+            InitializeComponent();
+        }
+
+        private void rdoToolGray_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoToolGray.Checked)
+                atcFtableBuilder.clsGlobals.gToolType = atcFtableBuilder.clsGlobals.ToolType.Gray;
+        }
+
+        private void rdoToolGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoToolGreen.Checked)
+                atcFtableBuilder.clsGlobals.gToolType = atcFtableBuilder.clsGlobals.ToolType.Green;
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            if (rdoToolGray.Checked || rdoToolGreen.Checked)
+                this.Close();
+        }
+    }
+}
