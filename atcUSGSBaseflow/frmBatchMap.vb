28d5ec2dd741f28ede9f3016600f21a6a7e8e4cd
tong.zhai <tong.zhai@respec.com>
2014-08-18 19:56:00 +0000

    more capabilities

--- atcUSGSBaseflow/frmBatchMap.vb
+++ atcUSGSBaseflow/frmBatchMap.vb
@@ -1,5 +1,6 @@
 ﻿Imports atcUtility
 Imports atcData
+Imports System.Windows.Forms
 
 Public Class frmBatchMap
     Private pListStations As atcCollection
@@ -30,7 +31,35 @@ Public Class frmBatchMap
         pBatchGroupCount += 1
     End Sub
 
-    Private Sub treeBFGroups_NodeMouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.TreeNodeMouseClickEventArgs) Handles treeBFGroups.NodeMouseClick
+    Private Sub treeBFGroups_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles treeBFGroups.MouseUp
+        ' Show menu only if Right Mouse button is clicked
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+            ' Point where mouse is clicked
+            Dim p As System.Drawing.Point = New System.Drawing.Point(e.X, e.Y)
+            ' Go to the node that the user clicked
+            Dim node As TreeNode = treeBFGroups.GetNodeAt(p)
+            If node IsNot Nothing Then
+                'm_OldSelectNode = treeBFGroups.SelectedNode
+                treeBFGroups.SelectedNode = node
+                'If node.Name.StartsWith("BatchGroup_") Then
+                'Else
+                'End If
+                cmsNode.Show(treeBFGroups, p)
+                ' Highlight the selected node
+                'treeBFGroups.SelectedNode = m_OldSelectNode
+                'm_OldSelectNode = Nothing
+            Else
 
+            End If
+        End If
+    End Sub
+
+    Private Sub cmsNode_ItemClicked(ByVal sender As Object, ByVal e As System.Windows.Forms.ToolStripItemClickedEventArgs) Handles cmsNode.ItemClicked
+        Dim lFound As String = ""
+        Dim p As System.Drawing.Point = New System.Drawing.Point(e.ClickedItem.Bounds.Location.X, e.ClickedItem.Bounds.Location.Y)
+        ' Go to the node that the user clicked
+        Dim node As TreeNode = treeBFGroups.GetNodeAt(p)
+        Dim lParentGroupName As String = node.Parent().Text
+        Dim lFullpath As String = node.FullPath
     End Sub
 End Class
