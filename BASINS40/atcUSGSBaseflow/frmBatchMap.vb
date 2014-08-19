6feba819db869b417d97520300b33cda770ca451
tzhai <tzhai@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2014-08-19 04:45:41 +0000

    more capability

--- BASINS40/atcUSGSBaseflow/frmBatchMap.vb
+++ BASINS40/atcUSGSBaseflow/frmBatchMap.vb
@@ -1,11 +1,24 @@
 ﻿Imports atcUtility
 Imports atcData
+Imports MapWinUtility
 Imports System.Windows.Forms
 
 Public Class frmBatchMap
     Private pListStations As atcCollection
+    Private pDataPath As String
     Private pBatchGroupCount As Integer = 1
+    Private pBFInputsGlobal As atcDataAttributes
+    Private pBFInputsGroups As atcCollection
 
+    Private ReadOnly Property GetDataFileFullPath(ByVal aStationId As String) As String
+        Get
+            If IO.Directory.Exists(pDataPath) Then
+                Return IO.Path.Combine(pDataPath, "NWIS\NWIS_discharge_" & aStationId & ".rdb")
+            Else
+                Return ""
+            End If
+        End Get
+    End Property
     Public Sub Initiate(ByVal aList As atcCollection)
         pListStations = aList
         If pListStations Is Nothing Then pListStations = New atcCollection()
@@ -13,11 +26,14 @@ Public Class frmBatchMap
 
     Private Sub frmBatchMap_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         lstStations.LeftLabel = "Stations from map"
+        lstStations.RightLabel = "Selected for a group"
         Dim lindex As Integer = 0
         For Each lStationID As String In pListStations
             lstStations.LeftItem(lindex) = lStationID
             lindex += 1
         Next
+        pBFInputsGlobal = New atcDataAttributes()
+        pBFInputsGroups = New atcCollection()
     End Sub
 
     Private Sub btnCreateGroup_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCreateGroup.Click
@@ -55,11 +71,127 @@ Public Class frmBatchMap
     End Sub
 
     Private Sub cmsNode_ItemClicked(ByVal sender As Object, ByVal e As System.Windows.Forms.ToolStripItemClickedEventArgs) Handles cmsNode.ItemClicked
-        Dim lFound As String = ""
-        Dim p As System.Drawing.Point = New System.Drawing.Point(e.ClickedItem.Bounds.Location.X, e.ClickedItem.Bounds.Location.Y)
-        ' Go to the node that the user clicked
-        Dim node As TreeNode = treeBFGroups.GetNodeAt(p)
-        Dim lParentGroupName As String = node.Parent().Text
+        Dim lcmdName As String = e.ClickedItem.Name
+
+        'Dim p As System.Drawing.Point = New System.Drawing.Point(e.ClickedItem.Bounds.Location.X, e.ClickedItem.Bounds.Location.Y)
+        '' Go to the node that the user clicked
+        'Dim node As TreeNode = treeBFGroups.GetNodeAt(p)
+        Dim node As TreeNode = treeBFGroups.SelectedNode
         Dim lFullpath As String = node.FullPath
+        Dim lGroupingName As String = "BatchGroup"
+        Select Case lcmdName
+            Case "cmsRemove"
+                If node.Text.StartsWith(lGroupingName) Then
+                    Dim lGroupNum As Integer = Integer.Parse(node.Text.Substring(lGroupingName.Length + 1))
+                    treeBFGroups.Nodes.Remove(node)
+                    pBatchGroupCount -= 1
+                    For Each lNode As TreeNode In treeBFGroups.Nodes
+                        If lNode.Text.StartsWith(lGroupingName) Then
+                            Dim lGroupNumRemains As Integer = Integer.Parse(lNode.Text.Substring(lGroupingName.Length + 1))
+                            If lGroupNumRemains > lGroupNum Then
+                                lNode.Text = lGroupingName & "_" & (lGroupNumRemains - 1).ToString
+                            End If
+                        End If
+                    Next
+                Else
+                    If node.Parent IsNot Nothing Then
+                        If node.Parent.Nodes.Count = 1 Then
+                            'remove the whole group
+                            Dim lParentGroupName As String = node.Parent().Text
+                            Dim lGroupNum As Integer = Integer.Parse(lParentGroupName.Substring(lGroupingName.Length + 1))
+                            treeBFGroups.Nodes.Remove(node.Parent)
+                            pBatchGroupCount -= 1
+                            For Each lNode As TreeNode In treeBFGroups.Nodes
+                                If lNode.Text.StartsWith(lGroupingName) Then
+                                    Dim lGroupNumRemains As Integer = Integer.Parse(lNode.Text.Substring(lGroupingName.Length + 1))
+                                    If lGroupNumRemains > lGroupNum Then
+                                        lNode.Text = lGroupingName & "_" & (lGroupNumRemains - 1).ToString
+                                    End If
+                                End If
+                            Next
+                        Else
+                            treeBFGroups.Nodes.Remove(node)
+                        End If
+                    End If
+                End If
+            Case "cmsPlotDur"
+                Dim lTsGroup As New atcTimeseriesGroup()
+                Dim lArgs As New atcDataAttributes()
+                lArgs.Add("Constituent", "streamflow")
+                If Not node.Text.StartsWith(lGroupingName) Then
+                    node = node.Parent()
+                End If
+                For Each lStationNode As TreeNode In node.Nodes
+                    Dim lstationId As String = lStationNode.Text
+                    Dim lDataPath As String = GetDataFileFullPath(lstationId)
+                    Dim lTsGroupTemp As atcTimeseriesGroup = clsBatchUtil.ReadTSFromRDB(lDataPath, lArgs)
+                    If lTsGroupTemp IsNot Nothing AndAlso lTsGroupTemp.Count > 0 Then
+                        lTsGroup.Add(lTsGroupTemp(0))
+                    End If
+                Next
+                If lTsGroup.Count > 0 Then
+                    atcUSGSUtility.atcUSGSScreen.GraphDataDuration(lTsGroup)
+                Else
+                    Logger.Msg("Need to download data first.", "Batch Map:Plot")
+                End If
+            Case "cmsGroupSetParm"
+
+        End Select
+    End Sub
+
+    Private Sub btnBrowseDataDir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowseDataDir.Click
+        Dim lFolder As New System.Windows.Forms.FolderBrowserDialog()
+        If lFolder.ShowDialog = Windows.Forms.DialogResult.OK Then
+            txtDataDir.Text = lFolder.SelectedPath
+            If IO.Directory.Exists(txtDataDir.Text) Then
+                pDataPath = txtDataDir.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub txtDataDir_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtDataDir.TextChanged
+        If IO.Directory.Exists(txtDataDir.Text.Trim()) Then
+            pDataPath = txtDataDir.Text.Trim()
+        End If
+    End Sub
+
+    Private Sub btnDownload_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDownload.Click
+        If Not String.IsNullOrEmpty(pDataPath) AndAlso IO.Directory.Exists(pDataPath) Then
+            Dim lStationsNeedDownload As New atcCollection()
+            For Each lStationID As String In pListStations
+                If Not IO.File.Exists(GetDataFileFullPath(lStationID)) Then
+                    lStationsNeedDownload.Add(lStationID, lStationID)
+                End If
+            Next
+            clsBatchUtil.SiteInfoDir = pDataPath
+            Try
+                If lStationsNeedDownload.Count > 0 Then
+                    clsBatchUtil.DownloadData(lStationsNeedDownload)
+                Else
+                    Logger.Msg("Data files already exists in " & pDataPath, "Batch Map:Download")
+                End If
+            Catch ex As Exception
+                Logger.Msg("Some issues occurred during download, please examine the RDB files in data directory:" & vbCrLf & _
+                           pDataPath, "Batch Map")
+                Return
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnPlotDuration_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPlotDuration.Click
+        Dim lTsGroup As New atcTimeseriesGroup()
+        Dim lArgs As New atcDataAttributes()
+        lArgs.Add("Constituent", "streamflow")
+        For I As Integer = 0 To lstStations.RightCount - 1
+            Dim lstationId As String = lstStations.RightItem(I)
+            Dim lDataPath As String = GetDataFileFullPath(lstationId)
+            Dim lTsGroupTemp As atcTimeseriesGroup = clsBatchUtil.ReadTSFromRDB(lDataPath, lArgs)
+            If lTsGroupTemp IsNot Nothing AndAlso lTsGroupTemp.Count > 0 Then
+                lTsGroup.Add(lTsGroupTemp(0))
+            End If
+        Next
+        If lTsGroup.Count > 0 Then
+            atcUSGSUtility.atcUSGSScreen.GraphDataDuration(lTsGroup)
+        End If
     End Sub
 End Class
