dd5f128d34ee0084b5140b55b851db3bb99d5415
anmishra <mishranurag@users.noreply.github.com>
2015-02-04 15:31:43 +0000

    Some control colors were changed to avoid issues in computers with Win XP color scheme.

--- HSPEXP/StartUp.vb
+++ HSPEXP/StartUp.vb
@@ -100,5 +100,16 @@ Public Class StartUp
 
     End Sub
 
-   
+    ' Reset all the controls to the user's default Control color.  
+    Private Sub ResetAllControlsBackColor(ByVal control As Control)
+        control.BackColor = SystemColors.Control
+        control.ForeColor = SystemColors.ControlText
+        If control.HasChildren Then
+            ' Recursively call this method for each child control. 
+            Dim childControl As Control
+            For Each childControl In control.Controls
+                ResetAllControlsBackColor(childControl)
+            Next childControl
+        End If
+    End Sub
 End Class
