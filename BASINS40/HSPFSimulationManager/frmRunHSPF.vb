03d06230ff1444a8b6d13e511103fafca120aabc
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2015-03-24 19:01:04 +0000

    Added reference to atcUCI and dependencies
    frmRunHSPF: added new form for selecting which UCI files will be run.
    
    modUCI.vb: new module to hold code for interacting with atcUCI

---
+++ BASINS40/HSPFSimulationManager/frmRunHSPF.vb
@@ -0,0 +1,87 @@
+﻿Imports atcUtility
+Imports MapWinUtility
+
+Public Class frmRunHSPF
+    Friend SchematicDiagram As HSPFSimulationManager.ctlSchematic
+
+    Private Sub frmRunHSPF_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If SchematicDiagram IsNot Nothing Then
+            Dim lModels As New List(Of clsIcon)
+            Dim lIcon As clsIcon
+            For Each lIcon In SchematicDiagram.AllIcons
+                lModels.Add(lIcon)
+            Next
+            Dim lIconIndex As Integer
+SortDownstream:
+            For lIconIndex = 0 To lModels.Count - 1
+                lIcon = lModels(lIconIndex)
+                If lIcon.DownstreamIcon IsNot Nothing Then
+                    Dim lDownstreamIndex As Integer = lModels.IndexOf(lIcon.DownstreamIcon)
+                    If lDownstreamIndex < lIconIndex Then
+                        lModels.RemoveAt(lDownstreamIndex)
+                        'After removing downstream icon from earlier in the list, lIconIndex now points at location just after lIcon
+                        lModels.Insert(lIconIndex, lIcon.DownstreamIcon)
+                        GoTo SortDownstream
+                    End If
+                End If
+            Next
+            For Each lIcon In lModels
+                lstModels.Items.Add(lIcon.WatershedName & ": " & lIcon.UciFileName, lIcon.Selected)
+            Next
+        End If
+    End Sub
+
+
+    Public Shared Sub RunUCI(ByVal aExeName As String, ByVal aUCIFilename As String)
+        Dim lHspfExe As String = atcUtility.FindFile("Please locate " & aExeName, aExeName)
+        If Not IO.File.Exists(lHspfExe) OrElse Not lHspfExe.ToLower.EndsWith(aExeName.ToLower) Then
+            lHspfExe = atcUtility.FindFile("Please locate " & aExeName, aExeName, , , True)
+            If Not IO.File.Exists(lHspfExe) OrElse Not lHspfExe.ToLower.EndsWith(aExeName.ToLower) Then
+                Logger.Msg("Unable to locate " & aExeName & ", not running.", aExeName)
+                Exit Sub
+            End If
+        End If
+
+        MsgBox("Run: " & aExeName & " on " & aUCIFilename, MsgBoxStyle.OkOnly, "Skipping RunUCI")
+        Exit Sub
+
+        Logger.Status("Running " & aUCIFilename & " (" & lHspfExe & ")")
+        MapWinUtility.LaunchProgram(lHspfExe, IO.Path.GetDirectoryName(aUCIFilename), aUCIFilename)
+    End Sub
+
+    'Private Sub btnWinHSPF_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnWinHSPF.Click
+    '    RunUCI("WinHSPF.exe", cboUciFiles.Text)
+    'End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        Dim lNumToRun As Integer = lstModels.CheckedItems.Count
+        Dim lFinishedRunning As Integer = 0
+        If lstModels.CheckedItems.Count = 0 Then
+            Logger.Msg("No models selected to run", "Run HSPF")
+        Else
+            For Each lSelection As String In lstModels.CheckedItems
+                Dim lColonPos As Integer = lSelection.IndexOf(":")
+                If lColonPos >= 0 Then
+                    Dim lUCIFilename As String = SafeSubstring(lSelection, lColonPos + 1)
+CheckUCIExists:
+                    If FileExists(lUCIFilename) Then
+                        RunUCI("WinHSPFlt.exe", lUCIFilename)
+                        Dim lUCIFile As atcUCI.HspfUci = OpenUCI(lUCIFilename)
+                        If Not RunComplete(lUCIFile) Then
+                            Logger.Msg(lSelection, MsgBoxStyle.Critical, "HSPF Run Did Not Complete")
+                            Exit Sub
+                        End If
+                    Else
+                        Select Case Logger.Msg(lUCIFilename, MsgBoxStyle.AbortRetryIgnore, "UCI File Not Found")
+                            Case MsgBoxResult.Abort : Exit Sub
+                            Case MsgBoxResult.Retry : GoTo CheckUCIExists
+                        End Select
+                    End If
+                End If
+                lFinishedRunning += 1
+                Logger.Progress(lFinishedRunning, lNumToRun)
+            Next
+        End If
+    End Sub
+
+End Class
---
+++ BASINS40/HSPFSimulationManager/modUCI.vb
@@ -0,0 +1,78 @@
+﻿Imports atcUCI
+Imports atcUtility
+Imports MapWinUtility
+
+Module modUCI
+
+    Public MessageFile As atcUCI.HspfMsg
+
+    Function OpenUCI(aUciFileName As String) As atcUCI.HspfUci
+        If MessageFile Is Nothing Then
+            MessageFile = New atcUCI.HspfMsg()
+            Dim lMsgFile As String = FindFile("Locate Message WDM", "hspfmsg.wdm")
+            If Not IO.File.Exists(lMsgFile) Then
+FindMsg:        lMsgFile = FindFile("Locate Message WDM", lMsgFile, "wdm", aUserVerifyFileName:=True)
+            End If
+            If Not IO.File.Exists(lMsgFile) OrElse Not lMsgFile.ToLower().EndsWith("hspfmsg.wdm") Then
+                Select Case Logger.Msg("HSPF message file (hspfmsg.wdm) not found.", MsgBoxStyle.AbortRetryIgnore)
+                    Case MsgBoxResult.Abort
+                        Throw New ApplicationException("HSPF message file (hspfmsg.wdm) not found.")
+                    Case MsgBoxResult.Retry
+                        GoTo FindMsg
+                    Case MsgBoxResult.Ignore
+                        Return Nothing
+                End Select
+            End If
+            MessageFile.Open(lMsgFile)
+        End If
+
+        Dim lUCI As New atcUCI.HspfUci
+        lUCI.FastReadUciForStarter(MessageFile, aUciFileName)
+        Return lUCI
+    End Function
+
+    Function GetEchoFileName(aUCI As atcUCI.HspfUci) As String
+        Dim lFilesBlock As HspfFilesBlk = aUCI.FilesBlock
+        For lIndex As Integer = 0 To lFilesBlock.Count - 1
+            If lFilesBlock.Value(lIndex).Typ = "MESSU" Then
+                Dim lUCIFileDirectory As String = IO.Path.GetDirectoryName(aUCI.Name)
+                Return AbsolutePath(lFilesBlock.Value(lIndex).Name.Trim(), lUCIFileDirectory)
+                Exit For
+            End If
+        Next
+        Return String.Empty
+    End Function
+
+    ''' <summary>
+    ''' True if echo file contains "End of Job"
+    ''' </summary>
+    Function RunComplete(aUCI As atcUCI.HspfUci) As Boolean
+        Dim lEchName As String = GetEchoFileName(aUCI)
+
+        If Not IO.File.Exists(lEchName) Then
+            Return False
+        End If
+        Dim lEchoFile As IO.FileStream = Nothing
+        Try
+            'Open up the ech file
+            lEchoFile = New IO.FileStream(lEchName, IO.FileMode.Open, IO.FileAccess.Read)
+            Dim lFileLength As Long = lEchoFile.Length
+            Dim lStartReading As Long = Math.Max(0, lFileLength - 1000)
+            Dim lReadLength As Long = lFileLength - lStartReading
+            lEchoFile.Position = lStartReading
+            Dim lStreamReader As New IO.StreamReader(lEchoFile, System.Text.Encoding.ASCII)
+            Dim lLastPartOfEchoFile As String = lStreamReader.ReadToEnd()
+            Return lLastPartOfEchoFile.Contains("End of Job")
+        Catch
+            Return False
+        Finally
+            If lEchoFile IsNot Nothing Then
+                Try
+                    lEchoFile.Close()
+                Catch
+                End Try
+            End If
+        End Try
+    End Function
+
+End Module
