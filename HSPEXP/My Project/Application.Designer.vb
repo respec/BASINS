3ba6a8bc4172ab5671ada0e30081e532016e7795
anmishra <mishranurag@users.noreply.github.com>
2015-05-08 00:15:55 +0000

    Single instance application.

--- HSPEXP/My Project/Application.Designer.vb
+++ HSPEXP/My Project/Application.Designer.vb
@@ -24,8 +24,8 @@ Namespace My
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Public Sub New()
             MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
-            Me.IsSingleInstance = false
-            Me.EnableVisualStyles = false
+            Me.IsSingleInstance = true
+            Me.EnableVisualStyles = true
             Me.SaveMySettingsOnExit = true
             Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterAllFormsClose
         End Sub
