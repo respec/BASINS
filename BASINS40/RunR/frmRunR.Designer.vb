eb36998a635b4c50c31b47d12587ad0c0c15a21f
mgray <mgray@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2015-09-11 20:33:32 +0000

    RunR: executable that runs an R script from a text file and writes the results to another text file.       Form frmRunR was created for testing and is not currently in use.

---
+++ BASINS40/RunR/frmRunR.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRunR
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmRunR))
+        Me.txtR = New System.Windows.Forms.TextBox()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.txtYears = New System.Windows.Forms.TextBox()
+        Me.lblYears = New System.Windows.Forms.Label()
+        Me.lblValues = New System.Windows.Forms.Label()
+        Me.txtValues = New System.Windows.Forms.TextBox()
+        Me.lblScript = New System.Windows.Forms.Label()
+        Me.lblConfidence = New System.Windows.Forms.Label()
+        Me.txtConfidence = New System.Windows.Forms.TextBox()
+        Me.lblPercent = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.ComboResult = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'txtR
+        '
+        Me.txtR.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtR.Location = New System.Drawing.Point(12, 143)
+        Me.txtR.Multiline = True
+        Me.txtR.Name = "txtR"
+        Me.txtR.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtR.Size = New System.Drawing.Size(625, 263)
+        Me.txtR.TabIndex = 0
+        Me.txtR.Text = resources.GetString("txtR.Text")
+        '
+        'btnRun
+        '
+        Me.btnRun.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRun.Location = New System.Drawing.Point(562, 412)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 1
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'txtYears
+        '
+        Me.txtYears.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtYears.Location = New System.Drawing.Point(57, 12)
+        Me.txtYears.Name = "txtYears"
+        Me.txtYears.Size = New System.Drawing.Size(580, 20)
+        Me.txtYears.TabIndex = 2
+        Me.txtYears.Text = resources.GetString("txtYears.Text")
+        '
+        'lblYears
+        '
+        Me.lblYears.AutoSize = True
+        Me.lblYears.Location = New System.Drawing.Point(12, 15)
+        Me.lblYears.Name = "lblYears"
+        Me.lblYears.Size = New System.Drawing.Size(34, 13)
+        Me.lblYears.TabIndex = 3
+        Me.lblYears.Text = "Years"
+        '
+        'lblValues
+        '
+        Me.lblValues.AutoSize = True
+        Me.lblValues.Location = New System.Drawing.Point(12, 41)
+        Me.lblValues.Name = "lblValues"
+        Me.lblValues.Size = New System.Drawing.Size(39, 13)
+        Me.lblValues.TabIndex = 5
+        Me.lblValues.Text = "Values"
+        '
+        'txtValues
+        '
+        Me.txtValues.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtValues.Location = New System.Drawing.Point(57, 38)
+        Me.txtValues.Name = "txtValues"
+        Me.txtValues.Size = New System.Drawing.Size(580, 20)
+        Me.txtValues.TabIndex = 4
+        Me.txtValues.Text = "3, 6, 6, 7, 7, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 14, 14, 16, 16, 16, 16, 18, 19" &
+    ", 20, 21, 22, 23, 26, 27, 28, 28, 28, 30, 33, 33, 34, 37, 42, 45"
+        '
+        'lblScript
+        '
+        Me.lblScript.AutoSize = True
+        Me.lblScript.Location = New System.Drawing.Point(12, 127)
+        Me.lblScript.Name = "lblScript"
+        Me.lblScript.Size = New System.Drawing.Size(102, 13)
+        Me.lblScript.TabIndex = 6
+        Me.lblScript.Text = "Computational Code"
+        '
+        'lblConfidence
+        '
+        Me.lblConfidence.AutoSize = True
+        Me.lblConfidence.Location = New System.Drawing.Point(12, 67)
+        Me.lblConfidence.Name = "lblConfidence"
+        Me.lblConfidence.Size = New System.Drawing.Size(99, 13)
+        Me.lblConfidence.TabIndex = 8
+        Me.lblConfidence.Text = "Confidence Interval"
+        '
+        'txtConfidence
+        '
+        Me.txtConfidence.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtConfidence.Location = New System.Drawing.Point(157, 64)
+        Me.txtConfidence.Name = "txtConfidence"
+        Me.txtConfidence.Size = New System.Drawing.Size(56, 20)
+        Me.txtConfidence.TabIndex = 7
+        Me.txtConfidence.Text = "95"
+        '
+        'lblPercent
+        '
+        Me.lblPercent.AutoSize = True
+        Me.lblPercent.Location = New System.Drawing.Point(219, 67)
+        Me.lblPercent.Name = "lblPercent"
+        Me.lblPercent.Size = New System.Drawing.Size(15, 13)
+        Me.lblPercent.TabIndex = 9
+        Me.lblPercent.Text = "%"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(12, 93)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(64, 13)
+        Me.lblResult.TabIndex = 10
+        Me.lblResult.Text = "Result Type"
+        '
+        'ComboResult
+        '
+        Me.ComboResult.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboResult.FormattingEnabled = True
+        Me.ComboResult.Items.AddRange(New Object() {"TRUE: there is a trend FALSE: No trend", "rho", "rho, P", "rho, P, TRUE/FALSE", "rho, P, confidence intervals", "rho, P, confidence intervals, TRUE/FALSE", "rho, P, VB/VBA integer for TRUE/FALSE", "rho, P, VB/VBA integer for TRUE/FALSE", "rho, P, confidence intervals, VB/VBA integer for TRUE/FALSE", "VB/VBA integer for TRUE/FALSE"})
+        Me.ComboResult.Location = New System.Drawing.Point(157, 90)
+        Me.ComboResult.Name = "ComboResult"
+        Me.ComboResult.Size = New System.Drawing.Size(480, 21)
+        Me.ComboResult.TabIndex = 11
+        '
+        'frmRunR
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(649, 447)
+        Me.Controls.Add(Me.ComboResult)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblPercent)
+        Me.Controls.Add(Me.lblConfidence)
+        Me.Controls.Add(Me.txtConfidence)
+        Me.Controls.Add(Me.lblScript)
+        Me.Controls.Add(Me.lblValues)
+        Me.Controls.Add(Me.txtValues)
+        Me.Controls.Add(Me.lblYears)
+        Me.Controls.Add(Me.txtYears)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.txtR)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmRunR"
+        Me.Text = "Run R"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtR As TextBox
+    Friend WithEvents btnRun As Button
+    Friend WithEvents txtYears As TextBox
+    Friend WithEvents lblYears As Label
+    Friend WithEvents lblValues As Label
+    Friend WithEvents txtValues As TextBox
+    Friend WithEvents lblScript As Label
+    Friend WithEvents lblConfidence As Label
+    Friend WithEvents txtConfidence As TextBox
+    Friend WithEvents lblPercent As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents ComboResult As ComboBox
+End Class
---
+++ BASINS40/RunR/modR.vb
@@ -0,0 +1,121 @@
+﻿Imports RDotNet
+
+Module modR
+    Dim pEngine As REngine = Nothing
+
+    Public Sub Main()
+        If My.Application.CommandLineArgs.Count > 1 Then
+            Try
+                Dim lEngine As REngine = GetEngine()
+                If lEngine IsNot Nothing Then
+                    Dim lResult As String() = {"No Result"}
+                    Dim lResultFilename As String = My.Application.CommandLineArgs(0)
+                    For Each lArg As String In My.Application.CommandLineArgs
+                        If lArg <> lResultFilename Then
+                            If IO.File.Exists(lArg) Then
+                                lResult = lEngine.Evaluate(System.IO.File.ReadAllText(lArg)).AsCharacter().ToArray()
+                            Else
+                                MsgBox("R file not found" & vbCrLf & lArg, MsgBoxStyle.Critical, "Run R")
+                            End If
+                        End If
+                    Next
+
+                    'lEngine.Evaluate(txtR.Text)
+
+                    'SetIntegerVectorFromString(lEngine, "InputYears", txtYears.Text)
+                    'SetDoubleVectorFromString(lEngine, "InputValues", txtValues.Text)
+                    'Dim lConfidencePercent As Double = 95
+                    'Double.TryParse(txtConfidence.Text, lConfidencePercent)
+                    'lEngine.SetSymbol("InputPercentConfidence", lEngine.CreateNumeric(lConfidencePercent))
+                    'lEngine.SetSymbol("InputWhat", lEngine.CreateInteger(ComboResult.SelectedIndex))
+                    'Dim lResult As String() = lEngine.Evaluate("fnGetTrend(InputYears, InputValues, InputPercentConfidence, InputWhat)").AsCharacter().ToArray()
+                    'Dim lResult As String() = lEngine.Evaluate("InputYears").AsCharacter().ToArray()
+                    IO.File.WriteAllLines(lResultFilename, lResult)
+                    'MsgBox(String.Join(Environment.NewLine, lResult), MsgBoxStyle.OkOnly, "Result")
+                End If
+            Catch ex As Exception
+                MsgBox(ex.ToString, MsgBoxStyle.Critical, "Error running R")
+            End Try
+        End If
+    End Sub
+
+    Public Function GetEngine() As REngine
+        If pEngine Is Nothing Then
+TryGetInstance:
+            Try
+                pEngine = REngine.GetInstance()
+            Catch ex As Exception
+                If ex.Message.Contains("Windows Registry key 'SOFTWARE\R-core' not found") Then
+                    If MsgBox("R was not found. Download R now?", MsgBoxStyle.YesNo, "Could not start R engine") = MsgBoxResult.Yes Then
+                        OpenFile("http://rweb.crmda.ku.edu/cran/bin/windows/base/")
+                        If MsgBox("Retry after installing R, or Cancel", MsgBoxStyle.RetryCancel) = MsgBoxResult.Retry Then
+                            GoTo TryGetInstance
+                        End If
+                    End If
+                Else
+                    MsgBox(ex.Message, MsgBoxStyle.Critical, "Could not start R engine")
+                End If
+            End Try
+        End If
+        Return pEngine
+    End Function
+
+    Public Sub DisposeEngine()
+        If pEngine IsNot Nothing Then
+            Try
+                pEngine.Dispose()
+            Catch
+            End Try
+            pEngine = Nothing
+        End If
+    End Sub
+
+    Public Sub SetIntegerVectorFromString(ByVal aEngine As REngine, ByVal aName As String, ByVal aValues As String)
+        Dim lList As New List(Of Integer)
+        For Each lNumberText As String In StringToList(aValues)
+            Dim lNumber As Integer
+            If Integer.TryParse(lNumberText, lNumber) Then
+                lList.Add(lNumber)
+            End If
+        Next
+        aEngine.SetSymbol(aName, aEngine.CreateIntegerVector(lList))
+    End Sub
+
+    Public Sub SetDoubleVectorFromString(ByVal aEngine As REngine, ByVal aName As String, ByVal aValues As String)
+        Dim lList As New List(Of Double)
+        For Each lNumberText As String In StringToList(aValues)
+            Dim lNumber As Double
+            If Double.TryParse(lNumberText, lNumber) Then
+                lList.Add(lNumber)
+            End If
+        Next
+        aEngine.SetSymbol(aName, aEngine.CreateNumericVector(lList))
+    End Sub
+
+    Private Function StringToList(ByVal aValues As String) As String()
+        aValues = aValues.Replace(", ", ",")
+        aValues = aValues.Replace(" ", ",")
+        While aValues.Contains(",,")
+            aValues = aValues.Replace(",,", ",")
+        End While
+        Return aValues.Split(",")
+    End Function
+
+    'Open a file using the default method the system would have used if it was double-clicked
+    Public Sub OpenFile(ByVal FileOrURL As String, Optional ByVal Wait As Boolean = False)
+        Dim newProcess As New Process
+        Try
+            If FileOrURL <> "" Then
+                'Use a .NET process() to launch the file or URL
+                newProcess.StartInfo.FileName = FileOrURL
+                newProcess.Start()
+                If Wait Then
+                    newProcess.WaitForExit()
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+End Module
