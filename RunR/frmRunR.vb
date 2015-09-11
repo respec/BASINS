e09e9518ed493f5b5eccba303d3dedcb8771be94
Mark Gray <MarkGrayRESPEC@users.noreply.github.com>
2015-09-11 20:33:32 +0000

    RunR: executable that runs an R script from a text file and writes the results to another text file.       Form frmRunR was created for testing and is not currently in use.

---
+++ RunR/frmRunR.vb
@@ -0,0 +1,35 @@
+﻿Imports System.ComponentModel
+Imports RDotNet
+
+Public Class frmRunR
+
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        Try
+            Dim lEngine As REngine = GetEngine()
+            If lEngine IsNot Nothing Then
+                lEngine.Evaluate(txtR.Text)
+                SetIntegerVectorFromString(lEngine, "InputYears", txtYears.Text)
+                SetDoubleVectorFromString(lEngine, "InputValues", txtValues.Text)
+                Dim lConfidencePercent As Double = 95
+                Double.TryParse(txtConfidence.Text, lConfidencePercent)
+                lEngine.SetSymbol("InputPercentConfidence", lEngine.CreateNumeric(lConfidencePercent))
+                lEngine.SetSymbol("InputWhat", lEngine.CreateInteger(ComboResult.SelectedIndex))
+                Dim lResult As String() = lEngine.Evaluate("fnGetTrend(InputYears, InputValues, InputPercentConfidence, InputWhat)").AsCharacter().ToArray()
+                'Dim lResult As String() = lEngine.Evaluate("InputYears").AsCharacter().ToArray()
+                MsgBox(String.Join(Environment.NewLine, lResult), MsgBoxStyle.OkOnly, "Result")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString, MsgBoxStyle.Critical, "Error running R")
+        End Try
+    End Sub
+
+    Private Sub frmRunR_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        DisposeEngine()
+    End Sub
+
+    Private Sub frmRunR_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboResult.SelectedIndex = 0
+        btnRun.Focus()
+    End Sub
+End Class
