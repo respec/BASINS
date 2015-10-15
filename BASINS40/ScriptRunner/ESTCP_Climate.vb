f58be0b64f4072e219d5d5aa6455cde5dec0cf07
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2015-10-15 21:03:40 +0000

    ESTCP_Climate.vb -- added additional utilities for generating climate scenario

--- BASINS40/ScriptRunner/ESTCP_Climate.vb
+++ BASINS40/ScriptRunner/ESTCP_Climate.vb
@@ -3,6 +3,7 @@ Imports MapWinUtility          'MapWinUtility.dll resides in the BASINS41\bin fo
 
 Imports atcUtility             'atcUtility.dll resides in the BASINS41\bin\Plugins\BASINS folder
 Imports atcData                'atcData.dll resides in the BASINS41\bin\Plugins\BASINS folder
+Imports atcMetCmp
 
 Imports atcHspfBinOut          'atcHspfBinOut.dll resides in the BASINS41\bin\Plugins\BASINS folder
 Imports atcTimeseriesSWAT      'atcTimeseriesSWAT.dll resides in the BASINS41\bin\Plugins\BASINS folder
@@ -13,6 +14,199 @@ Imports System.Text
 Public Module ESTCP_Climate
 
     Public Sub ScriptMain(ByRef aMapWin As IMapWin)
+        'ProduceClimateScenarioPrecAndAirTempForFB()
+        'ReproduceClimateScenarioPrecAndAirTempForACF()
+        'CombineTmaxTmin()
+        ComputeHamonPETFromHourlyAirTemp()
+    End Sub
+
+    Private Sub ComputeHamonPETFromHourlyAirTemp()
+        Dim lDataSource As atcWDM.atcDataSourceWDM = Nothing
+        Dim lWDMFileName As String = "D:\ESTCP\FB\MetDataExtended\FBmet.wdm"
+        If FileExists(lWDMFileName) Then
+            'check to see if this file is already open in this project
+            lDataSource = atcDataManager.DataSourceBySpecification(lWDMFileName)
+
+            If lDataSource Is Nothing Then 'need to open it here
+                lDataSource = New atcWDM.atcDataSourceWDM
+                If Not lDataSource.Open(lWDMFileName) Then
+                    lDataSource = Nothing
+                End If
+            End If
+        End If
+
+        '13 is air temp, now 2005 is the extended version
+        '16 is pevt   - can I get this from 13?
+        Dim lAirts As New atcTimeseries(Nothing)
+        lAirts = Nothing
+        lAirts = lDataSource.DataSets.ItemByKey(2005)
+        Dim lLat As Double = 32.5161
+        Dim lCTS() As Double = {0, 0.0055, 0.0055, 0.0055, 0.0055, 0.0055, 0.0055, 0.0055, 0.0055, 0.0055, 0.0055, 0.0055, 0.0055}
+
+        Dim lPETts As New atcTimeseries(Nothing)
+        lPETts = DisSolPet(PanEvaporationTimeseriesComputedByHamonX(lAirts, Nothing, True, lLat, lCTS), Nothing, 2, lLat)
+
+        lPETts.Attributes.SetValue("ID", 2013)
+        lDataSource.AddDataset(lPETts, atcDataSource.EnumExistAction.ExistAppend)
+        
+    End Sub
+
+    Private Sub CombineTmaxTmin()
+        'combine tmax and tmin, change from c to f
+        'open the source wdm file:
+        Dim lDataSource As atcWDM.atcDataSourceWDM = Nothing
+        Dim lWDMFileName As String = "D:\ESTCP\FB\MetDataExtended\FBmet.wdm"
+        If FileExists(lWDMFileName) Then
+            'check to see if this file is already open in this project
+            lDataSource = atcDataManager.DataSourceBySpecification(lWDMFileName)
+
+            If lDataSource Is Nothing Then 'need to open it here
+                lDataSource = New atcWDM.atcDataSourceWDM
+                If Not lDataSource.Open(lWDMFileName) Then
+                    lDataSource = Nothing
+                End If
+            End If
+        End If
+
+        For Each lDataSet As atcTimeseries In lDataSource.DataSets
+            Dim lDsn As Integer = lDataSet.Attributes.GetValue("Id")
+            If lDsn = 2001 Then
+                For Each lDataSetNew As atcTimeseries In lDataSource.DataSets
+                    Dim lDsnNew As Integer = lDataSetNew.Attributes.GetValue("Id")
+                    If lDsnNew = 2003 Then
+                        'convert to f
+                        For lIndex As Integer = 0 To lDataSetNew.numValues
+                            lDataSetNew.Values(lIndex) = (lDataSetNew.Values(lIndex) * 9 / 5) + 32
+                        Next
+                        'append ldatasetnew to ldataset
+                        lDataSetNew.Attributes.SetValue("ID", 2001)
+                        lDataSource.AddDataset(lDataSetNew, atcDataSource.EnumExistAction.ExistAppend)
+                    End If
+                Next
+            End If
+            If lDsn = 2002 Then
+                For Each lDataSetNew As atcTimeseries In lDataSource.DataSets
+                    Dim lDsnNew As Integer = lDataSetNew.Attributes.GetValue("Id")
+                    If lDsnNew = 2004 Then
+                        'convert to f
+                        For lIndex As Integer = 0 To lDataSetNew.numValues
+                            lDataSetNew.Values(lIndex) = (lDataSetNew.Values(lIndex) * 9 / 5) + 32
+                        Next
+                        'append ldatasetnew to ldataset
+                        lDataSetNew.Attributes.SetValue("ID", 2002)
+                        lDataSource.AddDataset(lDataSetNew, atcDataSource.EnumExistAction.ExistAppend)
+                    End If
+                Next
+            End If
+        Next
+
+    End Sub
+
+    Private Sub ProduceClimateScenarioPrecAndAirTempForFB()
+        'this sub applies prec and air temp changes to the FB met data for generating the climate scenario
+
+        'open the source wdm file:
+        Dim lDataSource As atcWDM.atcDataSourceWDM = Nothing
+        Dim lWDMFileName As String = "D:\ESTCP\FB\MetDataExtended\FBmet.wdm"
+        If FileExists(lWDMFileName) Then
+            'check to see if this file is already open in this project
+            lDataSource = atcDataManager.DataSourceBySpecification(lWDMFileName)
+
+            If lDataSource Is Nothing Then 'need to open it here
+                lDataSource = New atcWDM.atcDataSourceWDM
+                If Not lDataSource.Open(lWDMFileName) Then
+                    lDataSource = Nothing
+                End If
+            End If
+        End If
+
+        'open the WDM file for the climate results
+        Dim lOutputDataSource As atcWDM.atcDataSourceWDM = Nothing
+        Dim lOutputWDMFileName As String = "D:\ESTCP\FB\MetDataExtended\FBmetClimate.wdm"
+        If FileExists(lOutputWDMFileName) Then
+            'check to see if this file is already open in this project
+            lOutputDataSource = atcDataManager.DataSourceBySpecification(lOutputWDMFileName)
+
+            If lOutputDataSource Is Nothing Then 'need to open it here
+                lOutputDataSource = New atcWDM.atcDataSourceWDM
+                If Not lOutputDataSource.Open(lOutputWDMFileName) Then
+                    lOutputDataSource = Nothing
+                End If
+            End If
+        End If
+
+        'at this point one can loop through the timeseries of the file
+        For Each lDataSet As atcTimeseries In lDataSource.DataSets
+            Dim lConstituent As String = lDataSet.Attributes.GetValue("Constituent")
+            Dim lLoc As String = lDataSet.Attributes.GetValue("Location")
+            Dim lStanam As String = lDataSet.Attributes.GetValue("Stanam")
+            Dim lDsn As Integer = lDataSet.Attributes.GetValue("Id")
+            Dim lFileName As String = lDataSet.Attributes.GetValue("Data Source")
+
+            'data set numbers of interest:
+            '101-110 are precip in FB model
+            '13 is air temp at columbus, 16 is pevt at columbus
+
+            Dim lIsPrecip As Boolean = False
+            Dim lIsAirTemp As Boolean = False
+            Dim lThresh(12) As Double
+            Dim lLower(12) As Double
+            Dim lUpper(12) As Double
+            Dim lAdd(12) As Double
+
+            If lDsn > 100 And lDsn < 111 Then
+                'columbus precip
+                lIsPrecip = True
+                Dim lT1() As Double = {0.08, 0.1, 0.1173, 0.12, 0.1098, 0.11, 0.101, 0.11, 0.0998, 0.1, 0.1, 0.1}
+                lThresh = lT1
+                Dim lL1() As Double = {0.893504584940514, 0.828267674241468, 0.970722234194858, 0.891950243062045, 0.970506469322334, 0.551567211283874, 0.67801078320664, 0.818947966820425, 0.879370857035774, 1.25478163237953, 0.988475753550421, 1.25805472748197}
+                lLower = lL1
+                Dim lU1() As Double = {0.826286441072365, 1.21864270301569, 1.07371092138687, 0.726635267195552, 0.754843065844099, 0.859877760904651, 0.874591861128565, 0.691691596046413, 0.574252711757115, 0.940860733790508, 1.04684082928219, 0.93246526447717}
+                lUpper = lU1
+            ElseIf lDsn = 13 Then
+                'columbus air temp
+                lIsAirTemp = True
+                Dim lA1() As Double = {2.10158, 1.95728, 1.82882, 1.69035, 2.27918, 3.44878, 3.54368, 3.0908, 2.71039, 2.91283, 2.24485, 3.32161}
+                lAdd = lA1
+            End If
+
+            If lIsPrecip Then
+                Dim lDateA(6) As Integer
+                For lIndex As Integer = 0 To lDataSet.numValues
+                    'convert julian date into date array so we know what month this is
+                    Dim lDate As Double = lDataSet.Dates.Values(lIndex) - 0.0001
+                    J2Date(lDate, lDateA)
+
+                    Dim lValue As Double = lDataSet.Values(lIndex)
+                    If lValue > 0 Then
+                        If lValue > lThresh(lDateA(1) - 1) Then
+                            lValue = lValue * lUpper(lDateA(1) - 1)
+                        Else
+                            lValue = lValue * lLower(lDateA(1) - 1)
+                        End If
+                        lDataSet.Values(lIndex) = lValue
+                    End If
+                Next
+                lOutputDataSource.AddDataset(lDataSet)
+            ElseIf lIsAirTemp Then
+                Dim lDateA(6) As Integer
+                For lIndex As Integer = 0 To lDataSet.numValues
+                    'convert julian date into date array so we know what month this is
+                    Dim lDate As Double = lDataSet.Dates.Values(lIndex) - 0.0001
+                    J2Date(lDate, lDateA)
+
+                    Dim lValue As Double = lDataSet.Values(lIndex)
+                    lValue = lValue + (lAdd(lDateA(1) - 1) * 9.0 / 5.0)
+                    lDataSet.Values(lIndex) = lValue
+                Next
+                lOutputDataSource.AddDataset(lDataSet)
+            End If
+
+        Next
+    End Sub
+
+    Private Sub ReproduceClimateScenarioPrecAndAirTempForACF()
+        'this sub was used to verify that we've got the process down for generating the climate sceanrio prec and air temp 
 
         'open a wdm file:
         Dim lDataSource As atcWDM.atcDataSourceWDM = Nothing
