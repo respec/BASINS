e634f5eebdf3ba61f5acec7fb5fba4f215967245
pdudda <pdudda@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2015-11-12 20:03:01 +0000

    ESTCP_Climate.vb -- added sub to update FB land use

--- BASINS40/ScriptRunner/ESTCP_Climate.vb
+++ BASINS40/ScriptRunner/ESTCP_Climate.vb
@@ -4,6 +4,7 @@ Imports MapWinUtility          'MapWinUtility.dll resides in the BASINS41\bin fo
 Imports atcUtility             'atcUtility.dll resides in the BASINS41\bin\Plugins\BASINS folder
 Imports atcData                'atcData.dll resides in the BASINS41\bin\Plugins\BASINS folder
 Imports atcMetCmp
+Imports atcUCI
 
 Imports atcHspfBinOut          'atcHspfBinOut.dll resides in the BASINS41\bin\Plugins\BASINS folder
 Imports atcTimeseriesSWAT      'atcTimeseriesSWAT.dll resides in the BASINS41\bin\Plugins\BASINS folder
@@ -20,14 +21,105 @@ Public Module ESTCP_Climate
         'AppendBASINSAirTemp()
         'CorrectFtBenningAirTemp()
         'ProduceClimateScenarioPrecAndAirTempForFB()
-        ComputeHamonPETFromHourlyAirTemp()
+        'ComputeHamonPETFromHourlyAirTemp()
+        UpdateFBLandUse()
 
         'CombineTmaxTmin() -- never used for production 
+    End Sub
+
+    Private Sub UpdateFBLandUse()
+        Dim lMsg As New atcUCI.HspfMsg
+        lMsg.Open("hspfmsg.wdm")
+
+        Dim lOrigUCI As New HspfUci
+        lOrigUCI.FastReadUciForStarter(lMsg, "D:\ESTCP\FB\Model_EB\Upatoi.uci")
+
+        Dim lPerMultUpper As Double = 0.0189
+        Dim lImpMultUpper As Double = 0.0312
+        Dim lPerMultLower As Double = 0.0404
+        Dim lImpMultLower As Double = 0.0578
+        Dim lImpMult As Double = 0.0
+        Dim lPerMult As Double = 0.0
+        For Each lRchOper As HspfOperation In lOrigUCI.OpnBlks("RCHRES").Ids
+            If lRchOper.Id = 607 Or lRchOper.Id = 11 Or lRchOper.Id = 3 Or lRchOper.Id = 605 Or _
+               lRchOper.Id = 612 Or lRchOper.Id = 2 Or lRchOper.Id = 10 Or lRchOper.Id = 609 Or _
+               lRchOper.Id = 6 Or lRchOper.Id = 604 Or lRchOper.Id = 608 Or lRchOper.Id = 1 Or _
+               lRchOper.Id = 615 Or lRchOper.Id = 17 Or lRchOper.Id = 16 Or lRchOper.Id = 613 Or _
+               lRchOper.Id = 14 Or lRchOper.Id = 9 Or lRchOper.Id = 8 Or lRchOper.Id = 7 Or _
+               lRchOper.Id = 15 Or lRchOper.Id = 12 Or lRchOper.Id = 13 Or lRchOper.Id = 616 Or _
+               lRchOper.Id = 5 Or lRchOper.Id = 707 Or lRchOper.Id = 4 Or lRchOper.Id = 614 Then
+                'upper reaches
+                lPerMult = lPerMultUpper
+                lImpMult = lImpMultUpper
+            Else
+                'lower reaches
+                lPerMult = lPerMultLower
+                lImpMult = lImpMultLower
+            End If
+
+            Dim lAddedArea As Double = 0.0
+            Dim lPerOrigArea As Double = 0.0
+            Dim lImpOrigArea As Double = 0.0
+            Dim lPerNewArea As Double = 0.0
+            Dim lImpNewArea As Double = 0.0
+            For Each lConn As HspfConnection In lOrigUCI.Connections
+                If lConn.Target.VolName = "RCHRES" And lConn.Target.VolId = lRchOper.Id Then
+                    'this is a contributor to the current reach
+                    If lConn.Source.VolName = "IMPLND" Then
+                        lImpOrigArea += lConn.MFact
+                        lConn.MFact = lConn.MFact * (1.0 + lImpMult)
+                        lImpNewArea += lConn.MFact
+                    ElseIf lConn.Source.VolName = "PERLND" Then
+                        Dim lPerOpn As HspfOperation = lOrigUCI.OpnBlks("PERLND").OperFromID(lConn.Source.VolId)
+                        If lPerOpn.Description.StartsWith("U/C") Or lPerOpn.Description.StartsWith("Paved Road") Then
+                            lPerOrigArea += lConn.MFact
+                            lConn.MFact = lConn.MFact * (1.0 + lPerMult)
+                            lPerNewArea += lConn.MFact
+                        End If
+                    End If
+                End If
+            Next
+            'see how much area we added
+            lAddedArea = lPerNewArea - lPerOrigArea + lImpNewArea - lImpOrigArea
+            'need to subtract this area from the valid perlnds, first find out how much valid perlnd area exists
+            lPerOrigArea = 0.0
+            For Each lConn As HspfConnection In lOrigUCI.Connections
+                If lConn.Target.VolName = "RCHRES" And lConn.Target.VolId = lRchOper.Id Then
+                    'this is a contributor to the current reach
+                    If lConn.Source.VolName = "PERLND" Then
+                        Dim lPerOpn As HspfOperation = lOrigUCI.OpnBlks("PERLND").OperFromID(lConn.Source.VolId)
+                        If lPerOpn.Description.StartsWith("U/C") Or lPerOpn.Description.StartsWith("Paved Road") Or lPerOpn.Description.StartsWith("Water") Then
+                        Else
+                            lPerOrigArea += lConn.MFact
+                        End If
+                    End If
+                End If
+            Next
+            Dim lPerRemoval As Double = lAddedArea / lPerOrigArea
+            'need to remove equal percentage from all valid perlnds
+            For Each lConn As HspfConnection In lOrigUCI.Connections
+                If lConn.Target.VolName = "RCHRES" And lConn.Target.VolId = lRchOper.Id Then
+                    'this is a contributor to the current reach
+                    If lConn.Source.VolName = "PERLND" Then
+                        Dim lPerOpn As HspfOperation = lOrigUCI.OpnBlks("PERLND").OperFromID(lConn.Source.VolId)
+                        If lPerOpn.Description.StartsWith("U/C") Or lPerOpn.Description.StartsWith("Paved Road") Or lPerOpn.Description.StartsWith("Water") Then
+                        Else
+                            lConn.MFact = lConn.MFact * (1.0 - lPerRemoval)
+                        End If
+                    End If
+                End If
+            Next
+        Next
+
+        'save the uci under a new name
+        lOrigUCI.Name = "D:\ESTCP\FB\Model_EB\UpatoiFutureLU.uci"
+        lOrigUCI.Save()
     End Sub
 
     Private Sub ComputeHamonPETFromHourlyAirTemp()
         Dim lDataSource As atcWDM.atcDataSourceWDM = Nothing
         Dim lWDMFileName As String = "D:\ESTCP\FB\MetDataExtended\FBmetClimate.wdm"
+        'Dim lWDMFileName As String = "D:\ESTCP\FB\MetDataExtended\FBmetTrialUpdates.wdm"
         If FileExists(lWDMFileName) Then
             'check to see if this file is already open in this project
             lDataSource = atcDataManager.DataSourceBySpecification(lWDMFileName)
@@ -309,7 +401,7 @@ Public Module ESTCP_Climate
 
             'data set numbers of interest:
             '101-110 are precip in FB model
-            '13 is air temp at columbus, 16 is pevt at columbus
+            '13 is air temp at columbus, 16 is pevt at columbus, 11 is prec at columbus
 
             Dim lIsPrecip As Boolean = False
             Dim lIsAirTemp As Boolean = False
@@ -319,6 +411,7 @@ Public Module ESTCP_Climate
             Dim lAdd(12) As Double
 
             If lDsn > 100 And lDsn < 111 Then
+                'If lDsn = 11 Then
                 'columbus precip
                 lIsPrecip = True
                 Dim lT1() As Double = {0.08, 0.1, 0.1173, 0.12, 0.1098, 0.11, 0.101, 0.11, 0.0998, 0.1, 0.1, 0.1}
