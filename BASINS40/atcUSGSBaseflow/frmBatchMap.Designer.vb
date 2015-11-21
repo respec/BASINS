55cce0877c5c3139288379c1fbd6eab12770cf78
tzhai <tzhai@b5b63dcf-76f6-0310-9a88-d1fdb234d988>
2015-11-21 17:37:03 +0000

    revise data download, batch UI

--- BASINS40/atcUSGSBaseflow/frmBatchMap.Designer.vb
+++ BASINS40/atcUSGSBaseflow/frmBatchMap.Designer.vb
@@ -22,27 +22,31 @@ Partial Class frmBatchMap
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmBatchMap))
-        Me.Label1 = New System.Windows.Forms.Label
-        Me.txtDataDir = New System.Windows.Forms.TextBox
-        Me.btnBrowseDataDir = New System.Windows.Forms.Button
-        Me.lstStations = New atcControls.ATCoSelectListSortByProp
-        Me.treeBFGroups = New System.Windows.Forms.TreeView
-        Me.Label2 = New System.Windows.Forms.Label
-        Me.btnCreateGroup = New System.Windows.Forms.Button
-        Me.btnPlotDuration = New System.Windows.Forms.Button
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox
-        Me.btnParmForm = New System.Windows.Forms.Button
-        Me.txtParameters = New System.Windows.Forms.TextBox
-        Me.btnDoBatch = New System.Windows.Forms.Button
-        Me.btnSaveSpecs = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDataDir = New System.Windows.Forms.TextBox()
+        Me.btnBrowseDataDir = New System.Windows.Forms.Button()
+        Me.lstStations = New atcControls.ATCoSelectListSortByProp()
+        Me.treeBFGroups = New System.Windows.Forms.TreeView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCreateGroup = New System.Windows.Forms.Button()
+        Me.btnPlotDuration = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnParmForm = New System.Windows.Forms.Button()
+        Me.txtParameters = New System.Windows.Forms.TextBox()
+        Me.btnDoBatch = New System.Windows.Forms.Button()
+        Me.btnSaveSpecs = New System.Windows.Forms.Button()
         Me.cmsNode = New System.Windows.Forms.ContextMenuStrip(Me.components)
-        Me.cmsRemove = New System.Windows.Forms.ToolStripMenuItem
-        Me.cmsPlotDur = New System.Windows.Forms.ToolStripMenuItem
-        Me.cmsGroupSetParm = New System.Windows.Forms.ToolStripMenuItem
-        Me.cmsGlobalSetParm = New System.Windows.Forms.ToolStripMenuItem
-        Me.btnDownload = New System.Windows.Forms.Button
+        Me.cmsGlobalSetParm = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cmsGroupSetParm = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cmsPlotDur = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cmsRemove = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnDownload = New System.Windows.Forms.Button()
+        Me.btnGroupPlot = New System.Windows.Forms.Button()
+        Me.btnGroupRemove = New System.Windows.Forms.Button()
+        Me.btnGroupGroup = New System.Windows.Forms.Button()
+        Me.btnGroupGlobal = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.cmsNode.SuspendLayout()
         Me.SuspendLayout()
@@ -50,27 +54,24 @@ Partial Class frmBatchMap
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(17, 16)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(75, 17)
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Data Path:"
         '
         'txtDataDir
         '
-        Me.txtDataDir.Location = New System.Drawing.Point(103, 12)
-        Me.txtDataDir.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtDataDir.Location = New System.Drawing.Point(77, 10)
         Me.txtDataDir.Name = "txtDataDir"
-        Me.txtDataDir.Size = New System.Drawing.Size(383, 22)
+        Me.txtDataDir.Size = New System.Drawing.Size(288, 20)
         Me.txtDataDir.TabIndex = 1
         '
         'btnBrowseDataDir
         '
-        Me.btnBrowseDataDir.Location = New System.Drawing.Point(495, 10)
-        Me.btnBrowseDataDir.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnBrowseDataDir.Location = New System.Drawing.Point(371, 8)
         Me.btnBrowseDataDir.Name = "btnBrowseDataDir"
-        Me.btnBrowseDataDir.Size = New System.Drawing.Size(81, 28)
+        Me.btnBrowseDataDir.Size = New System.Drawing.Size(61, 23)
         Me.btnBrowseDataDir.TabIndex = 2
         Me.btnBrowseDataDir.Text = "Browse"
         Me.btnBrowseDataDir.UseVisualStyleBackColor = True
@@ -80,50 +81,45 @@ Partial Class frmBatchMap
         Me.lstStations.DisplayMember = ""
         Me.lstStations.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lstStations.LeftLabel = "Available:"
-        Me.lstStations.Location = New System.Drawing.Point(21, 46)
-        Me.lstStations.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.lstStations.Location = New System.Drawing.Point(16, 37)
         Me.lstStations.MoveDownTip = "Move Item Down In List"
         Me.lstStations.MoveUpTip = "Move Item Up In List"
         Me.lstStations.Name = "lstStations"
         Me.lstStations.RightLabel = "Selected:"
-        Me.lstStations.Size = New System.Drawing.Size(551, 186)
+        Me.lstStations.Size = New System.Drawing.Size(413, 151)
         Me.lstStations.SortMember = Nothing
         Me.lstStations.TabIndex = 3
         '
         'treeBFGroups
         '
-        Me.treeBFGroups.Location = New System.Drawing.Point(21, 260)
-        Me.treeBFGroups.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.treeBFGroups.Location = New System.Drawing.Point(16, 211)
         Me.treeBFGroups.Name = "treeBFGroups"
-        Me.treeBFGroups.Size = New System.Drawing.Size(181, 212)
+        Me.treeBFGroups.Size = New System.Drawing.Size(214, 173)
         Me.treeBFGroups.TabIndex = 4
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(21, 240)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Location = New System.Drawing.Point(16, 195)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(95, 17)
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Batch Groups"
         '
         'btnCreateGroup
         '
-        Me.btnCreateGroup.Location = New System.Drawing.Point(580, 75)
-        Me.btnCreateGroup.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnCreateGroup.Location = New System.Drawing.Point(435, 61)
         Me.btnCreateGroup.Name = "btnCreateGroup"
-        Me.btnCreateGroup.Size = New System.Drawing.Size(100, 44)
+        Me.btnCreateGroup.Size = New System.Drawing.Size(75, 36)
         Me.btnCreateGroup.TabIndex = 6
         Me.btnCreateGroup.Text = "Create Group"
         Me.btnCreateGroup.UseVisualStyleBackColor = True
         '
         'btnPlotDuration
         '
-        Me.btnPlotDuration.Location = New System.Drawing.Point(581, 128)
-        Me.btnPlotDuration.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnPlotDuration.Location = New System.Drawing.Point(436, 104)
         Me.btnPlotDuration.Name = "btnPlotDuration"
-        Me.btnPlotDuration.Size = New System.Drawing.Size(100, 28)
+        Me.btnPlotDuration.Size = New System.Drawing.Size(75, 23)
         Me.btnPlotDuration.TabIndex = 7
         Me.btnPlotDuration.Text = "Durations"
         Me.btnPlotDuration.UseVisualStyleBackColor = True
@@ -131,15 +127,13 @@ Partial Class frmBatchMap
         'GroupBox1
         '
         Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-                    Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.GroupBox1.Controls.Add(Me.btnParmForm)
         Me.GroupBox1.Controls.Add(Me.txtParameters)
-        Me.GroupBox1.Location = New System.Drawing.Point(213, 240)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Location = New System.Drawing.Point(236, 195)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.GroupBox1.Size = New System.Drawing.Size(465, 233)
+        Me.GroupBox1.Size = New System.Drawing.Size(273, 189)
         Me.GroupBox1.TabIndex = 8
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Group Parameters"
@@ -147,10 +141,9 @@ Partial Class frmBatchMap
         'btnParmForm
         '
         Me.btnParmForm.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.btnParmForm.Location = New System.Drawing.Point(8, 197)
-        Me.btnParmForm.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnParmForm.Location = New System.Drawing.Point(6, 160)
         Me.btnParmForm.Name = "btnParmForm"
-        Me.btnParmForm.Size = New System.Drawing.Size(100, 28)
+        Me.btnParmForm.Size = New System.Drawing.Size(75, 23)
         Me.btnParmForm.TabIndex = 1
         Me.btnParmForm.Text = "Form View"
         Me.btnParmForm.UseVisualStyleBackColor = True
@@ -158,24 +151,22 @@ Partial Class frmBatchMap
         'txtParameters
         '
         Me.txtParameters.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-                    Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtParameters.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtParameters.Location = New System.Drawing.Point(9, 20)
-        Me.txtParameters.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtParameters.Location = New System.Drawing.Point(7, 16)
         Me.txtParameters.Multiline = True
         Me.txtParameters.Name = "txtParameters"
         Me.txtParameters.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.txtParameters.Size = New System.Drawing.Size(447, 169)
+        Me.txtParameters.Size = New System.Drawing.Size(260, 138)
         Me.txtParameters.TabIndex = 0
         '
         'btnDoBatch
         '
         Me.btnDoBatch.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnDoBatch.Location = New System.Drawing.Point(579, 480)
-        Me.btnDoBatch.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnDoBatch.Location = New System.Drawing.Point(434, 390)
         Me.btnDoBatch.Name = "btnDoBatch"
-        Me.btnDoBatch.Size = New System.Drawing.Size(100, 28)
+        Me.btnDoBatch.Size = New System.Drawing.Size(75, 23)
         Me.btnDoBatch.TabIndex = 9
         Me.btnDoBatch.Text = "Do Batch"
         Me.btnDoBatch.UseVisualStyleBackColor = True
@@ -183,10 +174,9 @@ Partial Class frmBatchMap
         'btnSaveSpecs
         '
         Me.btnSaveSpecs.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnSaveSpecs.Location = New System.Drawing.Point(471, 480)
-        Me.btnSaveSpecs.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnSaveSpecs.Location = New System.Drawing.Point(353, 390)
         Me.btnSaveSpecs.Name = "btnSaveSpecs"
-        Me.btnSaveSpecs.Size = New System.Drawing.Size(100, 28)
+        Me.btnSaveSpecs.Size = New System.Drawing.Size(75, 23)
         Me.btnSaveSpecs.TabIndex = 10
         Me.btnSaveSpecs.Text = "Save Specs"
         Me.btnSaveSpecs.UseVisualStyleBackColor = True
@@ -195,47 +185,86 @@ Partial Class frmBatchMap
         '
         Me.cmsNode.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.cmsGlobalSetParm, Me.cmsGroupSetParm, Me.cmsPlotDur, Me.cmsRemove})
         Me.cmsNode.Name = "cmsGroup"
-        Me.cmsNode.Size = New System.Drawing.Size(226, 100)
-        '
-        'cmsRemove
-        '
-        Me.cmsRemove.Name = "cmsRemove"
-        Me.cmsRemove.Size = New System.Drawing.Size(225, 24)
-        Me.cmsRemove.Text = "Remove"
-        '
-        'cmsPlotDur
-        '
-        Me.cmsPlotDur.Name = "cmsPlotDur"
-        Me.cmsPlotDur.Size = New System.Drawing.Size(225, 24)
-        Me.cmsPlotDur.Text = "Plot Duration"
-        '
-        'cmsGroupSetParm
-        '
-        Me.cmsGroupSetParm.Name = "cmsGroupSetParm"
-        Me.cmsGroupSetParm.Size = New System.Drawing.Size(225, 24)
-        Me.cmsGroupSetParm.Text = "Set Group Parameters"
+        Me.cmsNode.Size = New System.Drawing.Size(190, 92)
         '
         'cmsGlobalSetParm
         '
         Me.cmsGlobalSetParm.Name = "cmsGlobalSetParm"
-        Me.cmsGlobalSetParm.Size = New System.Drawing.Size(225, 24)
+        Me.cmsGlobalSetParm.Size = New System.Drawing.Size(189, 22)
         Me.cmsGlobalSetParm.Text = "Set Global Parameters"
+        '
+        'cmsGroupSetParm
+        '
+        Me.cmsGroupSetParm.Name = "cmsGroupSetParm"
+        Me.cmsGroupSetParm.Size = New System.Drawing.Size(189, 22)
+        Me.cmsGroupSetParm.Text = "Set Group Parameters"
+        '
+        'cmsPlotDur
+        '
+        Me.cmsPlotDur.Name = "cmsPlotDur"
+        Me.cmsPlotDur.Size = New System.Drawing.Size(189, 22)
+        Me.cmsPlotDur.Text = "Plot Duration"
+        '
+        'cmsRemove
+        '
+        Me.cmsRemove.Name = "cmsRemove"
+        Me.cmsRemove.Size = New System.Drawing.Size(189, 22)
+        Me.cmsRemove.Text = "Remove"
         '
         'btnDownload
         '
-        Me.btnDownload.Location = New System.Drawing.Point(584, 10)
-        Me.btnDownload.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnDownload.Location = New System.Drawing.Point(438, 8)
         Me.btnDownload.Name = "btnDownload"
-        Me.btnDownload.Size = New System.Drawing.Size(100, 28)
+        Me.btnDownload.Size = New System.Drawing.Size(75, 23)
         Me.btnDownload.TabIndex = 11
         Me.btnDownload.Text = "Download"
         Me.btnDownload.UseVisualStyleBackColor = True
         '
+        'btnGroupPlot
+        '
+        Me.btnGroupPlot.Location = New System.Drawing.Point(187, 390)
+        Me.btnGroupPlot.Name = "btnGroupPlot"
+        Me.btnGroupPlot.Size = New System.Drawing.Size(48, 23)
+        Me.btnGroupPlot.TabIndex = 19
+        Me.btnGroupPlot.Text = "Plot"
+        Me.btnGroupPlot.UseVisualStyleBackColor = True
+        '
+        'btnGroupRemove
+        '
+        Me.btnGroupRemove.Location = New System.Drawing.Point(125, 390)
+        Me.btnGroupRemove.Name = "btnGroupRemove"
+        Me.btnGroupRemove.Size = New System.Drawing.Size(56, 23)
+        Me.btnGroupRemove.TabIndex = 18
+        Me.btnGroupRemove.Text = "Remove"
+        Me.btnGroupRemove.UseVisualStyleBackColor = True
+        '
+        'btnGroupGroup
+        '
+        Me.btnGroupGroup.Location = New System.Drawing.Point(69, 390)
+        Me.btnGroupGroup.Name = "btnGroupGroup"
+        Me.btnGroupGroup.Size = New System.Drawing.Size(50, 23)
+        Me.btnGroupGroup.TabIndex = 17
+        Me.btnGroupGroup.Text = "Group"
+        Me.btnGroupGroup.UseVisualStyleBackColor = True
+        '
+        'btnGroupGlobal
+        '
+        Me.btnGroupGlobal.Location = New System.Drawing.Point(16, 390)
+        Me.btnGroupGlobal.Name = "btnGroupGlobal"
+        Me.btnGroupGlobal.Size = New System.Drawing.Size(47, 23)
+        Me.btnGroupGlobal.TabIndex = 16
+        Me.btnGroupGlobal.Text = "Global"
+        Me.btnGroupGlobal.UseVisualStyleBackColor = True
+        '
         'frmBatchMap
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(695, 521)
+        Me.ClientSize = New System.Drawing.Size(521, 423)
+        Me.Controls.Add(Me.btnGroupPlot)
+        Me.Controls.Add(Me.btnGroupRemove)
+        Me.Controls.Add(Me.btnGroupGroup)
+        Me.Controls.Add(Me.btnGroupGlobal)
         Me.Controls.Add(Me.btnDownload)
         Me.Controls.Add(Me.btnSaveSpecs)
         Me.Controls.Add(Me.btnDoBatch)
@@ -249,7 +278,6 @@ Partial Class frmBatchMap
         Me.Controls.Add(Me.btnBrowseDataDir)
         Me.Controls.Add(Me.Label1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "frmBatchMap"
         Me.Text = "Base-flow Separation Batch From Map"
         Me.GroupBox1.ResumeLayout(False)
@@ -278,4 +306,8 @@ Partial Class frmBatchMap
     Friend WithEvents cmsGroupSetParm As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents cmsGlobalSetParm As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents btnDownload As System.Windows.Forms.Button
+    Friend WithEvents btnGroupPlot As Windows.Forms.Button
+    Friend WithEvents btnGroupRemove As Windows.Forms.Button
+    Friend WithEvents btnGroupGroup As Windows.Forms.Button
+    Friend WithEvents btnGroupGlobal As Windows.Forms.Button
 End Class
