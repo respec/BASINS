ed70168aa120231c6e89da2104f9559c28e27b39
PaulHummelRESPEC <paul.hummel@respec.com>
2018-02-09 17:23:59 -0500

    atcGraphForm.vb - in mnuSaveJson_Click, added assignment of axis to each curve's Tag so it is preserved when generating PNG; modGraph.vb - in GraphJsonToFile, parse added axis assignment at end of Tag and apply to each timeseries

--- atcGraphExperimental/atcGraphForm.vb
+++ atcGraphExperimental/atcGraphForm.vb
@@ -616,6 +616,14 @@ Public Class atcGraphForm
                         lAxis.Scale.FontSpec.Border.GradientFill.Brush = Nothing
                     Next
                     lPane.XAxis.Scale.FontSpec.Border.GradientFill.Brush = Nothing
+                    'also add axis location to Tag
+                    For i As Integer = 0 To lPane.CurveList.Count - 1
+                        If lPane.CurveList(i).IsY2Axis Then
+                            lPane.CurveList(i).Tag &= "|RIGHT"
+                        Else
+                            lPane.CurveList(i).Tag &= "|LEFT"
+                        End If
+                    Next
                 Next
 
                 Try
