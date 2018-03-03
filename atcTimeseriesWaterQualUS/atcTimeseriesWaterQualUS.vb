c0e11178bb98fe312df7f102fac9370b9cfbe47b
tong.zhai <tong.zhai@respec.com>
2018-03-02 22:07:32 -0500

    New Water Quality Data US library - New time series library for reading comprehensive water quality data from mutltiple sources - allows users to select location/constituents before generating time series

---
+++ atcTimeseriesWaterQualUS/atcTimeseriesWaterQualUS.vb
@@ -0,0 +1,191 @@
+﻿Imports atcData
+Imports atcUtility
+Imports MapWinUtility
+Imports System.Collections
+Imports System.IO
+Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.FileIO.TextFieldParser
+
+Public Class atcTimeseriesWaterQualUS
+    Inherits atcTimeseriesSource
+    Private Shared pFilter As String = "WaterQualityData US Files (*.csv)|*.csv;|All Files (*.*)|*.*"
+    Private pJulianInterval As Double = 1 'Add one day for daily values to record date at end of interval
+    Public RawDataGroup As New clsWQDUSLocations() 'Dictionary(Of String, clsWQDUSLocation)()
+    Public SelectAllLocations As Boolean = False
+    Public SelectAllConstituents As Boolean = False
+    Public SelectedLocations As New List(Of String)()
+    Public SelectedConstituents As New List(Of String)()
+    Public AllConstituents As New List(Of String)()
+
+    Public Overrides ReadOnly Property Description() As String
+        Get
+            Return "WaterQualityData US"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Name() As String
+        Get
+            Return "Timeseries::WaterQualityData US"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Category() As String
+        Get
+            Return "File"
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property CanOpen() As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property CanSave() As Boolean
+        Get
+            Return False
+        End Get
+    End Property
+
+    Public Overrides Function Open(ByVal aFileName As String,
+                      Optional ByVal aAttributes As atcData.atcDataAttributes = Nothing) As Boolean
+        If MyBase.Open(aFileName, aAttributes) Then
+            If Not IO.File.Exists(Specification) Then
+                Logger.Dbg("Opening new file " & Specification)
+                Return True
+            ElseIf IO.Path.GetFileName(Specification).ToLower.StartsWith("stations") Then
+                Throw New ApplicationException("Station file does not contain timeseries data: " & IO.Path.GetFileName(Specification))
+            Else
+                Try
+                    Dim lTimeStartOpen As Date = Now
+                    Logger.Dbg("OpenStartFor " & Specification)
+                    Dim CurrentRecord() As String
+                    Dim lDataColumnsValid As Boolean = True
+                    Dim lFileInfo As New FileInfo(Specification)
+                    Dim lSr As New StreamReader(Specification, True)
+                    Dim lEncoding As Text.Encoding = lSr.CurrentEncoding()
+                    lSr.Close()
+                    lSr = Nothing
+                    Dim lFileSize As Long = lFileInfo.Length
+                    Dim lRunningSize As Long = 0
+                    Using afile As New Microsoft.VisualBasic.FileIO.TextFieldParser(Specification)
+                        afile.TextFieldType = FileIO.FieldType.Delimited
+                        afile.Delimiters = New String() {","}
+                        afile.HasFieldsEnclosedInQuotes = True
+
+                        Dim first As Boolean = True
+                        ' parse the actual file
+                        Dim loc_id As String
+                        Dim loc As clsWQDUSLocation
+                        While Not afile.EndOfData
+                            Try
+                                CurrentRecord = afile.ReadFields
+                                lRunningSize += lEncoding.GetByteCount(String.Join(",", CurrentRecord))
+                                Logger.Progress(lRunningSize / lFileSize * 100.0, 100)
+                                If first Then
+                                    first = False
+                                    If Not clsWQDUSColumns.SetColumns(CurrentRecord) Then
+                                        lDataColumnsValid = False
+                                        Exit While
+                                    End If
+                                    Continue While
+                                Else
+                                    loc_id = CurrentRecord(clsWQDUSColumns.colMonitoringLocationIdentifier)
+                                    If RawDataGroup.ContainsKey(loc_id) Then
+                                        loc = RawDataGroup.Item(loc_id)
+                                    Else
+                                        loc = New clsWQDUSLocation(loc_id)
+                                        RawDataGroup.Add(loc_id, loc)
+                                    End If
+                                    loc.AddData(CurrentRecord)
+                                End If
+                            Catch ex As FileIO.MalformedLineException
+                                MsgBox("Line " & ex.Message & "is not valid and will be skipped.")
+                            End Try
+                        End While
+                    End Using
+                    Logger.Progress(100, 100)
+                    If lDataColumnsValid Then
+                        SelectedConstituents.Clear()
+                        SelectedLocations.Clear()
+                        Dim lfrm As New frmSelect(RawDataGroup, SelectedLocations, SelectedConstituents)
+                        If lfrm.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                            If SelectedLocations.Count > 0 AndAlso SelectedConstituents.Count > 0 Then
+                                Return BuildTimeseries()
+                            End If
+                        End If
+                    Else
+                        Return False
+                    End If
+                    Return True
+                Catch lException As Exception
+                    Logger.Progress(100, 100)
+                    Throw New ApplicationException("Exception reading '" & Specification & "': " & lException.Message, lException)
+                End Try
+            End If
+        End If
+        Return True
+    End Function
+
+    Public Sub New()
+        Filter = pFilter
+    End Sub
+
+    Public Function BuildTimeseries() As Boolean
+        If RawDataGroup.Count = 0 Then
+            Return False
+        End If
+        Dim lBuildOk As Boolean = True
+        Try
+            Dim lGroupBuilder As New atcTimeseriesGroupBuilder(Me)
+            Dim lTSBuilder As atcTimeseriesBuilder
+            For Each loc As clsWQDUSLocation In RawDataGroup.Values
+                If Not SelectAllLocations AndAlso Not SelectedLocations.Contains(loc.Location) Then
+                    Continue For
+                End If
+                For Each cons As clsWQDUSConstituent In loc.Constituents.Values
+                    For Each lcon_unit As String In cons.RecordGroup.Keys
+                        If Not SelectAllConstituents Then
+                            If Not SelectedConstituents.Contains(cons.Name & "-" & lcon_unit) Then
+                                Continue For
+                            End If
+                        End If
+                        Dim ldata As List(Of clsWQDUSDataPoint) = cons.RecordGroup.Item(lcon_unit)
+                        If ldata.Count > 0 Then
+                            lTSBuilder = lGroupBuilder.Builder(loc.Location & ":" & cons.Name & ":" & lcon_unit)
+                            With lTSBuilder.Attributes
+                                'Set attributes of newly created builder
+                                If Not .ContainsAttribute("Location") Then
+                                    .SetValue("Scenario", "Observed")
+                                    .SetValue("Location", loc.Location)
+                                    .SetValue("Constituent", cons.Name)
+                                    .SetValue("Units", lcon_unit)
+                                    '.SetValue("CCode", lCcode)
+                                    .SetValue("Point", True)
+                                    .AddHistory("Read from " & Specification)
+                                End If
+                            End With
+                            For I As Integer = 0 To ldata.Count - 1
+                                'ldt = Date.Parse(lTable.Value(lDateCol)).ToOADate
+                                If ldata(I).SetDataTime() Then
+                                    lTSBuilder.AddValue(ldata(I).atcDataTime, ldata(I).Value)
+                                    If Not String.IsNullOrEmpty(ldata(I).Attrib) Then
+                                        lTSBuilder.AddValueAttribute("RCode", ldata(I).Attrib)
+                                    End If
+                                Else
+                                    Continue For
+                                End If
+                            Next 'data point
+                        End If
+                    Next 'lcon_unit
+                Next 'cons
+            Next 'loc
+            lGroupBuilder.CreateTimeseriesAddToGroup(DataSets)
+            lBuildOk = True
+        Catch ex As Exception
+            lBuildOk = False
+            Logger.Msg("Build datasets failed in " & Specification, "WaterQualityData US Build Dataset")
+        End Try
+        Return lBuildOk
+    End Function
+End Class
---
+++ atcTimeseriesWaterQualUS/frmSelect.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSelect
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstLocations = New System.Windows.Forms.CheckedListBox()
+        Me.lstConstituents = New System.Windows.Forms.CheckedListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.spaneMain = New System.Windows.Forms.SplitContainer()
+        Me.btnClearLoc = New System.Windows.Forms.Button()
+        Me.txtMsgCons = New System.Windows.Forms.TextBox()
+        Me.btnClearCons = New System.Windows.Forms.Button()
+        Me.btnDoTser = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.spaneMain.Panel1.SuspendLayout()
+        Me.spaneMain.Panel2.SuspendLayout()
+        Me.spaneMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lstLocations
+        '
+        Me.lstLocations.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lstLocations.FormattingEnabled = True
+        Me.lstLocations.Location = New System.Drawing.Point(6, 19)
+        Me.lstLocations.Name = "lstLocations"
+        Me.lstLocations.Size = New System.Drawing.Size(313, 259)
+        Me.lstLocations.TabIndex = 0
+        '
+        'lstConstituents
+        '
+        Me.lstConstituents.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lstConstituents.FormattingEnabled = True
+        Me.lstConstituents.Location = New System.Drawing.Point(6, 19)
+        Me.lstConstituents.Name = "lstConstituents"
+        Me.lstConstituents.Size = New System.Drawing.Size(317, 259)
+        Me.lstConstituents.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Locations"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Constituents"
+        '
+        'spaneMain
+        '
+        Me.spaneMain.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.spaneMain.Location = New System.Drawing.Point(12, 12)
+        Me.spaneMain.Name = "spaneMain"
+        '
+        'spaneMain.Panel1
+        '
+        Me.spaneMain.Panel1.Controls.Add(Me.btnClearLoc)
+        Me.spaneMain.Panel1.Controls.Add(Me.lstLocations)
+        Me.spaneMain.Panel1.Controls.Add(Me.Label1)
+        '
+        'spaneMain.Panel2
+        '
+        Me.spaneMain.Panel2.Controls.Add(Me.txtMsgCons)
+        Me.spaneMain.Panel2.Controls.Add(Me.btnClearCons)
+        Me.spaneMain.Panel2.Controls.Add(Me.lstConstituents)
+        Me.spaneMain.Panel2.Controls.Add(Me.Label2)
+        Me.spaneMain.Size = New System.Drawing.Size(658, 398)
+        Me.spaneMain.SplitterDistance = 325
+        Me.spaneMain.TabIndex = 4
+        '
+        'btnClearLoc
+        '
+        Me.btnClearLoc.Location = New System.Drawing.Point(6, 285)
+        Me.btnClearLoc.Name = "btnClearLoc"
+        Me.btnClearLoc.Size = New System.Drawing.Size(50, 23)
+        Me.btnClearLoc.TabIndex = 3
+        Me.btnClearLoc.Text = "Clear"
+        Me.btnClearLoc.UseVisualStyleBackColor = True
+        '
+        'txtMsgCons
+        '
+        Me.txtMsgCons.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtMsgCons.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtMsgCons.Location = New System.Drawing.Point(59, 285)
+        Me.txtMsgCons.Multiline = True
+        Me.txtMsgCons.Name = "txtMsgCons"
+        Me.txtMsgCons.ReadOnly = True
+        Me.txtMsgCons.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtMsgCons.Size = New System.Drawing.Size(264, 110)
+        Me.txtMsgCons.TabIndex = 5
+        '
+        'btnClearCons
+        '
+        Me.btnClearCons.Location = New System.Drawing.Point(6, 285)
+        Me.btnClearCons.Name = "btnClearCons"
+        Me.btnClearCons.Size = New System.Drawing.Size(50, 23)
+        Me.btnClearCons.TabIndex = 4
+        Me.btnClearCons.Text = "Clear"
+        Me.btnClearCons.UseVisualStyleBackColor = True
+        '
+        'btnDoTser
+        '
+        Me.btnDoTser.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDoTser.Location = New System.Drawing.Point(491, 416)
+        Me.btnDoTser.Name = "btnDoTser"
+        Me.btnDoTser.Size = New System.Drawing.Size(98, 23)
+        Me.btnDoTser.TabIndex = 5
+        Me.btnDoTser.Text = "Make Timeseries"
+        Me.btnDoTser.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCancel.Location = New System.Drawing.Point(595, 416)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 6
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmSelect
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(682, 451)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnDoTser)
+        Me.Controls.Add(Me.spaneMain)
+        Me.Name = "frmSelect"
+        Me.Text = "Select Water Quality Data"
+        Me.spaneMain.Panel1.ResumeLayout(False)
+        Me.spaneMain.Panel1.PerformLayout()
+        Me.spaneMain.Panel2.ResumeLayout(False)
+        Me.spaneMain.Panel2.PerformLayout()
+        Me.spaneMain.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstConstituents As Windows.Forms.CheckedListBox
+    Friend WithEvents lstLocations As Windows.Forms.CheckedListBox
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents spaneMain As Windows.Forms.SplitContainer
+    Friend WithEvents btnClearLoc As Windows.Forms.Button
+    Friend WithEvents btnClearCons As Windows.Forms.Button
+    Friend WithEvents btnDoTser As Windows.Forms.Button
+    Friend WithEvents btnCancel As Windows.Forms.Button
+    Friend WithEvents txtMsgCons As Windows.Forms.TextBox
+End Class
