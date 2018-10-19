17062cb580feea1a1c06c611422d2e718ff4a1cc
tong.zhai <tong.zhai@respec.com>
2018-10-19 11:28:52 -0400

    draft groundwater watch

--- atcUSGSBaseflow/My Project/Settings.Designer.vb
+++ atcUSGSBaseflow/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.atcUSGSBaseflow.My.MySettings
+        Friend ReadOnly Property Settings() As Global.GroundwaterWatch.My.MySettings
             Get
-                Return Global.atcUSGSBaseflow.My.MySettings.Default
+                Return Global.GroundwaterWatch.My.MySettings.Default
             End Get
         End Property
     End Module
