eaa85e98b4cebd908d2baf32817cb7f99378769e
Tong Zhai <tong.zhai@respec.com>
2019-10-11 11:09:27 -0400

    starting DotSpatial solution
    - maintains BASINS code structure

---
+++ BASINSDS/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BASINSDS
+{
+    public partial class MainForm : Form
+    {
+        [Export("Shell", typeof(ContainerControl))]
+        private static ContainerControl shell;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            if (DesignMode) return;
+            shell = this;
+            appManager.LoadExtensions();
+        }
+    }
+}
