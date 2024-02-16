8d1a8c3887c9433e950a151ba9351b2ecc491f65
paulhummelRESPEC <paul.hummel@respec.com>
2024-02-16 14:55:33 -0500

    frmAbout - updated version name and support link

--- TimeseriesUtility/frmAbout.vb
+++ TimeseriesUtility/frmAbout.vb
@@ -1,6 +1,6 @@
 ﻿Public Class frmAbout
 
     Private Sub UpdateLink_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles UpdateLink.LinkClicked
-        atcUtility.OpenFile("http://aquaterra.com/TimeseriesUtility/")
+        atcUtility.OpenFile("https://www.respec.com/product/modeling-optimization/sara-timeseries-utility/")
     End Sub
 End Class
