9951729f7928180709f602e5101b60e73af870f1
Anurag Mishra <mishranurag@gmail.com>
2024-12-26 23:56:13 -0800

    For bacteria graphs added check to make log graphs and added titles for other bar graphs.

--- atcHspfSupport/modGraphBoxWhiskers.vb
+++ atcHspfSupport/modGraphBoxWhiskers.vb
@@ -6,6 +6,7 @@ Imports MapWinUtility
 Imports atcGraph
 Imports ZedGraph
 Imports System.Collections
+Imports System.Linq
 
 Public Class BoxWhiskerItem
 
@@ -57,24 +58,8 @@ Public Module modGraphBoxWhiskers
             vbCrLf & items.TimeSpan
         End If
 
-
-
-        'Select Case items.Constituent
-        '    Case "Sediment"
-        '        lGrapher.Title = "Box-Whisker plot of sediment loading rate from all land uses in " & items.Scenario & " model." &
-        '            vbCrLf & items.TimeSpan
-        '    Case "TotalP"
-        '        lGrapher.Title = "Box-Whisker plot of total phosphorus loading rate from all land uses in " & items.Scenario & " model." &
-        '            vbCrLf & items.TimeSpan
-        '    Case "TotalN"
-        '        lGrapher.Title = "Box-Whisker plot of total nitrogen loading rate from all land uses in " & items.Scenario & " model." &
-        '            vbCrLf & items.TimeSpan
-        '    Case Else
-        '        Exit Sub
-        'End Select
         lGrapher.DatasetsCollection = items.LabelValueCollection
         lGrapher.ShowOutliers = False
-
 
         lGrapher.OutputFile = aOutputFileName
 
@@ -120,6 +105,26 @@ Public Module modGraphBoxWhiskers
         'specify Y axis title
         'if not specified, then default Y axis title will be constructed
         lGrapher.YTitle = items.Constituent & " " & items.Units
+
+        Dim values As New List(Of Double)
+        For Each key As String In items.LabelValueCollection.Keys
+            For Each value As Double In items.LabelValueCollection.ItemByKey(key)
+                values.Add(value)
+            Next
+        Next
+
+
+        Dim YMinScale As Double = values.ToArray.Min
+        Dim YMaxScale As Double = values.ToArray.Max
+        If YMaxScale / YMinScale > 100 Then
+            lGrapher.YUseLog = True
+            lGrapher.ZedGraphCtrl.GraphPane.YAxis.Scale.Min = Math.Pow(10, Math.Floor(Math.Log10(YMinScale))) / 10
+            lGrapher.ZedGraphCtrl.GraphPane.YAxis.Scale.Max = Math.Pow(10, Math.Ceiling(Math.Log10(YMaxScale))) * 10
+
+            'This is not the most elegant way to select better scales for Fecal Coliform or E.Coli Graph,
+            'but it is better than what we had. Anurag - 12/26/2024
+
+        End If
 
         'specify if to use legend
         'True: use legend to show box category instead of x-axis labels
